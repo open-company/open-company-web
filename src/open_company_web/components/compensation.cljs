97bb4addee2630d91092bde5065e6c45c5442e31
bago2k4 <iacopo@rc0.eu>
2015-07-06 18:07:48 +0200

    handle dollars/percentage switch

--- src/open_company_web/components/compensation.cljs
+++ src/open_company_web/components/compensation.cljs
@@ -5,7 +5,38 @@
               [open-company-web.components.report-line :refer [report-line report-editable-line]]
               [open-company-web.utils :refer [thousands-separator handle-change]]))
 
+(defn switch-values
+  "Update all the values in the cursor with the values passed"
+  [cursor last-values]
+  (println cursor)
+  (println last-values)
+  (handle-change cursor (:founders last-values) :founders)
+  (handle-change cursor (:executives last-values) :executives)
+  (handle-change cursor (:employee last-values) :employee)
+  (handle-change cursor (:contractor last-values) :contractor))
+
+(defn calc-percentage
+  [dollar total]
+  (* 0.01 (Math/round (* (/ dollar total) 10000))))
+
+(defn dollars->percentage
+  "Calculate the percentage given the compensation component cursor"
+  [cursor]
+  (let [total (+ (:founders cursor) (:executives cursor) (:employee cursor) (:contractor cursor))]
+    (handle-change cursor (calc-percentage (:founders cursor) total) :founders)
+    (handle-change cursor (calc-percentage (:executives cursor) total) :executives)
+    (handle-change cursor (calc-percentage (:employee cursor) total) :employee)
+    (handle-change cursor (calc-percentage (:contractor cursor) total) :contractor)))
+
+(defn copy-compensation-state
+  "Copy the compensation state into the component state"
+  [owner cursor]
+  (when (:dollars cursor)
+    (om/set-state! owner :initial-values cursor)))
+
 (defcomponent compensation [data owner]
+  (will-mount [_]
+    (copy-compensation-state owner data))
   (render [_]
     (let [dollars (:dollars data)
           founders (:founders data)
@@ -15,6 +46,7 @@
           comment (:comment data)
           prefix (if dollars "$" "%")
           total-compensation (+ founders executives employee contractor)]
+      (println "+ " founders " " executives " " employee " " contractor " = " total-compensation)
       (dom/div {:class "report-list compensation"}
         (dom/h3 "Compensation: ")
         (dom/div
@@ -25,7 +57,8 @@
               :value "$"
               :id "report-type-$"
               :checked dollars
-              :on-click #(handle-change data true :dollars)})
+              :on-click (fn[e] (switch-values data (:initial-values (om/get-state owner)))
+                          (handle-change data true :dollars))})
             (dom/label {:class "switch-vis" :for "report-type-$"} " Dollars  ")
             (dom/input {
               :type "radio"
@@ -33,32 +66,38 @@
               :value "%"
               :id "report-type-%"
               :checked (not dollars)
-              :on-click #(handle-change data false :dollars)})
+              :on-click (fn[e] (handle-change data false :dollars)
+                               (copy-compensation-state owner data)
+                               (dollars->percentage data))})
             (dom/label {:class "switch-vis" :for "report-type-%"} "  Percent ")))
         (om/build report-editable-line {
           :cursor data
           :key :founders
           :prefix prefix
           :label "founders compensation this month"
-          :pluralize false})
+          :pluralize false
+          :on-change #(when dollars (om/set-state! owner :initial-values data))})
         (om/build report-editable-line {
           :cursor data
           :key :executives
           :prefix prefix
           :label "executives compensation this month"
-          :pluralize false})
+          :pluralize false
+          :on-change #(when dollars (om/set-state! owner :initial-values data))})
         (om/build report-editable-line {
           :cursor data
           :key :employee
           :prefix prefix
           :label "employees compensation this month"
-          :pluralize false})
+          :pluralize false
+          :on-change #(when dollars (om/set-state! owner :initial-values data))})
         (om/build report-editable-line {
           :cursor data
           :key :contractor
           :prefix prefix
           :label "contractors compensation this month"
-          :pluralize false})
+          :pluralize false
+          :on-change #(when dollars (om/set-state! owner :initial-values data))})
         (dom/div
           (om/build report-line {
             :prefix prefix
