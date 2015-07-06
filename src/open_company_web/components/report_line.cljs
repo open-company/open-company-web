48c51da85e3e6c9ff84ac56ddae7967f4b9b28ad
bago2k4 <iacopo@rc0.eu>
2015-07-06 18:12:55 +0200

    use an internal state to keep track of the changed number, save the number on enter or blur, use thousands separator only when necessary

--- src/open_company_web/components/report_line.cljs
+++ src/open_company_web/components/report_line.cljs
@@ -2,23 +2,42 @@
     (:require [om.core :as om :include-macros true]
               [om-tools.core :as om-core :refer-macros [defcomponent]]
               [om-tools.dom :as dom :include-macros true]
-              [open-company-web.utils :refer [thousands-separator handle-change get-event-int-value]]))
+              [open-company-web.utils :refer [thousands-separator handle-change String->Number]]))
 
 
 (defcomponent report-editable-line [view-data owner]
+  (will-mount [_]
+    (om/set-state! owner :editing false)
+    (om/set-state! owner :value ((:key view-data) (:cursor view-data))))
   (render [_]
     (let [data (:cursor view-data)
           key (:key view-data)
           prefix (:prefix view-data)
           label (:label view-data)
+          on-change (:on-change view-data)
           suffix (if (and (:pluralize view-data) (= (key data) 1)) "" "s")]
       (dom/div
         (if (> (count prefix) 0) (dom/span {:class "label"} (str prefix)))
         (dom/input {
           :class "editable-data"
           :ref (name key)
-          :value (thousands-separator (key data))
-          :onChange #(handle-change data (get-event-int-value %) key)
+          :value (if (:editing (om/get-state owner))
+                    (:value (om/get-state owner))
+                    (thousands-separator (:value (om/get-state owner))))
+          :onFocus #(om/set-state! owner :editing true)
+          :onBlur #(let [value (String->Number (.. % -target -value))]
+                    (om/set-state! owner :editing false)
+                    (om/set-state! owner :value value)
+                    (handle-change data value key)
+                    (if-not (= on-change nil) (on-change value)))
+          :onKeyPress #(let [value (.. % -target -value)]
+                        (om/set-state! owner :value value)
+                        (when (= (.-key %) "Enter")
+                          (do
+                            (handle-change data (String->Number value) key)
+                            (if-not (= on-change nil) (on-change value)))))
+          :onChange #(let [value (.. % -target -value)]
+                      (om/set-state! owner :value value))
         })
         (dom/span {:class "label"} (str " " label suffix))))))
 
