6a7997bd5433847bbc01b63b7cfee62f36356147
bago2k4 <iacopo@rc0.eu>
2015-07-13 14:48:00 +0200

    simple not found page

---
+++ src/open_company_web/components/page_not_found.cljs
@@ -0,0 +1,11 @@
+(ns open-company-web.components.page-not-found
+  (:require [om.core :as om :include-macros true]
+            [om-tools.core :as om-core :refer-macros [defcomponent]]
+            [om-tools.dom :as dom :include-macros true]
+            [open-company-web.components.link :refer [link]]))
+
+(defcomponent page-not-found [data owner]
+  (render [_]
+    (dom/div
+      (dom/h1 "Page not found")
+      (om/build link {:href "/" :name "Home"}))))
