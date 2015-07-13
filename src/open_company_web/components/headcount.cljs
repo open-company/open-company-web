206cf92d1d5c4a626f4a53d889729072aadf39bf
bago2k4 <iacopo@rc0.eu>
2015-07-13 21:38:00 +0200

    fix headcount's summary label and pluralization

--- src/open_company_web/components/headcount.cljs
+++ src/open_company_web/components/headcount.cljs
@@ -26,6 +26,7 @@
         (om/build report-editable-line {:cursor data :key :pt-employees :label "part-time employee"})
         (om/build report-editable-line {:cursor data :key :pt-contractors :label "part-time contractor"})
         (dom/div
-          (om/build report-line {:number total-headcount :label "total headcount" :pluralize false})", "
+          (om/build report-line {:number total-headcount :label "total" :pluralize false})
+          (dom/span ", ")
           (om/build report-line {:number full-time-equivalent :label "full-time equivalent"}))
         (om/build comment-component {:value comment})))))
