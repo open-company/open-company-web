d8ea7c166f9cc3aeb558826a5eef8b8fadaf7412
bago2k4 <iacopo@rc0.eu>
2015-07-13 17:21:14 +0200

    use router/nav! to switch view

--- src/open_company_web/components/link.cljs
+++ src/open_company_web/components/link.cljs
@@ -2,14 +2,14 @@
     (:require [om.core :as om :include-macros true]
               [om-tools.core :as om-core :refer-macros [defcomponent]]
               [om-tools.dom :as dom :include-macros true]
-              [secretary.core :as secretary]))
+              [secretary.core :as secretary]
+              [open-company-web.router :as router]))
 
 (defcomponent link [data owner]
   (render [_]
     (dom/a #js {
       :href (:href data)
-      :onClick (fn[e]
-                (-> e .preventDefault)
-                (secretary/dispatch! (:href data))
-                (.pushState (.-history js/window) "Test" "Title" (:href data)))
+      :onClick #(do
+                (-> % .preventDefault)
+                (router/nav! (:href data)))
       } (:name data))))
