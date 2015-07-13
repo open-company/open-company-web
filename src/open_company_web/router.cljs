cdd7423d3b70a1c0d1cbac8af335c4173c960e30
bago2k4 <iacopo@rc0.eu>
2015-07-13 14:47:48 +0200

    package to setup app routing and heper functions

---
+++ src/open_company_web/router.cljs
@@ -0,0 +1,38 @@
+(ns open-company-web.router
+  (:require [secretary.core :as secretary :include-macros true :refer-macros [defroute]]
+            [goog.events :as events])
+  (:import [goog.history Html5History EventType]
+           [goog History]))
+
+(defn get-token []
+  (str js/window.location.pathname js/window.location.search))
+
+(defn make-history []
+  (doto (Html5History.)
+    (.setPathPrefix (str js/window.location.protocol
+                         "//"
+                         js/window.location.host))
+    (.setUseFragment false)))
+
+(defn handle-url-change [e]
+  (println "handle-url-change Navigating: " (get-token))
+  ;; we are checking if this event is due to user action,
+  ;; such as click a link, a back button, etc.
+  ;; as opposed to programmatically setting the URL with the API
+  (when-not (.-isNavigation e)
+    ;; in this case, we're setting it
+    (println "Token set programmatically")
+    ;; let's scroll to the top to simulate a navigation
+    (js/window.scrollTo 0 0))
+  ;; dispatch on the token
+  (secretary/dispatch! (get-token)))
+
+(defonce history
+  (doto (make-history)
+     (goog.events/listen EventType.NAVIGATE
+       ;; wrap in a fn to allow live reloading
+       #(handle-url-change %))
+      (.setEnabled true)))
+
+(defn nav! [token]
+  (.setToken history token))
