f16f7edfb874ba38a7b20f522d43bad030bca9f1
bago2k4 <iacopo@rc0.eu>
2015-07-20 22:21:13 +0200

    moved app state to dispatcher, load company data when requesting company page

--- src/open_company_web/core.cljs
+++ src/open_company_web/core.cljs
@@ -2,91 +2,41 @@
   (:require [om.core :as om :include-macros true]
             [om-tools.dom :as dom :include-macros true]
             [secretary.core :as secretary :include-macros true :refer-macros [defroute]]
+            [cljs-flux.dispatcher :as flux]
             [open-company-web.router :refer [make-history handle-url-change]]
-            [open-company-web.components.page :refer [company company-not-found]]
+            [open-company-web.components.page :refer [company]]
             [open-company-web.components.list-companies :refer [list-companies]]
             [open-company-web.components.page-not-found :refer [page-not-found]]
             [open-company-web.raven :refer [raven-setup]]
+            [open-company-web.dispatcher :as dispatcher :refer [app-state]]
+            [open-company-web.api :as api]
             [goog.events :as events])
-  (:import [goog.history EventType]))
+  (:import [goog.history EventType])
+  (:require-macros [cljs.core.async.macros :refer [go go-loop]]))
 
 ;; setup Sentry error reporting
 (raven-setup)
-
-(defonce app-state (atom {
-  :OPEN {
-    :name "Transparency, LLC"
-    :symbol "OPEN"
-    :currency ["USD"]
-    :headcount {
-      :founders 2
-      :executives 0
-      :ft-employees 3
-      :ft-contractors 0
-      :pt-employees 0
-      :pt-contractors 2
-      :comment "Transparency headcount comment."
-    },
-    :finances {
-      :cash 173228
-      :revenue 2767
-      :costs 22184
-      :burn-rate -19417
-      :runway "9 months"
-      :comment "Transparency finances comment."
-    },
-    :compensation {
-      :percentage false
-      :founders 6357
-      :executives 0
-      :employees 5899
-      :contractors 2582
-      :comment "Transparency compensation comment."
-    }
-  }
-  :BUFFR {
-    :name "Buffer"
-    :symbol "BUFFR"
-    :currency ["USD"]
-    :headcount {
-      :founders 1
-      :executives 2
-      :ft-employees 1
-      :ft-contractors 2
-      :pt-employees 1
-      :pt-contractors 4
-      :comment "Buffer headcount comment."
-    },
-    :finances {
-      :cash 323232
-      :revenue 1234
-      :costs 11321
-      :burn-rate -10000
-      :runway "9 months"
-      :comment "Buffer finances comment."
-    },
-    :compensation {
-      :percentage true
-      :founders 40
-      :executives 40
-      :employees 10
-      :contractors 10
-      :comment "Buffer compensation comment."
-    }
-  }
-}))
 
 ;;Routes
 (defroute list-page-route "/" []
   (om/root list-companies app-state
     {:target (. js/document (getElementById "app"))}))
 
+(defn render-company [ticker loading]
+  (println "render-company " loading)
+  (swap! app-state assoc :ticker ticker)
+  (when loading
+    (swap! app-state assoc (keyword ticker) true))
+  (om/root company app-state
+    {:target (. js/document (getElementById "app"))}))
+
 (defroute editable-page-route "/companies/:symbol" {ticker :symbol}
-  (if-let [company-state ((keyword ticker) @app-state)]
-    (om/root company company-state
-      {:target (. js/document (getElementById "app"))})
-    (om/root company-not-found {:symbol ticker}
-      {:target (. js/document (getElementById "app"))})))
+  (do
+    (if-not (contains? ticker (keyword ticker))
+      (do
+        #(api/get-company ticker)
+        (render-company ticker true))
+      (render-company ticker false))))
 
 (defroute not-found-route "*" []
   (om/root page-not-found app-state
@@ -98,6 +48,10 @@
       ;; wrap in a fn to allow live reloading
       #(handle-url-change %))
     (.setEnabled true)))
+
+(js/setTimeout
+  #(api/get-company "BUFF")
+  1000)
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
