0fb2b8bbb59858c2d1798d5183c9190e3694da1d
bago2k4 <iacopo@rc0.eu>
2015-07-27 19:07:57 +0200

    add navbar

---
+++ src/open_company_web/components/navbar.cljs
@@ -0,0 +1,19 @@
+(ns open-company-web.components.navbar
+    (:require [om.core :as om :include-macros true]
+              [om-tools.core :as om-core :refer-macros [defcomponent]]
+              [om-tools.dom :as dom :include-macros true]
+              [open-company-web.components.link :refer [link]]
+              [om-bootstrap.nav :as n]))
+
+(defcomponent navbar [data owner]
+  (render [_]
+    (n/navbar {:inverse? true :fixed-top? true :fluid true :collapse? true}
+      (dom/div {:class "navbar-header"}
+        (om/build link {
+          :class "navbar-brand"
+          :href (str "/companies/" (:symbol data))
+          :name (str (:symbol data) " - " (:name data))}))
+      (dom/div {:id "navbar" :class "navbar-collapse collapse"}
+        (dom/ul {:class "nav navbar-nav navbar-right"}
+          (dom/li nil
+            (om/build link {:href "/logout" :name "Logout"})))))))
