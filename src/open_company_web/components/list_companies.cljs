9233718faef7c6fe64a29b79e5a389c6784aee0f
bago2k4 <iacopo@rc0.eu>
2015-07-28 19:37:20 +0200

    add navbar to list companies

--- src/open_company_web/components/list_companies.cljs
+++ src/open_company_web/components/list_companies.cljs
@@ -2,7 +2,8 @@
     (:require [om.core :as om :include-macros true]
               [om-tools.core :as om-core :refer-macros [defcomponent]]
               [om-tools.dom :as dom :include-macros true]
-              [open-company-web.components.link :refer [link]]))
+              [open-company-web.components.link :refer [link]]
+              [open-company-web.components.navbar :refer [navbar]]))
 
 (defcomponent list-page-item [data owner]
   (render [_]
@@ -12,9 +13,11 @@
 (defcomponent list-companies [data owner]
   (render [_]
     (let [company-list (:companies data)]
-      (dom/div
-        (dom/h1 "Companies:")
-        (if (> (count company-list) 0)
-          (dom/ul
-            (om/build-all list-page-item company-list))
-          (dom/h2 "No companies found."))))))
+      (dom/div {:class "row"}
+        (om/build navbar company-data)
+        (dom/div {:class "container-fluid"}
+          (dom/h1 "Companies:")
+          (if (> (count company-list) 0)
+            (dom/ul
+              (om/build-all list-page-item company-list))
+            (dom/h2 "No companies found.")))))))
