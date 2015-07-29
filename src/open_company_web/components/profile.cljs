b47f1e331aa9f4f340b33fcefd1fd9145c44bdf9
bago2k4 <iacopo@rc0.eu>
2015-07-29 15:36:55 +0200

    show profile on /SYMBOL and use # to distinguish tabs

--- src/open_company_web/components/profile.cljs
+++ src/open_company_web/components/profile.cljs
@@ -3,22 +3,51 @@
             [om-tools.core :as om-core :refer-macros [defcomponent]]
             [om-tools.dom :as dom :include-macros true]
             [open-company-web.components.link :refer [link]]
-            [open-company-web.components.profile-components.company :refer [company]]
+            [open-company-web.components.profile-components.company :refer [basics mission on-the-web]]
             [om-bootstrap.nav :as n]
-            [om-bootstrap.panel :as p]))
+            [om-bootstrap.panel :as p]
+            [open-company-web.router :as router]))
 
 (defcomponent profile [data owner]
   (will-mount [_]
     (om/set-state! owner :selected-tab 1))
   (render [_]
-    (dom/div
-      (n/nav {
-        :class "profile-tab-navigation"
-        :bs-style "tabs"
-        :active-key (om/get-state owner :selected-tab)
-        :on-select #(om/set-state! owner :selected-tab %) }
-        (n/nav-item {:key 1 :href "/companies/profile/company"} "Company")
-        (n/nav-item {:key 2 :href "/companies/profile/team"} "Team")
-        (n/nav-item {:key 3 :href "/companies/profile/products"} "Products / Services")
-        (n/nav-item {:key 4 :href "/companies/profile/Funding"} "Funding"))
-      (om/build company data))))
+    (let [symbol (:ticker data)
+          company-data ((keyword symbol) data)]
+      (dom/div
+        (n/nav {
+          :class "profile-tab-navigation"
+          :bs-style "tabs"
+          :active-key (om/get-state owner :selected-tab)
+          :on-select #(om/set-state! owner :selected-tab %) }
+          (let [url (str "/" symbol)]
+            (n/nav-item {
+              :key 1
+              :href url
+              :onClick (fn[e] (.preventDefault e) (router/nav! url))}
+              "Company"))
+          (let [url (str "/" symbol "#team")]
+            (n/nav-item {
+              :key 2
+              :href url
+              :onClick (fn[e] (.preventDefault e) (router/nav! url))}
+              "Team"))
+          (let [url (str "/" symbol "#products")]
+            (n/nav-item {
+              :key 3
+              :href url
+              :onClick (fn[e] (.preventDefault e) (router/nav! url))}
+              "Products / Services"))
+          (let [url (str "/" symbol "#funding")]
+            (n/nav-item {
+              :key 4
+              :href url
+              :onClick (fn[e] (.preventDefault e) (router/nav! url))}
+              "Funding")))
+        (case (om/get-state owner :selected-tab)
+
+          ;; Company
+          1 (dom/div {}
+              (om/build basics company-data)
+              (om/build mission company-data)
+              (om/build on-the-web company-data)))))))
