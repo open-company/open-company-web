0c9448405d57331aa89d32243172e9696ebb26d1
bago2k4 <iacopo@rc0.eu>
2015-07-31 16:36:08 +0200

    sidebar state now involve the router/path atom, fix sidebar urls

--- src/open_company_web/components/sidebar.cljs
+++ src/open_company_web/components/sidebar.cljs
@@ -2,21 +2,39 @@
     (:require [om.core :as om :include-macros true]
               [om-tools.core :as om-core :refer-macros [defcomponent]]
               [om-tools.dom :as dom :include-macros true]
-              [open-company-web.components.link :refer [link]]))
-
-
+              [open-company-web.components.link :refer [link]]
+              [open-company-web.router :as router]))
 
 (defcomponent sidebar [data owner]
+  (init-state [_]
+    {:path @router/path})
   (render [_]
-    (dom/div {:class "col-mid-1 sidebar"}
-      (dom/ul {:class "nav nav-sidebar"}
-        (dom/li {:class (if (= (:active data) "profile") "active" "")}
-          (om/build link {:href "/profile" :name "Profile"}))
-        (dom/li {:class (if (= (:active data) "organization") "active" "")}
-          (om/build link {:href "/organization" :name "Organization"}))
-        (dom/li {:class (if (= (:active data) "equity") "active" "")}
-          (om/build link {:href "/equity" :name "Equity"}))
-        (dom/li {:class (if (= (:active data) "agreements") "active" "")}
-          (om/build link {:href "/agreements" :name "Agreements"}))
-        (dom/li {:class (if (= (:active data) "reports") "active" "")}
-          (om/build link {:href "/reports" :name "Reports"}))))))
+    (let [path (om/get-state owner :path)
+          symbol (:symbol path)
+          profile-url (str "/" symbol)
+          organization-url (str "/" symbol "/organization")
+          equity-url (str "/" symbol "/equity")
+          agreements-url (str "/" symbol "/agreements")
+          reports-url (str "/" symbol "/summary")
+          is-profile (= (:active data) "profile")
+          is-organization (= (:active data) "organization")
+          is-equity (= (:active data) "equity")
+          is-agreements (= (:active data) "agreements")
+          is-report (= (:active data) "reports")]
+      (dom/div {:class "col-mid-1 sidebar"}
+        (dom/ul {:class "nav nav-sidebar"}
+          ; profile
+          (dom/li {:class (if is-profile "active" "")}
+            (om/build link {:href profile-url :name "Profile"}))
+          ; organization
+          (dom/li {:class (if is-organization "active" "")}
+            (om/build link {:href organization-url :name "Organization"}))
+          ; equity
+          (dom/li {:class (if is-equity "active" "")}
+            (om/build link {:href equity-url :name "Equity"}))
+          ; agreements
+          (dom/li {:class (if is-agreements "active" "")}
+            (om/build link {:href agreements-url :name "Agreements"}))
+          ; reports
+          (dom/li {:class (if is-report "active" "")}
+            (om/build link {:href reports-url :name "Reports"})))))))
