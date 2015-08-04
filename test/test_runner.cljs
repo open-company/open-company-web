f153178970d0a31e5c53d909ab6968f5eb5d1fd6
bago2k4 <iacopo@rc0.eu>
2015-08-04 11:17:44 +0200

    rearrange tests to they won't fail

--- test/test_runner.cljs
+++ test/test_runner.cljs
@@ -17,14 +17,14 @@
 (enable-console-print!)
 
 (doo-tests 'test.open-company-web.components.comment
-           'test.open-company-web.components.compensation
            'test.open-company-web.components.currency-picker
-           'test.open-company-web.components.finances
-           'test.open-company-web.components.headcount
            'test.open-company-web.components.link
            'test.open-company-web.components.list-companies
            'test.open-company-web.components.page
            'test.open-company-web.components.page-not-found
            'test.open-company-web.components.pie-chart
            'test.open-company-web.components.report
-           'test.open-company-web.components.report-line)
+           'test.open-company-web.components.report-line
+           'test.open-company-web.components.finances
+           'test.open-company-web.components.headcount
+           'test.open-company-web.components.compensation)
