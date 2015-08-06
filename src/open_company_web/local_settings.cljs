90311eb587946f6095f6fc96ee67bdd9a542f673
bago2k4 <iacopo@rc0.eu>
2015-08-07 01:33:36 +0200

    local settings file

---
+++ src/open_company_web/local_settings.cljs
@@ -0,0 +1,8 @@
+(ns open-company-web.local-settings)
+
+;; Sentry
+(def local-dsn "https://eab4fe25a2ab4ea1b1d6e10185d63844@app.getsentry.com/48160")
+(def local-whitelist-array ["locahost" "127.0.0.1"])
+
+;; API
+(def api-server-domain "http://localhost:3000")
