c14943b45205d3599264ca73663013abaa12587a
bago2k4 <iacopo@rc0.eu>
2015-09-01 22:49:10 +0200

    add externals

---
+++ resources/public/js/externs.js
@@ -0,0 +1,13 @@
+// jQuery
+var $ = function(){};
+var jQuery = function(){};
+// google
+var google = {};
+google.visualization = {};
+google.visualization.NumberFormat = {};
+google.visualization.NumberFormat.format = function(){};
+google.visualization.DataTable = {};
+google.visualization.DataTable.addColumn = function(){};
+google.visualization.DataTable.addRows = function(){};
+google.visualization.PieChart = {};
+google.visualization.PieChart.draw = function(){};
