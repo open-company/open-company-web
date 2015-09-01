ca19f9d4a83db1841f59898acd9e068e078127e5
Sean Johnson <sean@snootymonkey.com>
2015-09-01 09:17:06 -0400

    Update dependencies.

--- project.clj
+++ project.clj
@@ -10,11 +10,11 @@
 
   :dependencies [
     [org.clojure/clojure "1.7.0"] ; Lisp on the JVM http://clojure.org/documentation
-    [org.clojure/clojurescript "1.7.107"] ; ClojureScript compiler https://github.com/clojure/clojurescript
+    [org.clojure/clojurescript "1.7.122"] ; ClojureScript compiler https://github.com/clojure/clojurescript
     [org.clojure/core.async "0.1.346.0-17112a-alpha"] ; Async library https://github.com/clojure/core.async
     [org.omcljs/om "0.9.0" :exclusions [cljsjs/react]] ; Cljs interface to React https://github.com/omcljs/om
     [cljs-http "0.1.37"] ; HTTP for cljs https://github.com/r0man/cljs-http
-    [prismatic/schema "1.0.0-alpha1"] ; Dependency of om-tools https://github.com/Prismatic/schema
+    [prismatic/schema "1.0.0"] ; Dependency of om-tools https://github.com/Prismatic/schema
     [prismatic/plumbing "0.4.4"] ; Dependency of om-tools https://github.com/Prismatic/plumbing
     [prismatic/om-tools "0.3.12"] ; Tools for Om https://github.com/Prismatic/om-tools
     [secretary "2.0.0.1-260a59"] ; Client-side router https://github.com/gf3/secretary
