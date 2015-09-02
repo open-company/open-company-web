071d99e1a6d92d84bedc5dea138e54ef23d4dc84
bago2k4 <iacopo@rc0.eu>
2015-09-02 13:55:00 +0200

    remove export from format-state func, fix component name, change preselected value key, get users from data map

--- src/open_company_web/components/user_selector.cljs
+++ src/open_company_web/components/user_selector.cljs
@@ -10,7 +10,7 @@
       real-name
       (:name user))))
 
-(defn ^:export format-state [state]
+(defn format-state [state]
   (if (not (.-id state)) (.-text state))
   (let [text (.-text state)
         el (.$ js/window (.-element state))
@@ -18,21 +18,23 @@
         my-temp (.$ js/window (str "<span><img class=\"user-icon\" src=\"" img "\" />" (.-text state) "</span>"))]
     my-temp))
 
-(defcomponent user-select [data owner]
+(defcomponent user-selector [data owner]
   (will-mount [_]
+    (cdr/add-script! "//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js")
     (cdr/add-script! "/lib/select2/js/select2.js")
     (cdr/add-style! "/lib/select2/css/select2.css"))
   (did-mount [_]
     (.setTimeout js/window (fn []
                              (let [us (.$ js/window ".user-selector")]
                                (.select2 us (clj->js {"templateResult" format-state
-                                                      "templateSelection" format-state})))) 1000))
+                                                      "templateSelection" format-state}))))
+                 1000))
   (render [_]
     (dom/div {:class "col-md-4"}
       (dom/select {:class "user-selector"
-                   :value (:logged-in-user data)
+                   :value (:value data)
                    :style {"width" "100%"}}
-        (for [user utils/users]
+        (for [user (:users data)]
           (dom/option {
                        :value (:id user)
                        :disabled (:is_bot user)
