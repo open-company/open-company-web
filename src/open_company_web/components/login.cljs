c07b6355fe4b7e753c152776a16b6085df295f03
bago2k4 <iacopo@rc0.eu>
2015-09-07 20:50:48 +0200

    fix login component

--- src/open_company_web/components/login.cljs
+++ src/open_company_web/components/login.cljs
@@ -6,16 +6,16 @@
 (defcomponent login [data owner]
   (render [_]
     (dom/div {:class "login-container"}
-      (case 
-        
+      (dom/h1 "Login with slack:")
+      (case
+
         (:loading data)
         (dom/h2 "Loading...")
-        
+
         (and (not (:loading data)) (contains? data :auth-settings))
-        (dom/h1 "Login with slack:"
-          (dom/a {:href (:full-url (:auth-settings data))} 
-            (dom/img {:alt "Login with Slack"
-                      :height "40"
-                      :width "139"
-                      :src "https://platform.slack-edge.com/img/add_to_slack.png"
-                      :srcset "https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"})))))))
+        (dom/a {:href (:full-url (:auth-settings data))}
+          (dom/img {:alt "Login with Slack"
+                    :height "40"
+                    :width "139"
+                    :src "https://platform.slack-edge.com/img/add_to_slack.png"
+                    :srcset "https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"}))))))
