a70fa46472fe12691716ea4badd9075db04d7c84
bago2k4 <iacopo@rc0.eu>
2015-09-08 20:18:41 +0200

    add login component tests

--- test/test_ui_components.cljs
+++ test/test_ui_components.cljs
@@ -4,7 +4,8 @@
             [test.open-company-web.components.currency-picker]
             [test.open-company-web.components.link]
             [test.open-company-web.components.pie-chart]
-            [test.open-company-web.components.user-selector]))
+            [test.open-company-web.components.user-selector]
+            [test.open-company-web.components.login]))
 
 (enable-console-print!)
 
@@ -16,4 +17,5 @@
   'test.open-company-web.components.currency-picker
   'test.open-company-web.components.link
   'test.open-company-web.components.pie-chart
-  'test.open-company-web.components.user-selector)
+  'test.open-company-web.components.user-selector
+  'test.open-company-web.components.login)
