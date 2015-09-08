cbf636b317ac6c9267ed8bc3d3d5d93bfea9acb3
bago2k4 <iacopo@rc0.eu>
2015-09-08 17:52:14 +0200

    remove console logging, do not explicitly set domain and secure if the domain is localhost (chrome bug)

--- src/open_company_web/lib/cookies.cljs
+++ src/open_company_web/lib/cookies.cljs
@@ -1,6 +1,5 @@
 (ns open-company-web.lib.cookies
-  (:require [goog.net.cookies :as cks]
-            [shodan.console :as console]))
+  (:require [goog.net.cookies :as cks]))
 
 (def cookies-static-obj (goog.net.Cookies. js/document))
 
@@ -8,12 +7,11 @@
   ([c-name c-value]
     (set-cookie! c-name c-value -1))
   ([c-name c-value expiracy]
-    (console/log "settings: " c-name c-value expiracy)
-    (.set cookies-static-obj (name c-name) c-value expiracy "/"))
+    (.set cookies-static-obj (name c-name) c-value expiracy))
   ([c-name c-value expiracy c-path c-domain c-secure]
-    (console/log "settings: " c-name c-value expiracy c-path c-domain c-secure)
-    (.set cookies-static-obj (name c-name) c-value expiracy c-path c-domain (or c-secure false))))
+    (if (= c-domain "localhost")
+      (.set cookies-static-obj (name c-name) c-value expiracy c-path)
+      (.set cookies-static-obj (name c-name) c-value expiracy c-path c-domain c-secure))))
 
 (defn get-cookie [c-name]
-  (console/log "Getting: " c-name "->" (.get cookies-static-obj (name c-name)))
   (.get cookies-static-obj (name c-name)))
