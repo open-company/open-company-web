44b150e93c2bad323fdbbb3ceec14fcaef3dd9dd
bago2k4 <iacopo@rc0.eu>
2015-09-08 16:34:35 +0200

    improve cookie lib

--- src/open_company_web/lib/cookies.cljs
+++ src/open_company_web/lib/cookies.cljs
@@ -1,16 +1,19 @@
 (ns open-company-web.lib.cookies
-  (:require [goog.net.cookies :as cks]))
+  (:require [goog.net.cookies :as cks]
+            [shodan.console :as console]))
 
-(def cookies-static-obj (goog.net.Cookies. js/window js/document))
+(def cookies-static-obj (goog.net.Cookies. js/document))
 
 (defn set-cookie!
   ([c-name c-value]
     (set-cookie! c-name c-value -1))
   ([c-name c-value expiracy]
-    (println "settings: " c-name c-value)
-    (.set cookies-static-obj (name c-name) c-value expiracy)))
+    (console/log "settings: " c-name c-value expiracy)
+    (.set cookies-static-obj (name c-name) c-value expiracy "/"))
+  ([c-name c-value expiracy c-path c-domain c-secure]
+    (console/log "settings: " c-name c-value expiracy c-path c-domain c-secure)
+    (.set cookies-static-obj (name c-name) c-value expiracy c-path c-domain (or c-secure false))))
 
 (defn get-cookie [c-name]
-  (if (keyword? c-name)
-    (.get cookies-static-obj (name c-name))
-    (.get cookies-static-obj c-name)))
+  (console/log "Getting: " c-name "->" (.get cookies-static-obj (name c-name)))
+  (.get cookies-static-obj (name c-name)))
