44bbdb671413e5d4431d69b9f9835381d28393b3
bago2k4 <iacopo@rc0.eu>
2015-09-16 19:18:36 +0200

    move out users data, add format-value based on browser locale and new period format string

--- src/open_company_web/lib/utils.cljs
+++ src/open_company_web/lib/utils.cljs
@@ -90,152 +90,31 @@
 
     ""))
 
+(defn month-string [month]
+  (case month
+    "01" "January"
+    "02" "February"
+    "03" "March"
+    "04" "April"
+    "05" "May"
+    "06" "June"
+    "07" "July"
+    "08" "August"
+    "09" "September"
+    "10" "October"
+    "11" "November"
+    "12" "December"))
+
+(defn period-string [period]
+  (let [[year month] (clojure.string/split period "-")
+        month-str (month-string month)]
+    (if (or (= month "01") (= month "12"))
+      (str month-str " " year)
+      month-str)))
+
 (defn get-periods [prefix n]
  (let [r (range 1 (+ n 1))]
     (into [] (for [a r] (str prefix a)))))
 
-(def users [
-  { :tz_label "Pacific Daylight Time"
-    :deleted false
-    :is_bot false
-    :color "e7392d"
-    :real_name "Iacopo Carraro"
-    :name "iacopo"
-    :has_files true
-    :tz nil
-    :is_primary_owner false
-    :is_restricted false
-    :is_admin false
-    :is_ultra_restricted false
-    :status nil
-    :id "U06STCKLN"
-    :is_owner false
-    :has_2fa false
-    :profile {
-      :email "iacopo@opencompany.io"
-      :first_name "Iacopo"
-      :real_name_normalized "Iacopo Carraro"
-      :image_48 "https://secure.gravatar.com/avatar/98b5456ea1c562024f41501ffd7bc3c6.jpg?s=48&d=https%3A%2F%2Fslack.global.ssl.fastly.net%2F66f9%2Fimg%2Favatars%2Fava_0022-48.png"
-      :image_192 "https://secure.gravatar.com/avatar/98b5456ea1c562024f41501ffd7bc3c6.jpg?s=192&d=https%3A%2F%2Fslack.global.ssl.fastly.net%2F66f9%2Fimg%2Favatars%2Fava_0022.png"
-      :real_name "Iacopo Carraro"
-      :image_72 "https://secure.gravatar.com/avatar/98b5456ea1c562024f41501ffd7bc3c6.jpg?s=72&d=https%3A%2F%2Fslack.global.ssl.fastly.net%2F66f9%2Fimg%2Favatars%2Fava_0022-72.png"
-      :image_24 "https://secure.gravatar.com/avatar/98b5456ea1c562024f41501ffd7bc3c6.jpg?s=24&d=https%3A%2F%2Fslack.global.ssl.fastly.net%2F66f9%2Fimg%2Favatars%2Fava_0022-24.png"
-      :image_32 "https://secure.gravatar.com/avatar/98b5456ea1c562024f41501ffd7bc3c6.jpg?s=32&d=https%3A%2F%2Fslack.global.ssl.fastly.net%2F66f9%2Fimg%2Favatars%2Fava_0022-32.png"
-      :last_name "Carraro"}
-    :tz_offset -25200}
-  { :tz_label "Pacific Daylight Time"
-    :deleted false
-    :is_bot false
-    :color "3c989f"
-    :real_name "Scott Johnson"
-    :name "scott"
-    :has_files false
-    :tz nil
-    :is_primary_owner false
-    :is_restricted false
-    :is_admin false
-    :is_ultra_restricted false
-    :status nil
-    :id "U06SU26MD"
-    :is_owner false
-    :profile {
-      :email "scott@opencompany.io"
-      :first_name "Scott"
-      :real_name_normalized "Scott Johnson"
-      :image_48 "https://secure.gravatar.com/avatar/dd620d96970e0f7565cb4c12044eb7f9.jpg?s=48&d=https%3A%2F%2Fslack.global.ssl.fastly.net%2F66f9%2Fimg%2Favatars%2Fava_0018-48.png"
-      :image_192 "https://secure.gravatar.com/avatar/dd620d96970e0f7565cb4c12044eb7f9.jpg?s=192&d=https%3A%2F%2Fslack.global.ssl.fastly.net%2F66f9%2Fimg%2Favatars%2Fava_0018.png"
-      :real_name "Scott Johnson"
-      :image_72 "https://secure.gravatar.com/avatar/dd620d96970e0f7565cb4c12044eb7f9.jpg?s=72&d=https%3A%2F%2Fslack.global.ssl.fastly.net%2F3654%2Fimg%2Favatars%2Fava_0018-72.png"
-      :image_24 "https://secure.gravatar.com/avatar/dd620d96970e0f7565cb4c12044eb7f9.jpg?s=24&d=https%3A%2F%2Fslack.global.ssl.fastly.net%2F66f9%2Fimg%2Favatars%2Fava_0018-24.png"
-      :image_32 "https://secure.gravatar.com/avatar/dd620d96970e0f7565cb4c12044eb7f9.jpg?s=32&d=https%3A%2F%2Fslack.global.ssl.fastly.net%2F66f9%2Fimg%2Favatars%2Fava_0018-32.png"
-      :last_name "Johnson"
-    }
-    :tz_offset -25200}
-  { :tz_label "Pacific Daylight Time"
-    :deleted false
-    :is_bot false
-    :color "9f69e7"
-    :real_name ""
-    :name "sean"
-    :has_files true
-    :tz nil
-    :is_primary_owner true
-    :is_restricted false
-    :is_admin true
-    :is_ultra_restricted false
-    :status nil
-    :id "U06SBTXJR"
-    :is_owner true
-    :profile {
-      :real_name ""
-      :real_name_normalized ""
-      :email "sean@opencompany.io"
-      :image_24 "https://secure.gravatar.com/avatar/f5b8fc1affa266c8072068f811f63e04.jpg?s=24&d=https%3A%2F%2Fslack.global.ssl.fastly.net%2F66f9%2Fimg%2Favatars%2Fava_0020-24.png"
-      :image_32 "https://secure.gravatar.com/avatar/f5b8fc1affa266c8072068f811f63e04.jpg?s=32&d=https%3A%2F%2Fslack.global.ssl.fastly.net%2F66f9%2Fimg%2Favatars%2Fava_0020-32.png"
-      :image_48 "https://secure.gravatar.com/avatar/f5b8fc1affa266c8072068f811f63e04.jpg?s=48&d=https%3A%2F%2Fslack.global.ssl.fastly.net%2F66f9%2Fimg%2Favatars%2Fava_0020-48.png"
-      :image_72 "https://secure.gravatar.com/avatar/f5b8fc1affa266c8072068f811f63e04.jpg?s=72&d=https%3A%2F%2Fslack.global.ssl.fastly.net%2F66f9%2Fimg%2Favatars%2Fava_0020-72.png"
-      :image_192 "https://secure.gravatar.com/avatar/f5b8fc1affa266c8072068f811f63e04.jpg?s=192&d=https%3A%2F%2Fslack.global.ssl.fastly.net%2F3654%2Fimg%2Favatars%2Fava_0020.png"
-    }
-    :tz_offset -25200}
-  { :tz_label "Pacific Daylight Time"
-    :deleted false
-    :is_bot false
-    :color "4bbe2e"
-    :real_name "Stuart Levinson"
-    :name "stuart"
-    :has_files true
-    :tz nil
-    :is_primary_owner false
-    :is_restricted false
-    :is_admin false
-    :is_ultra_restricted false
-    :status nil
-    :id "U06SQLDFT"
-    :is_owner false
-    :profile {
-      :email "stuart@opencompany.io"
-      :first_name "Stuart"
-      :real_name_normalized "Stuart Levinson"
-      :image_48 "https://secure.gravatar.com/avatar/6ef85399c45b7affe7fc8fb361a3366f.jpg?s=48&d=https%3A%2F%2Fslack.global.ssl.fastly.net%2F66f9%2Fimg%2Favatars%2Fava_0015-48.png"
-      :image_192 "https://secure.gravatar.com/avatar/6ef85399c45b7affe7fc8fb361a3366f.jpg?s=192&d=https%3A%2F%2Fslack.global.ssl.fastly.net%2F66f9%2Fimg%2Favatars%2Fava_0015.png"
-      :real_name "Stuart Levinson"
-      :image_72 "https://secure.gravatar.com/avatar/6ef85399c45b7affe7fc8fb361a3366f.jpg?s=72&d=https%3A%2F%2Fslack.global.ssl.fastly.net%2F66f9%2Fimg%2Favatars%2Fava_0015-72.png"
-      :image_24 "https://secure.gravatar.com/avatar/6ef85399c45b7affe7fc8fb361a3366f.jpg?s=24&d=https%3A%2F%2Fslack.global.ssl.fastly.net%2F66f9%2Fimg%2Favatars%2Fava_0015-24.png"
-      :image_32 "https://secure.gravatar.com/avatar/6ef85399c45b7affe7fc8fb361a3366f.jpg?s=32&d=https%3A%2F%2Fslack.global.ssl.fastly.net%2F66f9%2Fimg%2Favatars%2Fava_0015-32.png"
-      :last_name "Levinson"
-    }
-    :tz_offset -25200}
-  { :tz_label "Pacific Daylight Time"
-    :deleted false
-    :is_bot true
-    :color "674b1b"
-    :real_name "Transparency Bot"
-    :name "transparencybot"
-    :has_files false
-    :tz nil
-    :is_primary_owner false
-    :is_restricted false
-    :is_admin false
-    :is_ultra_restricted false
-    :status nil
-    :id "U09GS25L4"
-    :is_owner false
-    :profile {
-      :image_512 "https://avatars.slack-edge.com/2015-08-24/9570128512_e722b04e32ec23b1b6ec_192.jpg"
-      :first_name "Transparency"
-      :real_name_normalized "Transparency Bot"
-      :image_48 "https://avatars.slack-edge.com/2015-08-24/9570128512_e722b04e32ec23b1b6ec_48.jpg"
-      :image_192 "https://avatars.slack-edge.com/2015-08-24/9570128512_e722b04e32ec23b1b6ec_192.jpg"
-      :real_name "Transparency Bot"
-      :image_original "https://avatars.slack-edge.com/2015-08-24/9570128512_e722b04e32ec23b1b6ec_original.jpg"
-      :image_72 "https://avatars.slack-edge.com/2015-08-24/9570128512_e722b04e32ec23b1b6ec_72.jpg"
-      :image_24 "https://avatars.slack-edge.com/2015-08-24/9570128512_e722b04e32ec23b1b6ec_24.jpg"
-      :title "Helps open your company"
-      :image_32 "https://avatars.slack-edge.com/2015-08-24/9570128512_e722b04e32ec23b1b6ec_32.jpg"
-      :last_name "Bot"
-      :image_1024 "https://avatars.slack-edge.com/2015-08-24/9570128512_e722b04e32ec23b1b6ec_192.jpg"
-      :bot_id "B09GS6LSW"
-    }
-    :tz_offset -25200
-  }
-])
+(defn format-value [value]
+  (.toLocaleString value))
