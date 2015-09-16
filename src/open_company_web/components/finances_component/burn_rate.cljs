83bd96548d8cf43ef3e0eaf2a6ef9d461fdef47c
bago2k4 <iacopo@rc0.eu>
2015-09-16 21:44:32 +0200

    show chart for burn-rate and runaway

--- src/open_company_web/components/finances_component/burn_rate.cljs
+++ src/open_company_web/components/finances_component/burn_rate.cljs
@@ -2,9 +2,20 @@
   (:require [om.core :as om :include-macros true]
             [om-tools.core :as om-core :refer-macros [defcomponent]]
             [om-tools.dom :as dom :include-macros true]
-            [open-company-web.components.link :refer [link]]
-            [open-company-web.router :as router]))
+            [open-company-web.router :as router]
+            [open-company-web.lib.utils :as utils]
+            [open-company-web.lib.iso4217 :refer [iso4217]]
+            [open-company-web.components.charts :refer [column-chart]]
+            [open-company-web.components.finances-component.utils :refer [get-chart-data]]))
 
 (defcomponent burn-rate [data owner]
   (render [_]
-    (dom/h1 "Burn Rate")))
+    (let [finances-data (:data (:oc:finances data))
+          value-set (first finances-data)
+          burn-rate-val (utils/format-value (:burn-rate value-set))
+          period (utils/period-string (:period value-set))
+          cur-symbol (utils/get-symbol-for-currency-code (:currency data))]
+      (dom/div {:class "section burn-rate"}
+        (dom/h3 {} (str cur-symbol burn-rates-val))
+        (dom/p {} period)
+        (om/build column-chart (get-chart-data finances-data cur-symbol :burn-rate "Burn Rate"))))))
--- src/open_company_web/components/finances_component/runaway.cljs
+++ src/open_company_web/components/finances_component/runaway.cljs
@@ -2,9 +2,20 @@
   (:require [om.core :as om :include-macros true]
             [om-tools.core :as om-core :refer-macros [defcomponent]]
             [om-tools.dom :as dom :include-macros true]
-            [open-company-web.components.link :refer [link]]
-            [open-company-web.router :as router]))
+            [open-company-web.router :as router]
+            [open-company-web.lib.utils :as utils]
+            [open-company-web.lib.iso4217 :refer [iso4217]]
+            [open-company-web.components.charts :refer [column-chart]]
+            [open-company-web.components.finances-component.utils :refer [get-chart-data]]))
 
 (defcomponent runaway [data owner]
   (render [_]
-    (dom/h1 "Runaway")))
+    (let [finances-data (:data (:oc:finances data))
+          value-set (first finances-data)
+          runaway-val (utils/format-value (:runaway value-set))
+          period (utils/period-string (:period value-set))
+          cur-symbol (utils/get-symbol-for-currency-code (:currency data))]
+      (dom/div {:class "section runaway"}
+        (dom/h3 {} runaway-val)
+        (dom/p {} period)
+        (om/build column-chart (get-chart-data finances-data "" :runaway "Runaway"))))))
