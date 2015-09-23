701951176dfb0c65cecdd45da333b3a36abdfc74
bago2k4 <iacopo@rc0.eu>
2015-09-23 22:15:07 +0200

    show edit button if necessary

--- src/open_company_web/components/finances_component/cash.cljs
+++ src/open_company_web/components/finances_component/cash.cljs
@@ -6,7 +6,8 @@
             [open-company-web.lib.utils :as utils]
             [open-company-web.lib.iso4217 :refer [iso4217]]
             [open-company-web.components.charts :refer [column-chart]]
-            [open-company-web.components.finances-component.utils :refer [get-chart-data]]))
+            [open-company-web.components.finances-component.utils :refer [get-chart-data]]
+            [open-company-web.components.utility-components :refer [editable-pen]]))
 
 (defcomponent cash [data owner]
   (render [_]
@@ -16,6 +17,8 @@
           period (utils/period-string (:period value-set))
           cur-symbol (utils/get-symbol-for-currency-code (:currency data))]
       (dom/div {:class "section cash"}
-        (dom/h3 {} (str cur-symbol cash-val))
+        (dom/h3 {}
+                (str cur-symbol cash-val)
+                (om/build editable-pen {}))
         (dom/p {} period)
         (om/build column-chart (get-chart-data finances-data cur-symbol :cash "Cash"))))))
--- src/open_company_web/components/finances_component/costs.cljs
+++ src/open_company_web/components/finances_component/costs.cljs
@@ -6,7 +6,8 @@
             [open-company-web.lib.utils :as utils]
             [open-company-web.lib.iso4217 :refer [iso4217]]
             [open-company-web.components.charts :refer [column-chart]]
-            [open-company-web.components.finances-component.utils :refer [get-chart-data]]))
+            [open-company-web.components.finances-component.utils :refer [get-chart-data]]
+            [open-company-web.components.utility-components :refer [editable-pen]]))
 
 (defcomponent costs [data owner]
   (render [_]
@@ -16,6 +17,8 @@
           period (utils/period-string (:period value-set))
           cur-symbol (utils/get-symbol-for-currency-code (:currency data))]
       (dom/div {:class "section costs"}
-        (dom/h3 {} (str cur-symbol costs-val))
+        (dom/h3 {}
+                (str cur-symbol costs-val)
+                (om/build editable-pen {}))
         (dom/p {} period)
         (om/build column-chart (get-chart-data finances-data cur-symbol :costs "Costs"))))))
