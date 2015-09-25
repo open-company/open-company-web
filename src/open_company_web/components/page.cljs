8ffebc7b9fb2061d4b9f13a1d29f60a429e61180
bago2k4 <iacopo@rc0.eu>
2015-09-25 20:14:14 +0200

    adapt page to the new data

--- src/open_company_web/components/page.cljs
+++ src/open_company_web/components/page.cljs
@@ -1,30 +1,28 @@
 (ns open-company-web.components.page
-    (:require [om.core :as om :include-macros true]
-              [om-tools.core :as om-core :refer-macros [defcomponent]]
-              [om-tools.dom :as dom :include-macros true]
-              [open-company-web.lib.utils :refer [handle-change]]
-              [open-company-web.components.headcount :refer [headcount]]
-              [open-company-web.components.finances :refer [finances]]
-              [open-company-web.components.compensation :refer [compensation]]
-              [open-company-web.components.currency-picker :refer [currency-picker]]
-              [open-company-web.components.navbar :refer [navbar]]
-              [open-company-web.components.link :refer [link]]
-              [open-company-web.components.sidebar :refer [sidebar]]
-              [open-company-web.components.profile :refer [profile]]
-              [open-company-web.router :as router]
-              [clojure.string :as str]))
+  (:require [om.core :as om :include-macros true]
+            [om-tools.core :as om-core :refer-macros [defcomponent]]
+            [om-tools.dom :as dom :include-macros true]
+            [open-company-web.lib.utils :refer [handle-change]]
+            [open-company-web.components.currency-picker :refer [currency-picker]]
+            [open-company-web.components.navbar :refer [navbar]]
+            [open-company-web.components.link :refer [link]]
+            [open-company-web.components.sidebar :refer [sidebar]]
+            [open-company-web.components.profile :refer [profile]]
+            [open-company-web.router :as router]
+            [open-company-web.components.section-selector :refer [section-selector]]
+            [clojure.string :as str]))
 
 (enable-console-print!)
 
 (defcomponent company [data owner]
   (render [_]
-    (let [ticker (:ticker @router/path)
-          company-data ((keyword ticker) data)
+    (let [slug (:slug @router/path)
+          company-data ((keyword slug) data)
           reports (filterv #(= (:rel %) "report") (:links company-data))]
       (dom/div {:class "company-container row"}
         (om/build navbar company-data)
         (dom/div {:class "container-fluid"}
-          (om/build sidebar {:active "profile"})
+          (om/build sidebar data)
           (dom/div {:class "col-md-11 col-md-offset-1 main"}
             (cond
 
@@ -32,10 +30,13 @@
               (dom/div
                 (dom/h4 "Loading data..."))
 
-              (contains? company-data :symbol)
+              (contains? @router/path :section)
+              (om/build section-selector data)
+
+              (contains? company-data :slug)
               (om/build profile data)
 
               :else
               (dom/div
-                (dom/h2 (str (:ticker @router/path) " not found"))
+                (dom/h2 (str (:slug @router/path) " not found"))
                 (om/build link {:href "/" :name "Back home"})))))))))
