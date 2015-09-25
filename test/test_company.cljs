4b790f00f61c2e1fa3aabc3d0616f7ab22495511
bago2k4 <iacopo@rc0.eu>
2015-09-25 20:16:30 +0200

    remove old tests

--- test/test_company.cljs
+++ test/test_company.cljs
@@ -1,16 +1,6 @@
 (ns test.test-company
   (:require [doo.runner :refer-macros [doo-tests]]
-            [test.open-company-web.components.report]
-            [test.open-company-web.components.report-line]
-            [test.open-company-web.components.compensation]
-            [test.open-company-web.components.finances]
-            [test.open-company-web.components.headcount]
-            [test.open-company-web.components.new-report-popover]
             [test.open-company-web.components.profile]
-            [test.open-company-web.components.profile-components.company]
-            [test.open-company-web.components.report.compensation-section]
-            [test.open-company-web.components.report.finances-section]
-            [test.open-company-web.components.report.headcount-section]
             [test.open-company-web.components.report.percentage-switch]))
 
 (enable-console-print!)
@@ -19,15 +9,5 @@
 ;; we don't run out of memory on CI server
 
 (doo-tests 
-  'test.open-company-web.components.report
-  'test.open-company-web.components.report-line
-  'test.open-company-web.components.finances
-  'test.open-company-web.components.headcount
-  'test.open-company-web.components.compensation
-  'test.open-company-web.components.new-report-popover
   'test.open-company-web.components.profile
-  'test.open-company-web.components.profile-components.company
-  'test.open-company-web.components.report.compensation-section
-  'test.open-company-web.components.report.finances-section
-  'test.open-company-web.components.report.headcount-section
   'test.open-company-web.components.report.percentage-switch)
