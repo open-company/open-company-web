0ac19a8fa0f1f81081461d11f50c05ce9f86b19a
bago2k4 <iacopo@rc0.eu>
2015-09-28 19:27:20 +0200

    add section selector tests

---
+++ test/open_company_web/components/section_selector.cljs
@@ -0,0 +1,33 @@
+(ns test.open-company-web.components.section-selector
+    (:require [cljs.test :refer-macros [deftest async testing is are use-fixtures]]
+              [cljs-react-test.simulate :as sim]
+              [cljs-react-test.utils :as tu]
+              [om.core :as om :include-macros true]
+              [dommy.core :as dommy :refer-macros [sel1 sel]]
+              [open-company-web.components.section-selector :refer [section-selector]]
+              [om.dom :as dom :include-macros true]
+              [open-company-web.router :as router]))
+
+(enable-console-print!)
+
+; dynamic mount point for components
+(def ^:dynamic c)
+
+(def test-atom {
+  :name "Buffer"
+  :currency "USD"
+  :slug "buffer"
+  :sections []
+})
+
+(deftest test-section-selector-component
+  (testing "Section selector component"
+    (router/set-route! ["companies" "buffer" "section" "cash"]
+                       {:slug "buffer" :section "section" :tab "cash"})
+    (let [c (tu/new-container!)
+          app-state (atom test-atom)
+          _ (om/root section-selector app-state {:target c})
+          section-selector-node (sel1 c [:h4])]
+      (is (not (nil? section-selector-node)))
+      (is (= (.-innerHTML section-selector-node) "Section section not found"))
+      (tu/unmount! c))))
