dfb394e2869272ac44296ac14bd879a9cf6073e4
bago2k4 <iacopo@rc0.eu>
2015-09-28 19:25:26 +0200

    add missing company tests

--- test/test_company.cljs
+++ test/test_company.cljs
@@ -1,13 +1,23 @@
 (ns test.test-company
   (:require [doo.runner :refer-macros [doo-tests]]
             [test.open-company-web.components.profile]
-            [test.open-company-web.components.report.percentage-switch]))
+            [test.open-company-web.components.finances-pieces.burn-rate]
+            [test.open-company-web.components.finances-pieces.cash]
+            [test.open-company-web.components.finances-pieces.costs]
+            [test.open-company-web.components.finances-pieces.revenue]
+            [test.open-company-web.components.finances-pieces.runway]
+            [test.open-company-web.components.finances-pieces.finances]))
 
 (enable-console-print!)
 
 ;; Break tests into somewhat logical groupings of 1/2 dozen or less tests so
 ;; we don't run out of memory on CI server
 
-(doo-tests 
+(doo-tests
   'test.open-company-web.components.profile
-  'test.open-company-web.components.report.percentage-switch)
+  'test.open-company-web.components.finances-pieces.burn-rate
+  'test.open-company-web.components.finances-pieces.cash
+  'test.open-company-web.components.finances-pieces.costs
+  'test.open-company-web.components.finances-pieces.revenue
+  'test.open-company-web.components.finances-pieces.runway
+  'test.open-company-web.components.finances-pieces.finances)
