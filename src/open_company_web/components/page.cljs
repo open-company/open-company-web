987b23a3f9abc73d0b5ce9606f0f31101fe800ef
bago2k4 <iacopo@rc0.eu>
2015-10-01 19:12:46 +0200

    remove sidebar, single section show fix, company data edit under /profile, add all sections route, code cleanup

--- src/open_company_web/components/page.cljs
+++ src/open_company_web/components/page.cljs
@@ -2,28 +2,25 @@
   (:require [om.core :as om :include-macros true]
             [om-tools.core :as om-core :refer-macros [defcomponent]]
             [om-tools.dom :as dom :include-macros true]
-            [open-company-web.lib.utils :refer [handle-change]]
-            [open-company-web.components.currency-picker :refer [currency-picker]]
             [open-company-web.components.navbar :refer [navbar]]
             [open-company-web.components.link :refer [link]]
-            [open-company-web.components.sidebar :refer [sidebar]]
             [open-company-web.components.profile :refer [profile]]
             [open-company-web.router :as router]
             [open-company-web.components.section-selector :refer [section-selector]]
-            [clojure.string :as str]))
+            [open-company-web.components.all-sections :refer [all-sections]]
+            [clojure.string :as str]
+            [open-company-web.lib.utils :as utils]))
 
 (enable-console-print!)
 
 (defcomponent company [data owner]
   (render [_]
     (let [slug (:slug @router/path)
-          company-data ((keyword slug) data)
-          reports (filterv #(= (:rel %) "report") (:links company-data))]
+          company-data ((keyword slug) data)]
       (dom/div {:class "company-container row"}
         (om/build navbar company-data)
         (dom/div {:class "container-fluid"}
-          (om/build sidebar data)
-          (dom/div {:class "col-md-11 col-md-offset-1 main"}
+          (dom/div {:class "col-md-12 main"}
             (cond
 
               (:loading data)
@@ -31,10 +28,13 @@
                 (dom/h4 "Loading data..."))
 
               (contains? @router/path :section)
-              (om/build section-selector data)
+              (om/build section-selector {:data company-data :section (keyword (:section @router/path))})
 
-              (contains? company-data :slug)
+              (utils/in? (:route @router/path) "profile")
               (om/build profile data)
+
+              (and (not (contains? data :loading)) (contains? data (keyword slug)))
+              (om/build all-sections data)
 
               :else
               (dom/div
