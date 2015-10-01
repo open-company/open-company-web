50262670211e77b119855e80b5ea356e2912ddf9
bago2k4 <iacopo@rc0.eu>
2015-10-01 19:16:57 +0200

    link the update-footer to the single section page

--- src/open_company_web/components/update_footer.cljs
+++ src/open_company_web/components/update_footer.cljs
@@ -2,10 +2,12 @@
   (:require [om.core :as om :include-macros true]
             [om-tools.core :as om-core :refer-macros [defcomponent]]
             [om-tools.dom :as dom :include-macros true]
-            [open-company-web.lib.utils :as utils]))
+            [open-company-web.lib.utils :as utils]
+            [open-company-web.router :as router]))
 
 (defcomponent update-footer [data owner]
   (render [_]
     (dom/div {:class "update-footer"}
-      (dom/p {:class "timeago"} (utils/time-since (:updated-at data)))
-      (dom/img {:src (:image (:author data)) :title (:name (:author data)) :class "author-image"}))))
+      (dom/a {:href (str "/companies/" (:slug @router/path) "/" (name (:section data)))}
+        (dom/p {:class "timeago"} (utils/time-since (:updated-at data)))
+        (dom/img {:src (:image (:author data)) :title (:name (:author data)) :class "author-image"})))))
