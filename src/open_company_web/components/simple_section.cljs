7ca3c669e1b493552fbe9c5995b5af40c9b6df97
bago2k4 <iacopo@rc0.eu>
2015-10-05 23:01:37 +0200

    add revisions-navigator component

--- src/open_company_web/components/simple_section.cljs
+++ src/open_company_web/components/simple_section.cljs
@@ -7,20 +7,23 @@
             [open-company-web.components.update-footer :refer (update-footer)]
             [open-company-web.components.rich-editor :refer (rich-editor)]
             [open-company-web.components.editable-title :refer (editable-title)]
-            [open-company-web.lib.utils :as utils]))
+            [open-company-web.lib.utils :as utils]
+            [open-company-web.components.revisions-navigator :refer [revisions-navigator]]))
 
 (defcomponent simple-section [data owner]
   (render [_]
     (let [company-data (:company-data data)
           section (:section data)
+          section-data (section company-data)
           read-only (:read-only data)]
       (if (:loading company-data)
         (dom/h4 {} "Loading data...")
         (dom/div {:class "simple-section section"}
-          (om/build editable-title {:title (:title (section company-data))
+          (om/build editable-title {:title (:title section-data)
                                     :section section
                                     :read-only read-only})
-          (dom/div {:class "challenges-body"}
+          (dom/div {:class "simple-section-body"}
             (om/build rich-editor {:read-only read-only
-                                   :section-data (section company-data)
-                                   :section section})))))))
+                                   :section-data section-data
+                                   :section section}))
+          (om/build revisions-navigator (:revisions section-data)))))))
