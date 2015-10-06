7fb4a003896e0ed20a0e48663ef142e56838a5bb
bago2k4 <iacopo@rc0.eu>
2015-10-06 13:46:56 +0200

    avoid to reload the url, use the router/nav! to override the url and dispatch the route

--- src/open_company_web/components/revisions_navigator.cljs
+++ src/open_company_web/components/revisions_navigator.cljs
@@ -5,9 +5,16 @@
             [open-company-web.lib.utils :as utils]
             [open-company-web.router :as router]))
 
+(defn nav! [e rev]
+  (println rev)
+  (.preventDefault e)
+  (router/nav! rev))
+
 (defcomponent revisions-navigator [data owner]
   (render [_]
     (let [as-of (:as-of (:query-params @router/path))
+          sorted-revisions (utils/sort-revisions data)
+          as-of (if (nil? as-of) (:updated-at (last sorted-revisions)) as-of)
           rev-first (utils/revision-first data as-of)
           rev-prev (utils/revision-prev data as-of)
           rev-first (if (= rev-prev rev-first) nil rev-first)
@@ -17,19 +24,23 @@
       (dom/div {:class "revisions-navigator"}
         (dom/div {:class "revisions-navigator-left"}
           (when rev-first
-            (dom/a {:href rev-first}
+            (dom/a {:href rev-first
+                    :on-click #(nav! % rev-first)}
               (dom/div {:class "double-prev"}
                 (dom/i {:class "fa fa-backward"}))))
           (when rev-prev
-            (dom/a {:href rev-prev}
+            (dom/a {:href rev-prev
+                    :on-click #(nav! % rev-prev)}
               (dom/div {:class "single-prev"}
                 (dom/i {:class "fa fa-caret-left"})))))
         (dom/div {:class "revisions-navigator-right"}
           (when rev-last
-            (dom/a {:href rev-last}
+            (dom/a {:href rev-last
+                    :on-click #(nav! % rev-last)}
               (dom/div {:class "double-next"}
                 (dom/i {:class "fa fa-forward"}))))
           (when rev-next
-            (dom/a {:href rev-next}
+            (dom/a {:href rev-next
+                    :on-click #(nav! % rev-next)}
               (dom/div {:class "single-next"}
                 (dom/i {:class "fa fa-caret-right"})))))))))
