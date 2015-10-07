6d3bf7eab77869afbde369586eb0d12c8a3e0e18
bago2k4 <iacopo@rc0.eu>
2015-10-07 21:24:38 +0200

    revisions navigation works

--- src/open_company_web/components/revisions_navigator.cljs
+++ src/open_company_web/components/revisions_navigator.cljs
@@ -3,42 +3,102 @@
             [om-tools.core :as om-core :refer-macros [defcomponent]]
             [om-tools.dom :as dom :include-macros true]
             [open-company-web.lib.utils :as utils]
-            [open-company-web.router :as router]))
+            [open-company-web.router :as router]
+            [open-company-web.api :as api]))
 
-(defn nav-revision! [e rev]
+(defn revision-next
+  "Return the first future revision"
+  [revisions as-of]
+  (if (> (count revisions) 0)
+    (loop [idx (dec (count revisions))
+           next-rev nil]
+      (let [rev (get revisions idx)]
+        (if (<= (compare (:updated-at rev) as-of) 0)
+          next-rev
+          (if (= idx 0)
+            rev
+            (recur (dec idx)
+                   rev)))))
+    nil))
+
+(defn revision-last [revisions as-of]
+  (let [last-revision (last revisions)]
+    (if (and
+          last-revision
+          (not= last-revision (revision-next revisions as-of)))
+      last-revision
+      nil)))
+
+(defn revision-prev
+  "Return the first future revision"
+  [revisions as-of]
+  (if (> (count revisions) 0)
+    (loop [idx 0
+           prev-rev nil]
+      (let [rev (get revisions idx)]
+        (if (>= (compare (:updated-at rev) as-of) 0)
+          prev-rev
+          (if (= idx (dec (count revisions)))
+            rev ;return the last possible value as it's past
+            (recur (inc idx)
+                   rev)))))
+      nil))
+
+(defn revision-first [revisions as-of]
+  (let [rev (first revisions)]
+    (if (and rev
+             (< (compare (:updated-at rev) as-of) 0)
+             (not= rev (revision-prev revisions as-of)))
+      rev
+      false)))
+
+(defn nav-revision! [e rev owner data read-only]
   (.preventDefault e)
-  (router/nav! rev))
+  (let [section (:section data)
+        commentary (:commentary data)]
+    ((:navigate-cb data))
+    (om/update-state! owner :as-of (fn [_] (:updated-at rev)))
+    (api/load-revision rev (keyword (:slug @router/path)) section read-only commentary)))
 
 (defcomponent revisions-navigator [data owner]
-  (render [_]
+  (init-state [_]
     (let [revisions (:revisions data)
-          as-of (:as-of (:query-params @router/path))
-          as-of (if (nil? as-of) (utils/as-of-now) as-of)
-          actual-revision (:actual-revision data)
-          rev-first (utils/revision-first revisions as-of)
-          rev-prev  (utils/revision-prev revisions as-of)
-          rev-next  (utils/revision-next revisions as-of actual-revision)
-          rev-last  (if rev-next (utils/revision-last revisions as-of actual-revision) nil)]
+          sorted-revisions (utils/sort-revisions revisions)
+          last-revision (last sorted-revisions)]
+      {:as-of (:updated-at data)}))
+  (render [_]
+    (let [revisions (utils/sort-revisions (:revisions data))
+          last-revision (last revisions)
+          as-of (om/get-state owner :as-of)
+          rev-first (revision-first revisions as-of)
+          rev-prev  (revision-prev revisions as-of)
+          rev-next  (revision-next revisions as-of)
+          rev-last  (if rev-next (revision-last revisions as-of) nil)
+          section (:section data)
+          commentary (:commentary data)]
       (dom/div {:class "revisions-navigator"}
-        (dom/div {:class "revisions-navigator-left"}
-          (when rev-first
-            (dom/a {:href rev-first
-                    :on-click #(nav-revision! % rev-first)}
-              (dom/div {:class "double-prev"}
-                (dom/i {:class "fa fa-backward"}))))
-          (when rev-prev
-            (dom/a {:href rev-prev
-                    :on-click #(nav-revision! % rev-prev)}
-              (dom/div {:class "single-prev"}
-                (dom/i {:class "fa fa-caret-left"})))))
-        (dom/div {:class "revisions-navigator-right"}
-          (when rev-last
-            (dom/a {:href rev-last
-                    :on-click #(nav-revision! % rev-last)}
-              (dom/div {:class "double-next"}
-                (dom/i {:class "fa fa-forward"}))))
-          (when rev-next
-            (dom/a {:href rev-next
-                    :on-click #(nav-revision! % rev-next)}
-              (dom/div {:class "single-next"}
-                (dom/i {:class "fa fa-caret-right"})))))))))
+        (if (:loading data)
+          (dom/div {:style {:text-align "center"}} "Loading...")
+          (dom/div {}
+            (dom/div {:class "revisions-navigator-left"}
+              (when rev-first
+                (dom/a {:href (:href rev-first)
+                        :on-click #(nav-revision! % rev-first owner data true)}
+                  (dom/div {:class "double-prev"}
+                    (dom/i {:class "fa fa-backward"}))))
+              (when rev-prev
+                (dom/a {:href (:href rev-prev)
+                        :on-click #(nav-revision! % rev-prev owner data true)}
+                  (dom/div {:class "single-prev"}
+                    (dom/i {:class "fa fa-caret-left"})))))
+            (dom/div {:class "revisions-navigator-right"}
+              (when rev-last
+                (dom/a {:href (:href rev-last)
+                        :on-click #(nav-revision! % rev-last owner data false)}
+                  (dom/div {:class "double-next"}
+                    (dom/i {:class "fa fa-forward"}))))
+              (when rev-next
+                (dom/a {:href (:href rev-next)
+                        :on-click #(nav-revision! % rev-next owner data (= (:updated-at rev-next) (:updated-at last-revision)))}
+                  (dom/div {:class "single-next"}
+                    (dom/i {:class "fa fa-caret-right"})))))))))))
