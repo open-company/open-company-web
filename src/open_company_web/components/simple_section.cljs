f4a7063d5ceb46595d3f87e85fa404469d80644e
bago2k4 <iacopo@rc0.eu>
2015-10-07 12:09:13 +0200

    pass the save channel to the subcomponents, listen for the save-channel to save with api

--- src/open_company_web/components/simple_section.cljs
+++ src/open_company_web/components/simple_section.cljs
@@ -1,4 +1,5 @@
 (ns open-company-web.components.simple-section
+  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [om.core :as om :include-macros true]
             [om-tools.core :as om-core :refer-macros [defcomponent]]
             [om-tools.dom :as dom :include-macros true]
@@ -7,10 +8,29 @@
             [open-company-web.components.update-footer :refer (update-footer)]
             [open-company-web.components.rich-editor :refer (rich-editor)]
             [open-company-web.components.editable-title :refer (editable-title)]
+            [open-company-web.api :refer [save-or-create-section]]
             [open-company-web.lib.utils :as utils]
-            [open-company-web.components.revisions-navigator :refer [revisions-navigator]]))
+            [open-company-web.components.revisions-navigator :refer [revisions-navigator]]
+            [cljs.core.async :refer [put! chan <!]]
+            [open-company-web.dispatcher :as dispatcher]))
 
 (defcomponent simple-section [data owner]
+  (init-state [_]
+    (let [save-channel (chan)
+          section (:section data)]
+      (utils/add-channel (str "save-section-" section) save-channel))
+    {})
+  (will-mount [_]
+    (let [save-change (utils/get-channel (str "save-section-" (:section data)))]
+        (go (loop []
+          (let [change (<! save-change)
+                cursor @dispatcher/app-state
+                slug (:slug @router/path)
+                company-data ((keyword slug) cursor)
+                section (:section data)
+                section-data (section company-data)]
+            (save-or-create-section section-data)
+            (recur))))))
   (render [_]
     (let [company-data (:company-data data)
           section (:section data)
@@ -19,11 +39,13 @@
       (if (:loading company-data)
         (dom/h4 {} "Loading data...")
         (dom/div {:class "simple-section section"}
-          (om/build editable-title {:title (:title section-data)
+          (om/build editable-title {:section-data section-data
                                     :section section
-                                    :read-only read-only})
+                                    :read-only read-only
+                                    :save-channel (str "save-section-" section)})
           (dom/div {:class "simple-section-body"}
             (om/build rich-editor {:read-only read-only
                                    :section-data section-data
-                                   :section section}))
+                                   :section section
+                                   :save-channel (str "save-section-" section)}))
           (om/build revisions-navigator (:revisions section-data)))))))
