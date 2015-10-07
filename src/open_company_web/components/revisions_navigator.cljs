ecdb270fedaf0bd97e3425001c335bbfb5f180c2
bago2k4 <iacopo@rc0.eu>
2015-10-07 14:21:39 +0200

    fix revision links and click callback name

--- src/open_company_web/components/revisions_navigator.cljs
+++ src/open_company_web/components/revisions_navigator.cljs
@@ -5,39 +5,40 @@
             [open-company-web.lib.utils :as utils]
             [open-company-web.router :as router]))
 
-(defn nav! [e rev]
-  (println rev)
+(defn nav-revision! [e rev]
   (.preventDefault e)
   (router/nav! rev))
 
 (defcomponent revisions-navigator [data owner]
   (render [_]
-    (let [as-of (:as-of (:query-params @router/path))
+    (let [revisions (:revisions data)
+          as-of (:as-of (:query-params @router/path))
           as-of (if (nil? as-of) (utils/as-of-now) as-of)
-          rev-first (utils/revision-first data as-of)
-          rev-prev  (utils/revision-prev data as-of)
-          rev-next  (utils/revision-next data as-of)
-          rev-last  (utils/revision-last data as-of)]
+          actual-revision (:actual-revision data)
+          rev-first (utils/revision-first revisions as-of)
+          rev-prev  (utils/revision-prev revisions as-of)
+          rev-next  (utils/revision-next revisions as-of actual-revision)
+          rev-last  (if rev-next (utils/revision-last revisions as-of actual-revision) nil)]
       (dom/div {:class "revisions-navigator"}
         (dom/div {:class "revisions-navigator-left"}
           (when rev-first
             (dom/a {:href rev-first
-                    :on-click #(nav! % rev-first)}
+                    :on-click #(nav-revision! % rev-first)}
               (dom/div {:class "double-prev"}
                 (dom/i {:class "fa fa-backward"}))))
           (when rev-prev
             (dom/a {:href rev-prev
-                    :on-click #(nav! % rev-prev)}
+                    :on-click #(nav-revision! % rev-prev)}
               (dom/div {:class "single-prev"}
                 (dom/i {:class "fa fa-caret-left"})))))
         (dom/div {:class "revisions-navigator-right"}
           (when rev-last
             (dom/a {:href rev-last
-                    :on-click #(nav! % rev-last)}
+                    :on-click #(nav-revision! % rev-last)}
               (dom/div {:class "double-next"}
                 (dom/i {:class "fa fa-forward"}))))
           (when rev-next
             (dom/a {:href rev-next
-                    :on-click #(nav! % rev-next)}
+                    :on-click #(nav-revision! % rev-next)}
               (dom/div {:class "single-next"}
                 (dom/i {:class "fa fa-caret-right"})))))))))
