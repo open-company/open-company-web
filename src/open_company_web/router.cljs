0a5ee0cfd95eb8003db1cff7caaf90dc4dc886e2
bago2k4 <iacopo@rc0.eu>
2015-10-07 14:26:04 +0200

    fix issue with query string being summed instead of replaced

--- src/open_company_web/router.cljs
+++ src/open_company_web/router.cljs
@@ -1,6 +1,7 @@
 (ns open-company-web.router
-  (:require [secretary.core :as secretary])
-  (:import [goog.history Html5History]))
+  (:require [secretary.core :as secretary]
+            [goog.history.Html5History :as history5]))
+  ; (:import [goog.history Html5History]))
 
 (enable-console-print!)
 
@@ -14,14 +15,32 @@
 (defn get-token []
   (str js/window.location.pathname js/window.location.search))
 
+; this is needed as of this
+; https://code.google.com/p/closure-library/source/detail?spec=svn88dc096badf091f380b4c2b4a6514184511de657&r=88dc096badf091f380b4c2b4a6514184511de657
+; setToken doen't replace the query string, it only attach it at the end
+; solution here: https://github.com/Sparrho/supper/blob/master/src-cljs/supper/history.cljs
+(defn build-transformer
+  "Custom transformer is needed to replace query parameters, rather
+  than adding to them.
+  See: https://gist.github.com/pleasetrythisathome/d1d9b1d74705b6771c20"
+  []
+  (let [transformer (goog.history.Html5History.TokenTransformer.)]
+    (set! (.. transformer -retrieveToken)
+          (fn [path-prefix location]
+            (str (.-pathname location) (.-search location))))
+    (set! (.. transformer -createUrl)
+          (fn [token path-prefix location]
+            (str path-prefix token)))
+    transformer))
+
 (defn make-history []
-  (doto (Html5History.)
+  (doto (goog.history.Html5History. js/window (build-transformer))
     (.setPathPrefix (str js/window.location.protocol
                          "//"
                          js/window.location.host))
     (.setUseFragment false)))
 
-; FIXME: remove the worning of history not found
+; FIXME: remove the warning of history not found
 (defn nav! [token]
   (swap! path {})
   (.setToken open-company-web.core/history token))
