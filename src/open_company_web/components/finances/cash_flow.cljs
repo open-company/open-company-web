031acb4b0f3295a57f8d7b7c536e5b34d9959e5b
bago2k4 <iacopo@rc0.eu>
2015-10-15 16:03:54 +0200

    use column-chart component from the charts ns

--- src/open_company_web/components/finances/cash_flow.cljs
+++ src/open_company_web/components/finances/cash_flow.cljs
@@ -7,9 +7,9 @@
             [open-company-web.components.charts :refer [column-chart]]
             [open-company-web.components.utility-components :refer [editable-pen]]))
 
-(def columns 4)
+(def columns 5)
 
-(defn chart-data-at-index [data idx prefix suffix]
+(defn chart-data-at-index [data idx]
   (let [rev-idx (- (dec (min (count data) columns)) idx)
         obj (get data rev-idx)
         cash-flow (- (:revenue obj) (:costs obj))
@@ -23,13 +23,13 @@
      cash-flow
      (str "fill-color: " (if cash-flow-pos? "green" "red"))]))
 
-(defn- get-chart-data [data prefix keyw column-name]
+(defn- get-chart-data [data prefix]
   "Vector of max *columns elements of [:Label value]"
   (let [chart-data (partial chart-data-at-index data)
-        placeholder-vect (subvec [0 1 2 3 4 5 6] 0 (min (count data) columns))]
+        placeholder-vect (subvec (range columns) 0 (min (count data) columns))]
     { :prefix prefix
       ; :columns [["string" column-name] ["number" "Costs"] ["number" "Revenue"] ["number" "Cash flow"] {:role "style"}]
-      :columns [["string" column-name]
+      :columns [["string" "Period"]
                 ["number" "Costs"]
                 #js {"type" "string" "role" "style"}
                 ["number" "Revenue"]
@@ -37,50 +37,6 @@
                 ["number" "Cash flow"]
                 #js {"type" "string" "role" "style"}]
       :values (into [] (map chart-data placeholder-vect))}))
-
-(defn cash-flow-add-rows [columns data currency-symbol]
-  (let [data-table (js/google.visualization.DataTable.)
-        formatter (js/google.visualization.NumberFormat. #js {
-                    "negativeColor" "red",
-                    "negativeParens" true
-                    "pattern" "###,###.##"
-                    "prefix" (if (= currency-symbol "%") "" currency-symbol)
-                    "suffix" (if-not (= currency-symbol "%") "" "%")})]
-    (doseq [x columns]
-      (if (vector? x)
-        (.addColumn data-table (first x) (second x))
-        (.addColumn data-table x)))
-    (.addRows data-table (clj->js data))
-    (.format formatter data-table 1)
-    (.format formatter data-table 3)
-    (.format formatter data-table 5)
-    data-table))
-
-(defn cash-flow-draw-chart [currency-symbol columns data dom-node]
-  (when (.-google js/window)
-    (let [data-table (cash-flow-add-rows columns data currency-symbol)
-          options (clj->js {
-                    :title  ""
-                    :width 600
-                    :height 250
-                    ; :colors [ "red" "green" "green"]
-                    :legend #js {"position" "none"}
-                    :vAxis #js {"minValue" 0}})]
-      (when dom-node (.draw (js/google.visualization.ColumnChart. dom-node) data-table options)))))
-
-(defcomponent cash-flow-chart [chart-data owner]
-  (did-mount [_]
-    (cash-flow-draw-chart (:prefix chart-data)
-                       (:columns chart-data)
-                       (:values chart-data)
-                       (.getDOMNode (om/get-ref owner "column-chart"))))
-  (did-update [_ _ _]
-    (cash-flow-draw-chart (:prefix chart-data)
-                       (:columns chart-data)
-                       (:values chart-data)
-                       (.getDOMNode (om/get-ref owner "column-chart"))))
-  (render [_]
-    (dom/div #js {:className "chart-container column-chart" :ref "column-chart" })))
 
 (defcomponent cash-flow [data owner]
   (render [_]
@@ -96,4 +52,4 @@
                 cash-val
                 (om/build editable-pen {:click-callback (:editable-click-callback data)}))
         (dom/p {} period)
-        (om/build cash-flow-chart (get-chart-data finances-data cur-symbol :cash "Cash"))))))
+        (om/build column-chart (get-chart-data finances-data cur-symbol))))))
