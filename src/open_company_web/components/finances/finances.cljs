76e3c4548a308aaa4e333310276ae13e12370a78
bago2k4 <iacopo@rc0.eu>
2015-10-15 19:27:18 +0200

    add back in cash chart, show runway chart only if necessary

--- src/open_company_web/components/finances/finances.cljs
+++ src/open_company_web/components/finances/finances.cljs
@@ -4,6 +4,7 @@
             [om-tools.core :as om-core :refer-macros [defcomponent]]
             [om-tools.dom :as dom :include-macros true]
             [open-company-web.router :as router]
+            [open-company-web.components.finances.cash :refer (cash)]
             [open-company-web.components.finances.cash-flow :refer (cash-flow)]
             [open-company-web.components.finances.revenue :refer (revenue)]
             [open-company-web.components.finances.costs :refer (costs)]
@@ -32,7 +33,7 @@
     (let [company-data (:company-data data)
           finances-data (:finances company-data)
           notes-data (:notes finances-data)]
-      {:focus "cash-flow"
+      {:focus "cash"
        :read-only false}))
   (will-mount [_]
     (let [save-change (utils/get-channel "save-section-finances")]
@@ -60,7 +61,8 @@
           company-data (:company-data data)
           finances-data (:finances company-data)
           notes-data (:notes finances-data)
-          cash-classes (str classes (when (= focus "cash-flow") " active"))
+          cash-classes (str classes (when (= focus "cash") " active"))
+          cash-flow-classes (str classes (when (= focus "cash-flow") " active"))
           revenue-classes (str classes (when (= focus "revenue") " active"))
           costs-classes (str classes (when (= focus "costs") " active"))
           burn-rate-classes (str classes (when (= focus "burn-rate") " active"))
@@ -68,7 +70,10 @@
           read-only (or (:loading finances-data) (om/get-state owner :read-only))
           subsection-data {:company-data company-data
                            :read-only read-only
-                           :editable-click-callback (:editable-click-callback data)}]
+                           :editable-click-callback (:editable-click-callback data)}
+          sum-revenues (apply + (map #(:revenue %) (:data finances-data)))
+          first-title (if (pos? sum-revenues) "Cash flow" "Burn rate")
+          need-runway (some #(contains? % :runway) (:data finances-data))]
       (dom/div {:class "row" :id "section-finances"}
         (dom/div {:class "finances"}
           (om/build editable-title {:read-only read-only
@@ -79,43 +84,30 @@
             (dom/a {:href "#"
                     :class cash-classes
                     :title "Cash"
+                    :data-tab "cash"
+                    :on-click #(subsection-click % owner)} "Cash")
+            (dom/a {:href "#"
+                    :class cash-flow-classes
+                    :title first-title
                     :data-tab "cash-flow"
-                    :on-click #(subsection-click % owner)} "Cash flow")
-            (dom/a {:href "#"
-                    :class revenue-classes
-                    :title "Revenue"
-                    :data-tab "revenue"
-                    :on-click #(subsection-click % owner)} "Revenue")
-            (dom/a {:href "#"
-                    :class costs-classes
-                    :title "Costs"
-                    :data-tab "costs"
-                    :on-click #(subsection-click % owner)} "Costs")
-            (dom/a {:href "#"
-                    :class burn-rate-classes
-                    :title "Burn rate"
-                    :data-tab "burn-rate"
-                    :on-click #(subsection-click % owner)} "Burn rate")
-            (dom/a {:href "#"
-                    :class runway-classes
-                    :title "Runway"
-                    :data-tab "runway"
-                    :on-click #(subsection-click % owner)} "Runway"))
+                    :on-click #(subsection-click % owner)} first-title)
+            (when need-runway
+              (dom/a {:href "#"
+                      :class runway-classes
+                      :title "Runway"
+                      :data-tab "runway"
+                      :on-click #(subsection-click % owner)} "Runway")))
           (dom/div {:class (utils/class-set {:finances-body true
                                              :editable (not read-only)})}
             (case focus
 
+              "cash"
+              (om/build cash subsection-data)
+
               "cash-flow"
-              (om/build cash-flow subsection-data)
-
-              "revenue"
-              (om/build revenue subsection-data)
-
-              "costs"
-              (om/build costs subsection-data)
-
-              "burn-rate"
-              (om/build burn-rate subsection-data)
+              (if (pos? sum-revenues)
+                (om/build cash-flow subsection-data)
+                (om/build costs subsection-data))
 
               "runway"
               (om/build runway subsection-data))
