86c13560321a41202463ab90cb19645a20f0cd67
Sean Johnson <sean@snootymonkey.com>
2015-10-16 13:19:38 -0400

    Minor sprucing up of the login interstitial page.

--- src/open_company_web/components/login.cljs
+++ src/open_company_web/components/login.cljs
@@ -5,23 +5,26 @@
 
 (defcomponent login [data owner]
   (render [_]
-    (dom/div {:class "login-container"}
-      (dom/h1 "Login with slack:")
-      (case
+    (dom/div {:class "row"}
+      (dom/div {:class "login-container col-md-5 col-md-offset-1"}
+        (dom/h1 "OpenCompany login with Slack:")
+        (case
 
-        (:loading data)
-        (dom/h2 "Loading...")
+          (:loading data)
+          (dom/h2 "Loading...")
 
-        (and (not (:loading data)) (contains? data :auth-settings))
-        (dom/div {}
-          (when (contains? data :access)
-            (let [msg (if (= (:access data) "denied")
-                        "OpenCompany requires verification with your Slack team. Please allow access."
-                        "There is a temporary error validating with Slack. Please try again later.")]
-              (dom/h4 {:class "login-error-message"} msg)))
-          (dom/a {:href (:full-url (:auth-settings data))}
-            (dom/img {:alt "Login with Slack"
-                      :height "40"
-                      :width "139"
-                      :src "https://platform.slack-edge.com/img/add_to_slack.png"
-                      :srcSet "https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"})))))))
+          (and (not (:loading data)) (contains? data :auth-settings))
+          (dom/div {}
+            (if-let [access (:access data)]
+              (let [msg (if (= access "denied")
+                "OpenCompany requires verification with your Slack team. Please allow access."
+                "There is a temporary error validating with Slack. Please try again later.")]
+                (dom/h4 {:class "login-error-message"} msg)))
+            (dom/a {:href (:full-url (:auth-settings data))}
+              (dom/img {
+                :alt "Login with Slack"
+                :height "40"
+                :width "139"
+                :src "https://platform.slack-edge.com/img/add_to_slack.png"
+                :srcSet "https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
+              }))))))))
