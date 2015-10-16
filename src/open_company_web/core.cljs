d2241b0f14ae86b0c4e9030450e8e8937ab9caf1
bago2k4 <iacopo@rc0.eu>
2015-10-16 16:16:34 +0200

    add a login wall that check on token change and redirect the user after login to the original path he was headed to

--- src/open_company_web/core.cljs
+++ src/open_company_web/core.cljs
@@ -25,19 +25,31 @@
 ; is undefined because it breaks tests
 (if-let [target (. js/document (getElementById "app"))]
   (do
+
     (defroute login-route "/login" {:keys [query-params]}
       (if (contains? query-params :jwt)
-        (do
-          (println "jwt: " (jwt/decode (:jwt query-params)))
+        (do ; contains :jwt so auth went well
           (cook/set-cookie! :jwt (:jwt query-params) (* 60 60 24 60) "/" ls/jwt-cookie-domain ls/jwt-cookie-secure)
           ;redirect to dashboard
-          (utils/redirect! "/companies"))
+          (if-let [login-redirect (cook/get-cookie :login-redirect)]
+            (do
+              ; remove the login redirect cookie
+              (cook/remove-cookie! :login-redirect)
+              ; redirect to the initial path
+              (utils/redirect! login-redirect))
+            ; redirect to / if no cookie is set
+            (utils/redirect! "/")))
         (do
+          (when (contains? query-params :login-redirect)
+            (cook/set-cookie! :login-redirect (:login-redirect query-params)))
           ; save route
           (router/set-route! ["login"] {})
           ; load data from api
           (swap! app-state assoc :loading true)
           (api/get-auth-settings)
+          (when (contains? query-params :access)
+            ;login went bad, add the error message to the app-state
+            (swap! app-state assoc :access (:access query-params)))
           ; render component
           (om/root login app-state {:target target}))))
 
@@ -123,6 +135,13 @@
                                  section-edit-route
                                  not-found-route]))
 
+    (defn login-wall []
+      (let [token (router/get-token)]
+        (when-not (.startsWith token "/login")
+          (if (cook/get-cookie :jwt)
+            true
+            (utils/redirect! (str "/login?login-redirect=" (router/get-token)))))))
+
     (defn handle-url-change [e]
       ;; we are checking if this event is due to user action,
       ;; such as click a link, a back button, etc.
@@ -131,6 +150,8 @@
         ;; in this case, we're setting it so
         ;; let's scroll to the top to simulate a navigation
         (js/window.scrollTo 0 0))
+      ; check if the user is logged in
+      (login-wall)
       ;; dispatch on the token
       (dispatch! (router/get-token)))))
 
