7654ed82724d69bc5956df075fc1327cdabc4023
bago2k4 <iacopo@rc0.eu>
2015-10-19 16:26:24 +0200

    show negative value in tooltip if necessary, add custom tooltip for all type of charts

--- src/open_company_web/components/finances/cash_flow.cljs
+++ src/open_company_web/components/finances/cash_flow.cljs
@@ -9,34 +9,41 @@
 
 (def columns 5)
 
-(defn chart-data-at-index [data idx]
+(defn chart-data-at-index [data prefix idx]
   (let [data (to-array data)
         rev-idx (- (dec (min (count data) columns)) idx)
         obj (get data rev-idx)
         cash-flow (- (:revenue obj) (:costs obj))
+        cash-flow (if (js/isNaN cash-flow) 0 cash-flow)
         cash-flow-pos? (pos? cash-flow)
-        cash-flow (utils/abs (if (js/isNaN cash-flow) 0 cash-flow))]
+        abs-cash-flow (utils/abs cash-flow)]
     [(utils/period-string (:period obj))
      (:costs obj)
      "fill-color: red"
+     (str (utils/period-string (:period obj)) " Costs: " prefix (.toLocaleString (:costs obj)))
      (:revenue obj)
      "fill-color: green"
-     cash-flow
-     (str "fill-color: " (if cash-flow-pos? "green" "red"))]))
+     (str (utils/period-string (:period obj)) " Revenue: " prefix (.toLocaleString (:revenue obj)))
+     abs-cash-flow
+     (str "fill-color: " (if cash-flow-pos? "green" "red"))
+     (str (utils/period-string (:period obj)) " Cash flow: -" prefix (.toLocaleString abs-cash-flow))]))
 
 (defn- get-chart-data [data prefix]
   "Vector of max *columns elements of [:Label value]"
-  (let [chart-data (partial chart-data-at-index data)
+  (let [chart-data (partial chart-data-at-index data prefix)
         placeholder-vect (range (min (count data) columns))]
     { :prefix prefix
       ; :columns [["string" column-name] ["number" "Costs"] ["number" "Revenue"] ["number" "Cash flow"] {:role "style"}]
       :columns [["string" "Period"]
                 ["number" "Costs"]
                 #js {"type" "string" "role" "style"}
+                #js {"type" "string" "role" "tooltip"}
                 ["number" "Revenue"]
                 #js {"type" "string" "role" "style"}
+                #js {"type" "string" "role" "tooltip"}
                 ["number" "Cash flow"]
-                #js {"type" "string" "role" "style"}]
+                #js {"type" "string" "role" "style"}
+                #js {"type" "string" "role" "tooltip"}]
       :values (into [] (map chart-data placeholder-vect))
       :pattern "###,###.##"}))
 
--- src/open_company_web/components/finances/utils.cljs
+++ src/open_company_web/components/finances/utils.cljs
@@ -3,21 +3,24 @@
 
 (def columns 7)
 
-(defn chart-data-at-index [data keyw idx]
+(defn chart-data-at-index [data keyw column-name prefix idx]
   (let [data (to-array data)
         rev-idx (- (- (min (count data) columns) 1) idx)
         obj (get data rev-idx)]
-    [(utils/period-string (:period obj)) (keyw obj)]))
+    [(utils/period-string (:period obj))
+     (keyw obj)
+     (str (utils/period-string (:period obj)) " " column-name ": " prefix (.toLocaleString (keyw obj)))]))
 
 (defn- get-chart-data [data prefix keyw column-name & [style fill-color pattern]]
   "Vector of max *columns elements of [:Label value]"
-  (let [chart-data (partial chart-data-at-index data keyw)
+  (let [chart-data (partial chart-data-at-index data keyw column-name prefix)
         placeholder-vect (into [] (range (min (count data) columns)))
         columns [["string" column-name]
-                 ["number" (utils/camel-case-str (name keyw))]]
+                 ["number" (utils/camel-case-str (name keyw))]
+                 #js {"type" "string" "role" "tooltip"}]
         columns (if style (conj columns style) columns)
         values (into [] (map chart-data placeholder-vect))
-        values (if fill-color (map #(assoc % 2 fill-color) values) values)]
+        values (if fill-color (map #(assoc % 3 fill-color) values) values)]
     { :prefix prefix
       :columns columns
       :values values
