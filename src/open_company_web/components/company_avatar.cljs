35c1a5729f2a4945cca0a847f77896afc144d0c7
bago2k4 <iacopo@rc0.eu>
2015-10-20 17:37:09 +0200

    render company avatar only if there is a slug

--- src/open_company_web/components/company_avatar.cljs
+++ src/open_company_web/components/company_avatar.cljs
@@ -11,28 +11,29 @@
 
 (defcomponent company-avatar [data owner]
   (render [_]
-    (let [slug (:slug @router/path)
-          first-letter (if (contains? (:company-data data) :name)
-                         (first (:name (:company-data data)))
-                         (first slug))
-          company-home (str "/companies/" slug)
-          width (if (:width data)
-                  (:width data)
-                  40)
-          height (if (:height data)
-                   (:height data)
-                   40)
-          bd-radius (int (/ width 2))]
-      (dom/div {:class (utils/class-set {:company-avatar true
-                                         :navbar-brand (:navbar-brand data)})}
-        (dom/a {:href company-home
-                :on-click (fn [e]
-                            (.preventDefault e)
-                            (router/nav! company-home))}
-          (dom/div {:class "company-avatar-img"
-                    :style {:width (get-px width)
-                            :height (get-px height)
-                            :border-radius (get-px bd-radius)}
-                    :src (jwt/get-key :avatar)
-                    :title (jwt/get-key :real-name)}
-            (dom/span {:class "company-avatar-name"} (clojure.string/upper-case first-letter))))))))
+    (when (:company-data data)
+      (let [slug (:slug @router/path)
+            first-letter (if (contains? (:company-data data) :name)
+                           (first (:name (:company-data data)))
+                           (first slug))
+            company-home (str "/companies/" slug)
+            width (if (:width data)
+                    (:width data)
+                    40)
+            height (if (:height data)
+                     (:height data)
+                     40)
+            bd-radius (int (/ width 2))]
+        (dom/div {:class (utils/class-set {:company-avatar true
+                                           :navbar-brand (:navbar-brand data)})}
+          (dom/a {:href company-home
+                  :on-click (fn [e]
+                              (.preventDefault e)
+                              (router/nav! company-home))}
+            (dom/div {:class "company-avatar-img"
+                      :style {:width (get-px width)
+                              :height (get-px height)
+                              :border-radius (get-px bd-radius)}
+                      :src (jwt/get-key :avatar)
+                      :title (jwt/get-key :real-name)}
+              (dom/span {:class "company-avatar-name"} (clojure.string/upper-case first-letter)))))))))
