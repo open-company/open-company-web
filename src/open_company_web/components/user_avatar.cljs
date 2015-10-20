d425f19c2acfb5031619fa76a4443d4d43656cbf
bago2k4 <iacopo@rc0.eu>
2015-10-20 18:07:16 +0200

    assume that the user avatar is always square, use only one size to customize it

--- src/open_company_web/components/user_avatar.cljs
+++ src/open_company_web/components/user_avatar.cljs
@@ -3,29 +3,27 @@
             [om-tools.core :as om-core :refer-macros [defcomponent]]
             [om-tools.dom :as dom :include-macros true]
             [open-company-web.lib.jwt :as jwt]
+            [open-company-web.lib.utils :as utils]
             [open-company-web.router :as router]))
 
-(defn get-px [n]
-  (str n "px"))
+(def df-user-avatar-size 30)
 
 (defcomponent user-avatar [data owner]
   (render [_]
-    (let [width (if (:width data)
-                  (:width data)
-                  30)
-          height (if (:height data)
-                   (:height data)
-                   30)
-          bd-radius (int (/ width 2))]
+    (let [av-size (if (:size data)
+                    (:size data)
+                    df-user-avatar-size)
+          px-size (utils/px av-size)
+          bd-radius (utils/px (int (/ av-size 2)))]
       (dom/div {:class "user-avatar"}
         (dom/a {:href "/profile"
                 :on-click (fn [e]
                             (.preventDefault e)
                             (router/nav! "/profile"))}
           (dom/img {:class "user-avatar-img"
-                    :style {:width (get-px width)
-                            :height (get-px height)
-                            :border-radius (get-px bd-radius)}
+                    :style {:width px-size
+                            :height px-size
+                            :border-radius bd-radius}
                     :src (jwt/get-key :avatar)
                     :title (jwt/get-key :real-name)})
           (dom/span {:class "user-avatar-name"} (jwt/get-key :name)))))))
