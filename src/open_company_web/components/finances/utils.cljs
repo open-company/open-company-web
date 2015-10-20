b52508b5ceadf3888800f2c69cc90001cb7bb3c3
bago2k4 <iacopo@rc0.eu>
2015-10-20 10:12:40 +0200

    add an optional suffix for the tooltip label

--- src/open_company_web/components/finances/utils.cljs
+++ src/open_company_web/components/finances/utils.cljs
@@ -3,21 +3,21 @@
 
 (def columns 7)
 
-(defn chart-data-at-index [data keyw column-name prefix idx]
+(defn chart-data-at-index [data keyw column-name prefix suffix idx]
   (let [data (to-array data)
         rev-idx (- (- (min (count data) columns) 1) idx)
         obj (get data rev-idx)
         value (keyw obj)
         label (if value
-                (str (utils/period-string (:period obj)) " " column-name ": " prefix (.toLocaleString (keyw obj)))
+                (str (utils/period-string (:period obj)) " " column-name ": " prefix (.toLocaleString (keyw obj)) suffix)
                 "Profitable")]
     [(utils/period-string (:period obj))
      value
      label]))
 
-(defn- get-chart-data [data prefix keyw column-name & [style fill-color pattern]]
+(defn- get-chart-data [data prefix keyw column-name & [style fill-color pattern tooltip-suffix]]
   "Vector of max *columns elements of [:Label value]"
-  (let [chart-data (partial chart-data-at-index data keyw column-name prefix)
+  (let [chart-data (partial chart-data-at-index data keyw column-name prefix tooltip-suffix)
         placeholder-vect (into [] (range (min (count data) columns)))
         columns [["string" column-name]
                  ["number" (utils/camel-case-str (name keyw))]
