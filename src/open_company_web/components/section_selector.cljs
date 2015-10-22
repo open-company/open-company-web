1e5d6431d605762436cbcf65591109f4c4b38551
bago2k4 <iacopo@rc0.eu>
2015-10-22 19:09:33 +0200

    add sections-focuser function

--- src/open_company_web/components/section_selector.cljs
+++ src/open_company_web/components/section_selector.cljs
@@ -5,16 +5,40 @@
             [open-company-web.router :as router]
             [open-company-web.components.finances.finances :refer (finances)]
             [open-company-web.components.finances.finances-edit :refer (finances-edit)]
-            [open-company-web.components.simple-section :refer (simple-section)]))
+            [open-company-web.components.simple-section :refer (simple-section)]
+            [open-company-web.lib.utils :as utils]
+            [shodan.inspection :refer [inspect]]
+            [shodan.console :as console]))
+
+(defn sections-focuser []
+  (let [scroll-top (.scrollTop (.$ js/window js/window))
+        sections (.$ js/window ".section")
+        window-height (.height (.$ js/window js/window))
+        pivot (int (* (/ window-height 100) 50))
+        tot (- window-height pivot)]
+    (.each sections
+           (fn [n item]
+             (let [$-item (.$ js/window item)
+                   item-offset-top (.-top (.offset $-item))
+                   distance (- item-offset-top scroll-top)
+                   el-tot (- distance pivot)]
+               (if-not (> distance pivot)
+                 (.css $-item #js {"opacity" "1"})
+                 (let [opac-percent (/ (* el-tot 100) tot)]
+                   (.css $-item #js {"opacity" (str (- 1 (/ opac-percent 100)))}))))))))
 
 (defcomponent section-selector [data owner]
   (init-state [_]
     {:finances-edit false})
+  (did-mount [_]
+    (let [jq-body (.$ js/window js/window)]
+      (.scroll jq-body #(sections-focuser)))
+    (sections-focuser))
   (render [_]
     (let [section (:section data)
           read-only (:read-only data)
           company-data (:data data)]
-      (dom/div {:class "section-selector"}
+      (dom/div #js {:className "section-selector" :ref "section-selector"}
         (cond
           ; finances edit
           (and (= section :finances) (om/get-state owner :finances-edit))
