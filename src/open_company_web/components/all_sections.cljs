24da5c6e8c0a893b96ff8ff8e2a46e43aa73db17
bago2k4 <iacopo@rc0.eu>
2015-10-26 20:07:11 +0100

    do not pass all sections to section-selector

--- src/open_company_web/components/all_sections.cljs
+++ src/open_company_web/components/all_sections.cljs
@@ -17,13 +17,10 @@
           company-data ((keyword slug) data)
           sorted-section-keys (utils/sort-section-keys company-data)]
       (dom/div {:class "sections-container"}
-        (let [merge-partial (partial merge {:data company-data})
-              sections-data (into [] (map (fn [section]{:data company-data
-                                               :section section})
-                                 sorted-section-keys))]
-          (for [idx (range 0 (count sections-data))]
-            (let [section (sections-data idx)]
-              (dom/div {}
-                (om/build section-selector section)
-                (when (not= idx (dec (count sections-data)))
-                  (dom/hr {:class "section-separator"}))))))))))
+        (for [section sorted-section-keys]
+          (let [section-data (section company-data)]
+            (dom/div {}
+              (om/build section-selector {:section-data section-data
+                                          :section section})
+              (when (not= section (last sorted-section-keys))
+                (dom/hr {:class "section-separator"})))))))))
