ab87154aca65d3a9e690212450973d6098857252
bago2k4 <iacopo@rc0.eu>
2015-10-29 15:25:07 +0100

    add growth and growth-metric tests

---
+++ test/open_company_web/components/growth/growth.cljs
@@ -0,0 +1,41 @@
+(ns test.open-company-web.components.growth.growth
+    (:require [cljs.test :refer-macros [deftest async testing is are use-fixtures]]
+              [cljs-react-test.simulate :as sim]
+              [cljs-react-test.utils :as tu]
+              [om.core :as om :include-macros true]
+              [dommy.core :as dommy :refer-macros [sel1 sel]]
+              [open-company-web.components.growth.growth :refer [growth]]
+              [om.dom :as dom :include-macros true]
+              [open-company-web.router :as router]))
+
+(enable-console-print!)
+
+; dynamic mount point for components
+(def ^:dynamic c)
+
+(def test-atom {
+  :section "test"
+  :section-data {
+    :title "Test"
+    :notes {
+      :body "Test body"}
+    :metrics [
+      {:slug "test-metric"
+       :name "Test metric"
+       :target "high"
+       :unit "tests"}]
+    :data [
+        {:period "2015-10"
+         :slug "test-metric"
+         :value 123456}]}})
+
+(deftest test-growth-component
+  (testing "Growth component"
+    (router/set-route! ["companies" "buffer" "growth"]
+                       {:slug "buffer" :section "growth"})
+    (let [c (tu/new-container!)
+          app-state (atom test-atom)
+          _ (om/root growth app-state {:target c})
+          growth-node (sel1 c [:div.growth])]
+      (is (not (nil? growth-node)))
+      (tu/unmount! c))))
