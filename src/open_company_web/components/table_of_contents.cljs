db7d924e67ef153da5a9562c7c1f76d0752f81ca
bago2k4 <iacopo@rc0.eu>
2015-11-02 12:53:39 +0100

    fix table-of-contents with categories from api

--- src/open_company_web/components/table_of_contents.cljs
+++ src/open_company_web/components/table_of_contents.cljs
@@ -6,25 +6,23 @@
             [open-company-web.lib.utils :as utils]))
 
 (defcomponent table-of-contents [data owner]
-  ; (did-mount [_]
-  ;   (if-let [toc-obj (om/get-ref owner "table-of-contents")]
-  ;     (let [toc-node (.getDOMNode toc-obj)
-  ;           $-toc (.$ js/window toc-node)
-  ;           win-height (.height (.$ js/window js/window))]
-  ;       (.css $-toc #js {"height" (str (- win-height 50) "px")}))))
   (render [_]
-    (dom/div #js {:className "table-of-contents" :ref "table-of-contents"}
-      (dom/div {:class "table-of-contents-inner"}
-        (dom/div {:class "category"} (dom/h3 "CATEGORY"))
-        (for [section (utils/get-section-keys data)]
-          (let [section-data (section data)]
-            (dom/div {:class "category-section"}
-              (dom/a {:href "#"
-                      :on-click (fn [e]
-                                  (.preventDefault e)
-                                  (let [section-el (.$ js/window (str "#section-" (name section)))
-                                        section-offset (.offset section-el)
-                                        top (- (.-top section-offset) 60)]
-                                    (.scrollTo js/$ #js {"top" (str top "px") "left" "0px"} 500)))}
-                (dom/p {:class "section-title"} (:title section-data))
-                (dom/p {:class "section-date"} (utils/time-since (:updated-at section-data)))))))))))
+    (let [sections (:sections data)
+          categories (into [] (keys sections))]
+      (dom/div #js {:className "table-of-contents" :ref "table-of-contents"}
+        (dom/div {:class "table-of-contents-inner"}
+          (for [category categories]
+            (dom/div {:class "category-container"}
+              (dom/div {:class "category"} (dom/h3 (utils/camel-case-str (name category))))
+              (for [section (into [] (get sections category))]
+                (let [section-data ((keyword section) data)]
+                  (dom/div {:class "category-section"}
+                    (dom/a {:href "#"
+                            :on-click (fn [e]
+                                        (.preventDefault e)
+                                        (let [section-el (.$ js/window (str "#section-" (name section)))
+                                              section-offset (.offset section-el)
+                                              top (- (.-top section-offset) 60)]
+                                          (.scrollTo js/$ #js {"top" (str top "px") "left" "0px"} 500)))}
+                      (dom/p {:class "section-title"} (:title section-data))
+                      (dom/p {:class "section-date"} (utils/time-since (:updated-at section-data))))))))))))))
