ef1162c8b13ed46ca7373fc56ad8c0bef18b238f
bago2k4 <iacopo@rc0.eu>
2015-11-05 18:11:53 +0100

    handle the revision navigation and the animation to switch revision

--- src/open_company_web/components/section_selector.cljs
+++ src/open_company_web/components/section_selector.cljs
@@ -7,39 +7,100 @@
             [open-company-web.components.growth.growth :refer (growth)]
             [open-company-web.components.finances.finances-edit :refer (finances-edit)]
             [open-company-web.components.simple-section :refer (simple-section)]
-            [open-company-web.lib.utils :as utils]))
+            [open-company-web.lib.utils :as utils]
+            [shodan.console :as console]))
+
+(defn editing! [owner v]
+  (om/update-state! owner :editing (fn [_]v)))
+
+(defn section-component [section-name editable]
+  (cond
+    (and (= :finances (keyword section-name)) editable)
+    finances-edit
+
+    (= :finances (keyword section-name))
+    finances
+
+    (= :growth (keyword section-name))
+    growth
+
+    :else
+    simple-section))
+
+(defn revisions-navigation-cb [owner section-name as-of]
+  (om/update-state! owner :as-of (fn [_]as-of)))
+
+(defn animate-section-translation [owner section-name]
+  (let [old-box-id (str "#sec-box-old-" section-name)
+        new-box-id (str "#sec-box-new-" section-name)]
+    (.fadeTo (.$ js/window old-box-id) 500 0)
+    (.fadeTo (.$ js/window new-box-id) 500 1 #(om/update-state! owner :old-as-of (fn [_](om/get-state owner :as-of))))
+    (utils/scroll-to-id (str "sec-box-" section-name))))
+
+(defn prepare-animation [owner section-name]
+  (let [old-box-id (str "#sec-box-old-" section-name)
+        new-box-id (str "#sec-box-new-" section-name)]
+    (.css (.$ js/window old-box-id) "opacity" 1)
+    (.css (.$ js/window new-box-id) "opacity" 0)
+    (.setTimeout js/window #(animate-section-translation owner section-name) 1)))
+
+(defn setup-box-height [section-name]
+  (let [container-id (str "#sec-box-" section-name)
+        box-id (str "#sec-box-new-" section-name)
+        height (.height (.$ js/window box-id))]
+    (.css (.$ js/window container-id) "height" (str height "px"))))
+
+(defn calc-height-imgs-onload [section-name]
+  (-> (.$ js/window (str "#sec-box-" section-name))
+      (.bind "DOMSubtreeModified"
+             (fn [] (-> (.$ js/window (str "#sec-box-" section-name " img"))
+                        (.bind "load" #(setup-box-height section-name)))))))
 
 (defcomponent section-selector [data owner]
   (init-state [_]
-    {:finances-edit false})
+    (let [as-of (:updated-at (:section-data data))]
+      {:editing false
+       :old-as-of as-of
+       :as-of as-of}))
+  (did-mount [_]
+    (let [section-name (name (:section data))]
+      (setup-box-height section-name)
+      ; listens for dom changes and recalculate the height when img tags load
+      (calc-height-imgs-onload section-name)))
+  (did-update [_ _ _]
+    (setup-box-height (name (:section data))))
   (render [_]
-    (let [section (:section data)
-          read-only (:read-only data)
-          section-data (:section-data data)]
+    (let [showing-revision (om/get-state owner :as-of)
+          old-revision (om/get-state owner :old-as-of)
+          section (:section data)
+          actual-data (:section-data data)
+          section-data (utils/select-section-data actual-data section showing-revision)
+          old-section-data (utils/select-section-data actual-data section old-revision)
+          read-only (or (:loading section-data) (not (= showing-revision (:updated-at actual-data))))
+          editing (om/get-state owner :editing)
+          animating (not (= showing-revision old-revision))]
+      ; (when (= :update section)
+      ;   (console/log "section-selector render" showing-revision (= showing-revision old-revision)))
       (dom/div #js {:className "section-selector" :ref "section-selector"}
-        (cond
-          ; growth
-          (= section :growth)
-          (om/build growth {:section :growth
-                            :section-data section-data})
-          ; finances edit
-          (and (= section :finances) (om/get-state owner :finances-edit))
-          (om/build finances-edit {:section-data section-data
-                                   :section :finances
-                                   :loading (:loading data)
-                                   :close-edit-cb #(om/update-state! owner :finances-edit (fn [_] false))})
-          ; finances
-          (and (= section :finances) (not (om/get-state owner :finances-edit)))
-          (om/build finances {:section :finances
-                              :section-data section-data
-                              :loading (:loading data)
-                              :editable-click-callback #(om/update-state! owner :finances-edit (fn [_] true))})
-          ; else it is a simple section
-          section-data
-          (om/build simple-section {:read-only read-only
-                                    :section section
-                                    :section-data section-data
-                                    :loading (:loading data)})
-          ; section not found
-          :else
-          (dom/h4 {} (str "Section " (name section) " not found")))))))
+        (dom/div {:class "section-animator-box" :id (str "sec-box-" (name section))}
+          (when animating
+            ; (.setTimeout js/window #(prepare-animation (name section)) 1)
+            (prepare-animation owner (name section))
+            (dom/div {:class "section-box-old" :id (str "sec-box-old-" (name section))}
+              (om/build (section-component section editing) {:section-data old-section-data
+                                                             :section section
+                                                             :actual-as-of (:updated-at actual-data)
+                                                             :loading (:loading data)
+                                                             :revisions-navigation-cb #(revisions-navigation-cb owner section %)
+                                                             :close-edit-cb #(editing! owner false)
+                                                             :editable-click-callback #(editing! owner true)
+                                                             :read-only read-only})))
+          (dom/div {:class "section-box-new" :id (str "sec-box-new-" (name section))}
+            (om/build (section-component section editing) {:section-data section-data
+                                                           :section section
+                                                           :actual-as-of (:updated-at actual-data)
+                                                           :loading (:loading data)
+                                                           :revisions-navigation-cb #(revisions-navigation-cb owner section %)
+                                                           :close-edit-cb #(editing! owner false)
+                                                           :editable-click-callback #(editing! owner true)
+                                                           :read-only read-only})))))))
