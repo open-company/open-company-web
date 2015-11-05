76ec3772dfa5a6732dd6dbe0d480c9da6fcb4d4c
bago2k4 <iacopo@rc0.eu>
2015-11-05 23:27:24 +0100

    add box height animation, do not autoresize the container while animating, improvements to the animation prep

--- src/open_company_web/components/section_selector.cljs
+++ src/open_company_web/components/section_selector.cljs
@@ -27,51 +27,58 @@
     simple-section))
 
 (defn revisions-navigation-cb [owner section-name as-of]
+  (let [showing-revision (om/get-state owner :as-of)]
+    (when (not (= as-of showing-revision))
+      (om/update-state! owner :animating (fn [_]true))))
   (om/update-state! owner :as-of (fn [_]as-of)))
 
-(def anim-d 1000)
+(def anim-d 800)
+
+(defn box-height [section-name]
+  (let [new-box (.$ js/window (str "#sec-box-new-" section-name))
+        new-box-height (.height new-box)]
+    (.height new-box)))
 
 (defn animate-section-translation [owner section-name]
   (let [old-box (.$ js/window (str "#sec-box-old-" section-name))
-        new-box (.$ js/window (str "#sec-box-new-" section-name))]
+        new-box (.$ js/window (str "#sec-box-new-" section-name))
+        new-box-height (box-height section-name)
+        box (.$ js/window (str "#sec-box-" section-name))]
+    (.animate box #js {"height" (str new-box-height "px")} anim-d)
     (.fadeTo old-box anim-d 0)
-    (.fadeTo new-box anim-d 1
-             #(om/update-state! owner :old-as-of (fn [_](om/get-state owner :as-of))))
+    (.fadeTo new-box anim-d 1 (fn []
+                                (let [as-of (om/get-state owner :as-of)]
+                                  (om/update-state! owner :old-as-of (fn [_]as-of))
+                                  (om/update-state! owner :animating (fn [_]false)))))
     (utils/scroll-to-id (str "sec-box-" section-name) anim-d)))
 
-(defn prepare-animation [owner section-name]
-  (let [old-box (.$ js/window (str "#sec-box-old-" section-name))
-        new-box (.$ js/window (str "#sec-box-new-" section-name))
-        new-box-height (.height new-box)]
-    (.css old-box "opacity" 1)
-    (.css new-box "opacity" 0)
-    (.setTimeout js/window #(animate-section-translation owner section-name) 1)))
+(defn setup-box-height [section-name owner]
+  (when (not (om/get-state owner :animating))
+    (let [container-id (str "#sec-box-" section-name)
+          box-id (str "#sec-box-new-" section-name)
+          height (.height (.$ js/window box-id))]
+      (.css (.$ js/window container-id) "height" (str height "px")))))
 
-(defn setup-box-height [section-name]
-  (let [container-id (str "#sec-box-" section-name)
-        box-id (str "#sec-box-new-" section-name)
-        height (.height (.$ js/window box-id))]
-    (.css (.$ js/window container-id) "height" (str height "px"))))
-
-(defn calc-height-imgs-onload [section-name]
+(defn calc-height-imgs-onload [section-name owner]
   (-> (.$ js/window (str "#sec-box-" section-name))
       (.bind "DOMSubtreeModified"
-             (fn [] (setup-box-height section-name)
+             (fn [] (setup-box-height section-name owner)
                     (-> (.$ js/window (str "#sec-box-" section-name " img"))
                         (.unbind "load")
-                        (.bind "load" #(setup-box-height section-name)))))))
+                        (.bind "load" #(setup-box-height section-name owner)))))))
 
 (defcomponent section-selector [data owner]
   (init-state [_]
     (let [as-of (:updated-at (:section-data data))]
       {:editing false
        :old-as-of as-of
-       :as-of as-of}))
+       :as-of as-of
+       :animating false}))
   (did-mount [_]
     (let [section-name (name (:section data))]
-      (setup-box-height section-name)
+      (setup-box-height section-name owner)
       ; listens for dom changes and recalculate the height when img tags load
-      (calc-height-imgs-onload section-name)))
+      (calc-height-imgs-onload section-name owner)))
   (render [_]
     (let [showing-revision (om/get-state owner :as-of)
           old-revision (om/get-state owner :old-as-of)
@@ -82,14 +89,12 @@
           read-only (or (:loading section-data) (not (= showing-revision (:updated-at actual-data))))
           editing (om/get-state owner :editing)
           animating (not (= showing-revision old-revision))]
-      ; (when (= :update section)
-      ;   (console/log "section-selector render" showing-revision (= showing-revision old-revision)))
       (dom/div #js {:className "section-selector" :ref "section-selector"}
         (dom/div {:class "section-animator-box" :id (str "sec-box-" (name section))}
           (when animating
-            ; (.setTimeout js/window #(prepare-animation (name section)) 1)
-            (prepare-animation owner (name section))
-            (dom/div {:class "section-box-old" :id (str "sec-box-old-" (name section))}
+            (dom/div {:class "section-box-old"
+                      :id (str "sec-box-old-" (name section))
+                      :style {:opacity 1}}
               (om/build (section-component section editing) {:section-data old-section-data
                                                              :section section
                                                              :actual-as-of (:updated-at actual-data)
@@ -97,8 +102,10 @@
                                                              :revisions-navigation-cb #(revisions-navigation-cb owner section %)
                                                              :close-edit-cb #(editing! owner false)
                                                              :editable-click-callback #(editing! owner true)
-                                                             :read-only read-only})))
-          (dom/div {:class "section-box-new" :id (str "sec-box-new-" (name section))}
+                                                             :read-only (not (= (:updated-at old-section-data) (:updated-at actual-data)))})))
+          (dom/div {:class "section-box-new"
+                    :id (str "sec-box-new-" (name section))
+                    :style {:opacity (if animating 0 1)}}
             (om/build (section-component section editing) {:section-data section-data
                                                            :section section
                                                            :actual-as-of (:updated-at actual-data)
@@ -106,4 +113,6 @@
                                                            :revisions-navigation-cb #(revisions-navigation-cb owner section %)
                                                            :close-edit-cb #(editing! owner false)
                                                            :editable-click-callback #(editing! owner true)
-                                                           :read-only read-only})))))))
+                                                           :read-only (not (= (:updated-at section-data) (:updated-at actual-data)))}))
+          (when animating
+            (.setTimeout js/window #(animate-section-translation owner (name section)) 1)))))))
