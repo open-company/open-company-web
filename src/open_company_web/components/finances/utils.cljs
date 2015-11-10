6c583810c1ca7e6efd2fb04fb753636da8a13d8a
bago2k4 <iacopo@rc0.eu>
2015-11-10 19:47:44 +0100

    always show 12 months in the finances charts

--- src/open_company_web/components/finances/utils.cljs
+++ src/open_company_web/components/finances/utils.cljs
@@ -3,29 +3,71 @@
             [open-company-web.router :as router]
             [open-company-web.dispatcher :as dispatcher]))
 
-(def columns 7)
+(def columns-num 12)
 
 (defn chart-data-at-index [data keyw column-name prefix suffix idx]
   (let [data (to-array data)
-        rev-idx (- (- (min (count data) columns) 1) idx)
-        obj (get data rev-idx)
+        rev-idx (- (- (min (count data) columns-num) 1) idx)
+        has-value (< idx (count data))
+        obj (if has-value (get data rev-idx) 0)
         value (keyw obj)
         label (if value
                 (str (utils/period-string (:period obj)) " " column-name ": " prefix (.toLocaleString (keyw obj)) suffix)
-                "Profitable")]
-    [(utils/period-string (:period obj) :short-month)
+                "Profitable")
+        period (if has-value
+                 (utils/period-string (:period obj) :short-month)
+                 )]
+    [period
      value
      label]))
 
+(defn- get-past-period [period diff]
+  (let [[year month] (clojure.string/split period "-")
+        int-year (int year)
+        int-month (int month)
+        diff-month (- int-month diff)
+        change-year (<= diff-month 0)
+        fix-month (if change-year (+ columns-num diff-month) diff-month)
+        fix-year (if change-year (dec int-year) int-year)]
+    (str fix-year "-" (utils/add-zero fix-month))))
+
+(defn- get-previous-period [period]
+  (let [[year month] (clojure.string/split period "-")
+        int-year (int year)
+        int-month (int month)
+        dec-month (dec int-month)
+        fix-year (if (zero? dec-month) (dec int-year) int-year)
+        fix-month (if (zero? dec-month) 12 dec-month)]
+    (str fix-year "-" (utils/add-zero fix-month))))
+
+(defn placeholder-data [data]
+  (if (>= (count data) columns-num)
+    data
+    (let [first-period (or (:period (last data)) (utils/current-period))
+          rest-data (- columns-num (count data))
+          diff (- columns-num (count data))
+          plc-vec (vec (reverse (range rest-data)))
+          vect (map (fn [n]
+                      {:period (get-past-period first-period (- diff n))
+                       :cash 0
+                       :revenue 0
+                       :costs 0
+                       :runway 0
+                       :burn-rate 0
+                       :value 0})
+                    plc-vec)]
+      (concat data vect))))
+
 (defn- get-chart-data [data prefix keyw column-name & [style fill-color pattern tooltip-suffix]]
-  "Vector of max *columns elements of [:Label value]"
-  (let [chart-data (partial chart-data-at-index data keyw column-name prefix tooltip-suffix)
-        placeholder-vect (into [] (range (min (count data) columns)))
+  "Vector of max *columns-num elements of [:Label value]"
+  (let [fixed-data (placeholder-data data)
+        chart-data (partial chart-data-at-index fixed-data keyw column-name prefix tooltip-suffix)
+        placeholder-vect (vec (range columns-num))
         columns [["string" column-name]
                  ["number" (utils/camel-case-str (name keyw))]
                  #js {"type" "string" "role" "tooltip"}]
         columns (if style (conj columns style) columns)
-        values (into [] (map chart-data placeholder-vect))
+        values (vec (map chart-data placeholder-vect))
         values (if fill-color (map #(assoc % 3 fill-color) values) values)]
     { :prefix prefix
       :columns columns
