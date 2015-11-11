87fbc4fc5c5b5986f88066b8398d81c0303d68e1
bago2k4 <iacopo@rc0.eu>
2015-11-11 16:09:53 +0100

    move label above the chart, show correct subtitle

--- src/open_company_web/components/finances/runway.cljs
+++ src/open_company_web/components/finances/runway.cljs
@@ -6,7 +6,24 @@
             [open-company-web.lib.iso4217 :refer [iso4217]]
             [open-company-web.components.charts :refer [column-chart]]
             [open-company-web.components.finances.utils :as finances-utils]
-            [open-company-web.components.utility-components :refer [editable-pen]]))
+            [open-company-web.components.utility-components :refer [editable-pen]]
+            [goog.string :as gstring]))
+
+(defn get-rounded-runway [runway-days & [flags]]
+  (cond
+    (< runway-days 90)
+    (str runway-days " days")
+    (< runway-days (* 30 24))
+    (if (utils/in? flags :round)
+      (str (gstring/format "%.2f" (/ runway-days 30)) "months")
+      (str (quot runway-days 30) " months"))
+    :else
+    (if (utils/in? flags :round)
+      (str (gstring/format "%.2f" (/ runway-days (* 30 12))) " years")
+      (str (quot runway-days (* 30 12)) " years"))))
+
+(defn get-runway-subtitle [cash avg-burn-rate runway-days cur-symbol]
+  (str cur-symbol (.toLocaleString cash) " ÷ " cur-symbol (.toLocaleString avg-burn-rate) " 3-months avg. burn: " (get-rounded-runway runway-days [:round])))
 
 (defcomponent runway [data owner]
   (render [_]
@@ -15,13 +32,22 @@
           sorted-finances (sort #(sort-pred %1 %2) finances-data)
           value-set (first sorted-finances)
           runway-value (:runway value-set)
-          runway (if (or (nil? runway-value) (neg? runway-value))
+          is-profitable (or (nil? runway-value) (neg? runway-value))
+          runway (if is-profitable
                     "Profitable"
                     (str (utils/format-value runway-value) " days"))
           period (utils/period-string (:period value-set))
           currency (finances-utils/get-currency-for-current-company)
-          cur-symbol (utils/get-symbol-for-currency-code (:currency data))]
+          cur-symbol (utils/get-symbol-for-currency-code (:currency data))
+          runway-string (if is-profitable
+                          runway
+                          (get-rounded-runway runway-value))]
       (dom/div {:class (str "section runway" (when (:read-only data) " read-only"))}
+        (dom/div {:class "chart-footer-container chart-header"}
+          (dom/div {:class "target-actual-container"}
+            (dom/div {:class "actual-container"}
+              (dom/h3 {:class "actual green"} runway-string)
+              (dom/h3 {:class "actual-label gray"} (get-runway-subtitle (:cash value-set) (:avg-burn-rate value-set) runway-value cur-symbol)))))
         (om/build column-chart (finances-utils/get-chart-data sorted-finances
                                                               ""
                                                               :runway
@@ -29,12 +55,4 @@
                                                               #js {"type" "string" "role" "style"}
                                                               "fill-color: #26C485"
                                                               "###,### days"
-                                                              " days"))
-        (dom/div {:class "chart-footer-container"}
-          (dom/div {:class "target-actual-container"}
-            (dom/div {:class "actual-container"}
-              (dom/h3 {:class "actual green"} runway)
-              (dom/h3 {:class "actual-label"} "ACTUAL"))))
-        (dom/div {:class "chart-footer-container"}
-          (dom/div {:class "period-container"}
-            (dom/p {} period)))))))
+                                                              " days"))))))
