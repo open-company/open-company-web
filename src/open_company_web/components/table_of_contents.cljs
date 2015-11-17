95006a4a9ec24b1d9f7d6d2aba20e5108f574e77
bago2k4 <iacopo@rc0.eu>
2015-11-17 22:13:36 +0100

    handle the add section click and fix some issues

--- src/open_company_web/components/table_of_contents.cljs
+++ src/open_company_web/components/table_of_contents.cljs
@@ -1,88 +1,99 @@
 (ns open-company-web.components.table-of-contents
+  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [om.core :as om :include-macros true]
             [om-tools.core :as om-core :refer-macros [defcomponent]]
             [om-tools.dom :as dom :include-macros true]
+            [cljs.core.async :refer [chan <!]]
             [open-company-web.router :as router]
             [open-company-web.lib.utils :as utils]
-            [open-company-web.api :as api]))
+            [open-company-web.api :as api]
+            [open-company-web.dispatcher :as dispatcher]
+            [open-company-web.components.new-section-popover :refer (new-section-popover)]))
 
-(def first-cat-placeholder "first-category")
+(def first-cat-placeholder "firstcategoryplaceholder")
 
+(defn get-category-section-info [e]
+  (let [target (.-target e)
+        target-id (.-id target)
+        parent-id (.-id (.-parentNode target))
+        el-id (if (empty? target-id)
+                          parent-id
+                          target-id)
+        $el (.$ js/window (str "#" el-id))
+        category (.data $el "category")
+        section (.data $el "section")]
+    {:category category :section section}))
 
-
-(defn setup-plus-position [e]
-  (let [target (.$ js/window (.-target e))
-        offset (.position target)
-        t-top (- (.-top offset) 5)
-        t-left (+ (.-left offset) 208)]
-    (-> (.$ js/window ".add-section")
-        (.css #js {"left" t-left "top" t-top}))))
-
-(defn show-popover [e owner]
+(defn show-popover [e]
   (when (.-$ js/window) ; avoid tests crash
-    (om/update-state! owner :hover-new-section (fn [_]false))
-    (om/update-state! owner :hover-add-section (fn [_]false))
-    (let [section (or (om/get-state owner :hover-new-section)
-                      (om/get-state owner :hover-add-section))
-          plus-offset (.position (.$ js/window ".add-section"))
-          popover (.$ js/window ".new-section-popover-container")
-          window-scrolltop (.scrollTop (.$ js/window js/window))]
+    (let [{:keys [category section] :as catsec} (get-category-section-info e)
+          $info (.$ js/window "#last-add-section-info")]
+      (.data $info "category" category)
+      (.data $info "section" section))
+    (let [popover (.$ js/window "#new-section-popover-container")]
       (.click popover (fn [e]
                         (.fadeOut popover 400 #(.css popover #js {"display" "none"}))))
-      (.css popover #js {"top" (str (+ (.-top plus-offset) 40 window-scrolltop) "px")
-                         "left" (str (+ (.-left plus-offset) 100) "px")})
       (.setTimeout js/window #(.fadeIn popover 400) 0))))
 
-(defn setup-hover-events [owner]
+(defn add-popover-container [data]
   (when (.-$ js/window) ; avoid tests crash
-    (-> (.$ js/window ".new-section")
-        (.hover (fn [e]
-                  (setup-plus-position e)
-                  (om/update-state! owner :hover-new-section (fn [_](.-id (.-target e)))))
-                (fn [e]
-                  (.setTimeout js/window
-                    #(om/update-state! owner :hover-new-section (fn [_]false))
-                    1))))
-    (-> (.$ js/window ".add-section")
-        (.hover (fn [e]
-                  (om/update-state! owner :hover-add-section (fn [_](om/get-state owner :hover-new-section))))
-                (fn [e]
-                  (om/update-state! owner :hover-add-section (fn [_]false)))))))
-
-(defn add-popover-container []
-  (when (.-$ js/window) ; avoid tests crash
-    (let [popover (.$ js/window "<div class='new-section-popover-container'></div>")
+    (let [popover (.$ js/window "<div id='new-section-popover-container'></div>")
           body (.$ js/window (.-body js/document))]
-      (.append body popover))))
+      ; if the new-section-popover div is not present add it
+      (when-not (pos? (.-length (.$ js/window "body div#new-section-popover-container")))
+        (.append body popover))
+      ; if the new-section-popover component has not been mount, render it
+      (when-not (pos? (.-length (.$ js/window "body div#new-section-popover-container div.new-section-popover")))
+        (.setTimeout js/window
+                     #(om/root new-section-popover
+                               dispatcher/app-state
+                               {:target (.getElementById js/document "new-section-popover-container")})
+                     1000)))))
+
+(defcomponent add-section [data owner]
+  (render [_]
+    (let [section (:section data)
+          category (:category data)]
+      (dom/div {:id (str "new-section-*-" (name section))
+                :class "new-section"
+                :data-category (name category)
+                :data-section (name section)
+                :on-click #(show-popover %)}
+        (dom/div {:class "new-section-internal"})
+        (dom/div {:class "add-section"
+                  :data-category ""
+                  :data-section ""
+                  :on-click #(show-popover %)}
+          (dom/i {:class "fa fa-plus"})))))
 
 (defcomponent table-of-contents [data owner]
-  (init-state [_]
-    {:hover-add-section false
-     :hover-new-section false})
   (did-mount [_]
-    (setup-hover-events owner)
-    (add-popover-container))
-  (did-update [_ _ _]
-    (setup-hover-events owner))
+    (let [add-section-chan (chan)]
+      (utils/add-channel add-section-chan "add-section")
+      (go (loop []
+        (let [change (<! add-section-chan)
+              cursor @dispatcher/app-state
+              slug (:slug @router/path)
+              company-data ((keyword slug) cursor)
+              section (:section data)
+              section-data (section company-data)]
+          (recur)))))
+    (add-popover-container data))
   (render [_]
     (let [sections (:sections data)
           categories (:categories data)]
       (dom/div #js {:className "table-of-contents" :ref "table-of-contents"}
-        (dom/div {:class (utils/class-set {:add-section true
-                                           :show (or (om/get-state owner :hover-add-section)
-                                                      (om/get-state owner :hover-new-section))})
-                  :on-click #(show-popover % owner)}
-          (dom/i {:class "fa fa-plus"}))
+        (dom/div {:id "last-add-section-info"
+                  :data-category ""
+                  :data-section ""})
         (dom/div {:class "table-of-contents-inner"}
           (for [category categories]
             (dom/div {:class "category-container"}
-              (dom/div {:class (utils/class-set {:category true :empty (zero? (count ((keyword category) sections)))})} (dom/h3 (utils/camel-case-str (name category))))
-              (dom/div {:id (str "new-section-" first-cat-placeholder)
-                        :class (utils/class-set {:new-section true
-                                                 :hover (or (= (om/get-state owner :hover-new-section) (str "new-section-" first-cat-placeholder))
-                                                            (= (om/get-state owner :hover-add-section) (str "new-section-" first-cat-placeholder)))})
-                        :on-click #(show-popover % owner)}
-                (dom/div {:class "new-section-internal"}))
+              (dom/div {:class (utils/class-set {:category true
+                                                 :empty (zero? (count ((keyword category) sections)))})}
+                       (dom/h3 (utils/camel-case-str (name category))))
+              (om/build add-section {:category category
+                                     :section first-cat-placeholder})
               (for [section (into [] (get sections (keyword category)))]
                 (let [section-data ((keyword section) data)]
                   (dom/div {}
@@ -94,10 +105,6 @@
                                           (.preventDefault e)
                                           (utils/scroll-to-section (name section)))}
                         (dom/p {:class "section-title"} (:title section-data))
-                        (dom/p {:class "section-date"} (utils/time-since (:updated-at section-data)))))
-                    (dom/div {:id (str "new-section-" (name section))
-                              :class (utils/class-set {:new-section true
-                                                       :hover (or (= (om/get-state owner :hover-new-section) (str "new-section-" (name section)))
-                                                                  (= (om/get-state owner :hover-add-section) (str "new-section-" (name section))))})
-                              :on-click #(show-popover % owner)}
-                      (dom/div {:class "new-section-internal"}))))))))))))
+                        (dom/p {:class "section-date"} (utils/time-since (:updated-at section-data))))
+                      (om/build add-section {:category category
+                                             :section section}))))))))))))
