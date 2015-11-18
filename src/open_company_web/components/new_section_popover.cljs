f12370c1ac9b551b087d0f9e137b7e315c67d710
bago2k4 <iacopo@rc0.eu>
2015-11-18 21:18:58 +0100

    filter sections to avoid adding a section that is already present

--- src/open_company_web/components/new_section_popover.cljs
+++ src/open_company_web/components/new_section_popover.cljs
@@ -4,6 +4,8 @@
             [om-tools.dom :as dom :include-macros true]
             [open-company-web.lib.utils :as utils]
             [open-company-web.api :as api]
+            [open-company-web.router :as router]
+            [open-company-web.dispatcher :as dispatcher]
             [cljs.core.async :refer (put!)]))
 
 (defn add-section [e category section section-body]
@@ -11,17 +13,30 @@
   (let [ch (utils/get-channel "add-section")]
     (put! ch {:category category :section section :section-defaults (dissoc section-body :name :core)})))
 
+(defn filter-sections [old-sections new-sections]
+  (filter #(not (utils/in? old-sections (:name %)))
+          new-sections))
+
+(defn filter-categories [old-categories new-categories]
+  (vec (for [category new-categories]
+         (let [category-kw (keyword (:name category))]
+           (merge category
+                  {:sections (filter-sections (category-kw old-categories) (:sections category))})))))
+
 (defcomponent new-section-popover [data owner]
   (init-state [_]
     (api/get-new-sections)
     {})
   (render [_]
-    (let [sections (:new-section data)]
+    (let [slug (keyword (:slug @router/path))
+          company-data (slug data)
+          old-sections (:sections company-data)
+          sections (filter-categories old-sections (:categories (:new-section data)))]
       (dom/div {:class "new-section-popover"}
         (dom/img {:src "/img/add_new_section_icon.png" :width "80" :height "82"})
         (dom/h4 {} "Add a new section")
         (dom/div {:class "new-section-container"}
-          (for [category (:categories sections)]
+          (for [category sections]
             (when (pos? (count (:sections category)))
               (dom/div {:class "new-section-category"}
                 (dom/h3 {} (:title category))
