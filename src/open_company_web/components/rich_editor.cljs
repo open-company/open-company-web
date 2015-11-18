9d99dc6229e70b1c639da9be65759e32d42d4340
bago2k4 <iacopo@rc0.eu>
2015-11-18 19:47:45 +0100

    fix the empty check for the body

--- src/open_company_web/components/rich_editor.cljs
+++ src/open_company_web/components/rich_editor.cljs
@@ -110,7 +110,7 @@
           section (:section data)
           read-only (:read-only data)
           editing (om/get-state owner :editing)
-          no-data (nil? (:author section-data)) ; if we have no author means we had no data
+          no-data (empty? (:body section-data)) ; if we have no author means we have no data
           should-show-placeholder (and (not editing) no-data)
           placeholder "Finances notes here..."
           body (if should-show-placeholder placeholder (:body section-data))
