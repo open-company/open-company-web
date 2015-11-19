b4639a54cc063afc5a45b4b47f6b7275f3edd80c
bago2k4 <iacopo@rc0.eu>
2015-11-19 21:18:46 +0100

    fix section popover to show the right icons and handle the missing icon case

--- src/open_company_web/components/new_section_popover.cljs
+++ src/open_company_web/components/new_section_popover.cljs
@@ -23,20 +23,67 @@
            (merge category
                   {:sections (filter-sections (category-kw old-categories) (:sections category))})))))
 
+(def icons-path "/img/section-icons/")
+
+(def icons-list [
+  ; progress
+  "progress-update"
+  "progress-highlights"
+  "progress-growth"
+  "progress-challenges"
+  "progress-team"
+  "progress-product"
+  "progress-customer-service"
+  "progress-marketing"
+  "progress-press"
+  "progress-business-development"
+  "progress-help"
+  "progress-kudos"
+  ; company
+  "company-diversity"
+  "company-values"
+  "company-privacy"
+  "company-mission"
+  ; financial
+  "financial-finances"
+  "financial-compensation"
+  "financial-ownership"])
+
 (defn get-section-image [category section]
-  "/img/new_section_placeholder_image.svg")
+  (let [nm (str category "-" section)]
+    (if (utils/in? icons-list nm)
+      (str icons-path nm ".svg")
+      (str icons-path "placeholder.svg"))))
+
+(def class-counter (atom -1))
+
+(defn get-color-class []
+  (swap! class-counter (fn [v] (mod (inc v) 3)))
+  (case @class-counter
+    0 "green"
+    1 "darkblue"
+    2 "lightblue"))
+
+(defn replace-svg []
+  (when (.-svgcss js/window)
+    (.setTimeout js/window #(.svgcss js/window) 1)))
 
 (defcomponent new-section-popover [data owner]
   (init-state [_]
     (api/get-new-sections)
     {})
+  (did-mount [_]
+    (replace-svg))
+  (did-update [_ _ _]
+    (replace-svg))
   (render [_]
     (let [slug (keyword (:slug @router/path))
           company-data (slug data)
           old-sections (:sections company-data)
           sections (filter-categories old-sections (:categories (:new-section data)))]
       (dom/div {:class "new-section-popover"}
-        (dom/div {:class "new-section-popover-close"})
+        (dom/div {:class "new-section-popover-close"
+                  :on-click #(put! (utils/get-channel "close-new-section-popover") {})})
         (dom/img {:src "/img/add_new_section_icon.png" :width "80" :height "82"})
         (dom/h4 {} "Add a new section")
         (dom/div {:class "new-section-container"}
@@ -47,12 +94,23 @@
                 (dom/h3 {} (:title category))
                 (dom/div {:class "new-section-category-sections"}
                   (for [section (:sections category)]
-                    (dom/div {:class "new-section-category-section"}
-                      (dom/a {:href "" :on-click #(add-section % (:name category) (:name section) section)}
-                        (dom/div {:class "section-icon-container"}
-                          (dom/img {:class (str "section-placeholder " (:name category) "-" (:name section) "-icon")
-                                    :src (get-section-image category section)}))
-                        (dom/div {:class "section-details"}
-                          (dom/h4 {:class "section-title"} (:title section))
-                          (dom/p {:class "section-description"} (:description section)))))))))))
+                    (let [color-class (get-color-class)
+                          section-name (:name section)
+                          category-name (:name category)
+                          section-uniq (str category-name "-" section-name)
+                          section-img-class (str section-uniq "-icon")
+                          section-inner-class (str section-uniq "-inner")]
+                      (dom/div {:class "new-section-category-section"}
+                        (dom/a {:href ""
+                                :on-click #(add-section % category-name section-name section)}
+                          (dom/div {:class "section-icon-container"}
+                            (dom/div {:class (utils/class-set {:section-icon-inner true
+                                                               section-inner-class true
+                                                               color-class true
+                                                               :icon-placeholder (not (utils/in? icons-list section-uniq))})}
+                              (dom/img {:class (str "section-placeholder svg " section-img-class)
+                                        :src (get-section-image category-name section-name)})))
+                          (dom/div {:class "section-details"}
+                            (dom/h4 {:class "section-title"} (:title section))
+                            (dom/p {:class "section-description"} (:description section))))))))))))
         (dom/div {:class "gradient bottom-gradient"})))))
