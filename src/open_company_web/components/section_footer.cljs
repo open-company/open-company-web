527c92d045cc9f6469b8782b4bca19c74978d45d
bago2k4 <iacopo@rc0.eu>
2015-11-23 18:35:46 +0100

    add a section-footer component that shows the revisions navigator when the data is readable and the cancel/save buttons when the data is editable

---
+++ src/open_company_web/components/section_footer.cljs
@@ -0,0 +1,20 @@
+(ns open-company-web.components.section-footer
+  (:require [om.core :as om :include-macros true]
+            [om-tools.core :as om-core :refer-macros (defcomponent)]
+            [om-tools.dom :as dom :include-macros true]
+            [open-company-web.components.revisions-navigator :refer (revisions-navigator)]))
+
+(defcomponent section-footer [data owner]
+
+  (render [_]
+    (let [editing (:editing data)]
+      (dom/div {:class "section-footer"}
+        (if-not editing
+          (om/build revisions-navigator data)
+          (dom/div {:class "save-section"}
+            (dom/button {:class "oc-btn oc-cancel"
+                         :on-click (:cancel-edit-cb data)}
+                        "CANCEL")
+            (dom/button {:class "oc-btn oc-success"
+                         :on-click (fn [e])}
+                        "SAVE")))))))
