772d75a8155cb648891ce7cffa45ae95bdff419b
bago2k4 <iacopo@rc0.eu>
2015-11-24 22:43:16 +0100

    show the cancel button only if it's not a newly created section, disable save button if needed

--- src/open_company_web/components/section_footer.cljs
+++ src/open_company_web/components/section_footer.cljs
@@ -2,7 +2,7 @@
   (:require [om.core :as om :include-macros true]
             [om-tools.core :as om-core :refer-macros (defcomponent)]
             [om-tools.dom :as dom :include-macros true]
-            [open-company-web.components.revisions-navigator :refer (revisions-navigator)]))
+            [open-company-web.lib.utils :as utils]))
 
 (defcomponent section-footer [data owner]
 
@@ -10,10 +10,16 @@
     (let [editing (:editing data)]
       (dom/div {:class "section-footer"}
         (dom/div {:class "save-section"}
-          (dom/button {:class "oc-btn oc-cancel"
-                       :on-click (:cancel-cb data)}
-                      "CANCEL")
-          (dom/button {:class "oc-btn oc-success"
+          (when (not (:is-new-section data))
+            (dom/button {:class "oc-btn oc-cancel"
+                         :on-click (:cancel-cb data)}
+                        "CANCEL"))
+          (dom/button {:class (utils/class-set {:oc-btn true
+                                                :oc-success true
+                                                :disabled (:save-disabled data)})
+                       :disabled (:save-disabled data)
                        :on-click (fn [e]
                                    ((:save-cb data)))}
-                      "SAVE"))))))
+                      (if (:is-new-section data)
+                        "CREATE"
+                        "SAVE")))))))
