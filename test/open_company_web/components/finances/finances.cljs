8bcffde74dfeceeaf040b28193034810ceb0ea07
bago2k4 <iacopo@rc0.eu>
2015-11-25 16:03:57 +0100

    fix finances tests and added editing, cancel and save tests

--- test/open_company_web/components/finances/finances.cljs
+++ test/open_company_web/components/finances/finances.cljs
@@ -7,23 +7,44 @@
               [open-company-web.components.finances.finances :refer [finances]]
               [om.dom :as dom :include-macros true]
               [open-company-web.data.company :refer (company)]
-              [open-company-web.router :as router]))
+              [open-company-web.router :as router]
+              [open-company-web.lib.utils :as utils]))
 
 (enable-console-print!)
 
 ; dynamic mount point for components
 (def ^:dynamic c)
 
-(def test-atom {:buffer company})
+(def company-data (utils/fix-sections company))
 
+(def test-atom {:section-data (:finances company-data)
+                :section :finances
+                :actual-as-of (:updated-at (:finances company-data))
+                :revisions-navigation-cb #()
+                :read-only false})
 
 (deftest test-finances-component
   (testing "Finances component"
-    (router/set-route! ["companies" "buffer" "finances" "cash"]
-                       {:slug "buffer" :section "finances" :tab "cash"})
+    (router/set-route! ["companies" "buffer"]
+                       {:slug "buffer"})
     (let [c (tu/new-container!)
           app-state (atom test-atom)
-          _ (om/root finances app-state {:target c})
-          finances-node (sel1 c [:div.finances])]
-      (is (not (nil? finances-node)))
+          _ (om/root finances app-state {:target c})]
+      (is (not (nil? (sel1 c [:div.finances]))))
+      ; editable click
+      (sim/click (sel1 c [:i.editable-pen]) nil)
+      (om.core/render-all)
+      (is (not (nil? (sel1 c [:div.save-section]))))
+      ; cancel click
+      (sim/click (sel1 c [:div.save-section :button.oc-cancel]) nil)
+      (om.core/render-all)
+      (is (nil? (sel1 c [:div.save-section])))
+      ; editable again
+      (sim/click (sel1 c [:h2.editable-title]) nil)
+      (om.core/render-all)
+      (is (not (nil? (sel1 c [:div.save-section]))))
+      ; save click
+      (sim/click (sel1 c [:div.save-section :button.oc-success]) nil)
+      (om.core/render-all)
+      (is (nil? (sel1 c [:div.save-section])))
       (tu/unmount! c))))
