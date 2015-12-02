9c2948cb60556fdea0af0e070dd87671246997dc
bago2k4 <iacopo@rc0.eu>
2015-12-02 12:47:39 +0100

    show only 3 devcards: a simple section, finances and growth

--- src/open_company_web/lib/devcards.cljs
+++ src/open_company_web/lib/devcards.cljs
@@ -4,8 +4,10 @@
             [open-company-web.components.simple-section :refer (simple-section)]
             [open-company-web.components.finances.finances :refer (finances)]
             [open-company-web.components.growth.growth :refer (growth)]
+            [open-company-web.components.section-selector :refer (section-selector)]
             [open-company-web.lib.utils :as utils]
             [open-company-web.data.company :as company-data]
+            [open-company-web.caches :as caches]
             [open-company-web.router :as router]))
 
 (router/set-route! ["companies" "buffer"] {:slug "buffer"})
@@ -18,82 +20,38 @@
 
 (def growth-data (:growth fixed-company-data))
 
-(defcard text-rw-section
-  "A read/write text section component"
-  (dc/om-root simple-section {})
+(reset! caches/revisions company-data/revisions)
+
+(defcard text-section
+  "A text section component"
+  (dc/om-root section-selector {})
   {
     :section :challenges
     :section-data challenges-data
-    :read-only false
   }
   {
-    :inspect-data false
+    :inspect-data true
     :history true
   })
 
-(defcard text-ro-section
-  "A read-only text section component"
-  (dc/om-root simple-section {})
-  {
-    :section :challenges
-    :section-data challenges-data
-    :read-only true
-  }
-  {
-    :inspect-data false
-    :history true
-  })
-
-(defcard finances-rw
-  "A read/write finances component"
-  (dc/om-root finances {})
+(defcard finances
+  "A finances component"
+  (dc/om-root section-selector {})
   {
     :section :finances
     :section-data finances-data
-    :read-only false
-    :editable-cb #()
   }
   {
     :inspect-data true
     :history true
   })
 
-(defcard finances-ro
-  "A read/write finances component"
-  (dc/om-root finances {})
-  {
-    :section :finances
-    :section-data finances-data
-    :read-only true
-    :editable-cb #()
-  }
-  {
-    :inspect-data true
-    :history true
-  })
-
-(defcard growth-rw
-  "A read/write growth component"
-  (dc/om-root growth {})
+(defcard growth
+  "A growth component"
+  (dc/om-root section-selector {})
   {
     :section :growth
     :section-data growth-data
-    :read-only false
-    :editable-cb #()
-  }
-  {
-    :inspect-data true
-    :history true
-  })
-
-(defcard growth-ro
-  "A read/write growth component"
-  (dc/om-root growth {})
-  {
-    :section :growth
-    :section-data growth-data
-    :read-only true
-    :editable-cb #()
   }
   {
     :inspect-data true
