5c99f4fe7bd7c7a56815031b99cb26f94b123ad4
bago2k4 <iacopo@rc0.eu>
2015-12-04 19:38:02 +0100

    use horizontal axis animation, clean up function names

--- src/open_company_web/components/charts.cljs
+++ src/open_company_web/components/charts.cljs
@@ -4,6 +4,23 @@
             [om-tools.dom :as dom :include-macros true]
             [open-company-web.lib.utils :as utils]))
 
+(defn- get-max [chart-data]
+  (let [values (:values chart-data)
+        max-val (apply max (map #(nth % 1) values))]
+    max-val))
+
+(defn- get-pagination [owner data]
+  (let [max-show (om/get-state owner :max-show)
+        data-count (count data)]
+    (if (>= max-show data-count)
+      #js {"min" 0
+           "max" (min max-show data-count)}
+      (let [page (om/get-state owner :page)
+            to (max max-show (- data-count (* page max-show)))
+            from (max 0 (- to max-show))]
+        #js {"min" from
+             "max" to}))))
+
 (defn add-columns-and-rows [columns data currency-symbol pattern]
   (let [data-table (js/google.visualization.DataTable.)
         formatter (js/google.visualization.NumberFormat. #js {
@@ -16,69 +33,49 @@
       (if (vector? x)
         (.addColumn data-table (first x) (second x))
         (.addColumn data-table x)))
-    (.addRows data-table data)
+    (.addRows data-table (clj->js data))
     (doseq [idx (range (count columns))]
       (let [column (columns idx)]
         (when (and (vector? column) (= (first column) "number"))
           (.format formatter data-table idx))))
     data-table))
 
-(defn get-column-chart-options [data column-thickness]
+(defn chart-options [owner data column-thickness max-value startup]
   (clj->js {:title  ""
             :width (min 600 (* 50 (count data)))
             :height 290
-            :animation #js {"startup" true
-                            "duration" 1000
+            :animation #js {"startup" startup
+                            "duration" 400
                             "easing" "out"}
             :legend #js {"position" "none"}
             :vAxis #js {"minValue" 0
+                        "maxValue" max-value
                         "gridlineColor" "transparent"
                         "baselineColor" "transparent"
                         "textPosition" "none"}
-            :hAxis #js {"textStyle" #js {"fontSize" 9}}
+            :hAxis #js {"textStyle" #js {"fontSize" 9}
+                        "viewWindow" (get-pagination owner data)}
             :chartArea #js {"left" 0 "top" 30 "width" "100%" "height" "80%"}
             :bar #js { "groupWidth" column-thickness}}))
 
-(defn column-draw-chart [owner currency-symbol columns pattern data column-thickness dom-node]
+(defn draw-chart [owner currency-symbol columns pattern data column-thickness dom-node]
   (when (.-google js/window)
     (let [data-table (add-columns-and-rows columns data currency-symbol pattern)
-          options (get-column-chart-options data column-thickness)]
+          options (chart-options owner data column-thickness (om/get-state owner :max-value) true)]
       (when dom-node
         (let [column-chart (js/google.visualization.ColumnChart. dom-node)]
           (.draw column-chart data-table options)
           (om/set-state! owner :chart column-chart)
           (om/set-state! owner :chart-computed-data data-table))))))
 
-(defn get-paginated-values [owner chart-data]
-  (let [max-show (:max-show chart-data)
-        all-values (vec (js->clj (:values chart-data)))
-        page (om/get-state owner :page)
-        from (* page max-show)
-        to (min (+ (* page max-show) max-show) (count all-values))
-        reversed-values (vec (reverse all-values))]
-    (clj->js
-      (if (< (count all-values) max-show)
-        all-values
-        (vec (reverse (subvec reversed-values from to)))))))
-
 (defn get-column-thickness [chart-data]
   (if (contains? chart-data :column-thickness)
     (:column-thickness chart-data)
     "14"))
 
-(defn draw-chart [owner chart-data]
-  (let [values (get-paginated-values owner chart-data)]
-    (column-draw-chart owner
-                       (:prefix chart-data)
-                       (:columns chart-data)
-                       (:pattern chart-data)
-                       values
-                       (get-column-thickness chart-data)
-                       (.getDOMNode (om/get-ref owner "column-chart")))))
-
 (defn check-scrolls-and-draw [owner chart-data initial-check]
   (when-not (om/get-state owner :animated)
-    (when-let [chart-ref (om/get-ref owner "column-chart")]
+    (when-let [chart-ref (om/get-ref owner "charts")]
       (let [chart-node (.getDOMNode chart-ref)
             $chart-node (.$ js/window chart-node)
             chart-offset-top (.-top (.offset $chart-node))
@@ -88,7 +85,13 @@
             win-scroll  (+ window-scroll-top window-outer-height)]
         (when (and (> chart-offset-top window-scroll-top)
                    (< (+ chart-offset-top 290) win-scroll))
-          (draw-chart owner chart-data)
+          (draw-chart owner
+                      (:prefix chart-data)
+                      (:columns chart-data)
+                      (:pattern chart-data)
+                      (:values chart-data)
+                      (get-column-thickness chart-data)
+                      (.getDOMNode (om/get-ref owner "column-chart")))
           (om/set-state! owner :animated true))))))
 
 (defn draw-chart-when-visible [owner chart-data]
@@ -99,14 +102,12 @@
                  (check-scrolls-and-draw owner chart-data false)))))
 
 (defn redraw-chart [owner chart-data]
-  (let [new-values (get-paginated-values owner chart-data)
-        data-table (om/get-state owner :chart-computed-data)
-        column-thickness (get-column-thickness chart-data)
-        chart-options (get-column-chart-options new-values column-thickness)]
-    ;; remove all the present values
-    (.removeRows data-table 0 (.getNumberOfRows data-table))
-    ;; add all the new values
-    (.insertRows data-table 0 new-values)
+  (let [data-table (om/get-state owner :chart-computed-data)
+        chart-options (chart-options owner
+                                     (:values chart-data)
+                                     (get-column-thickness chart-data)
+                                     (om/get-state owner :max-value)
+                                     false)]
     (.draw (om/get-state owner :chart) data-table chart-options)))
 
 (defn previous-values [owner chart-data]
@@ -123,16 +124,22 @@
     {:animated false
      :page 0
      :chart nil
-     :chart-computed-data nil})
+     :chart-computed-data nil
+     :max-show (:max-show chart-data)
+     :max-value (get-max chart-data)})
 
   (did-mount [_]
     (.setTimeout js/window #(draw-chart-when-visible owner chart-data) 200))
+
+  (will-receive-props [_ next-props]
+    (om/set-state! owner :max-value (get-max next-props)))
 
   (render [_]
     (let [data-count (count (:values chart-data))
           page (om/get-state owner :page)
           max-show (:max-show chart-data)]
-      (dom/div {:class "charts"}
+      (dom/div #js {:className "charts"
+                    :ref "charts"}
         (dom/div {:class (utils/class-set {:prev-values true
                                            :values-navigator true
                                            :hidden (not (> data-count (+ (* page max-show) max-show)))})
