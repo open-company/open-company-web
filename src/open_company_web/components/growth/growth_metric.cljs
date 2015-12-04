45764f8622b8d3ed1b28600509e3d49a80906738
bago2k4 <iacopo@rc0.eu>
2015-12-04 19:39:53 +0100

    fix get-past-period for monthly interval

--- src/open_company_web/components/growth/growth_metric.cljs
+++ src/open_company_web/components/growth/growth_metric.cljs
@@ -5,7 +5,9 @@
             [open-company-web.lib.utils :as utils]
             [open-company-web.components.charts :refer (column-chart)]
             [clojure.string :as clj-str]
-            [open-company-web.lib.oc-colors :as occ]))
+            [open-company-web.lib.oc-colors :as occ]
+            [cljs-time.core :as t]
+            [cljs-time.format :as f]))
 
 (defn get-columns-num [interval]
   (case interval
@@ -41,23 +43,26 @@
                   label])]
     values))
 
-(defn- get-past-period [period diff columns-num]
+(defn- monthly-past-period [period diff]
   (let [[year month] (clojure.string/split period "-")
-        int-year (int year)
-        int-month (int month)
-        diff-month (- int-month diff)
-        change-year (<= diff-month 0)
-        fix-month (if change-year (+ columns-num diff-month) diff-month)
-        fix-year (if change-year (dec int-year) int-year)]
-    (str fix-year "-" (utils/add-zero fix-month))))
+          period-date (t/date-time (int year) (int month))
+          past-date (t/minus period-date (t/months diff))
+          formatter (utils/get-formatter "monthly")]
+      (f/unparse formatter past-date)))
 
-(defn chart-placeholder-data [initial-data]
+(defn- get-past-period [period diff interval]
+  (cond
+    ;; TODO: add get-past-period for weekly and quarterly
+    (= interval "monthly")
+    (monthly-past-period period diff)))
+
+(defn chart-placeholder-data [initial-data interval]
   (let [first-period (:period (last initial-data))
         last-period (:period (first initial-data))
         months-diff (utils/periods-diff-in-months first-period last-period)]
     (vec
       (for [idx (range 0 (inc months-diff))]
-        (let [prev-period (get-past-period last-period idx (count initial-data))
+        (let [prev-period (get-past-period last-period idx interval)
               period-exists (utils/period-exists prev-period initial-data)]
           (if period-exists
             (some #(when (= (:period %) prev-period) %) initial-data)
@@ -68,7 +73,7 @@
 (defn- get-chart-data
   "Vector of max *columns elements of [:Label value]"
   [data prefix column-name tooltip-suffix interval]
-  (let [fixed-data (chart-placeholder-data data)
+  (let [fixed-data (chart-placeholder-data data interval)
         has-target (some #(:target %) data)
         chart-data (partial chart-data-at-index fixed-data column-name prefix tooltip-suffix has-target interval)
         columns (if has-target
