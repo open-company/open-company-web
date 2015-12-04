2c851baec894acb0fb87a7a189ecb92d7a4ce1fb
bago2k4 <iacopo@rc0.eu>
2015-12-04 14:21:24 +0100

    add pagination with animation for the column chart, remove unused pie chart

--- src/open_company_web/components/charts.cljs
+++ src/open_company_web/components/charts.cljs
@@ -1,46 +1,10 @@
 (ns open-company-web.components.charts
   (:require [om.core :as om :include-macros true]
             [om-tools.core :as om-core :refer-macros (defcomponent)]
-            [om-tools.dom :as dom :include-macros true]))
+            [om-tools.dom :as dom :include-macros true]
+            [open-company-web.lib.utils :as utils]))
 
-(defn pie-add-rows [columns data currency-symbol]
-  (let [data-table (js/google.visualization.DataTable.)
-        formatter (js/google.visualization.NumberFormat. #js {
-                    "negativeColor" "red",
-                    "negativeParens" true
-                    "pattern" "###,###.##"
-                    "prefix" (if (= currency-symbol "%") "" currency-symbol)
-                    "suffix" (if (not (= currency-symbol "%")) "" "%")})]
-    (doseq [x columns]
-      (.addColumn data-table (first x) (second x)))
-    (.addRows data-table (clj->js data))
-    (.format formatter data-table 1)
-    data-table))
-
-(defn pie-draw-chart [currency-symbol columns data dom-node]
-  (when (.-google js/window)
-    (let [data-table (pie-add-rows columns data currency-symbol)
-          options (clj->js {
-                    :title  ""
-                    :width 600
-                    :height 250})]
-      (when dom-node (.draw (js/google.visualization.PieChart. dom-node) data-table options)))))
-
-(defcomponent pie-chart [chart-data owner]
-  (did-mount [_]
-    (pie-draw-chart (:prefix chart-data)
-                    (:columns chart-data)
-                    (:values chart-data)
-                    (.getDOMNode (om/get-ref owner "pie-chart"))))
-  (did-update [_ _ _]
-    (pie-draw-chart (:prefix chart-data)
-                    (:columns chart-data)
-                    (:values chart-data)
-                    (.getDOMNode (om/get-ref owner "pie-chart"))))
-  (render [_]
-    (dom/div #js {:className "chart-container pie-chart" :ref "pie-chart" })))
-
-(defn column-add-rows [columns data currency-symbol pattern]
+(defn add-columns-and-rows [columns data currency-symbol pattern]
   (let [data-table (js/google.visualization.DataTable.)
         formatter (js/google.visualization.NumberFormat. #js {
                     "negativeColor" "red",
@@ -52,42 +16,65 @@
       (if (vector? x)
         (.addColumn data-table (first x) (second x))
         (.addColumn data-table x)))
-    (.addRows data-table (clj->js data))
+    (.addRows data-table data)
     (doseq [idx (range (count columns))]
       (let [column (columns idx)]
         (when (and (vector? column) (= (first column) "number"))
           (.format formatter data-table idx))))
     data-table))
 
-(defn column-draw-chart [currency-symbol columns pattern data column-thickness dom-node]
+(defn get-column-chart-options [data column-thickness]
+  (clj->js {:title  ""
+            :width (min 600 (* 50 (count data)))
+            :height 290
+            :animation #js {"startup" true
+                            "duration" 1000
+                            "easing" "out"}
+            :legend #js {"position" "none"}
+            :vAxis #js {"minValue" 0
+                        "gridlineColor" "transparent"
+                        "baselineColor" "transparent"
+                        "textPosition" "none"}
+            :hAxis #js {"textStyle" #js {"fontSize" 9}}
+            :chartArea #js {"left" 0 "top" 30 "width" "100%" "height" "80%"}
+            :bar #js { "groupWidth" column-thickness}}))
+
+(defn column-draw-chart [owner currency-symbol columns pattern data column-thickness dom-node]
   (when (.-google js/window)
-    (let [data-table (column-add-rows columns data currency-symbol pattern)
-          options (clj->js {
-                    :title  ""
-                    :width (min 600 (* 50 (count data)))
-                    :height 290
-                    :animation #js {"startup" true
-                                    "duration" 1000
-                                    "easing" "out"}
-                    :legend #js {"position" "none"}
-                    :vAxis #js {"minValue" 0
-                                "gridlineColor" "transparent"
-                                "baselineColor" "transparent"
-                                "textPosition" "none"}
-                    :hAxis #js {"textStyle" #js {"fontSize" 9}}
-                    :chartArea #js {"left" 0 "top" 30 "width" "100%" "height" "80%"}
-                    :bar #js { "groupWidth" column-thickness}})]
-      (when dom-node (.draw (js/google.visualization.ColumnChart. dom-node) data-table options)))))
+    (let [data-table (add-columns-and-rows columns data currency-symbol pattern)
+          options (get-column-chart-options data column-thickness)]
+      (when dom-node
+        (let [column-chart (js/google.visualization.ColumnChart. dom-node)]
+          (.draw column-chart data-table options)
+          (om/set-state! owner :chart column-chart)
+          (om/set-state! owner :chart-computed-data data-table))))))
+
+(defn get-paginated-values [owner chart-data]
+  (let [max-show (:max-show chart-data)
+        all-values (vec (js->clj (:values chart-data)))
+        page (om/get-state owner :page)
+        from (* page max-show)
+        to (min (+ (* page max-show) max-show) (count all-values))
+        reversed-values (vec (reverse all-values))]
+    (clj->js
+      (if (< (count all-values) max-show)
+        all-values
+        (vec (reverse (subvec reversed-values from to)))))))
+
+(defn get-column-thickness [chart-data]
+  (if (contains? chart-data :column-thickness)
+    (:column-thickness chart-data)
+    "14"))
 
 (defn draw-chart [owner chart-data]
-  (column-draw-chart (:prefix chart-data)
-                     (:columns chart-data)
-                     (:pattern chart-data)
-                     (:values chart-data)
-                     (if (contains? chart-data :column-thickness)
-                       (:column-thickness chart-data)
-                       "14")
-                     (.getDOMNode (om/get-ref owner "column-chart"))))
+  (let [values (get-paginated-values owner chart-data)]
+    (column-draw-chart owner
+                       (:prefix chart-data)
+                       (:columns chart-data)
+                       (:pattern chart-data)
+                       values
+                       (get-column-thickness chart-data)
+                       (.getDOMNode (om/get-ref owner "column-chart")))))
 
 (defn check-scrolls-and-draw [owner chart-data initial-check]
   (when-not (om/get-state owner :animated)
@@ -111,13 +98,49 @@
                (fn [e]
                  (check-scrolls-and-draw owner chart-data false)))))
 
+(defn redraw-chart [owner chart-data]
+  (let [new-values (get-paginated-values owner chart-data)
+        data-table (om/get-state owner :chart-computed-data)
+        column-thickness (get-column-thickness chart-data)
+        chart-options (get-column-chart-options new-values column-thickness)]
+    ;; remove all the present values
+    (.removeRows data-table 0 (.getNumberOfRows data-table))
+    ;; add all the new values
+    (.insertRows data-table 0 new-values)
+    (.draw (om/get-state owner :chart) data-table chart-options)))
+
+(defn previous-values [owner chart-data]
+  (om/set-state! owner :page (inc (om/get-state owner :page)))
+  (redraw-chart owner chart-data))
+
+(defn next-values [owner chart-data]
+  (om/set-state! owner :page (dec (om/get-state owner :page)))
+  (redraw-chart owner chart-data))
+
 (defcomponent column-chart [chart-data owner]
 
   (init-state [_]
-    {:animated false})
+    {:animated false
+     :page 0
+     :chart nil
+     :chart-computed-data nil})
 
   (did-mount [_]
     (.setTimeout js/window #(draw-chart-when-visible owner chart-data) 200))
 
   (render [_]
-    (dom/div #js {:className "chart-container column-chart" :ref "column-chart" })))
+    (let [data-count (count (:values chart-data))
+          page (om/get-state owner :page)
+          max-show (:max-show chart-data)]
+      (dom/div {:class "charts"}
+        (dom/div {:class (utils/class-set {:prev-values true
+                                           :values-navigator true
+                                           :hidden (not (> data-count (+ (* page max-show) max-show)))})
+                  :on-click #(previous-values owner chart-data)}
+          (dom/i {:class "fa fa-caret-left"}))
+        (dom/div #js {:className "chart-container column-chart" :ref "column-chart" })
+        (dom/div {:class (utils/class-set {:next-values true
+                                           :values-navigator true
+                                           :hidden (zero? (om/get-state owner :page))})
+                  :on-click #(next-values owner chart-data)}
+          (dom/i {:class "fa fa-caret-right"}))))))
