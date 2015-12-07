78578cc3ce32d8d799811b82b8b433d51b2427b3
bago2k4 <iacopo@rc0.eu>
2015-12-07 23:00:10 +0100

    use the unified section remove function

--- src/open_company_web/components/table_of_contents_item.cljs
+++ src/open_company_web/components/table_of_contents_item.cljs
@@ -4,7 +4,8 @@
             [om-tools.dom :as dom :include-macros true]
             [open-company-web.router :as router]
             [open-company-web.lib.utils :as utils]
-            [open-company-web.api :as api]))
+            [open-company-web.api :as api]
+            [open-company-web.lib.section-utils :as section-utils]))
 
 (defcomponent table-of-contents-item [data owner]
   
@@ -17,7 +18,7 @@
                 :id (str "section-sort--" (name section))}
         (dom/div {:class "category-section"}
           (dom/div {:class "category-section-close"
-                    :on-click #(api/remove-section (name section))})
+                    :on-click #(section-utils/remove-section (name section))})
           (dom/a {:href "#"
                   :on-click (fn [e]
                               (.preventDefault e)
