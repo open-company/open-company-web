26b4dff6b717d7c252c3b176d4fc3a6c19b57d2d
bago2k4 <iacopo@rc0.eu>
2015-12-09 16:24:51 +0100

    make sure there is always enough room for the tooltip when there is 1 only one value

--- src/open_company_web/components/charts.cljs
+++ src/open_company_web/components/charts.cljs
@@ -42,7 +42,7 @@
 
 (defn chart-options [owner data column-thickness max-value startup]
   (clj->js {:title  ""
-            :width (min 600 (* 50 (count data)))
+            :width (min 600 (max (* 50 (count data)) 120))
             :height 290
             :animation #js {"startup" startup
                             "duration" 400
