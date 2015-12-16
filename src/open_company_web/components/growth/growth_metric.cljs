f6ebeef5ae1deb308ac31ccf366bbb46734c9786
bago2k4 <iacopo@rc0.eu>
2015-12-16 14:37:29 +0100

    fix chart data with placeholders getter

--- src/open_company_web/components/growth/growth_metric.cljs
+++ src/open_company_web/components/growth/growth_metric.cljs
@@ -1,12 +1,12 @@
 (ns open-company-web.components.growth.growth-metric
   (:require [om.core :as om :include-macros true]
-            [om-tools.core :as om-core :refer-macros (defcomponent)]
+            [om-tools.core :refer-macros (defcomponent)]
             [om-tools.dom :as dom :include-macros true]
             [open-company-web.lib.utils :as utils]
             [open-company-web.components.charts :refer (column-chart)]
-            [clojure.string :as clj-str]
             [open-company-web.lib.oc-colors :as occ]
             [open-company-web.components.utility-components :refer (editable-pen)]
+            [open-company-web.components.growth.utils :as growth-utils]
             [cljs-time.core :as t]
             [cljs-time.format :as f]))
 
@@ -15,7 +15,7 @@
     "monthly" 12
     8))
 
-(defn get-graph-tooltip [period label prefix value suffix]
+(defn get-graph-tooltip [label prefix value suffix]
   (str label
        ": "
        (or prefix "")
@@ -25,10 +25,10 @@
 (defn chart-data-at-index [data column-name prefix suffix has-target interval idx]
   (let [data (to-array data)
         obj (get (vec (reverse data)) idx)
-        value (:value obj)
+        value (or (:value obj) 0)
         target (or (:target obj) 0)
-        label (get-graph-tooltip (:period obj) column-name prefix value suffix)
-        target-label (get-graph-tooltip (:period obj) "target" prefix (.toLocaleString target) suffix)
+        label (get-graph-tooltip column-name prefix value suffix)
+        target-label (get-graph-tooltip "target" prefix (.toLocaleString target) suffix)
         period (utils/get-period-string (:period obj) interval [:short])
         values (if has-target
                  [period
@@ -57,24 +57,10 @@
     (= interval "monthly")
     (monthly-past-period period diff)))
 
-(defn chart-placeholder-data [initial-data interval]
-  (let [first-period (:period (last initial-data))
-        last-period (:period (first initial-data))
-        months-diff (utils/periods-diff first-period last-period interval)]
-    (vec
-      (for [idx (range 0 (inc months-diff))]
-        (let [prev-period (get-past-period last-period idx interval)
-              period-exists (utils/period-exists prev-period initial-data)]
-          (if period-exists
-            (some #(when (= (:period %) prev-period) %) initial-data)
-            {:period prev-period
-             :slug (:slug first-period)
-             :value nil}))))))
-
 (defn- get-chart-data
   "Vector of max *columns elements of [:Label value]"
-  [data prefix column-name tooltip-suffix interval]
-  (let [fixed-data (chart-placeholder-data data interval)
+  [data prefix slug column-name tooltip-suffix interval]
+  (let [fixed-data (growth-utils/chart-placeholder-data data slug interval)
         has-target (some #(:target %) data)
         chart-data (partial chart-data-at-index fixed-data column-name prefix tooltip-suffix has-target interval)
         columns (if has-target
@@ -134,9 +120,10 @@
                 (dom/div {:class "actual-container"}
                   (dom/h3 {:class "actual blue"} actual-with-label
                     (om/build editable-pen {:click-callback (:start-data-editing-cb data)}))
-                  (dom/h3 {:class "actual-label gray"} (str "as of " (utils/get-period-string (:period actual-set) interval))))))
+                  (dom/h3 {:class "actual-label gray"} (str "as of " period)))))
             (om/build column-chart (get-chart-data sorted-metric
                                                    fixed-cur-unit
+                                                   (:slug metric-info)
                                                    (:name metric-info)
                                                    unit
                                                    interval))))))))
