59228e0007c3859faffa45ee0e0238815b705a59
bago2k4 <iacopo@rc0.eu>
2015-12-22 14:05:57 +0100

    fix tests

--- test/open_company_web/components/finances/costs.cljs
+++ test/open_company_web/components/finances/costs.cljs
@@ -6,17 +6,24 @@
               [dommy.core :as dommy :refer-macros [sel1 sel]]
               [open-company-web.components.finances.costs :refer [costs]]
               [om.dom :as dom :include-macros true]
-              [open-company-web.data.company :refer (company)]))
+              [open-company-web.data.company :refer (company)]
+              [open-company-web.lib.utils :as utils]))
 
 (enable-console-print!)
 
 ; dynamic mount point for components
 (def ^:dynamic c)
 
+(def test-atom {
+  :section-data (:finances (utils/fix-sections company))
+  :read-only false
+  :currency "USD"
+  :start-editing-cb #()})
+
 (deftest test-costs-component
   (testing "Costs component"
     (let [c (tu/new-container!)
-          app-state (atom company)
+          app-state (atom test-atom)
           _ (om/root costs app-state {:target c})
           chart-node (sel1 c [:div.section.costs])]
       (is (not (nil? chart-node)))
