060b3ccebaafe77223811e2074507752c471a2f5
bago2k4 <iacopo@rc0.eu>
2016-01-11 19:48:51 +0100

    code cleaning

--- src/open_company_web/components/login_button.cljs
+++ src/open_company_web/components/login_button.cljs
@@ -16,10 +16,9 @@
                           (when-not (.startsWith current-token "/login")
                             (cook/set-cookie! :login-redirect current-token))
                           (set! (.-location js/window) full-url))}
-             (dom/img {
-                       :alt "Login with Slack"
-                       :height "40"
-                       :width "139"
-                       :src "https://platform.slack-edge.com/img/add_to_slack.png"
-                       :srcSet "https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
-                      })))))
+        (dom/img {:alt "Login with Slack"
+                  :height "40"
+                  :width "139"
+                  :src "https://platform.slack-edge.com/img/add_to_slack.png"
+                  :srcSet (str "https://platform.slack-edge.com/img/add_to_slack.png 1x, "
+                               "https://platform.slack-edge.com/img/add_to_slack@2x.png 2x")})))))
