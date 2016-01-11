7542874e69de1f31eb715fa5d78c0bac9fd2fcd1
bago2k4 <iacopo@rc0.eu>
2016-01-11 17:39:26 +0100

    use the new login button component

--- src/open_company_web/components/login.cljs
+++ src/open_company_web/components/login.cljs
@@ -1,7 +1,8 @@
 (ns open-company-web.components.login
   (:require [om.core :as om :include-macros true]
-            [om-tools.core :as om-core :refer-macros [defcomponent]]
-            [om-tools.dom :as dom :include-macros true]))
+            [om-tools.core :as om-core :refer-macros (defcomponent)]
+            [om-tools.dom :as dom :include-macros true]
+            [open-company-web.components.login-button :refer (login-button)]))
 
 (defcomponent login [data owner]
   (render [_]
@@ -20,11 +21,4 @@
                 "OPENcompany requires verification with your Slack team. Please allow access."
                 "There is a temporary error validating with Slack. Please try again later.")]
                 (dom/h4 {:class "login-error-message"} msg)))
-            (dom/a {:href (:full-url (:auth-settings data))}
-              (dom/img {
-                :alt "Login with Slack"
-                :height "40"
-                :width "139"
-                :src "https://platform.slack-edge.com/img/add_to_slack.png"
-                :srcSet "https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
-              }))))))))
+            (om/build login-button data)))))))
