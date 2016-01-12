e9c6e7fe13a89c1c542152d4cdefd66f10f337cd
Sean Johnson <sean@snootymonkey.com>
2016-01-12 15:56:14 -0500

    Change Slack button in top right for logged out user into Sign in / Sign up button.

--- src/open_company_web/components/ui/login_button.cljs
+++ src/open_company_web/components/ui/login_button.cljs
@@ -10,15 +10,10 @@
   (render [_]
     (let [full-url (:full-url (:auth-settings data))
           current-token (router/get-token)]
-      (dom/a {:href full-url
-              :on-click (fn [e]
-                          (.preventDefault e)
-                          (when-not (.startsWith current-token "/login")
-                            (cook/set-cookie! :login-redirect current-token))
-                          (set! (.-location js/window) full-url))}
-        (dom/img {:alt "Login with Slack"
-                  :height "40"
-                  :width "139"
-                  :src "https://platform.slack-edge.com/img/add_to_slack.png"
-                  :srcSet (str "https://platform.slack-edge.com/img/add_to_slack.png 1x, "
-                               "https://platform.slack-edge.com/img/add_to_slack@2x.png 2x")})))))
+      (dom/button {:class "btn btn-default login-button"
+                   :on-click (fn [e]
+                    (.preventDefault e)
+                    (when-not (.startsWith current-token "/login")
+                      (cook/set-cookie! :login-redirect current-token))
+                    (set! (.-location js/window) full-url))}
+        "Sign in / Sign up"))))
