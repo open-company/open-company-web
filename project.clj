dc6c17763e146edea74cef105898ed2cf276ff76
Sean Johnson <sean@snootymonkey.com>
2016-01-12 10:30:12 -0500

    Update dependencies.

--- project.clj
+++ project.clj
@@ -9,8 +9,8 @@
   :min-lein-version "2.5.1" ; highest version supported by Travis-CI as of 7/5/2015
 
   :dependencies [
-    [org.clojure/clojure "1.8.0-RC3"] ; Lisp on the JVM http://clojure.org/documentation
-    [org.clojure/clojurescript "1.7.189"] ; ClojureScript compiler https://github.com/clojure/clojurescript
+    [org.clojure/clojure "1.8.0-RC5"] ; Lisp on the JVM http://clojure.org/documentation
+    [org.clojure/clojurescript "1.7.228"] ; ClojureScript compiler https://github.com/clojure/clojurescript
     ; --- DO NOT UPDATE OM, the 1.x.x code is Om Next and requires changes on our part https://github.com/omcljs/om/wiki/Quick-Start-(om.next)
     [org.omcljs/om "0.9.0" :exclusions [cljsjs/react]] ; Cljs interface to React https://github.com/omcljs/om
     [cljs-http "0.1.39"] ; HTTP for cljs https://github.com/r0man/cljs-http
@@ -31,7 +31,7 @@
 
   :plugins [
     [lein-cljsbuild "1.1.2"] ; ClojureScript compiler https://github.com/emezeske/lein-cljsbuild
-    [lein-figwheel "0.5.0-2"] ; Dynamic development environment https://github.com/bhauman/lein-figwheel
+    [lein-figwheel "0.5.0-3"] ; Dynamic development environment https://github.com/bhauman/lein-figwheel
     [lein-ancient "0.6.8"] ; Check for outdated dependencies https://github.com/xsc/lein-ancient
     [lein-doo "0.1.6"] ; A plugin to run tests in many JS environments https://github.com/bensu/doo
     [lein-deps-tree "0.1.2"] ; Print a tree of project dependencies https://github.com/the-kenny/lein-deps-tree
