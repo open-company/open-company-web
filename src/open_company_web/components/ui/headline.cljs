273344e3d3a7d80cdc7015f5f6af44e5fb8f185b
bago2k4 <iacopo@rc0.eu>
2016-01-27 18:47:21 +0100

    headline component

---
+++ src/open_company_web/components/ui/headline.cljs
@@ -0,0 +1,28 @@
+(ns open-company-web.components.ui.headline
+  (:require [om.core :as om :include-macros true]
+            [om-tools.core :as om-core :refer-macros (defcomponent)]
+            [om-tools.dom :as dom :include-macros true]
+            [open-company-web.lib.utils :as utils]
+            [open-company-web.router :as router]
+            [cljs-dynamic-resources.core :as cdr]))
+
+(defcomponent headline [data owner]
+  (render [_]
+    (let [headline (:headline data)]
+      (dom/div {:class "headline"}
+        (dom/textarea #js {:ref "headline-textarea"
+                           :id (str "headline-" (name (:section data)))
+                           :className (utils/class-set {:headline-textarea true
+                                                        :edit (:editing data)})
+                           :placeholder (:placeholder data)
+                           :maxLength 100
+                           :disabled (:read-only data)
+                           :value headline
+                           :onFocus #((:start-editing-cb data))
+                           :onChange #(let [value (.. % -target -value)]
+                                        ((:change-cb data) value))
+                           :onBlur (fn [e]
+                                     ((:cancel-if-needed-cb data)))
+                           :onKeyDown #(cond
+                                         (= (.-key %) "Escape")
+                                         ((:cancel-if-needed-cb data)))})))))
