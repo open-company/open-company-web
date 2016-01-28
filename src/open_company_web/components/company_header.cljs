c2cbabee479d86e72deb84847b1a3e54be3602a4
bago2k4 <iacopo@rc0.eu>
2016-01-28 13:45:10 +0100

    remove new navbar, use old navbar

--- src/open_company_web/components/company_header.cljs
+++ src/open_company_web/components/company_header.cljs
@@ -2,6 +2,7 @@
   (:require [om.core :as om :include-macros true]
             [om-tools.core :as om-core :refer-macros [defcomponent]]
             [om-tools.dom :as dom :include-macros true]
+            [open-company-web.components.navbar :refer (navbar)]
             [open-company-web.components.ui.company-avatar :refer (company-avatar)]
             [open-company-web.components.category-nav :refer (category-nav)]
             [open-company-web.router :as router]))
@@ -10,20 +11,10 @@
  
   (render [_]
     (let [company-data (:company-data data)]
-      (dom/nav {:class "navbar navbar-fixed-top company-header"}
-        (dom/div {:class "row"}
-          
-          ;; Company logo
-          (dom/div {:class "col-xs-4 company-logo"}
-            (when (contains? @router/path :slug)
-              (om/build company-avatar {:company-data company-data :navbar-brand true})))
-          
-          ;; White space
-          (dom/div {:class "col-xs-4"})
-          
-          ;; Share
-          (dom/div {:class "col-xs-4 text-right company-share"}
-            (dom/button {:type "button" :class "btn btn-default round"} "Share")))
+      (dom/div {:class "company-header row-fluid"}
+
+        ;; navbar
+        (om/build navbar data)
         
         ;; Company name
         (dom/div {:class "row"}
