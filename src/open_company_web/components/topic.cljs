58b7c14441c8783aff33f405e6a8540d229bfe66
bago2k4 <iacopo@rc0.eu>
2016-01-29 20:56:27 +0100

    add growth chart as headline, fix expanded state while switching category

--- src/open_company_web/components/topic.cljs
+++ src/open_company_web/components/topic.cljs
@@ -2,7 +2,63 @@
   (:require [om.core :as om :include-macros true]
             [om-tools.core :as om-core :refer-macros [defcomponent]]
             [om-tools.dom :as dom :include-macros true]
-            [open-company-web.lib.utils :as utils]))
+            [open-company-web.lib.utils :as utils]
+            [open-company-web.components.finances.utils :as finances-utils]
+            [open-company-web.components.growth.utils :as growth-utils]
+            [open-company-web.components.ui.charts :refer (column-chart)]))
+
+(defn- get-body [section-data section]
+  (cond
+    (= section :finances)
+    (:body (:notes section-data))
+
+    (= section :growth)
+    (:body (:notes section-data))
+
+    :else
+    (:body section-data)))
+
+(defcomponent topic-headline [data owner]
+  (render [_]
+    (dom/div {:class "topic-headline"} (:headline data))))
+
+(defcomponent topic-headline-finances [data owner]
+  (render [_]
+    (let [data (:data data)
+          sorter (utils/sort-by-key-pred :period true)
+          sorted-data (sort sorter data)
+          actual (last sorted-data)
+          actual-label (str "as of " (finances-utils/get-as-of-string (:period actual)))]
+      (dom/div {:class "topic-headline chart-header-container"}
+        (dom/div {:class "target-actual-container"}
+          (dom/div {:class "actual-container"}
+            (dom/h3 {:class "actual green"} (:cash actual))
+            (dom/h3 {:class "actual-label gray"} actual-label)))))))
+
+(defcomponent topic-headline-growth [data owner options]
+  (render [_]
+    (let [metric-info (first (:metrics data))
+          metric-data (filter #(= (:slug metric-info) (:slug %)) (:data data))
+          sort-pred (utils/sort-by-key-pred :period true)
+          sorted-metric (vec (sort sort-pred metric-data))
+          interval (:interval metric-info)
+          metric-unit (:unit metric-info)
+          fixed-cur-unit (when (= metric-unit "currency")
+                           (utils/get-symbol-for-currency-code (:currency options)))
+          unit (when (= metric-unit "%") "%")]
+      (dom/div {:class "topic-headline topic-headline-growth group"}
+        (dom/div {:class "chart-header-container"}
+          (dom/div {:class "target-actual-container"}
+            (dom/div {:class "actual-container"}
+              (dom/h3 {:class "actual blue"} (:name metric-info)))))
+        (om/build column-chart
+                  (growth-utils/get-chart-data sorted-metric
+                                               fixed-cur-unit
+                                               (:slug metric-info)
+                                               (:name metric-info)
+                                               unit
+                                               interval)
+                  {:opts {:chart-height 50 :chart-width 300}})))))
 
 (defcomponent topic [data owner options]
 
@@ -13,12 +69,30 @@
     (let [company-data (:company-data data)
           section (keyword (:section-name options))
           section-data (company-data section)
-          expanded (om/get-state owner :expanded)]
+          expanded (om/get-state owner :expanded)
+          section-body (get-body section-data section)]
       (dom/div {:class "topic"
+                ; :key (str "topic-" (name section))
                 :on-click #(om/set-state! owner :expanded (not expanded))}
+
+        ;; Topic title
         (dom/div {:class "topic-title"} (:title section-data))
-        (dom/div {:class "topic-headline"} (:headline section-data))
+
+        ;; Topic headline
+        (cond
+          (= section :finances)
+          (om/build topic-headline-finances section-data)
+
+          (= section :growth)
+          (om/build topic-headline-growth section-data {:opts {:currency (:currency company-data)}})
+
+          :else
+          (om/build topic-headline section-data))
+
+        ;; Topic date
         (dom/div {:class "topic-date"} (utils/time-since (:updated-at section-data)))
+
+        ;; Topic body
         (dom/div #js {:className (utils/class-set {:topic-body true
                                                    :expanded expanded})
-                      :dangerouslySetInnerHTML (clj->js {"__html" (:body section-data)})})))))
+                      :dangerouslySetInnerHTML (clj->js {"__html" section-body})})))))
--- src/open_company_web/components/topic_list.cljs
+++ src/open_company_web/components/topic_list.cljs
@@ -12,7 +12,8 @@
             active-category (keyword (:active-category data))
             active-sections (get-in company-data [:sections active-category])]
         (for [section-name active-sections]
-          (dom/div {:class "topic-row"}
+          (dom/div {:class "topic-row"
+                    :key (str "topic-row-" (name section-name))}
             (om/build topic {:loading (:loading company-data)
                              :company-data company-data
                              :active-category active-category}
