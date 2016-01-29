9c82aa48f0dd931610c504c9990192820046af10
bago2k4 <iacopo@rc0.eu>
2016-01-29 13:56:31 +0100

    show topic body when clicking on the topic section, copy css for img and h1, h2 from section editing

--- src/open_company_web/components/topic.cljs
+++ src/open_company_web/components/topic.cljs
@@ -6,11 +6,19 @@
 
 (defcomponent topic [data owner options]
 
+  (init-state [_]
+    {:expanded false})
+
   (render [_]
     (let [company-data (:company-data data)
           section (keyword (:section-name options))
-          section-data (company-data section)]
-      (dom/div {:class "col-xs-12 topic"}
+          section-data (company-data section)
+          expanded (om/get-state owner :expanded)]
+      (dom/div {:class "col-xs-12 topic"
+                :on-click #(om/set-state! owner :expanded (not expanded))}
         (dom/div {:class "topic-title"} (:title section-data))
         (dom/div {:class "topic-headline"} (:headline section-data))
-        (dom/div {:class "topic-date"} (utils/time-since (:updated-at section-data)))))))
+        (dom/div {:class "topic-date"} (utils/time-since (:updated-at section-data)))
+        (dom/div #js {:className (utils/class-set {:topic-body true
+                                                   :expanded expanded})
+                      :dangerouslySetInnerHTML (clj->js {"__html" (:body section-data)})})))))
