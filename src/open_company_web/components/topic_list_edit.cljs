13a40986cb514ef5ff7ae0380fdaeab20ef96d0a
bago2k4 <iacopo@rc0.eu>
2016-02-04 22:47:59 +0100

    listen for cancel and save event from the navbar buttons, cancel editing when event is posted, add deploy key to check images

--- src/open_company_web/components/topic_list_edit.cljs
+++ src/open_company_web/components/topic_list_edit.cljs
@@ -1,10 +1,13 @@
 (ns open-company-web.components.topic-list-edit
+  (:require-macros [cljs.core.async.macros :refer (go)])
   (:require [om.core :as om :include-macros true]
             [om-tools.core :as om-core :refer-macros [defcomponent]]
             [om-tools.dom :as dom :include-macros true]
+            [cljs.core.async :refer (chan <!)]
             [open-company-web.api :as api]
             [open-company-web.router :as router]
             [open-company-web.caches :as caches]
+            [open-company-web.local-settings :as ls]
             [open-company-web.lib.utils :as utils]
             [open-company-web.components.topic :refer (topic)]
             [open-company-web.components.manage-topic :refer (manage-topic)]))
@@ -12,9 +15,23 @@
 (defcomponent topic-list-edit [data owner options]
 
   (init-state [_]
+    (let [save-ch (chan)
+          cancel-ch (chan)]
+      (utils/add-channel "save-bt-navbar" save-ch)
+      (utils/add-channel "cancel-bt-navbar" cancel-ch))
     (when (empty? @caches/new-sections)
       (api/get-new-sections))
     {:active-topics (get-in (:company-data data) [:sections (keyword (:active-category data))])})
+
+  (did-mount [_]
+    (let [save-ch (utils/get-channel "save-bt-navbar")]
+      (go (loop []
+        (let [change (<! save-ch)]
+          (println "save ch!" change)))))
+    (let [cancel-ch (utils/get-channel "cancel-bt-navbar")]
+      (go (loop []
+        (let [change (<! cancel-ch)]
+          ((:cancel-editing-cb options)))))))
 
   (render [_]
     (if (empty? @caches/new-sections)
@@ -26,7 +43,7 @@
         (dom/div {:class "topic-list-edit fix-top-margin-scrolling group"}
           (for [section category-sections]
             (let [active (utils/in? active-sections (:name section))
-                  check-src (if active "/img/check_checked.png" "/img/check_empty.png")]
+                  check-src (str "/img/check_" (if active "checked" "empty") ".png?" ls/deploy-key)]
               (dom/div {:class (utils/class-set {:topic-edit true
                                                  :group true
                                                  :active active})
