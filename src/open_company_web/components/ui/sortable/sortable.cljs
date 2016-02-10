b9df12974c1e1db150865110e8f207cb4661504b
bago2k4 <iacopo@rc0.eu>
2016-02-10 15:27:26 +0100

    call delegate methods if set

--- src/open_company_web/components/ui/sortable/sortable.cljs
+++ src/open_company_web/components/ui/sortable/sortable.cljs
@@ -52,9 +52,24 @@
         #js {:className (str "no-select " (when (su/dragging? owner) "dragging"))
              :style style
              :ref "draggable"
-             :onMouseDown #(su/drag-start % data owner)
-             :onMouseUp   #(su/drag-stop % data owner)
-             :onMouseMove #(su/drag % data owner)}
+             :onMouseDown (fn [e]
+                            (when (contains? options :will-start-drag)
+                              ((:will-start-drag options) e (:id data)))
+                            (su/drag-start e data owner)
+                            (when (contains? options :did-start-drag)
+                              ((:did-start-drag options) e (:id data))))
+             :onMouseUp   (fn [e]
+                            (when (contains? options :will-stop-drag)
+                              ((:will-stop-drag options) e (:id data)))
+                            (su/drag-stop e data owner)
+                            (when (contains? options :did-stop-drag)
+                              ((:did-stop-drag options) e (:id data))))
+             :onMouseMove (fn [e]
+                            (when (contains? options :will-drag)
+                              ((:will-drag options) e (:id data)))
+                            (su/drag e data owner)
+                            (when (contains? options :did-drag)
+                              ((:did-drag options) e (:id data))))}
         (om/build (:item data) (dissoc data :item) {:opts options})))))
 
 (defcomponent sortable [{:keys [items sort container-classes] :as data} owner options]
