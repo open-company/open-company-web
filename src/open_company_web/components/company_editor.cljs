40c6727541163dbc591c53992c95d21866c28c51
Martin Klepsch <martinklepsch@googlemail.com>
2016-02-12 12:23:28 +0100

    small improvements to company editor UI

--- src/open_company_web/components/company_editor.cljs
+++ src/open_company_web/components/company_editor.cljs
@@ -2,19 +2,32 @@
   (:require [om.core :as om :include-macros true]
             [om-tools.core :as om-core :refer-macros (defcomponent)]
             [om-tools.dom :as dom :include-macros true]
+            [open-company-web.components.navbar :refer (navbar)]
             [open-company-web.dispatcher :as dis]
-            [cljs-flux.dispatcher :as flux]
-            [open-company-web.components.ui.cell :refer (cell)]))
+            [open-company-web.lib.utils :as utils]))
 
 (defcomponent company-editor [data owner]
   (render [_]
-    (dom/div {}
-      (dom/input {:type "text"
-                  :placeholder "Company Name"
-                  :value (-> data :company-editor :name)
-                  :on-change #(dis/dispatch! [:input [:company-editor :name] (.. % -target -value)])})
-      (dom/input {:type "text"
-                  :placeholder "Description"
-                  :value (-> data :company-editor :description)
-                  :on-change #(dis/dispatch! [:input [:company-editor :description] (.. % -target -value)])})
-      (dom/button {:on-click #(dis/dispatch! [:company-submit])} "Submit"))))
+    (utils/update-page-title "OPENcompany - Create your company")
+    (dom/div {:class "company-container container"}
+      (om/build navbar data)
+      (dom/div {:class "container-fluid"}
+        (dom/div {:class "col-md-6 col-md-offset-3 main"}
+          (dom/form {:style {:margin-top "3em"}}
+            (dom/div {:class "form-group"}
+              (dom/label "Company Name")
+              (dom/input {:type "text"
+                          :class "form-control"
+                          :placeholder "e.g. Apple"
+                          :value (-> data :company-editor :name)
+                          :on-change #(dis/dispatch! [:input [:company-editor :name] (.. % -target -value)])}))
+            (dom/div {:class "form-group"}
+              (dom/label {:class "col"} "Tagline")
+              (dom/input {:type "text"
+                          :class "form-control"
+                          :placeholder "e.g. Think different."
+                          :value (-> data :company-editor :description)
+                          :on-change #(dis/dispatch! [:input [:company-editor :description] (.. % -target -value)])}))
+            (dom/button {:class "btn btn-primary"
+                         :on-click #(do (.preventDefault %) (dis/dispatch! [:company-submit]))}
+                        "Create Company")))))))
