e872c6ce467519489fdad41ba7d118da0e557b44
Martin Klepsch <martinklepsch@googlemail.com>
2016-02-13 14:59:39 +0100

    dont delay clearing of loading flag

--- src/open_company_web/actions.cljs
+++ src/open_company_web/actions.cljs
@@ -6,9 +6,6 @@
             [open-company-web.caches :as cache]
             [open-company-web.api :as api]))
 
-(defn- delay-remove-loading []
-  (.setTimeout js/window #(dispatch! [:clear-loading]) 100))
-
 ;; ---- Generic Actions Dispatch
 ;; This is a small generic abstraction to handle "actions".
 ;; An `action` is a transformation on the app state.
@@ -53,39 +50,38 @@
 
 (defmethod action :auth-settings [db [_ body]]
   (when body
-    (delay-remove-loading) ; remove loading key
-    ;; add auth-settings data
-    (assoc db :auth-settings body)))
+    (-> db
+        (assoc :auth-settings body)
+        (dissoc :loading))))
 
 (defmethod action :revision [db [_ body]]
   (when body
     (let [fixed-section (utils/fix-section (:body body) (:section body) true)
           assoc-in-coll [(:slug body) (:section body) (:updated-at fixed-section)]]
       (swap! cache/revisions assoc-in assoc-in-coll fixed-section)
-      (delay-remove-loading))) ; remove loading key
+      (dissoc db :loading))) ; remove loading key
   db)
 
 (defmethod action :section [db [_ body]]
   (if body
     (let [fixed-section (utils/fix-section (:body body) (:section body))]
-      (delay-remove-loading) ; remove loading key
-      (assoc-in db [(:slug body) (:section body)] fixed-section))
+      (-> db
+          (assoc-in [(:slug body) (:section body)] fixed-section)
+          (dissoc :loading)))
     db))
 
 (defmethod action :company [db [_ body]]
   (if body
     ;; add section name inside each section
     (let [updated-body (utils/fix-sections body)]
-      (delay-remove-loading) ; remove loading key
-      (assoc db (keyword (:slug updated-body)) updated-body))
+      (-> db
+          (assoc (keyword (:slug updated-body)) updated-body)
+          (dissoc :loading)))
     db))
 
 (defmethod action :companies [db [_ body]]
   (if body
-    (do
-      (delay-remove-loading) ; remove loading key)
-      (assoc db :companies (:companies (:collection body))))
+    (-> db
+     (assoc :companies (:companies (:collection body)))
+     (dissoc :loading))
     db))
-
-(defmethod action :clear-loading [db _]
-  (dissoc db :loading))
