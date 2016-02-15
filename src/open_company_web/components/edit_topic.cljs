95c707e81d907de932f318d5d2e94173125a8095
bago2k4 <iacopo@rc0.eu>
2016-02-15 22:33:59 +0100

    load medium-editor css rules when the component is initialised, setup medium editor from cljsjs package when the component is mounted

--- src/open_company_web/components/edit_topic.cljs
+++ src/open_company_web/components/edit_topic.cljs
@@ -1,12 +1,21 @@
 (ns open-company-web.components.edit-topic
   (:require-macros [cljs.core.async.macros :refer (go)])
-  (:require [cljs.core.async :refer (chan <!)]
+  (:require [cljsjs.medium-editor]
+            [cljs.core.async :refer (chan <!)]
             [om.core :as om :include-macros true]
             [om-tools.core :as om-core :refer-macros [defcomponent]]
             [om-tools.dom :as dom :include-macros true]
             [dommy.core :refer-macros (sel1)]
             [open-company-web.lib.utils :as utils]
-            [open-company-web.api :as api]))
+            [open-company-web.api :as api]
+            [cljs-dynamic-resources.core :as cdr]))
+
+(def editor-options #js {
+  :toolbar #js {
+    :static true
+    :align "left"
+    ; :sticky true
+    :buttons #js ["bold" "italic" "underline" "anchor" "h2" "quote"]}})
 
 (defn add-navbar-channels []
   (let [save-ch (chan)
@@ -18,12 +27,19 @@
   (utils/remove-channel "save-bt-navbar")
   (utils/remove-channel "cancel-bt-navbar"))
 
+(defn setup-medium-editor [owner]
+  (let [editor (new js/MediumEditor "textarea.body-editor" (clj->js editor-options))]
+    (om/set-state! owner :medium-editor editor)))
+
 (defcomponent edit-topic [{:keys [section section-data] :as data} owner options]
 
   (init-state [_]
+    (cdr/add-style! "/lib/medium-editor/css/medium-editor.css")
+    (cdr/add-style! "/lib/medium-editor/css/themes/beagle.css")
     {:title (:title section-data)
      :headline (:headline section-data)
-     :body (:body section-data)})
+     :body (:body section-data)
+     :medium-editor nil})
 
   (will-mount [_]
     ((:navbar-editing-cb options) true "Edit")
@@ -33,6 +49,7 @@
     (remove-navbar-channels))
 
   (did-mount [_]
+    (setup-medium-editor owner)
     (let [save-ch (utils/get-channel "save-bt-navbar")]
       (go (loop []
         (let [change (<! save-ch)]
@@ -59,6 +76,7 @@
                         :value headline
                         :onChange #(om/set-state! owner :headline (.. % -target -value))}))
       (dom/div {:class "edit-topic-body"}
-        (dom/div #js {:ref "topic-body"
-                      :contentEditable true
-                      :dangerouslySetInnerHTML (clj->js {"__html" body})})))))
+        (dom/textarea #js {:ref "topic-body"
+                           :className "body-editor"
+                           :value body
+                           :onChange #(om/set-state! owner :body (.. % -target -value))})))))
