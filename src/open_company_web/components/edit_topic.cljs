35925986eb6a2fae25c7273f02adda950a6b2483
bago2k4 <iacopo@rc0.eu>
2016-02-15 13:14:28 +0100

    improve topic edit component, add cancel and save callback (save does not work just yet)

--- src/open_company_web/components/edit_topic.cljs
+++ src/open_company_web/components/edit_topic.cljs
@@ -1,10 +1,55 @@
 (ns open-company-web.components.edit-topic
-  (:require [om.core :as om :include-macros true]
+  (:require-macros [cljs.core.async.macros :refer (go)])
+  (:require [cljs.core.async :refer (chan <!)]
+            [om.core :as om :include-macros true]
             [om-tools.core :as om-core :refer-macros [defcomponent]]
             [om-tools.dom :as dom :include-macros true]
             [dommy.core :refer-macros (sel1)]
             [open-company-web.lib.utils :as utils]))
 
-(defcomponent edit-topic [data owner options]
+(defn add-navbar-channels []
+  (let [save-ch (chan)
+        cancel-ch (chan)]
+    (utils/add-channel "save-bt-navbar" save-ch)
+    (utils/add-channel "cancel-bt-navbar" cancel-ch)))
+
+(defn remove-navbar-channels []
+  (utils/remove-channel "save-bt-navbar")
+  (utils/remove-channel "cancel-bt-navbar"))
+
+(defcomponent edit-topic [{:keys [section section-data] :as data} owner options]
+
+  (init-state [_]
+    {})
+
+  (will-mount [_]
+    ((:navbar-editing-cb options) true "Edit")
+    (add-navbar-channels))
+
+  (will-unmount [_]
+    (remove-navbar-channels))
+
+  (did-mount [_]
+    (let [save-ch (utils/get-channel "save-bt-navbar")]
+      (go (loop []
+        (let [change (<! save-ch)]
+          (println "save-ch change!!")))))
+    (let [cancel-ch (utils/get-channel "cancel-bt-navbar")]
+      (go (loop []
+        (let [change (<! cancel-ch)]
+          ((:dismiss-topic-editing-cb options)))))))
+
   (render [_]
-    (dom/div {} (str "Editing topic" (:section data)))))
+    (dom/div {:class "edit-topic"}
+      (dom/div {:class "edit-topic-title"}
+        (dom/input #js {:ref "topic-title"
+                        :maxLength 256
+                        :value (:title section-data)}))
+      (dom/div {:class "edit-topic-headline"}
+        (dom/input #js {:ref "topic-headline"
+                        :maxLength 256
+                        :value (:headline section-data)}))
+      (dom/div {:class "edit-topic-body"}
+        (dom/div #js {:ref "topic-body"
+                      :contentEditable true
+                      :dangerouslySetInnerHTML (clj->js {"__html" (:body section-data)})})))))
