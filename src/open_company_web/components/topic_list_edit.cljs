f6b750a09aa0ddcbe846a5e05f74941f488ca1c2
bago2k4 <iacopo@rc0.eu>
2016-02-23 22:05:16 +0100

    use Sortable.js instead of the sortable-list component since it has mobile support

--- src/open_company_web/components/topic_list_edit.cljs
+++ src/open_company_web/components/topic_list_edit.cljs
@@ -12,7 +12,7 @@
             [open-company-web.lib.utils :as utils]
             [open-company-web.components.topic :refer (topic)]
             [open-company-web.components.manage-topic :refer (manage-topic)]
-            [open-company-web.components.ui.sortable-list :refer (sortable-list)]))
+            [cljs-dynamic-resources.core :as cdr]))
 
 (defcomponent item [data owner options]
   (render [_]
@@ -22,20 +22,19 @@
           active (utils/in? active-topics section)
           section-name (or (:name section-data) (utils/camel-case-str section))
           section-title (or (:title section-data) section-name)
-          section-description (or (:description section-data) "")]
+          section-description (or (:description section-data) "")
+          check-img (str "/img/check_" (if active "checked" "empty") ".png?" ls/deploy-key)]
       (dom/div {:class (utils/class-set {:topic-edit true
                                          :group true
                                          :topic-sortable true
-                                         (str "topic-" section-name) true
+                                         (str "topic-" section) true
                                          :active active})
-                :data-sectionname section-name
-                :on-click #((:item-click options) (:id data))
                 :key (str "topic-edit-" section-name)}
         (dom/div {:class "topic-edit-internal group"}
           (dom/div {:class "topic-edit-labels"}
             (dom/h3 {:class "topic-title oc-header"} section-title)
             (dom/label {:class "topic-description"} section-description))
-          (dom/img {:class "check" :src (str "/img/check_" (if active "checked" "empty") ".png?" ls/deploy-key)}))))))
+          (dom/img {:class "check" :src check-img}))))))
 
 (defn get-sections-data [category-sections]
   (apply merge
@@ -66,9 +65,22 @@
         next-ret
         (recur next-ret (next sections))))))
 
+(defn setup-sortable [owner]
+  (when (and (om/get-state owner :did-mount) (om/get-state owner :sortable-loaded))
+    (let [ul-node (sel1 :ul.topic-list-sortable)]
+      (.create js/Sortable ul-node (clj->js {:onEnd #(println "onEnd" %)
+                                             :onSort (fn [_]
+                                                         (let [li-elements (sel [:ul.topic-list-sortable :li])
+                                                               items (map #(.-itemname (.-dataset %)) li-elements)]
+                                                           (om/set-state! owner :sorted-sections items)))})))))
+
 (defcomponent topic-list-edit [data owner options]
 
   (init-state [_]
+    (cdr/add-script! "/lib/Sortable.js/Sortable.js"
+                     (fn []
+                       (om/set-state! owner :sortable-loaded true)
+                       (setup-sortable owner)))
     (let [save-ch (chan)
           cancel-ch (chan)]
       (utils/add-channel "save-bt-navbar" save-ch)
@@ -82,9 +94,13 @@
           sections-list (vec (map :section-name category-sections))
           cleaned-sections (ordered-sections active-topics sections-list)]
       {:active-topics active-topics
-       :sorted-sections cleaned-sections}))
+       :sorted-sections cleaned-sections
+       :sortable-loaded false
+       :did-mount false}))
 
   (did-mount [_]
+    (om/set-state! owner :did-mount true)
+    (setup-sortable owner)
     (let [save-ch (utils/get-channel "save-bt-navbar")]
       (go (loop []
         (let [change (<! save-ch)]
@@ -96,27 +112,37 @@
         (let [change (<! cancel-ch)]
           ((:cancel-editing-cb options)))))))
 
-  (render [_]
+  (render-state [_ {:keys [sorted-sections active-topics]}]
+    (println "topic-list-edit render" (om/get-state owner :sorted-sections))
     (if (empty? @caches/new-sections)
       (dom/h2 {} "Loading sections...")
       (let [active-category (:active-category options)
             all-sections (:new-sections options)
             category-sections (:sections (first (filter #(= (:name %) active-category) (:categories all-sections))))
-            active-topics (om/get-state owner :active-topics)]
+            items (get-sections-data category-sections)]
         (dom/div {:class "topic-list-edit fix-top-margin-scrolling group no-select"}
-          (om/build sortable-list
-                    {:sort (om/get-state owner :sorted-sections)
-                     :items (get-sections-data category-sections)
-                     :item item
-                     :to-item {
-                       :active-topics active-topics}}
-                    {:opts
-                     (merge options {:height 68
-                                     :did-change-sort
-                                     (fn [items]
-                                       (om/set-state! owner :sorted-sections items))
-                                     :item-click
-                                     (fn [section]
-                                       (if (utils/in? active-topics section)
-                                         (om/set-state! owner :active-topics (utils/vec-dissoc active-topics (name section)))
-                                         (om/set-state! owner :active-topics (concat active-topics [(name section)]))))})}))))))
+          (apply dom/ul
+                 ; props
+                 {:class "topic-list-sortable"
+                  :key "topic-list-edit"}
+                 ; childrens
+                 (map (fn [item-name]
+                        (dom/li {:data-itemname item-name
+                                 :key (str item-name (rand 5))
+                                 :on-click (fn []
+                                             (let [new-active-topics (if (utils/in? active-topics item-name)
+                                                                       (utils/vec-dissoc active-topics item-name)
+                                                                       (concat active-topics [item-name]))]
+                                               (om/set-state! owner :active-topics new-active-topics)))}
+                          (om/build item {:id item-name
+                                          :item-data (get items (keyword item-name))
+                                          :active-topics active-topics}))) sorted-sections)))))))
+
+
+
+
+
+
+
+
+
