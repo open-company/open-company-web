f57ba18ef901060cd0c063460738bfa751072442
bago2k4 <iacopo@rc0.eu>
2016-02-25 23:16:21 +0100

    prevent react from using the same container for editing state and not, add listeners for save and cancel (not working yet)

--- src/open_company_web/components/topic_list.cljs
+++ src/open_company_web/components/topic_list.cljs
@@ -1,5 +1,7 @@
 (ns open-company-web.components.topic-list
-  (:require [om.core :as om :include-macros true]
+  (:require-macros [cljs.core.async.macros :refer (go)])
+  (:require [cljs.core.async :refer (chan <!)]
+            [om.core :as om :include-macros true]
             [om-tools.core :as om-core :refer-macros [defcomponent]]
             [om-tools.dom :as dom :include-macros true]
             [open-company-web.router :as router]
@@ -41,12 +43,27 @@
 (defcomponent topic-list [data owner options]
 
   (init-state [_]
+    (let [save-ch (chan)
+          cancel-ch (chan)]
+      (utils/add-channel "save-bt-navbar" save-ch)
+      (utils/add-channel "cancel-bt-navbar" cancel-ch))
     {:editing false
-     :new-sections-requested false})
+     :new-sections-requested false
+     :save-bt-active false})
 
   (did-mount [_]
     (when-not (:read-only (:company-data data))
-      (get-new-sections-if-needed owner)))
+      (get-new-sections-if-needed owner))
+    ; save all the changes....
+    (let [save-ch (utils/get-channel "save-bt-navbar")]
+      (go (loop []
+        (let [change (<! save-ch)]
+          (let [active-topics (om/get-state owner :active-topics)]
+            ((:save-sections-cb options) active-topics))))))
+    (let [cancel-ch (utils/get-channel "cancel-bt-navbar")]
+      (go (loop []
+        (let [change (<! cancel-ch)]
+          ((:cancel-editing-cb options)))))))
 
   (did-update [_ _ _]
     (when-not (:read-only (:company-data data))
@@ -57,21 +74,25 @@
           editing (om/get-state owner :editing)]
       (if editing
         (let [categories (vec (map :name (:categories (slug @caches/new-sections))))]
-          (dom/div {:class "topic-list-edit-container"}
+          (dom/div {:class "topic-list-edit-container"
+                    :key "topic-list-edit-container"}
             (for [cat categories]
               (om/build topic-list-edit (assoc data :active (= cat (:active-category data)))
                         {:key cat
                          :opts {:new-sections (slug @caches/new-sections)
                                 :active-category cat
                                 :save-sections-cb (partial save-sections-cb owner data options)
-                                :save-bt-active-cb (:save-bt-active-cb options)
+                                :save-bt-active-cb (fn [active]
+                                                     (om/set-state! owner :save-bt-active (or (om/get-state owner :save-bt-active) active))
+                                                     ((:save-bt-active-cb options) (om/get-state owner :save-bt-active)))
                                 :cancel-editing-cb (fn []
                                                      (om/set-state! owner :editing false)
                                                      ((:navbar-editing-cb options) false))}}))))
         (let [company-data (:company-data data)
               active-category (keyword (:active-category data))
               active-sections (get-in company-data [:sections active-category])]
-          (dom/div {:class "topic-list fix-top-margin-scrolling"}
+          (dom/div {:class "topic-list fix-top-margin-scrolling"
+                    :key "topic-list"}
             (dom/div {:class "topic-list-internal"}
               (for [section-name active-sections
                     :let [sd (->> section-name keyword (get company-data))]]
