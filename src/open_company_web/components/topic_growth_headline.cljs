29871d16d401524506766227d212c5fe8ec081b6
bago2k4 <iacopo@rc0.eu>
2016-03-11 14:16:38 +0100

    growth headline component with pillboxes

---
+++ src/open_company_web/components/topic_growth_headline.cljs
@@ -0,0 +1,17 @@
+(ns open-company-web.components.topic-growth-headline
+  (:require [om.core :as om :include-macros true]
+            [om-tools.core :as om-core :refer-macros [defcomponent]]
+            [om-tools.dom :as dom :include-macros true]
+            [dommy.core :refer-macros (sel1)]
+            [open-company-web.lib.utils :as utils]))
+
+(defcomponent topic-growth-headline [data owner options]
+  (render [_]
+    (let [metrics (:metrics data)]
+      (dom/div {:class (utils/class-set {:topic-headline-inner true
+                                         :topic-growth-headline true
+                                         :group true
+                                         :collapse (:expanded data)})}
+        (for [metric metrics]
+          (dom/label {:class "pillbox"} (:name metric)))
+        (dom/div {:class "topic-headline-inner"} (:headline data))))))
