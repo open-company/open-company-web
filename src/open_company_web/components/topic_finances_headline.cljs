d742c894a68a78e898de3d831cea06be49b9a29f
bago2k4 <iacopo@rc0.eu>
2016-03-15 15:31:53 +0100

    do not show positive runway, fix burn label and value depending on revenues and costs

--- src/open_company_web/components/topic_finances_headline.cljs
+++ src/open_company_web/components/topic_finances_headline.cljs
@@ -10,9 +10,18 @@
   (render [_]
     (let [sort-pred (utils/sort-by-key-pred :period true)
           finances-data (sort sort-pred (:data data))
+          all-revenues (apply + (map :revenue finances-data))
           actual (last finances-data)
           currency (utils/get-symbol-for-currency-code (:currency options))
-          runway (finances-utils/fix-runway (:runway actual))]
+          runway (finances-utils/fix-runway (:runway actual))
+          burn-label (if (pos? all-revenues)
+                        (if (pos? (:burn-rate actual))
+                          "Cash Flow"
+                          "Burn Rate")
+                        "Costs")
+          burn-value (utils/with-metric-prefix (if (pos? all-revenues)
+                                                (utils/abs (:burn-rate actual))
+                                                (:costs actual)))]
       (dom/div {:class (utils/class-set {:topic-headline-inner true
                                          :topic-finances-headline true
                                          :group true
@@ -22,9 +31,10 @@
             (dom/div {:class "label"} "Cash")
             (dom/div {:class "value"} (str currency (utils/with-metric-prefix (:cash actual)))))
           (dom/div {:class "finances-metric burn-rate"}
-            (dom/div {:class "label"} "Burn")
-            (dom/div {:class "value"} (str currency (utils/with-metric-prefix (utils/abs (:burn-rate actual))))))
-          (dom/div {:class "finances-metric runway"}
-            (dom/div {:class "label"} "Runway")
-            (dom/div {:class "value"} (finances-utils/get-rounded-runway runway [:round :remove-trailing-zero]))))
+            (dom/div {:class "label"} burn-label)
+            (dom/div {:class "value"} (str currency burn-value)))
+          (when-not (zero? runway)
+            (dom/div {:class "finances-metric runway"}
+              (dom/div {:class "label"} "Runway")
+              (dom/div {:class "value"} (finances-utils/get-rounded-runway runway [:round :remove-trailing-zero])))))
         (dom/div {:class "topic-headline-inner"} (:headline data))))))
