b41aa73e015a4c3861b5c34bbcd18a20f34b1b48
bago2k4 <iacopo@rc0.eu>
2016-03-15 14:49:26 +0100

    add test for metric prefix funciton

---
+++ test/open_company_web/lib/utils.cljs
@@ -0,0 +1,23 @@
+(ns test.open-company-web.lib.utils
+  (:require [cljs.test :refer-macros [deftest async testing is]]
+            [open-company-web.lib.utils :as utils]))
+
+(def values {
+  "1"      1.0012345
+  "1.1"    1.1012345
+  "10"     10.001234
+  "10.1"   10.101234
+  "100"    100.00123
+  "100.1"  100.10123
+  "1k"     1001.1234
+  "1.1k"   1101.1234
+  "1M"     1001234
+  "1.1M"   1101234
+  "10M"    10001234
+  "10.1M"  10112345
+})
+
+(deftest test-utils
+  (testing "Metric prefix functions"
+    (doseq [v (vals values)]
+      (is v (get values (utils/with-metric-prefix v))))))
