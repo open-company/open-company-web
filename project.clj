9c48d1739f837b0a60121096b39ac850303c5d27
bago2k4 <iacopo@rc0.eu>
2016-03-16 17:23:37 +0100

    add d3 as cljsjs dependency with externs

--- project.clj
+++ project.clj
@@ -28,6 +28,7 @@
     [funcool/cuerdas "0.7.1"] ; String manipulation library for Clojure(Script) https://github.com/funcool/cuerdas
     [cljsjs/react "0.14.3-0"] ; A Javascript library for building user interfaces https://github.com/cljsjs/packages
     [medley "0.7.3"] ; lightweight library of useful, mostly pure functions that are "missing" from clojure.core
+    [cljsjs/d3 "3.5.7-1"] ; d3 externs https://clojars.org/cljsjs/d3
   ]
 
   :plugins [
