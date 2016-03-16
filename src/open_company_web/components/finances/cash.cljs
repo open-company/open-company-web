315492b7a300c799a347cd1c7e68b12b1d2e12e6
bago2k4 <iacopo@rc0.eu>
2016-03-16 16:58:55 +0100

    use d3 chart for cash

--- src/open_company_web/components/finances/cash.cljs
+++ src/open_company_web/components/finances/cash.cljs
@@ -4,8 +4,12 @@
             [om-tools.dom :as dom :include-macros true]
             [open-company-web.lib.utils :as utils]
             [open-company-web.components.ui.charts :refer (column-chart)]
+            [open-company-web.components.ui.d3-charts :refer (d3-column-chart)]
             [open-company-web.components.finances.utils :as finances-utils]
             [open-company-web.lib.oc-colors :as occ]))
+
+(defn- get-d3-chart-data [sorted-data]
+  {:chart-data (vec (map #(hash-map :cash (:cash %) :period (:period %)) sorted-data))})
 
 (defcomponent cash [data owner options]
   
@@ -20,7 +24,10 @@
           chart-opts (when (contains? options :chart-size)
                         {:opts {:chart-height (:height (:chart-size options))
                                 :chart-width (:width (:chart-size options))
-                                :chart-color (occ/get-color-by-kw :oc-green-regular)}})]
+                                :chart-keys [:cash]
+                                :chart-color (occ/get-color-by-kw :oc-green-light)
+                                :chart-selected-color (occ/get-color-by-kw :oc-green-regular)
+                                :prefix (utils/get-symbol-for-currency-code currency)}})]
       (dom/div {:class (utils/class-set {:section true
                                          :cash true
                                          :read-only (:read-only data)})
@@ -31,10 +38,4 @@
               (dom/div {:class "actual-container"}
                 (dom/h3 {:class "actual green"} cash-val)
                 (dom/h3 {:class "actual-label gray"} (str "as of " (finances-utils/get-as-of-string (:period value-set))))))))
-        (om/build column-chart (finances-utils/get-chart-data sorted-finances
-                                                              cur-symbol
-                                                              :cash
-                                                              "Cash"
-                                                              #js {"type" "string" "role" "style"}
-                                                              (occ/fill-color :oc-green-light))
-                               chart-opts)))))
+        (om/build d3-column-chart (get-d3-chart-data sorted-finances) chart-opts)))))
