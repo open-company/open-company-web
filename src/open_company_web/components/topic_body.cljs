acc8872a945ef40dc3a94d692347aef615efd6d1
bago2k4 <iacopo@rc0.eu>
2016-03-17 23:07:55 +0100

    fix chart height on mobile, it was too small

--- src/open_company_web/components/topic_body.cljs
+++ src/open_company_web/components/topic_body.cljs
@@ -32,7 +32,7 @@
                                   :read-only true}
                                  {:opts {:show-title false
                                          :show-revisions-navigation false
-                                         :chart-size {:height (if (utils/is-mobile) 150 290)
+                                         :chart-size {:height (if (utils/is-mobile) 200 290)
                                                       :width (if (utils/is-mobile) 320 650)}}})
 
           (= section-kw :finances)
@@ -44,7 +44,7 @@
                                     :read-only true}
                                    {:opts {:show-title false
                                            :show-revisions-navigation false
-                                           :chart-size {:height (if (utils/is-mobile) 90 290)
+                                           :chart-size {:height (if (utils/is-mobile) 200 290)
                                                         :width (if (utils/is-mobile) 320 480)}}}))
         (dom/div #js {:className "topic-body-inner group"
                       :dangerouslySetInnerHTML (clj->js {"__html" (str section-body "<p style='height:1px;margin-top:0px;padding-top:0px;'> </p>")})})))))
