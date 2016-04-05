4c0c3841381a3a97309fd82630e95325fbf66866
bago2k4 <iacopo@rc0.eu>
2016-04-05 14:40:08 +0200

    make sure to return the width

--- resources/public/lib/js-utils/svg-utils.js
+++ resources/public/lib/js-utils/svg-utils.js
@@ -1,7 +1,6 @@
 function SVGgetWidth(el) {
-  console.log(el, typeof el, el.node().getBBox, el.node().bounds);
   if (typeof el !== "undefined"){
-    el.node().getBBox().width;
+    return el.node().getBBox().width;
   }else{
     return null;
   }
