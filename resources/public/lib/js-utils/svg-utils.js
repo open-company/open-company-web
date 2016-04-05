6124607c7667e460225e2ac87351cea965201d35
bago2k4 <iacopo@rc0.eu>
2016-04-05 14:28:25 +0200

    add .node call before getBBox

--- resources/public/lib/js-utils/svg-utils.js
+++ resources/public/lib/js-utils/svg-utils.js
@@ -1,7 +1,7 @@
 function SVGgetWidth(el) {
-  console.log(el, typeof el);
+  console.log(el, typeof el, el.getBBox);
   if (typeof el !== "undefined"){
-    el.getBBox().width;
+    el.node().getBBox().width;
   }else{
     return null;
   }
