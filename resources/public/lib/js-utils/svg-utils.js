cb2e432326d1e90ad23a61b0c822ae5e741a23a8
bago2k4 <iacopo@rc0.eu>
2016-04-05 14:28:55 +0200

    test for bounds functino

--- resources/public/lib/js-utils/svg-utils.js
+++ resources/public/lib/js-utils/svg-utils.js
@@ -1,5 +1,5 @@
 function SVGgetWidth(el) {
-  console.log(el, typeof el, el.getBBox);
+  console.log(el, typeof el, el.node().getBBox, el.node().bounds);
   if (typeof el !== "undefined"){
     el.node().getBBox().width;
   }else{
