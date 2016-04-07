b8b47baa0053fb26ff2343e7c58370981de5f901
Martin Klepsch <martinklepsch@googlemail.com>
2016-04-07 17:31:01 +0200

    remove class that doesnt exist

--- src/open_company_web/components/topic_list.cljs
+++ src/open_company_web/components/topic_list.cljs
@@ -175,7 +175,7 @@
         (let [company-data (:company-data data)
               active-category (keyword (:active-category data))
               category-topics (get active-topics active-category)]
-          (dom/div {:class "topic-list fix-top-margin-scrolling"
+          (dom/div {:class "topic-list"
                     :key "topic-list"}
             (when selected-topic
               (om/build topic-overlay {:section selected-topic
