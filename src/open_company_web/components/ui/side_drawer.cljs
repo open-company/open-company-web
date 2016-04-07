6e40895369a293166ad89f9be33b2838c596ee68
bago2k4 <iacopo@rc0.eu>
2016-04-07 14:46:23 +0200

    simplify the side-drawer component, make all the data and options to pass to the subcomponent transparent to it

--- src/open_company_web/components/ui/side_drawer.cljs
+++ src/open_company_web/components/ui/side_drawer.cljs
@@ -8,14 +8,6 @@
             [open-company-web.lib.utils :as utils]
             [open-company-web.components.topic-list-edit :refer (topic-list-edit)]))
 
-(defn get-active-topics [company-data category]
-  (if (= category "all")
-    (apply concat (vals (:sections company-data)))
-    (get-in company-data [:sections (keyword category)])))
-
-(defn open-value [open]
-  (if open "open" "close"))
-
 (defcomponent side-drawer [data owner options]
 
   (init-state [_]
@@ -23,25 +15,13 @@
 
   (will-receive-props [_ next-props]
     (when-not (= (:open next-props) (:open data))
-      (om/set-state! owner :open (open-value (:open next-props)))))
+      (om/set-state! owner :open (if (:open next-props) "open" "close")))))
 
   (render-state [_ {:keys [open]}]
-    (let [cat "company"
-          slug (keyword (:slug @router/path))
-          company-data (:company-data data)
-          categories (:categories company-data)
-          active-topics (apply merge (map #(hash-map (keyword %) (get-active-topics company-data %)) categories))]
-      (dom/div {:class (utils/class-set {:side-drawer true
-                                         :group true
-                                         :open-drawer (= open "open")
-                                         :close-drawer (= open "close")})}
-        (dom/div {:class "side-drawer-internal"}
-          (when (and open (slug @caches/new-sections))
-            (om/build topic-list-edit
-                      (merge data {:active true
-                                   :category cat
-                                   :new-sections (slug @caches/new-sections)
-                                   :active-topics (get (:active-topics data) (keyword cat))})
-                      {:key cat
-                       :opts {:active-category (:active-category data)
-                              :did-change-sort #()}}))))))) ; (partial update-active-topics owner options (keyword cat))
+    (dom/div {:class (utils/class-set {:side-drawer true
+                                       :group true
+                                       :open-drawer (= open "open")
+                                       :close-drawer (= open "close")})}
+      (dom/div {:class "side-drawer-internal"}
+        (om/build topic-list-edit (:list-data data) {:key (:list-key data)
+                                                     :opts (:list-opts options)})))))
