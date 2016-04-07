ff5c45f7c3778ce2e5844c577c586798e8dc96f6
Martin Klepsch <martinklepsch@googlemail.com>
2016-04-07 16:31:58 +0200

    ensure height is reset before measuring

--- src/open_company_web/components/topic_list.cljs
+++ src/open_company_web/components/topic_list.cljs
@@ -102,10 +102,12 @@
 (def scrolled-to-top (atom false))
 
 (defn set-lis-height [owner]
-  (doall
-    (let [li-elems (sel (om/get-ref owner "topic-list-ul") [:li.topic-row])
-          max-height (apply max (map #(.-clientHeight %) li-elems))]
-      (for [li li-elems]
+  (let [lis (sel (om/get-ref owner "topic-list-ul") [:li.topic-row])]
+    (doseq [li lis]
+      (.removeAttribute li "style"))
+    (let [max-height (apply max (map #(.-clientHeight %) lis))]
+      (doseq [li lis]
+          (js/console.log "set-lis-height" li)
         (setStyle li #js {:height (str max-height "px")})))))
 
 (defn close-overlay-cb [owner]
