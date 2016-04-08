0127a86c9566827ad70d29d335216d11d237f03c
bago2k4 <iacopo@rc0.eu>
2016-04-08 15:59:29 +0200

    let only the active topics be draggable, remove the drag handle from the unactive topics, call the right callback when active topics changes

--- src/open_company_web/components/topic_list_edit.cljs
+++ src/open_company_web/components/topic_list_edit.cljs
@@ -31,7 +31,9 @@
           (dom/div {:class "right"
                     :style {:margin "13px 10px 0 0"}}
              (i/icon :check-square-09 {:accent-color (when-not active "transparent")}))
-          (dom/div {:class "topic-edit-handle group"})
+          (dom/div {:class (utils/class-set {:topic-edit-handle-placeholder (not active)
+                                             :topic-edit-handle active
+                                             :group true})})
           (dom/div {:class "topic-edit-labels"}
             (dom/h3 {:class "topic-title oc-header"} section-title)
             (dom/label {:class "topic-description"} section-description)))))))
@@ -46,17 +48,14 @@
 (defn setup-sortable [owner options]
   (when (and (om/get-state owner :did-mount) (om/get-state owner :sortable-loaded))
     (when-let [ul-node (om/get-ref owner "topic-list-sortable")]
-      (.create js/Sortable ul-node (clj->js #js {:handle ".topic-edit-handle"
-                                                 :onSort (fn [_]
-                                                           (let [li-active-elements (sel ul-node [:li.topic-active])
-                                                                 active-items (vec (map #(aget (.-dataset %) "itemname") li-active-elements))
-                                                                 li-unactive-elements (sel ul-node [:li.topic-unactive])
-                                                                 unactive-items (vec (map #(aget (.-dataset %) "itemname") li-unactive-elements))]
-                                                             (om/set-state! owner :active-topics active-items)
-                                                             (om/set-state! owner :unactive-topics unactive-items)
-                                                             ((:did-change-sort options) active-items)))})))))
+      (.create js/Sortable ul-node #js {:handle ".topic-edit-handle"
+                                        :onEnd (fn [_]
+                                                 (let [li-active-elements (sel ul-node [:li.topic-active])
+                                                       active-items (vec (map #(aget (.-dataset %) "itemname") li-active-elements))]
+                                                   (om/set-state! owner :active-topics active-items)
+                                                   ((:did-change-active-topics options) active-items)))}))))
 
-(defn topic-on-click [item-name owner did-change-sort]
+(defn topic-on-click [item-name owner did-change-active-topics]
   (let [active-topics (om/get-state owner :active-topics)
         unactive-topics (om/get-state owner :unactive-topics)
         is-active (utils/in? active-topics item-name)
@@ -68,7 +67,7 @@
                               (utils/vec-dissoc unactive-topics item-name))]
     (om/set-state! owner :active-topics (vec new-active-topics))
     (om/set-state! owner :unactive-topics (vec new-unactive-topics))
-    (did-change-sort new-active-topics)))
+    (did-change-active-topics new-active-topics)))
 
 (defn get-state [data]
   (let [active-topics (:active-topics data)
@@ -125,20 +124,21 @@
                            :key (apply str active-topics)}
                 (for [item-name active-topics]
                   (dom/li #js {:data-itemname item-name
-                               :className (utils/class-set {:topic-list-edit-li true
-                                                            :topic-active true
-                                                            :last-active-topic (= item-name (last active-topics))})
+                               :className "topic-list-edit-li topic-active"
                                :key (str "active-" item-name)
-                               :onClick #(topic-on-click item-name owner (:did-change-sort options))}
+                               :onClick #(topic-on-click item-name owner (:did-change-active-topics options))}
                     (om/build item {:id item-name
                                     :item-data (get items (keyword item-name))
-                                    :active-topics active-topics})))
+                                    :active-topics active-topics})))))
+            (dom/div {:class "topic-list-separator"})
+            (dom/div {}
+              (dom/ul #js {:className "topic-list-unactive"
+                           :key (apply str active-topics)}
                 (for [item-name unactive-topics]
                   (dom/li #js {:data-itemname item-name
                                :key (str "unactive-" item-name)
-                               :className (utils/class-set {:topic-list-edit-li true
-                                                            :topic-unactive true})
-                               :onClick #(topic-on-click item-name owner (:did-change-sort options))}
+                               :className "topic-list-edit-li topic-unactive"
+                               :onClick #(topic-on-click item-name owner (:did-change-active-topics options))}
                     (om/build item {:id item-name
                                     :item-data (get items (keyword item-name))
                                     :active-topics active-topics})))))))))))
