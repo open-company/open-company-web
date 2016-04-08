ea6c611aac57c7a99733426ae2ea79e78cb6099b
bago2k4 <iacopo@rc0.eu>
2016-04-08 22:14:57 +0200

    handle the close when the user click on the under div

--- src/open_company_web/components/ui/drawer_toggler.cljs
+++ src/open_company_web/components/ui/drawer_toggler.cljs
@@ -5,8 +5,9 @@
             [dommy.core :as dommy]
             [open-company-web.components.icon :refer (icon)]))
 
-(defn rotate [owner options]
-  (when (contains? options :click-cb)
+(defn rotate [owner options & [is-extern]]
+  (when (and (not is-extern)
+             (contains? options :click-cb))
     ((:click-cb options)))
   (when-let [drawer (om/get-ref owner "drawer-toggler")]
     (if (dommy/has-class? drawer :rotate45)
@@ -18,8 +19,14 @@
        (dommy/remove-class! drawer :rotate0)))))
 
 (defcomponent drawer-toggler [data owner options]
+
+  (will-receive-props [_ next-props]
+    (when (and (not= (:close data) (:close next-props))
+               (:close next-props))
+      (rotate owner options true)))
+
   (render [_]
     (dom/div #js {:className "drawer-toggler"
                   :ref "drawer-toggler"}
       (dom/button #js {:className "drawer-toggler-bt"
-                       :onClick #(rotate owner options)} (icon "circle-remove" {:size 24})))))
+                       :onClick #(rotate owner options)} (icon :circle-remove {:size 24})))))
