b72fbff3972e5dfb037f623ab8d61d86244a7940
bago2k4 <iacopo@rc0.eu>
2016-04-19 00:23:10 +0200

    move X button and side drawer to the right

--- src/open_company_web/local_settings.cljs
+++ src/open_company_web/local_settings.cljs
@@ -1,7 +1,8 @@
 (ns open-company-web.local-settings)
 
 ;; Sentry
-(def local-dsn nil) ; insert your Sentry public dsn here
+(def local-dsn "https://747713ae92c246d1a64bbce9aab3da34@app.getsentry.com/73174
+") ; insert your Sentry public dsn here
 (def local-whitelist-array ["locahost" "127.0.0.1"])
 
 ;; API
@@ -15,4 +16,4 @@
 (def jwt-cookie-secure false)
 
 ;; Deploy key
-(def deploy-key "")
+(def deploy-key "asd")
