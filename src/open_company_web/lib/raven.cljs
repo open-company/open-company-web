d23b117d079c66a6fe7937488730d85199aa700b
bago2k4 <iacopo@rc0.eu>
2016-04-18 16:42:13 +0200

    add isMobile tag to the sentry configuration

--- src/open_company_web/lib/raven.cljs
+++ src/open_company_web/lib/raven.cljs
@@ -1,13 +1,16 @@
 (ns open-company-web.lib.raven
   (:require [open-company-web.local-settings :as ls]
+            [open-company-web.lib.utils :as utils]
             [cljsjs.raven]))
 
-(def ravenParameter #js {:whitelistUrls ls/local-whitelist-array
-                         :release ls/deploy-key})
+(defn ravenParameters []
+  #js {:whitelistUrls ls/local-whitelist-array
+       :tags #js {:isMobile (utils/is-mobile)}
+       :release ls/deploy-key})
 
 (defn raven-setup []
   (when (and (exists? js/Raven) ls/local-dsn)
-    (.. js/Raven (config ls/local-dsn ravenParameter) install)))
+    (.. js/Raven (config ls/local-dsn (ravenParameters)) install)))
 
 (defn test-raven []
   (js/setTimeout #(.captureMessage js/Raven "Message from clojure" 1000))
