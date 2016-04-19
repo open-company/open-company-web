8eb193e26e7fe623811cdb7b4cc8da597da88d79
bago2k4 <iacopo@rc0.eu>
2016-04-19 23:21:43 +0200

    import dispatcher, add su-list with the proper key in the app-state

--- src/open_company_web/actions.cljs
+++ src/open_company_web/actions.cljs
@@ -1,7 +1,7 @@
 (ns open-company-web.actions
   (:require [medley.core :as med]
             [clojure.string :as string]
-            [open-company-web.dispatcher :refer [action dispatch!]]
+            [open-company-web.dispatcher :as dispatcher]
             [open-company-web.lib.utils :as utils]
             [open-company-web.router :as router]
             [open-company-web.caches :as cache]
@@ -15,15 +15,15 @@
 ;; The extended multimethod `action` is defined in the dispatcher
 ;; namespace to avoid cyclical dependencies between namespaces
 
-(defmethod action :default [db payload]
+(defmethod dispatcher/action :default [db payload]
   (js/console.warn "No handler defined for" (str (first payload)))
   (js/console.log "Full event: " (pr-str payload))
   db)
 
-(defmethod action :input [db [_ path value]]
+(defmethod dispatcher/action :input [db [_ path value]]
   (assoc-in db path value))
 
-(defmethod action :entry [db [_ {:keys [links]}]]
+(defmethod dispatcher/action :entry [db [_ {:keys [links]}]]
   (let [create-link    (med/find-first #(= (:rel %) "company-create") links)
         slug           (fn [co] (last (string/split (:href co) #"/")))
         [first second] (filter #(= (:rel %) "company") links)]
@@ -33,18 +33,18 @@
       create-link              (router/nav! "/create-company")))
   db)
 
-(defmethod action :company-submit [db _]
+(defmethod dispatcher/action :company-submit [db _]
   (api/post-company (:company-editor db))
   db)
 
-(defmethod action :company-created [db [_ body]]
+(defmethod dispatcher/action :company-created [db [_ body]]
   (if (:links body)
     (let [updated (utils/fix-sections body)]
       (router/nav! (str "/" (:slug updated)))
       (assoc db (keyword (:slug updated)) updated))
     db))
 
-(defmethod action :new-section [db [_ body]]
+(defmethod dispatcher/action :new-section [db [_ body]]
   (when body
     (let [slug (:slug body)
           response (:response body)]
@@ -52,13 +52,13 @@
       ;; signal to the app-state that the new-sections have been loaded
       (assoc-in db [(keyword slug) :new-sections] (rand 4)))))
 
-(defmethod action :auth-settings [db [_ body]]
+(defmethod dispatcher/action :auth-settings [db [_ body]]
   (when body
     (-> db
         (assoc :auth-settings body)
         (dissoc :loading))))
 
-(defmethod action :revision [db [_ body]]
+(defmethod dispatcher/action :revision [db [_ body]]
   (when body
     (let [fixed-section (utils/fix-section (:body body) (:section body) true)
           assoc-in-coll [(:slug body) (:section body) (:updated-at fixed-section)]]
@@ -66,7 +66,7 @@
       (dissoc db :loading))) ; remove loading key
   db)
 
-(defmethod action :section [db [_ body]]
+(defmethod dispatcher/action :section [db [_ body]]
   (if body
     (let [fixed-section (utils/fix-section (:body body) (:section body))]
       (-> db
@@ -74,7 +74,7 @@
           (dissoc :loading)))
     db))
 
-(defmethod action :company [db [_ {:keys [slug success status body]}]]
+(defmethod dispatcher/action :company [db [_ {:keys [slug success status body]}]]
   (cond
     success
     ;; add section name inside each section
@@ -87,24 +87,25 @@
     ;; probably some default failure handling should be added here
     :else db))
 
-(defmethod action :companies [db [_ body]]
+(defmethod dispatcher/action :companies [db [_ body]]
   (if body
     (-> db
      (assoc :companies (:companies (:collection body)))
      (dissoc :loading))
     db))
 
-(defmethod action :topic/toggle-expand [db [_ topic-kw]]
+(defmethod dispatcher/action :topic/toggle-expand [db [_ topic-kw]]
   (let [update-fn (fn [expanded? to-toggle]
                     (if (expanded? to-toggle)
                       (disj expanded? to-toggle)
                       (conj expanded? to-toggle)))]
     (update db :expanded-topics (fnil update-fn #{}) topic-kw)))
 
-(defmethod action :topic/reset-expanded [db _]
+(defmethod dispatcher/action :topic/reset-expanded [db _]
   (assoc db :expanded-topics #{}))
 
-(defmethod action :stakeholder-update [db [_ {:keys [slug response]}]]
+(defmethod dispatcher/action :stakeholder-update [db [_ {:keys [slug response]}]]
   (-> db
-    (assoc-in [(keyword slug) :su-list] response)
+    (assoc (dispatcher/su-list-key slug) response)
+    (assoc-in [(keyword slug) :su-list-loaded] true)
     (dissoc :loading)))
