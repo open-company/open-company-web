4af8ad5db4c7e6e81bad5c86aa42154f94c7ce9e
bago2k4 <iacopo@rc0.eu>
2016-04-19 22:21:38 +0200

    su-list component and style

---
+++ src/open_company_web/components/su_list.cljs
@@ -0,0 +1,104 @@
+(ns open-company-web.components.su-list
+  (:require [om.core :as om :include-macros true]
+            [om-tools.core :as om-core :refer-macros (defcomponent)]
+            [om-tools.dom :as dom :include-macros true]
+            [dommy.core :refer-macros (sel1)]
+            [open-company-web.router :as router]
+            [open-company-web.local-settings :as ls]
+            [open-company-web.api :as api]
+            [open-company-web.caches :as caches]
+            [open-company-web.lib.utils :as utils]
+            [open-company-web.components.navbar :refer (navbar)]
+            [open-company-web.components.topic-body :refer (topic-body)]
+            [open-company-web.components.company-header :refer [company-header]]
+            [open-company-web.components.section-selector :refer (section-selector)]
+            [open-company-web.components.ui.link :refer (link)]
+            [open-company-web.components.ui.side-drawer :refer (side-drawer)]
+            [open-company-web.components.ui.drawer-toggler :refer (drawer-toggler)]
+            [clojure.string :as str]
+            [goog.style :refer (setStyle)]))
+
+(defcomponent su-update [data owner]
+  (render [_]
+    (let [update (:update data)
+          js-date-upat (utils/js-date (:updated-at update))
+          month-string (utils/month-string-int (inc (.getMonth js-date-upat)))
+          topic-updated-at (str month-string " " (.getDate js-date-upat) ", " (.getFullYear js-date-upat))
+          intro (:intro update)]
+      (dom/div {:class "su-update"}
+        (dom/div {:class "su-title"} (:title update))
+        (dom/div {:class "su-date"} topic-updated-at)
+        (dom/div {:class "su-body"} (:body intro))))))
+
+(defcomponent stakeholder-updates [data owner]
+
+  (init-state [_] {
+    :drawer-open false})
+
+  (render-state [_ {:keys [drawer-open]}]
+    (let [su-updates (:stakeholder-updates (:collection (:su-list (:company-data data))))]
+      (dom/div {:class "updates"}
+
+        (dom/div {:class "updates-internal"}
+          (for [update su-updates]
+            (om/build su-update {:update update})))))))
+
+(defcomponent su-list [data owner]
+
+  (init-state [_]
+    {:drawer-open false
+     :toggler-top-margin false
+     :su-requested false})
+
+  (did-update [_ _ _]
+    ;; request the live SU if necessary
+    (let [slug (keyword (:slug @router/path))
+          company-data (get data slug)]
+      (when (and (not (om/get-state owner :su-requested)) (contains? company-data :links))
+        (api/get-stakeholder-update)
+        (om/set-state! owner :su-requested true))))
+
+  (render-state [_ {:keys [drawer-open]}]
+    (let [slug (keyword (:slug @router/path))
+          company-data (get data slug)
+          stakeholder-update-data (:stakeholder-update company-data)]
+
+      (utils/update-page-title (str "OpenCompany - " (:name company-data)))
+
+      (cond
+
+        ;; The data is still loading
+        (:loading data)
+        (dom/div
+          (dom/h4 "Loading data..."))
+
+        ;; Stakeholder update
+        (and (not (contains? data :loading)) (contains? data slug))
+        (dom/div {:class (utils/class-set {:stakeholder-update true
+                                           :navbar-offset (not (utils/is-mobile))})}
+          ;; Company / user header
+          (when-not (utils/is-mobile)
+            (om/build navbar data))
+            
+          ;; Company header
+          (om/build company-header {
+              :editing-topic true ; no category nav
+              :company-data company-data
+              :stakeholder-update true})
+          
+          (dom/div {:class "update-internal"}
+          
+            (dom/div {:class "sections"}; col-md-9 col-sm-12"}
+              ;; Stakeholder update topics
+              (om/build stakeholder-updates {:company-data company-data})
+              ;; Dashboard link
+              (when (utils/is-mobile)
+                (dom/div {:class "dashboard-link"}
+                  (om/build link {:href (str "/" (:slug company-data)) :name "View Dashboard"}))))))
+
+
+        ;; Error fallback
+        :else
+        (dom/div
+          (dom/h2 (str (name slug) " not found"))
+          (om/build link {:href "/" :name "Back home"}))))))
