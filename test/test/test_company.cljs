aa491250d2396462fcbb044eaacb2fcfe24abeb3
bago2k4 <iacopo@rc0.eu>
2016-04-24 01:45:46 +0200

    remove tests of removed components, fix some tests ns

--- test/test/test_company.cljs
+++ test/test/test_company.cljs
@@ -2,19 +2,15 @@
   (:require [doo.runner :refer-macros [doo-tests]]
             [test.open-company-web.components.company-profile]
             [test.open-company-web.components.company-editor]
-            [test.open-company-web.components.revisions-navigator]
+            [test.open-company-web.components.ui.revisions-navigator]
             [test.open-company-web.components.ui.company-avatar]
             [test.open-company-web.components.ui.user-avatar]
             [test.open-company-web.components.user-profile]
-            [test.open-company-web.components.finances.burn-rate]
             [test.open-company-web.components.finances.cash]
             [test.open-company-web.components.finances.cash-flow]
             [test.open-company-web.components.finances.costs]
-            [test.open-company-web.components.finances.revenue]
             [test.open-company-web.components.finances.runway]
-            [test.open-company-web.components.finances.finances]
             [test.open-company-web.components.finances.finances-edit]
-            [test.open-company-web.components.growth.growth]
             [test.open-company-web.components.growth.growth-metric]
             [test.open-company-web.components.growth.growth-edit]
             [test.open-company-web.components.growth.growth-metric-edit]
@@ -37,19 +33,15 @@
 (doo-tests
   'test.open-company-web.components.company-profile
   'test.open-company-web.components.company-editor
-  'test.open-company-web.components.revisions-navigator
+  'test.open-company-web.components.ui.revisions-navigator
   'test.open-company-web.components.ui.company-avatar
   'test.open-company-web.components.ui.user-avatar
   'test.open-company-web.components.user-profile
-  'test.open-company-web.components.finances.burn-rate
   'test.open-company-web.components.finances.cash
   'test.open-company-web.components.finances.cash-flow
   'test.open-company-web.components.finances.costs
-  'test.open-company-web.components.finances.revenue
   'test.open-company-web.components.finances.runway
-  'test.open-company-web.components.finances.finances
   'test.open-company-web.components.finances.finances-edit
-  'test.open-company-web.components.growth.growth
   'test.open-company-web.components.growth.growth-metric
   'test.open-company-web.components.growth.growth-edit
   'test.open-company-web.components.growth.growth-metric-edit
