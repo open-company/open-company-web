32ace13a15a106808de8e1a863ff284450dc71d8
bago2k4 <iacopo@rc0.eu>
2016-04-24 00:59:10 +0200

    use urls ns to get the right url for redirects and links

--- src/open_company_web/components/page_not_found.cljs
+++ src/open_company_web/components/page_not_found.cljs
@@ -2,10 +2,11 @@
   (:require [om.core :as om :include-macros true]
             [om-tools.core :as om-core :refer-macros (defcomponent)]
             [om-tools.dom :as dom :include-macros true]
+            [open-company-web.urls :as oc-urls]
             [open-company-web.components.ui.link :refer (link)]))
 
 (defcomponent page-not-found [data owner]
   (render [_]
     (dom/div
       (dom/h1 "Page not found")
-      (om/build link {:href "/" :name "Home"}))))
+      (om/build link {:href oc-urls/home :name "Home"}))))
