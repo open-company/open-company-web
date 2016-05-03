3c4210d5e51ca2a9c687c4ae78d226e4e166b610
Sean Johnson <sean@snootymonkey.com>
2016-05-03 17:52:36 -0400

    Juggle things around a bit to try to address externs issue.

--- src/open_company_web/lib/utils.cljs
+++ src/open_company_web/lib/utils.cljs
@@ -696,5 +696,6 @@
   with emoji image tags via the Emoji One lib and resources."
   [text]
   (set! (.-ascii js/emojione) true)
-  (let [text-string (or text "")] ; handle nil
-    (clj->js {"__html" (.toImage js/emojione text-string)})))
+  (let [text-string (or text "") ; handle nil
+        unicode-string (.toImage js/emojione text-string)]
+    (clj->js {"__html" unicode-string})))
