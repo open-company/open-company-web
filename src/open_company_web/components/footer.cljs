89ef07ba32d05c4754671e77980716c1d13c01ef
bago2k4 <iacopo@rc0.eu>
2016-05-04 14:19:44 +0200

    footer component with style

---
+++ src/open_company_web/components/footer.cljs
@@ -0,0 +1,18 @@
+(ns open-company-web.components.footer
+  (:require [om.core :as om :include-macros true]
+            [om-tools.core :as om-core :refer-macros [defcomponent]]
+            [om-tools.dom :as dom :include-macros true]
+            [open-company-web.urls :as oc-urls]))
+
+(defcomponent footer [data owner options]
+  (render [_]
+    (dom/div {:class "footer"}
+      (dom/div {:class "footer-cta"}
+        "Powered by OpenCompany, a simple way to lead in the open.")
+      (dom/div {:class "footer-bottom"}
+        (dom/a {:href oc-urls/home} "OC")
+        (dom/a {:href oc-urls/about} "ABOUT US")
+        (dom/a {:href "mailto:oc@opencompany.com"} "CONTACT US")
+        (dom/div {:class "footer-bottom-right"}
+          (dom/a {:class "twitter" :href oc-urls/oc-twitter :alt "twitter"} "twitter")
+          (dom/a {:class "github" :href oc-urls/oc-github :alt "github"} "github"))))))
