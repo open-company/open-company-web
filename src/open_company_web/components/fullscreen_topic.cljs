d9940bd94b6202478fe1bc13662b71fe409c8dbb
bago2k4 <iacopo@rc0.eu>
2016-05-04 17:41:11 +0200

    improve fullscreen topic

--- src/open_company_web/components/fullscreen_topic.cljs
+++ src/open_company_web/components/fullscreen_topic.cljs
@@ -2,11 +2,25 @@
   (:require [om.core :as om :include-macros true]
             [om-tools.core :as om-core :refer-macros (defcomponent)]
             [om-tools.dom :as dom :include-macros true]
+            [open-company-web.router :as router]
+            [open-company-web.caches :as cache]
             [open-company-web.lib.utils :as utils]
+            [open-company-web.components.ui.icon :refer (icon)]
             [dommy.core :as dommy :refer-macros (sel1 sel)]
             [goog.style :refer (setStyle)]
             [goog.fx.Animation.EventType :as EventType]
             [goog.fx.dom :refer (Fade)]))
+
+
+(defcomponent fullscreen-topic-internal [{:keys [topic topic-data] :as data} owner options]
+  (render [_]
+    (let []
+      (dom/div {:class "fullscreen-topic-internal"}
+        (dom/div {:class "topic-title"} (:title topic-data))
+        (dom/div {:class "topic-headline"} (:headline topic-data))
+        (dom/div {:class "separator"})
+        (dom/div {:class "topic-body"
+                  :dangerouslySetInnerHTML (clj->js {"__html" (utils/get-topic-body topic-data topic)})})))))
 
 (defn show-fullscreen-topic [owner]
   (dommy/add-class! (sel1 [:body]) :no-scroll)
@@ -23,12 +37,28 @@
 
 (defcomponent fullscreen-topic [{:keys [section section-data selected-metric currency] :as data} owner options]
 
+  (init-state [_]
+    {:as-of (:updated-at section-data)
+     :actual-as-of (:updated-at section-data)})
+
   (did-mount [_]
     (show-fullscreen-topic owner))
 
-  (render-state [_ {:keys [] :as state}]
-    (dom/div #js {:className "fullscreen-topic"
-                  :ref "fullscreen-topic"
-                  :onClick #(hide-fullscreen-topic owner options)}
-      (dom/div {:class "close"})
-      (dom/div {:class "fullscreen-topic-content"}))))
+  (render-state [_ {:keys [as-of actual-as-of] :as state}]
+    (let [section-kw (keyword section)
+          revisions (utils/sort-revisions (:revisions section-data))
+          prev-rev (utils/revision-prev revisions as-of)
+          next-rev (utils/revision-next revisions as-of)
+          slug (keyword (router/current-company-slug))
+          revisions-list (section-kw (slug @cache/revisions))
+          topic-data (utils/select-section-data section-data section-kw as-of)]
+      (dom/div #js {:className "fullscreen-topic"
+                    :ref "fullscreen-topic"}
+        (dom/div {:class "close"
+                  :on-click #(hide-fullscreen-topic owner options)}
+          (icon :circle-remove))
+        (om/build fullscreen-topic-internal {:topic section
+                                             :topic-data topic-data
+                                             :currency currency
+                                             :prev-rev prev-rev
+                                             :next-rev next-rev})))))
