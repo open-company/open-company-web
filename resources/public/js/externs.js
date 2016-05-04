80ca3f07c73cbd303e7a338ec463ccb9e12be10b
Sean Johnson <sean@snootymonkey.com>
2016-05-04 08:24:26 -0400

    Fix externs typo.

--- resources/public/js/externs.js
+++ resources/public/js/externs.js
@@ -3,5 +3,5 @@ var emojione = {};
 emojione.ascii = {};
 emojione.imageType = {};
 emojione.sprites = {};
-emojione.magePathSVGSprites = {};
+emojione.imagePathSVGSprites = {};
 emojione.toImage = function(){};
