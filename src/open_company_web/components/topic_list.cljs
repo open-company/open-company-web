56024375f1ba5be44157829819dfd3ff469fe339
bago2k4 <iacopo@rc0.eu>
2016-05-06 19:37:36 +0200

    set columns and card width with inline style

--- src/open_company_web/components/topic_list.cljs
+++ src/open_company_web/components/topic_list.cljs
@@ -4,8 +4,10 @@
             [om.core :as om :include-macros true]
             [om-tools.core :as om-core :refer-macros (defcomponent)]
             [om-tools.dom :as dom :include-macros true]
-            [dommy.core :refer-macros (sel)]
+            [dommy.core :refer-macros (sel1)]
             [goog.style :refer (setStyle)]
+            [goog.events :as events]
+            [goog.events.EventType :as EventType]
             [open-company-web.api :as api]
             [open-company-web.caches :as caches]
             [open-company-web.router :as router]
@@ -15,6 +17,81 @@
             [open-company-web.components.ui.side-drawer :refer (side-drawer)]
             [open-company-web.components.fullscreen-topic :refer (fullscreen-topic)]
             [open-company-web.components.ui.drawer-toggler :refer (drawer-toggler)]))
+
+;; 3 Columns
+(def c3-min-win-width 1006)
+(def c3-max-win-width 1800)
+(def c3-min-card-width 302)
+(def c3-max-card-width 500)
+(def c3-win-card-diff (- (/ c3-max-win-width c3-max-card-width) (/ c3-min-win-width c3-min-card-width)))
+(def c3-win-diff (- c3-max-win-width c3-min-win-width))
+(def c3-min-card-delta (/ c3-min-win-width c3-min-card-width))
+
+;; 2 Columns
+(def c2-min-win-width 684)
+(def c2-max-win-width 1005)
+(def c2-min-card-width 302)
+(def c2-max-card-width 462)
+(def c2-win-card-diff (- (/ c2-max-win-width c2-max-card-width) (/ c2-min-win-width c2-min-card-width)))
+(def c2-win-diff (- c2-max-win-width c2-min-win-width))
+(def c2-min-card-delta (/ c2-min-win-width c2-min-card-width))
+
+;; 1 Columns
+(def c1-min-win-width 320)
+(def c1-max-win-width 683)
+(def c1-min-card-width 302)
+(def c1-max-card-width 500)
+(def c1-win-card-diff (- (/ c1-max-win-width c1-max-card-width) (/ c1-min-win-width c1-min-card-width)))
+(def c1-win-diff (- c1-max-win-width c1-min-win-width))
+(def c1-min-card-delta (/ c1-min-win-width c1-min-card-width))
+
+(defn win-width [columns]
+  (let [ww (.-clientWidth (sel1 js/document :body))]
+    (case columns
+      3
+      (max (min ww c3-max-win-width) c3-min-win-width)
+      2
+      (max (min ww c2-max-win-width) c2-min-win-width)
+      1
+      (max (min ww c1-max-win-width) c1-min-win-width))))
+
+(defn get-min-win-width [columns]
+  (case columns
+    3 c3-min-win-width
+    2 c2-min-win-width
+    1 c1-min-win-width))
+
+(defn get-win-diff [columns]
+  (case columns
+    3 c3-win-diff
+    2 c2-win-diff
+    1 c1-win-diff))
+
+(defn get-win-card-diff [columns]
+  (case columns
+    3 c3-win-card-diff
+    2 c2-win-card-diff
+    1 c1-win-card-diff))
+
+(defn get-min-card-delta [columns]
+  (case columns
+    3 c3-min-card-delta
+    2 c2-min-card-delta
+    1 c1-min-card-delta))
+
+(defn calc-card-width [columns]
+  (let [ww (win-width columns)
+        ;; get params based on columns number
+        min-win-width (get-min-win-width columns)
+        win-diff (get-win-diff columns)
+        win-card-diff (get-win-card-diff columns)
+        min-card-delta (get-min-card-delta columns)
+        ;; calculations
+        win-delta-width (- ww min-win-width)
+        perc-win-delta  (/ (* win-delta-width 100) win-diff)
+        diff-delta      (* (/ win-card-diff 100) perc-win-delta)
+        delta           (+ min-card-delta diff-delta)]
+      (/ ww delta)))
 
 (defn get-new-sections-if-needed [owner]
   (when-not (om/get-state owner :new-sections-requested)
@@ -86,7 +163,8 @@
     ; make sure the calculation for the fixed navbar are correct
     (when-not @scrolled-to-top
       (set! (.-scrollTop (.-body js/document)) 0)
-      (reset! scrolled-to-top true)))
+      (reset! scrolled-to-top true))
+    (events/listen js/window EventType/RESIZE #(om/set-state! owner :columns (utils/columns-num))))
 
   (will-receive-props [_ next-props]
     (when-not (= (:company-data next-props) (:company-data data))
@@ -98,7 +176,8 @@
     (let [slug            (keyword (router/current-company-slug))
           company-data    (:company-data data)
           active-category (keyword (:active-category data))
-          category-topics (flatten (vals active-topics))]
+          category-topics (flatten (vals active-topics))
+          card-width      (calc-card-width columns)]
       (dom/div {:class "topic-list group"
                 :key "topic-list"}
         (when (and (not (:read-only company-data))
@@ -134,39 +213,48 @@
                                            :content-loaded (not (:loading data))})}
           (case columns
             1
-            (dom/div {:class "topics-column-container columns-1 group"}
-              (dom/div {:class "topics-column"}
+            (dom/div {:class "topics-column-container columns-1 group"
+                      :style #js {:width (str (+ card-width 60) "px")}}
+              (dom/div {:class "topics-column"
+                        :style #js {:width (str card-width "px")}}
                 (for [section-name category-topics]
                   (render-topic owner section-name company-data active-category))))
             2
-            (dom/div {:class "topics-column-container columns-2 group"}
-              (dom/div {:class "topics-column"}
+            (dom/div {:class "topics-column-container columns-2 group"
+                      :style #js {:width (str (+ (* card-width 2) 20 60) "px")}}
+              (dom/div {:class "topics-column"
+                        :style #js {:width (str card-width "px")}}
                 (for [idx (range (quot (count category-topics) 2))
                   :while (< idx (quot (count category-topics) 2))
                   :let [real-idx (* idx 2)
                         section-name (get (vec category-topics) real-idx)]]
                   (render-topic owner section-name company-data active-category)))
-              (dom/div {:class "topics-column"}
+              (dom/div {:class "topics-column"
+                        :style #js {:width (str card-width "px")}}
                 (for [idx (range (quot (count category-topics) 2))
                   :while (< idx (quot (count category-topics) 2))
                   :let [real-idx (inc (* idx 2))
                         section-name (get (vec category-topics) real-idx)]]
                   (render-topic owner section-name company-data active-category))))
             3
-            (dom/div {:class "topics-column-container columns-3 group"}
-              (dom/div {:class "topics-column"}
+            (dom/div {:class "topics-column-container group"
+                      :style #js {:width (str (+ (* card-width 3) 40 60) "px")}}
+              (dom/div {:class "topics-column"
+                        :style #js {:width (str card-width "px")}}
                 (for [idx (range (quot (count category-topics) 3))
                   :while (< idx (quot (count category-topics) 2))
                   :let [real-idx (* idx 3)
                         section-name (get (vec category-topics) real-idx)]]
                   (render-topic owner section-name company-data active-category)))
-              (dom/div {:class "topics-column"}
+              (dom/div {:class "topics-column"
+                        :style #js {:width (str card-width "px")}}
                 (for [idx (range (quot (count category-topics) 3))
                   :while (< idx (quot (count category-topics) 2))
                   :let [real-idx (inc (* idx 3))
                         section-name (get (vec category-topics) real-idx)]]
                   (render-topic owner section-name company-data active-category)))
-              (dom/div {:class "topics-column"}
+              (dom/div {:class "topics-column"
+                        :style #js {:width (str card-width "px")}}
                 (for [idx (range (quot (count category-topics) 3))
                   :while (< idx (quot (count category-topics) 2))
                   :let [real-idx (+ (* idx 3) 2)
