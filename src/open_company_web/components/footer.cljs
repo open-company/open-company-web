6853ec4461f663bc1878f287bca7eee94638ca01
bago2k4 <iacopo@rc0.eu>
2016-05-06 11:50:17 +0200

    rearrange footer to have the same width of the card columns

--- src/open_company_web/components/footer.cljs
+++ src/open_company_web/components/footer.cljs
@@ -3,6 +3,7 @@
             [om-tools.core :as om-core :refer-macros [defcomponent]]
             [om-tools.dom :as dom :include-macros true]
             [open-company-web.urls :as oc-urls]
+            [open-company-web.lib.utils :as utils]
             [open-company-web.components.ui.icon :refer (icon)]))
 
 (defcomponent footer [data owner options]
@@ -10,12 +11,13 @@
     (dom/div {:class "footer"}
       (dom/div {:class "footer-cta"}
         "Powered by OpenCompany, a simple way to lead in the open.")
-      (dom/div {:class "footer-bottom"}
-        (dom/a {:class "oc-logo" :href oc-urls/home} (dom/img {:src "/img/oc-logo-grey.svg"}))
-        (dom/a {:class "footer-link" :href oc-urls/about} "ABOUT US")
-        (dom/a {:class "footer-link" :target "_blank" :href "mailto:oc@opencompany.com"} "CONTACT US")
-        (dom/div {:class "footer-bottom-right"}
-          (dom/a {:class "twitter" :target "_blank" :href oc-urls/oc-twitter :alt "twitter"}
-            (dom/img {:src "/img/twitter.svg"}))
-          (dom/a {:class "github" :target "_blank" :href oc-urls/oc-github :alt "github"}
-            (dom/img {:src "/img/github.svg"})))))))
+      (dom/div {:class "footer-internal"}
+        (dom/div {:class (str "footer-bottom columns-" (utils/columns-num))}
+          (dom/a {:class "oc-logo" :href oc-urls/home} (dom/img {:src "/img/oc-logo-grey.svg"}))
+          (dom/a {:class "footer-link" :href oc-urls/about} "ABOUT US")
+          (dom/a {:class "footer-link" :target "_blank" :href "mailto:oc@opencompany.com"} "CONTACT US")
+          (dom/div {:class "footer-bottom-right"}
+            (dom/a {:class "twitter" :target "_blank" :href oc-urls/oc-twitter :alt "twitter"}
+              (dom/img {:src "/img/twitter.svg"}))
+            (dom/a {:class "github" :target "_blank" :href oc-urls/oc-github :alt "github"}
+              (dom/img {:src "/img/github.svg"}))))))))
