89ffcdc559e482f7c6334e7cb53161f64209f79b
bago2k4 <iacopo@rc0.eu>
2016-05-09 17:02:17 +0200

    get the columns-num from the component data

--- src/open_company_web/components/footer.cljs
+++ src/open_company_web/components/footer.cljs
@@ -3,7 +3,6 @@
             [om-tools.core :as om-core :refer-macros [defcomponent]]
             [om-tools.dom :as dom :include-macros true]
             [open-company-web.urls :as oc-urls]
-            [open-company-web.lib.utils :as utils]
             [open-company-web.components.ui.icon :refer (icon)]))
 
 (defcomponent footer [data owner options]
@@ -12,7 +11,7 @@
       (dom/div {:class "footer-cta"}
         "Powered by OpenCompany, a simple way to lead in the open.")
       (dom/div {:class "footer-internal"}
-        (dom/div {:class (str "footer-bottom columns-" (utils/columns-num))}
+        (dom/div {:class (str "footer-bottom columns-" (:columns-num data))}
           (dom/a {:class "oc-logo" :href oc-urls/home} (dom/img {:src "/img/oc-logo-grey.svg"}))
           (dom/a {:class "footer-link" :href oc-urls/about} "ABOUT US")
           (dom/a {:class "footer-link" :target "_blank" :href "mailto:oc@opencompany.com"} "CONTACT US")
