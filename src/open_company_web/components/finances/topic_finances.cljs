6ba64ec427e21e5aa3e84ee7519cf8711b50ede4
bago2k4 <iacopo@rc0.eu>
2016-05-09 20:36:14 +0200

    show chart with the correct metric selected when the user click a metric in the finances card

--- src/open_company_web/components/finances/topic_finances.cljs
+++ src/open_company_web/components/finances/topic_finances.cljs
@@ -17,31 +17,45 @@
               (om/get-state owner :focus))
      :finances-data (finances-utils/map-placeholder-data (:data section-data))}))
 
-(defn subsection-click [e owner]
+(defn pillbox-click [owner options e]
   (.preventDefault e)
   (let [tab  (.. e -target -dataset -tab)]
-    (om/update-state! owner :focus (fn [] tab)))
+    (om/update-state! owner :focus (fn [] tab))
+    (when (contains? options :topic-click)
+      ((:topic-click options) tab)))
   (.stopPropagation e))
 
 (defn has-revenues-or-costs [finances-data]
   (some #(or (not (zero? (:revenue %))) (not (zero? (:costs %)))) finances-data))
 
-(defn render-pillboxes [owner cash-classes needs-cash-flow cash-flow-classes needs-runway runway-classes first-title]
-  (dom/div {:class "pillbox-container finances"}
-    (dom/label {:class cash-classes
-                :title "Cash"
-                :data-tab "cash"
-                :on-click #(subsection-click % owner)} "Cash")
-    (when needs-cash-flow
-      (dom/label {:class cash-flow-classes
-                  :title first-title
-                  :data-tab "cash-flow"
-                  :on-click #(subsection-click % owner)} first-title))
-    (when needs-runway
-      (dom/label {:class runway-classes
-                  :title "Runway"
-                  :data-tab "runway"
-                  :on-click #(subsection-click % owner)} "Runway"))))
+(defn render-pillboxes [owner options]
+  (let [data (om/get-props owner)
+        section-data (:section-data data)
+        focus (om/get-state owner :focus)
+        classes "pillbox"
+        cash-classes (str classes (when (= focus "cash") " active"))
+        cash-flow-classes (str classes (when (= focus "cash-flow") " active"))
+        runway-classes (str classes (when (= focus "runway") " active"))
+        finances-row-data (:data section-data)
+        sum-revenues (apply + (map :revenue finances-row-data))
+        needs-runway (some #(neg? (:runway %)) finances-row-data)
+        first-title (if (pos? sum-revenues) "Cash flow" "Burn rate")
+        needs-cash-flow (has-revenues-or-costs finances-row-data)]
+    (dom/div {:class "pillbox-container finances"}
+      (dom/label {:class cash-classes
+                  :title "Cash"
+                  :data-tab "cash"
+                  :on-click (partial pillbox-click owner options)} "Cash")
+      (when needs-cash-flow
+        (dom/label {:class cash-flow-classes
+                    :title first-title
+                    :data-tab "cash-flow"
+                    :on-click (partial pillbox-click owner options)} first-title))
+      (when needs-runway
+        (dom/label {:class runway-classes
+                    :title "Runway"
+                    :data-tab "runway"
+                    :on-click (partial pillbox-click owner options)} "Runway")))))
 
 (defcomponent topic-finances [{:keys [section section-data currency] :as data} owner options]
 
@@ -54,16 +68,9 @@
       (om/set-state! owner (get-state owner next-props true))))
 
   (render-state [_ {:keys [focus] :as state}]
-    (let [classes "pillbox"
-          cash-classes (str classes (when (= focus "cash") " active"))
-          cash-flow-classes (str classes (when (= focus "cash-flow") " active"))
-          runway-classes (str classes (when (= focus "runway") " active"))
-          finances-row-data (:data section-data)
+    (let [finances-row-data (:data section-data)
           no-data (or (empty? finances-row-data) (utils/no-finances-data? finances-row-data))
           sum-revenues (apply + (map :revenue finances-row-data))
-          first-title (if (pos? sum-revenues) "Cash flow" "Burn rate")
-          needs-runway (some #(neg? (:runway %)) finances-row-data)
-          needs-cash-flow (has-revenues-or-costs finances-row-data)
           subsection-data {:section-data section-data
                            :read-only true
                            :currency currency}
@@ -78,7 +85,7 @@
         (dom/div {:class "section-container" :id "section-finances"}
           (dom/div {:class "composed-section finances group"}
             (when (:pillboxes-first options)
-              (render-pillboxes owner cash-classes needs-cash-flow cash-flow-classes needs-runway runway-classes first-title))
+              (render-pillboxes owner options))
             (dom/div {:class (utils/class-set {:composed-section-body true})}
               (case focus
 
@@ -93,4 +100,4 @@
                 "runway"
                 (om/build runway subsection-data subsection-options)))
             (when-not (:pillboxes-first options)
-              (render-pillboxes owner cash-classes needs-cash-flow cash-flow-classes needs-runway runway-classes first-title))))))))
+              (render-pillboxes owner options))))))))
