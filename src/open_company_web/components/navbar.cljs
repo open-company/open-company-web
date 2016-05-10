5e06698a15485c74a17c0ddb91b8b2c3eb1d9322
bago2k4 <iacopo@rc0.eu>
2016-05-10 16:52:13 +0200

    add animation classes on click to open and close the drawer

--- src/open_company_web/components/navbar.cljs
+++ src/open_company_web/components/navbar.cljs
@@ -2,6 +2,7 @@
   (:require [om.core :as om :include-macros true]
             [om-tools.core :as om-core :refer-macros (defcomponent)]
             [om-tools.dom :as dom :include-macros true]
+            [dommy.core :as dommy :refer-macros (sel1)]
             [open-company-web.components.ui.user-avatar :refer (user-avatar)]
             [open-company-web.components.ui.company-avatar :refer (company-avatar)]
             [open-company-web.components.ui.login-button :refer (login-button)]
@@ -9,8 +10,32 @@
             [open-company-web.router :as router]
             [open-company-web.local-settings :as ls]
             [open-company-web.lib.utils :as utils]
+            [open-company-web.lib.responsive :as responsive]
             [open-company-web.lib.jwt :as jwt]
-            [cljs.core.async :refer (put!)]))
+            [open-company-web.components.ui.icon :refer (icon)]
+            [cljs.core.async :refer (put!)]
+            [goog.events :as events]
+            [goog.events.EventType :as EventType]))
+
+(defn on-transition-end [owner body]
+  (doto body
+    (dommy/remove-class! :left)
+    (dommy/remove-class! :right)
+    (dommy/remove-class! :animating)
+    (dommy/toggle-class! :menu-visible))
+  (events/unlistenByKey (om/get-state owner :transition-end-listener)))
+
+(defn menu-click [owner e]
+  (.preventDefault e)
+  (let [body (sel1 [:body])
+        page (sel1 [:div.page])
+        menu (sel1 [:ul#menu])]
+    (dommy/add-class! body :animating)
+    (if (dommy/has-class? body :menu-visible)
+      (dommy/add-class! body :right)
+      (dommy/add-class! body :left))
+    (let [listener-key (events/listen page EventType/TRANSITIONEND #(on-transition-end owner body))]
+      (om/set-state! owner :transition-end-listener listener-key))))
 
 (defcomponent navbar [data owner]
 
@@ -26,6 +51,9 @@
           (om/build company-avatar data)
           (dom/ul {:class "nav navbar-nav navbar-right"}
             (dom/li {}
-              (if (jwt/jwt)
-                (om/build user-avatar {})
-                (om/build login-button data)))))))))
+              (if (utils/is-mobile)
+                (dom/div {:on-click (partial menu-click owner)}
+                  (icon "menu-34"))
+                (if (jwt/jwt)
+                  (om/build user-avatar {})
+                  (om/build login-button data))))))))))
