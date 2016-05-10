6c6c4310fd0844eb12d85be0d1234a1b93957aa6
bago2k4 <iacopo@rc0.eu>
2016-05-10 19:29:28 +0200

    render links differently on mobile, add US to about and contacts link text on big web

--- src/open_company_web/components/footer.cljs
+++ src/open_company_web/components/footer.cljs
@@ -3,6 +3,7 @@
             [om-tools.core :as om-core :refer-macros [defcomponent]]
             [om-tools.dom :as dom :include-macros true]
             [open-company-web.urls :as oc-urls]
+            [open-company-web.lib.utils :as utils]
             [open-company-web.components.ui.icon :refer (icon)]))
 
 (defcomponent footer [data owner options]
@@ -12,11 +13,19 @@
         "Powered by OpenCompany, a simple way to lead in the open.")
       (dom/div {:class "footer-internal"}
         (dom/div {:class (str "footer-bottom columns-" (:columns-num data))}
-          (dom/a {:class "oc-logo" :href oc-urls/home} (dom/img {:src "/img/oc-logo-grey.svg"}))
-          (dom/a {:class "footer-link" :href oc-urls/about} "ABOUT US")
-          (dom/a {:class "footer-link" :target "_blank" :href "mailto:oc@opencompany.com"} "CONTACT US")
-          (dom/div {:class "footer-bottom-right"}
+          (when-not (utils/is-mobile)
+            (dom/a {:class "oc-logo" :href oc-urls/home} (dom/img {:src "/img/oc-logo-grey.svg"})))
+          (dom/a {:class "footer-link" :href oc-urls/about} (str "ABOUT"  (when-not (utils/is-mobile) " US")))
+          (dom/a {:class "footer-link" :target "_blank" :href "mailto:oc@opencompany.com"} (str "CONTACT" (when-not (utils/is-mobile) " US")))
+          (when (utils/is-mobile)
             (dom/a {:class "twitter" :target "_blank" :href oc-urls/oc-twitter :alt "twitter"}
-              (dom/img {:src "/img/twitter.svg"}))
+              (dom/img {:src "/img/twitter.svg"})))
+          (when (utils/is-mobile)
             (dom/a {:class "github" :target "_blank" :href oc-urls/oc-github :alt "github"}
-              (dom/img {:src "/img/github.svg"}))))))))
+              (dom/img {:src "/img/github.svg"})))
+          (when-not (utils/is-mobile)
+            (dom/div {:class "footer-bottom-right"}
+              (dom/a {:class "twitter" :target "_blank" :href oc-urls/oc-twitter :alt "twitter"}
+                (dom/img {:src "/img/twitter.svg"}))
+              (dom/a {:class "github" :target "_blank" :href oc-urls/oc-github :alt "github"}
+                (dom/img {:src "/img/github.svg"})))))))))
