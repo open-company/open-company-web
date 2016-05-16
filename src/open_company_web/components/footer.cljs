6b775828e450a5d3a7ad1f4fe5bb5a6715c05253
bago2k4 <iacopo@rc0.eu>
2016-05-16 18:16:22 +0200

    fix footer width and margins

--- src/open_company_web/components/footer.cljs
+++ src/open_company_web/components/footer.cljs
@@ -8,24 +8,30 @@
 
 (defcomponent footer [data owner options]
   (render [_]
-    (dom/div {:class "footer"}
-      (dom/div {:class "footer-cta"}
-        "Powered by OpenCompany")
-      (dom/div {:class "footer-internal"}
-        (dom/div {:class (str "footer-bottom columns-" (:columns-num data))}
-          (when-not (responsive/is-mobile)
-            (dom/a {:class "oc-logo" :href oc-urls/home} (dom/img {:src "/img/oc-logo-grey.svg"})))
-          (dom/a {:class "footer-link" :href oc-urls/about} (str "ABOUT"  (when-not (responsive/is-mobile) " US")))
-          (dom/a {:class "footer-link" :target "_blank" :href "mailto:oc@opencompany.com"} (str "CONTACT" (when-not (responsive/is-mobile) " US")))
-          (when (responsive/is-mobile)
-            (dom/a {:class "twitter" :target "_blank" :href oc-urls/oc-twitter :alt "twitter"}
-              (dom/img {:src "/img/twitter.svg"})))
-          (when (responsive/is-mobile)
-            (dom/a {:class "github" :target "_blank" :href oc-urls/oc-github :alt "github"}
-              (dom/img {:src "/img/github.svg"})))
-          (when-not (responsive/is-mobile)
-            (dom/div {:class "footer-bottom-right"}
+    (let [columns-num (:columns-num data)
+          card-width   (:card-width data)
+          footer-width (+ (* card-width columns-num)    ; cards width
+                          (* 20 (dec columns-num))      ; cards right margin
+                          (when (> columns-num 1) 60))]
+      (dom/div {:class "footer"}
+        (dom/div {:class "footer-cta"}
+          "Powered by OpenCompany")
+        (dom/div {:class "footer-internal"}
+          (dom/div {:class "footer-bottom"
+                    :style #js {:width (str footer-width "px")}}
+            (when-not (responsive/is-mobile)
+              (dom/a {:class "oc-logo" :href oc-urls/home} (dom/img {:src "/img/oc-logo-grey.svg"})))
+            (dom/a {:class "footer-link" :href oc-urls/about} (str "ABOUT"  (when-not (responsive/is-mobile) " US")))
+            (dom/a {:class "footer-link" :target "_blank" :href "mailto:oc@opencompany.com"} (str "CONTACT" (when-not (responsive/is-mobile) " US")))
+            (when (responsive/is-mobile)
               (dom/a {:class "twitter" :target "_blank" :href oc-urls/oc-twitter :alt "twitter"}
-                (dom/img {:src "/img/twitter.svg"}))
+                (dom/img {:src "/img/twitter.svg"})))
+            (when (responsive/is-mobile)
               (dom/a {:class "github" :target "_blank" :href oc-urls/oc-github :alt "github"}
-                (dom/img {:src "/img/github.svg"})))))))))
+                (dom/img {:src "/img/github.svg"})))
+            (when-not (responsive/is-mobile)
+              (dom/div {:class "footer-bottom-right"}
+                (dom/a {:class "twitter" :target "_blank" :href oc-urls/oc-twitter :alt "twitter"}
+                  (dom/img {:src "/img/twitter.svg"}))
+                (dom/a {:class "github" :target "_blank" :href oc-urls/oc-github :alt "github"}
+                  (dom/img {:src "/img/github.svg"}))))))))))
