c68c922a88a304c126d83e239fe621c2b08e40f1
bago2k4 <iacopo@rc0.eu>
2016-05-16 15:04:42 +0200

    do not select a topic when the popover appear, do not wrap at the top and bottom of the popover with keys, use back to hide the popover

--- src/open_company_web/components/ui/add_topic_popover.cljs
+++ src/open_company_web/components/ui/add_topic_popover.cljs
@@ -5,8 +5,11 @@
             [dommy.core :as dommy :refer-macros (sel1)]
             [goog.events :as events]
             [goog.events.EventType :as EventType]
+            [goog.history.EventType :as HistoryEventType]
             [open-company-web.api :as api]
+            [open-company-web.urls :as oc-urls]
             [open-company-web.caches :as caches]
+            [open-company-web.router :as router]
             [open-company-web.lib.utils :as utils]))
 
 (defn is-child-of-popover [el]
@@ -17,9 +20,13 @@
         (recur (.-parentNode cur-el))
         false))))
 
+(defn dismiss-popover [options]
+  (.replaceState js/history #js {} "Dashboard" (oc-urls/company))
+  ((:dismiss-popover options)))
+
 (defn on-click-out [owner options e]
   (when (not (is-child-of-popover (.-target e)))
-    ((:dismiss-popover options))))
+    (dismiss-popover options)))
 
 (defn on-click-in [owner options e]
   (.stopPropagation e))
@@ -30,14 +37,14 @@
         unactive-equal? (= (set unactive-topics) (set (:unactive-topics old-state)))]
     {:active-topics active-topics-list
      :unactive-topics (if unactive-equal? (:unactive-topics old-state) unactive-topics)
-     :highlighted-topic 0}))
+     :highlighted-topic -1}))
 
 (defn add-topic-click [owner options topic]
   (let [all-topics (om/get-props owner :all-topics)
         topic-data (->> topic keyword (get all-topics))
         category-name (:category topic-data)]
     ((:did-change-active-topics options) category-name topic)
-    ((:dismiss-popover options))))
+    (dismiss-popover options)))
 
 (def down-arrow-key-code 40)
 (def up-arrow-key-code 38)
@@ -59,14 +66,16 @@
         (let [topic (get (vec unactive-topics) (om/get-state owner :highlighted-topic))]
           (add-topic-click owner options topic))
         (= key-code esc-key-code)
-        ((:dismiss-popover options))
+        (dismiss-popover options)
         :else
         ; arrow key: change focus and scroll the parent div
         (let [cur-highlighted  (om/get-state owner :highlighted-topic)
               idx-fn           (cond
                                  (= key-code down-arrow-key-code) inc
                                  (= key-code up-arrow-key-code) dec)
-              next-highlighted (mod (idx-fn cur-highlighted) (count unactive-topics))
+              min-idx          0
+              max-idx          (dec (count unactive-topics))
+              next-highlighted (min (max (idx-fn cur-highlighted) min-idx) max-idx)
               topics-to-add    (om/get-ref owner "topics-to-add")
               new-topic-div    (om/get-ref owner (str "potential-topic-" next-highlighted))]
           (set! (.-scrollTop topics-to-add) (- (.-offsetTop new-topic-div) (.-offsetTop topics-to-add)))
@@ -76,6 +85,9 @@
             (= key-code up-arrow-key-code)
             (om/set-state! owner :highlighted-topic next-highlighted)))))))
 
+(defn history-nav [options]
+  ((:dismiss-popover options)))
+
 (defcomponent add-topic-popover [{:keys [all-topics active-topics-list column] :as data} owner options]
 
   (init-state [_]
@@ -83,11 +95,16 @@
       (api/get-new-sections))
     (get-state data nil))
 
+  (will-mount [_]
+    (.pushState js/history #js {} "Add topic" (str (oc-urls/company) "#add-topic")))
+
   (did-mount [_]
     (let [click-listener (events/listen (sel1 [:body]) EventType/CLICK (partial on-click-out owner options))
-          kb-listener (events/listen (sel1 [:body]) EventType/KEYDOWN (partial kb-key-down owner options))]
+          kb-listener (events/listen (sel1 [:body]) EventType/KEYDOWN (partial kb-key-down owner options))
+          nav-listener (events/listen @router/history HistoryEventType/NAVIGATE (partial history-nav options))]
       (om/set-state! owner :click-out-listener click-listener)
-      (om/set-state! owner :kb-listener kb-listener)))
+      (om/set-state! owner :kb-listener kb-listener)
+      (om/set-state! owner :nav-listener nav-listener)))
 
   (will-receive-props [_ next-props]
     (when-not (= next-props data)
@@ -95,7 +112,8 @@
 
   (will-unmount [_]
     (events/unlistenByKey (om/get-state owner :click-out-listener))
-    (events/unlistenByKey (om/get-state owner :kb-listener)))
+    (events/unlistenByKey (om/get-state owner :kb-listener))
+    (events/unlistenByKey (om/get-state owner :nav-listener)))
 
   (render-state [_ {:keys [active-topics unactive-topics highlighted-topic]}]
     (dom/div {:class (str "add-topic-popover column-" column)
@@ -111,5 +129,6 @@
                     topic-data (->> topic keyword (get all-topics))]]
           (dom/div #js {:className (str "potential-topic" (when (= highlighted-topic idx) " highlighted"))
                         :ref (str "potential-topic-" idx)
+                        :onMouseOver #(om/set-state! owner :highlighted-topic idx)
                         :onClick #(add-topic-click owner options topic)}
             (:title topic-data)))))))
