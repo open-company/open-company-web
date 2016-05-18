ef799208c264f7068449f35c9c28c21fe298fb64
bago2k4 <iacopo@rc0.eu>
2016-05-18 19:13:57 +0200

    show ham icon always on mobile, show avatar if logged in and login button when not on big web, listen for close menu ch

--- src/open_company_web/components/navbar.cljs
+++ src/open_company_web/components/navbar.cljs
@@ -1,18 +1,20 @@
 (ns open-company-web.components.navbar
-  (:require [om.core :as om :include-macros true]
+  (:require-macros [cljs.core.async.macros :refer (go)])
+  (:require [cljs.core.async :refer (chan <!)]
+            [om.core :as om :include-macros true]
             [om-tools.core :as om-core :refer-macros (defcomponent)]
             [om-tools.dom :as dom :include-macros true]
+            [cljs.core.async :refer (put!)]
             [dommy.core :as dommy :refer-macros (sel1)]
-            [open-company-web.components.ui.user-avatar :refer (user-avatar)]
-            [open-company-web.components.ui.company-avatar :refer (company-avatar)]
-            [open-company-web.components.ui.login-button :refer (login-button)]
-            [om-bootstrap.nav :as n]
             [open-company-web.router :as router]
             [open-company-web.local-settings :as ls]
-            [open-company-web.lib.responsive :as responsive]
             [open-company-web.lib.jwt :as jwt]
+            [open-company-web.lib.utils :as utils]
+            [open-company-web.lib.responsive :as responsive]
             [open-company-web.components.ui.icon :refer (icon)]
-            [cljs.core.async :refer (put!)]
+            [open-company-web.components.ui.user-avatar :refer (user-avatar)]
+            [open-company-web.components.ui.login-button :refer (login-button)]
+            [open-company-web.components.ui.company-avatar :refer (company-avatar)]
             [goog.events :as events]
             [goog.events.EventType :as EventType]))
 
@@ -25,7 +27,8 @@
   (events/unlistenByKey (om/get-state owner :transition-end-listener)))
 
 (defn menu-click [owner e]
-  (.preventDefault e)
+  (when e
+    (.preventDefault e))
   (let [body (sel1 [:body])
         page (sel1 [:div.page])
         menu (sel1 [:ul#menu])]
@@ -37,6 +40,16 @@
       (om/set-state! owner :transition-end-listener listener-key))))
 
 (defcomponent navbar [data owner]
+
+  (init-state [_]
+    (utils/add-channel "close-side-menu" (chan)))
+
+  (did-mount [_]
+    (let [close-ch (utils/get-channel "close-side-menu")]
+      (go (loop []
+          (let [change (<! close-ch)]
+            (menu-click owner nil)
+            (recur))))))
 
   (render [_]
     (let [columns-num (:columns-num data)
@@ -50,9 +63,9 @@
           (om/build company-avatar data)
           (dom/ul {:class "nav navbar-nav navbar-right"}
             (dom/li {}
-              (if (jwt/jwt)
-                (if (responsive/is-mobile)
-                  (dom/div {:on-click (partial menu-click owner)}
+              (if (responsive/is-mobile)
+                (dom/div {:on-click (partial menu-click owner)}
                     (icon "menu-34"))
-                  (om/build user-avatar {:menu-click (partial menu-click owner)}))
-                (om/build login-button (assoc data :menu-click (partial menu-click owner)))))))))))
+                (if (jwt/jwt)
+                  (om/build user-avatar {:menu-click (partial menu-click owner)})
+                  (om/build login-button (assoc data :menu-click (partial menu-click owner))))))))))))
