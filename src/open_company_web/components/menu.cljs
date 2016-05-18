25c889a52d4bb57a6594acfa0a81f064ecbeac78
bago2k4 <iacopo@rc0.eu>
2016-05-18 17:55:33 +0200

    logout click remove jwt cookie and refresh page

--- src/open_company_web/components/menu.cljs
+++ src/open_company_web/components/menu.cljs
@@ -3,7 +3,14 @@
             [om-tools.core :as om-core :refer-macros [defcomponent]]
             [om-tools.dom :as dom :include-macros true]
             [open-company-web.urls :as oc-urls]
+            [open-company-web.lib.cookies :as cook]
             [open-company-web.lib.jwt :as jwt]))
+
+(defn logout-click [e]
+  (.preventDefault e)
+  (.stopPropagation e)
+  (cook/remove-cookie! :jwt)
+  (.reload js/location))
 
 (defcomponent menu [data owner options]
   (render [_]
@@ -11,7 +18,7 @@
       (when (jwt/jwt)
         (dom/li {} (dom/a {:title "PROFILE" :href oc-urls/user-profile} "PROFILE")))
       (when (jwt/jwt)
-        (dom/li {} (dom/a {:title "SIGN OUT" :href oc-urls/logout} "SIGN OUT")))
+        (dom/li {} (dom/a {:title "SIGN OUT" :href oc-urls/logout :on-click logout-click} "SIGN OUT")))
       (when-not (jwt/jwt)
         (dom/li {} (dom/a {:title "SIGN IN / SIGN UP" :href oc-urls/login} "SIGN IN / SIGN UP")))
       ; (dom/li {} (dom/a {:title "ABOUT US" :href oc-urls/about} "ABOUT US"))
