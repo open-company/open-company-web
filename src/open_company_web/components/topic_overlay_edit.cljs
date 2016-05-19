63aa5971a832ec99073f9aa41d5444b686ea1870
Martin Klepsch <martinklepsch@googlemail.com>
2016-05-19 18:56:20 +0200

    make sure set-state is called on-blur

--- src/open_company_web/components/topic_overlay_edit.cljs
+++ src/open_company_web/components/topic_overlay_edit.cljs
@@ -484,15 +484,17 @@
           (dom/div {:class (utils/class-set {:topic-edit-title-count true
                                              :transparent (not show-title-counter)})}
             (dom/label {:class "bold"} (- title-length-limit (count title))))
-        (dom/div #js {:className "topic-edit-headline emoji-autocomplete"
-                      :ref "topic-edit-headline"
-                      :contentEditable true
-                      :id (str "topic-edit-headline-" (name topic))
-                      :placeholder "Headline"
-                      :onBlur #(om/set-state! owner :show-headline-counter false)
-                      :onKeyUp (partial check-headline-count owner headline-length-limit)
-                      :onKeyDown (partial check-headline-count owner headline-length-limit)
-                      :dangerouslySetInnerHTML (clj->js {"__html" (:headline topic-data)})})
+        (dom/div {:className "topic-edit-headline emoji-autocomplete"
+                  :ref "topic-edit-headline"
+                  :contentEditable true
+                  :id (str "topic-edit-headline-" (name topic))
+                  :placeholder "Headline"
+                  :on-blur #(do (check-headline-count owner headline-length-limit %)
+                                (om/set-state! owner :show-headline-counter false))
+                  :on-key-up   #(check-headline-count owner headline-length-limit %)
+                  :on-key-down #(check-headline-count owner headline-length-limit %)
+                  :on-focus    #(check-headline-count owner headline-length-limit %)
+                  :dangerouslySetInnerHTML (clj->js {"__html" (:headline topic-data)})})
         (dom/div {:class (utils/class-set {:topic-edit-headline-count true
                                            :transparent (not show-headline-counter)})}
           (dom/label {:class "bold"} (- headline-length-limit (count-chars headline))))
