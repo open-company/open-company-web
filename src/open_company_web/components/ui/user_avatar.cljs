21b0c3a45f16968a5027b468754b8990ebadc709
bago2k4 <iacopo@rc0.eu>
2016-05-23 18:44:54 +0200

    remove unused requires

--- src/open_company_web/components/ui/user_avatar.cljs
+++ src/open_company_web/components/ui/user_avatar.cljs
@@ -2,8 +2,7 @@
   (:require [om.core :as om :include-macros true]
             [om-tools.core :as om-core :refer-macros (defcomponent)]
             [om-tools.dom :as dom :include-macros true]
-            [open-company-web.lib.jwt :as jwt]
-            [om-bootstrap.button :as b]))
+            [open-company-web.lib.jwt :as jwt]))
 
 (defcomponent user-avatar [data owner]
   (render [_]
