20c5b42529ccc569d35852abc98b1cbd689fd623
bago2k4 <iacopo@rc0.eu>
2016-05-25 16:13:47 +0200

    move filestack-uploader componet to its own namespace

--- src/open_company_web/components/topic_overlay_edit.cljs
+++ src/open_company_web/components/topic_overlay_edit.cljs
@@ -5,7 +5,6 @@
             [om-tools.core :as om-core :refer-macros [defcomponent]]
             [om-tools.dom :as dom :include-macros true]
             [dommy.core :refer-macros (sel1)]
-            [open-company-web.local-settings :as ls]
             [open-company-web.lib.utils :as utils]
             [open-company-web.lib.medium-editor-exts :as editor]
             [open-company-web.router :as router]
@@ -16,15 +15,11 @@
             [open-company-web.components.finances.utils :as finances-utils]
             [open-company-web.components.growth.growth-edit :refer (growth-edit)]
             [open-company-web.components.growth.utils :as growth-utils]
-            [open-company-web.components.ui.icon :as i]
+            [open-company-web.components.ui.filestack-uploader :refer (filestack-uploader)]
             [goog.events :as events]
-            [goog.style :as gstyle]
-            [goog.dom :as gdom]
-            [goog.dom.classlist :as cl]
             [goog.history.EventType :as EventType]
             [cljs-dynamic-resources.core :as cdr]
             [cljsjs.medium-editor] ; pulled in for cljsjs externs
-            [cljsjs.filestack] ; pulled in for cljsjs externs
             [clojure.string :as string]))
 
 (defn set-end-of-content-editable [content-editable-element]
@@ -263,82 +258,6 @@
                             with-finances-data)]
       with-growth-data)))
 
-(def placeholder-id (str (random-uuid)))
-
-(defn upload-file! [editor owner file]
-  (let [success-cb  (fn [success]
-                      (let [url    (.-url success)
-                            node   (gdom/createDom "img" #js {:src url})
-                            marker (gdom/getElement placeholder-id)]
-                        (gdom/replaceNode node marker))
-                      (gstyle/setStyle (gdom/getElement "file-upload-ui") #js {:display "none"})
-                      (om/set-state! owner {}))
-        error-cb    (fn [error] (js/console.log "error" error))
-        progress-cb (fn [progress]
-                      (om/set-state! owner {:state :show-progress
-                                            :progress progress}))]
-    (cond
-      (and (string? file) (not (string/blank? file)))
-      (js/filepicker.storeUrl file success-cb error-cb progress-cb)
-      file
-      (js/filepicker.store file #js {:name (.-name file)} success-cb error-cb progress-cb))))
-
-(defn insert-marker! []
-  (when-not (gdom/getElement placeholder-id)
-    (js/MediumEditor.util.insertHTMLCommand
-     js/document
-     (str "<span id=" placeholder-id "></span>"))))
-
-(defcomponent uploader [editor owner]
-  (did-mount [_]
-    (when-not (utils/is-test-env?)
-      (assert ls/filestack-key "FileStack API Key required")
-      (js/filepicker.setKey ls/filestack-key)))
-
-  (render-state [this _]
-    (dom/div {:id "file-upload-ui"
-              :style (merge {:transition ".2s"}
-                            (when (:state (om/get-state owner))
-                              {:right 0}))}
-      (dom/div {:class "flex"}
-        (dom/input {:id "file-upload-ui--select-trigger" :style {:display "none"} :type "file"
-                    :on-change #(upload-file! editor owner (-> % .-target .-files (aget 0)))})
-        (dom/button {:class "btn-reset p0"
-                     :style {:margin-right "13px"
-                             :transition ".2s"
-                             :transform (if (om/get-state owner :state) "rotate(135deg)")}
-                     :on-click (fn [_] (om/update-state! owner :state #(if % nil :show-options)))}
-          (i/icon :circle-add {:size 24}))
-        (case (:state (om/get-state owner))
-          :show-options
-          (dom/div (dom/button {:style {:font-size "14px"} :class "underline btn-reset p0"
-                                :on-click (fn [_]
-                                            (insert-marker!)
-                                            (.click (gdom/getElement "file-upload-ui--select-trigger")))}
-                     "Select an image")
-            (dom/span {:style {:font-size "14px"}} " or ")
-            (dom/button {:style {:font-size "14px"} :class "underline btn-reset p0"
-                         :on-click (fn [_]
-                                     (insert-marker!)
-                                     (om/set-state! owner :state :show-url-field))}
-                "provide an image URL"))
-          :show-progress
-          (dom/span (str "Uploading... " (om/get-state owner :progress) "%"))
-          :show-url-field
-          (dom/div (dom/input {:type "text" :style {:width 300} :auto-focus true
-                               :on-change #(do (om/set-state! owner :url (-> % .-target .-value)) true)
-                               :value (om/get-state owner :url)})
-            (dom/button {:style {:font-size "14px" :margin-left "1rem"} :class "underline btn-reset p0"
-                         :on-click #(upload-file! editor owner (om/get-state owner :url))}
-              "add")
-            (dom/button {:style {:font-size "14px" :margin-left "1rem" :opacity "0.5"}
-                         :class "underline btn-reset p0"
-                         :on-click (fn [_]
-                                     (gdom/removeNode (gdom/getElement placeholder-id))
-                                     (om/set-state! owner {}))}
-              "cancel"))
-          (dom/span))))))
-
 (defn headline-on-change [owner]
   (when-not (om/get-state owner :show-headline-counter)
     (om/set-state! owner :show-headline-counter true))
@@ -567,4 +486,4 @@
                     :ref "topic-overlay-edit-body"
                     :id (str "topic-edit-body-" (name topic))
                     :dangerouslySetInnerHTML (clj->js {"__html" topic-body})})
-          (om/build uploader (om/get-state owner :medium-editor)))))))
+          (om/build filestack-uploader (om/get-state owner :medium-editor)))))))
