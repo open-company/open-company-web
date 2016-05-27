e1bae11c5b250465005e8010f69648b7ae2614cd
bago2k4 <iacopo@rc0.eu>
2016-05-27 13:46:30 +0200

    change slack share message and fix spacing and size

--- src/open_company_web/components/su_preview.cljs
+++ src/open_company_web/components/su_preview.cljs
@@ -203,7 +203,7 @@
               (dom/a {:class "share-link-new-win" :href share-link :target "_blank"} "Open in new window")))
           (when slack-patched
             (dom/div {:class "su-preview-box"}
-              (dom/label {:class "slack-share-cta"} "YOUR SNAPSHOT WILL BE SHARED WITH ALL THE MEMBERS OF YOUR SLACK ORGANIZATION")
+              (dom/label {:class "slack-share-cta"} "SHARE SNAPSHOT WITH THE MEMBERS OF YOUR SLACK ORGANIZATION")
               (dom/textarea #js {:className "slack-share-textarea"
                                  :ref "slack-share-textarea"
                                  :placeholder "Add a note"})))
