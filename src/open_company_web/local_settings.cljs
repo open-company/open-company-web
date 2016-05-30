bb8abf2962f9a6637f13da365ed63541da920c36
bago2k4 <iacopo@rc0.eu>
2016-05-31 01:03:32 +0200

    go back to localhost instead of 127.0.0.1

--- src/open_company_web/local_settings.cljs
+++ src/open_company_web/local_settings.cljs
@@ -6,13 +6,13 @@
 (def local-whitelist-array ["localhost" "127.0.0.1"])
 
 ;; API
-(def api-server-domain "http://127.0.0.1:3000")
+(def api-server-domain "http://localhost:3000")
 
 ;; Auth
-(def auth-server-domain "http://127.0.0.1:3003")
+(def auth-server-domain "http://localhost:3003")
 
 ;; JWT
-(def jwt-cookie-domain "127.0.0.1")
+(def jwt-cookie-domain "localhost")
 (def jwt-cookie-secure false)
 
 ;; Deploy key
