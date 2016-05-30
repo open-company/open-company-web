f7d4e8dc70d3672a5992c87e99820f512c2b22fe
Sean Johnson <sean@snootymonkey.com>
2016-05-30 09:27:41 -0400

    Undo localhost settings change.

--- src/open_company_web/local_settings.cljs
+++ src/open_company_web/local_settings.cljs
@@ -3,16 +3,16 @@
 ;; Sentry
 (def local-dsn "https://747713ae92c246d1a64bbce9aab3da34@app.getsentry.com/73174") ; insert your Sentry public dsn here
 
-(def local-whitelist-array ["172.20.10.2" "127.0.0.1"])
+(def local-whitelist-array ["localhost" "127.0.0.1"])
 
 ;; API
-(def api-server-domain "http://172.20.10.2:3000")
+(def api-server-domain "http://127.0.0.1:3000")
 
 ;; Auth
-(def auth-server-domain "http://172.20.10.2:3003")
+(def auth-server-domain "http://127.0.0.1:3003")
 
 ;; JWT
-(def jwt-cookie-domain "172.20.10.2")
+(def jwt-cookie-domain "127.0.0.1")
 (def jwt-cookie-secure false)
 
 ;; Deploy key
