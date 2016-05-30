29108b32498b2b904efe2ebca0f4ea64d340f0e8
bago2k4 <iacopo@rc0.eu>
2016-05-30 18:07:05 +0200

    simplify su-preview component, removed old unused code of the first flow implemented

--- src/open_company_web/components/su_preview.cljs
+++ src/open_company_web/components/su_preview.cljs
@@ -11,47 +11,40 @@
             [cljsjs.react.dom]
             [cljsjs.clipboard]))
 
-(defn patch-stakeholder-update [owner]
-  (let [title (om/get-state owner :title)
-        topics (om/get-props owner :selected-topics)]
-    (api/patch-stakeholder-update {:title (or title "")
-                                   :sections topics})))
-
 (defn post-stakeholder-update [owner]
-  (om/set-state! owner :su-posting true)
-  (api/share-stakeholder-update))
-
-(defn preview-clicked [owner]
-  (om/set-state! owner :preview-loading true)
-  (patch-stakeholder-update owner))
-
-(defn slack-clicked [owner]
-  (om/set-state! owner :slack-loading true)
-  (patch-stakeholder-update owner))
-
-(defn email-clicked [owner]
-  (om/set-state! owner :email-loading true)
-  (patch-stakeholder-update owner))
+  (let [slack-textarea (.findDOMNode js/ReactDOM (om/get-ref owner "slack-share-textarea"))
+        slack-message (.-value slack-textarea)]
+    (api/share-stakeholder-update slack-message)))
 
 (defn copy-clicked [owner]
   (om/set-state! owner :share-link-copied true))
 
-(defn focus-title [owner]
-  (let [title (.findDOMNode js/ReactDOM (om/get-ref owner "title"))
-        title-val (.-value title)]
-    (.focus title)
-    (set! (.-value title) title-val)))
+(defn complete-url [su-url]
+  (let [protocol      (.. js/document -location -protocol)
+        host          (.. js/document -location -host)
+        fixed-su-slug (subs su-url 10)] ; remove /companies]
+    (str protocol "//" host fixed-su-slug)))
+
+(defn initial-state [owner]
+  (let [data (om/get-props owner)]
+    {:share-via-slack (:share-via-slack data)
+     :share-via-link (:share-via-link data)
+     :share-link-copied false
+     :share-link (when (:latest-su data) (complete-url (:latest-su data)))
+     :slack-sending false
+     :slack-sent false}))
 
 (defn cancel-clicked [owner options]
+  (om/set-state! owner (initial-state owner))
   ((:dismiss-su-preview options)))
 
 (defn slack-send-clicked [owner options]
-  (if (om/get-state owner :slack-posted)
-    ((:dismiss-su-preview options))
+  (if (om/get-state owner :slack-sent)
+    (cancel-clicked owner options)
     (let [slack-message-ta (.findDOMNode js/ReactDOM (om/get-ref owner "slack-share-textarea"))
           slack-message (.-value slack-message-ta)]
-      (om/set-state! owner :slack-posting true)
-      (utils/after 1000 #(api/share-stakeholder-update (or slack-message ""))))))
+      (om/set-state! owner :slack-sending true)
+      (api/share-stakeholder-update (or slack-message "")))))
 
 (defn select-share-link [owner]
   (when-let [input (.findDOMNode js/ReactDOM (om/get-ref owner "share-link-input"))]
@@ -60,130 +53,44 @@
 (defcomponent su-preview [data owner options]
 
   (init-state [_]
-    (let [title          (:su-title data)
-          old-title      (:title (:stakeholder-update (:company-data data)))
-          fixed-title    (if (or (:share-slack data)
-                                 (:share-email data))
-                          title
-                          (or old-title (utils/su-default-title)))]
-      {:title fixed-title
-       :preview-loading false
-       :slack-loading (:share-slack data)
-       :slack-patched false
-       :slack-posting false
-       :slack-posted false
-       :email-loading (:share-email data)
-       :share-link-copied false
-       :su-posting false
-       :su-posted false}))
+    {:share-via-slack (:share-via-slack data)
+     :share-via-link (:share-via-link data)
+     :share-link-copied false
+     :share-link (when (:latest-su data) (complete-url (:latest-su data)))
+     :slack-sending false
+     :slack-sent false})
 
   (did-mount [_]
-    (utils/disable-scroll)
-    (when-not (or (:share-slack data)
-                  (:share-email data))
-      (utils/after 600 #(focus-title owner)))
-    (when (or (:share-slack data)
-              (:share-email data))
-      (patch-stakeholder-update owner)))
+    (when (om/get-state owner :share-link)
+      (js/Clipboard. ".share-link-button"))
+    (utils/disable-scroll))
 
   (will-unmount [_]
     (utils/enable-scroll))
 
   (will-receive-props [_ next-props]
-    (om/set-state! owner :preview-loading false)
-    (cond
-      ; email clicked, patch done, going to post
-      (and (om/get-state owner :email-loading)
-           (not (om/get-state owner :su-posting))
-           (not (om/get-state owner :su-posted)))
-      (post-stakeholder-update owner)
-      ; email clicked, patch done, post done too
-      (om/get-state owner :su-posting)
-      (do
-        (om/set-state! owner :su-posted true)
-        ((:share-done-cb options))
-        (utils/after 600
-          #(let [protocol           (.. js/document -location -protocol)
-                 host               (.. js/document -location -host)
-                 latest-update-slug (dis/latest-stakeholder-update)
-                 fixed-su-slug      (subs latest-update-slug 10)
-                 su-url             (str protocol "//" host fixed-su-slug)]
-             (om/set-state! owner :share-link su-url))))
-      ; slack button pushed
-      (and (om/get-state owner :slack-loading)
-           (not (om/get-state owner :slack-patched))
-           (not (om/get-state owner :slack-posting)))
-      (om/set-state! owner :slack-patched true)
-      ; slack SU posted
-      (and (om/get-state owner :slack-patched)
-           (om/get-state owner :slack-posting))
-      (do
-        (om/set-state! owner :slack-posted true)
-        ((:share-done-cb options)))))
+    ; slack SU posted
+    (when (and (om/get-state owner :share-via-slack)
+               (om/get-state owner :slack-sending)
+               (not (om/get-state owner :slack-sent)))
+      (om/set-state! owner :slack-sent true)))
 
   (did-update [_ _ _]
     (when (om/get-state owner :share-link)
       (js/Clipboard. ".share-link-button")))
 
-  (render-state [_ {:keys [title
-                           preview-loading
-                           slack-loading
-                           email-loading
-                           su-posted
-                           share-link
-                           share-link-copied
-                           slack-patched
-                           slack-posting
-                           slack-posted]}]
+  (render-state [_ {:keys [share-via-slack share-via-link share-link-copied share-link slack-sending slack-sent]}]
     (let [company-data (:company-data data)]
       (dom/div {:class "su-preview"}
         (dom/div {:class (utils/class-set {:su-close-window true
-                                           :share-copy-window email-loading
-                                           :slack-patched-window slack-loading})}
+                                           :share-copy-window share-via-link
+                                           :slack-patched-window share-via-slack})}
           (dom/button {:on-click #(cancel-clicked owner options)}
             (icon :simple-remove {:stroke "4" :color "white" :accent-color "white"})))
         (dom/div {:class (utils/class-set {:su-preview-window true
-                                           :share-link-copy email-loading
-                                           :slack-message slack-loading})}
-          (when (and (not email-loading)
-                     (not slack-loading))
-            (dom/input #js {:className "su-title"
-                            :ref "title"
-                            :placeholder "Update title"
-                            :type "text"
-                            :onChange #(om/set-state! owner :title (.. % -target -value))
-                            :value title}))
-          (when (and (not email-loading)
-                     (not slack-loading))
-            (dom/div {:class "su-preview-box"}
-              (dom/div {:class "separator top-separator"})
-              (dom/div {:class "su-preview-title"} "PREVIEW")
-              (dom/a {:class "preview-button"
-                      :href (oc-urls/stakeholder-update-preview)
-                      :target "_blank"
-                      :on-click #(preview-clicked owner)}
-                (dom/div {:class "link-circle"}
-                  (if preview-loading
-                    (dom/img {:class "small-loading" :src "/img/small_loading.gif"})
-                    (icon :link-72 {:size 20 :color "rgba(78, 90, 107, 0.5)" :accent-color "rgba(78, 90, 107, 0.5)"})))
-                (dom/label {} "VIEW PREVIEW"))
-              (dom/div {:class "separator"})
-              (dom/div {:class "su-preview-ready"} "READY TO SHARE?")
-              (dom/button {:class "ready-slack-button"
-                           :on-click #(slack-clicked owner)}
-                (dom/div {:class "slack-circle"}
-                  (if slack-loading
-                    (dom/img {:class "small-loading" :src "/img/small_loading.gif"})
-                    (dom/img {:class "slack-icon" :src "/img/Slack_Icon.png"})))
-                (dom/label {} "SHARE ON SLACK"))
-              (dom/button {:class "ready-mail-button"
-                           :on-click #(email-clicked owner)}
-                (dom/div {:class "mail-circle"}
-                  (if email-loading
-                    (dom/img {:class "small-loading" :src "/img/small_loading.gif"})
-                    (icon :email-84 {:size 20 :color "rgba(78, 90, 107, 0.5)" :accent-color "rgba(78, 90, 107, 0.5)"})))
-                (dom/label {} "SHARE URL"))))
-          (when email-loading
+                                           :share-link-copy share-via-link
+                                           :slack-message share-via-slack})}
+          (when share-via-link
             (dom/div {:class "su-preview-box"}
               (dom/label {:class "share-link-cta"} "SHARE THIS PRIVATE URL")
               (dom/div {:class "share-link-box"}
@@ -198,25 +105,23 @@
                              :data-clipboard-target "#share-link-input"
                              :on-click #(copy-clicked owner)} (if share-link-copied "COPIED ✓" "COPY")))
               (dom/a {:class "share-link-new-win" :href share-link :target "_blank"} "Open in new window")))
-          (when slack-loading
+          (when share-via-slack
             (dom/div {:class "su-preview-box"}
               (dom/label {:class "slack-share-cta"} "SHARE THIS SNAPSHOT WITH THE MEMBERS OF YOUR SLACK TEAM")
               (dom/textarea #js {:className "slack-share-textarea"
                                  :ref "slack-share-textarea"
                                  :placeholder "Add a note"})))
-          (when slack-patched
+          (when share-via-slack
             (dom/button {:class "slack-send-button"
                          :on-click #(slack-send-clicked owner options)}
               (cond
-                (and slack-patched
-                     slack-posting
-                     (not slack-posted))
-                  (dom/img {:class "small-loading" :src "/img/small_loading.gif"})
-                (and slack-patched
-                     slack-posting
-                     slack-posted)
-                  "SENT ✓"
+                (and slack-sending
+                     (not slack-sent))
+                (dom/img {:class "small-loading" :src "/img/small_loading.gif"})
+                (and slack-sending
+                     slack-sent)
+                "SENT ✓"
                 :else
-                  "SEND")))
+                "SEND")))
           (dom/button {:class "cancel-button"
-                       :on-click #(cancel-clicked owner options)} (if su-posted "DONE" "CANCEL")))))))
+                       :on-click #(cancel-clicked owner options)} (if share-via-link "DONE" "CANCEL")))))))
