8d212e0296bcfd707fa63da2beab9470b577a0c8
bago2k4 <iacopo@rc0.eu>
2016-06-01 20:04:21 +0200

    handle the add custom topic click and title input

--- src/open_company_web/components/ui/add_topic_popover.cljs
+++ src/open_company_web/components/ui/add_topic_popover.cljs
@@ -10,7 +10,9 @@
             [open-company-web.urls :as oc-urls]
             [open-company-web.caches :as caches]
             [open-company-web.router :as router]
-            [open-company-web.lib.utils :as utils]))
+            [open-company-web.dispatcher :as dis]
+            [open-company-web.lib.utils :as utils]
+            [cljsjs.react.dom]))
 
 (defn is-child-of-popover [el]
   (loop [cur-el el]
@@ -43,7 +45,9 @@
     {:active-topics active-topics-list
      :archived-topics-list archived-topics-list
      :unactive-topics (if unactive-equal? (:unactive-topics old-state) unactive-topics)
-     :highlighted-topic nil}))
+     :highlighted-topic nil
+     :adding-custom-topic false
+     :custom-topic-title ""}))
 
 (defn add-topic-click [owner options topic]
   (let [all-topics (om/get-props owner :all-topics)
@@ -58,41 +62,64 @@
 (def esc-key-code 27)
 
 (defn kb-key-down [owner options e]
-  (let [key-code        (.-keyCode e)
-        unactive-topics (om/get-state owner :unactive-topics)]
-    (when (or (= key-code down-arrow-key-code)
+  (when-not (om/get-state owner :adding-custom-topic)
+    (let [key-code        (.-keyCode e)
+          unactive-topics (om/get-state owner :unactive-topics)]
+      (when (or (= key-code down-arrow-key-code)
+                (= key-code up-arrow-key-code)
+                (= key-code enter-key-code)
+                (= key-code esc-key-code))
+        (.stopPropagation e)
+        (.preventDefault e)
+        (cond
+          (= key-code enter-key-code)
+          ; enter key: select topic
+          (when-let [topic (om/get-state owner :highlighted-topic)]
+            (add-topic-click owner options topic))
+          (= key-code esc-key-code)
+          (dismiss-popover options)
+          :else
+          ; arrow key: change focus and scroll the parent div
+          (let [cur-highlighted  (om/get-state owner :highlighted-topic)
+                idx-fn           (cond
+                                   (= key-code down-arrow-key-code) inc
+                                   (= key-code up-arrow-key-code) dec)
+                min-idx          0
+                max-idx          (dec (count unactive-topics))
+                next-highlighted (min (max (idx-fn cur-highlighted) min-idx) max-idx)
+                topics-to-add    (om/get-ref owner "topics-to-add")
+                new-topic-div    (om/get-ref owner (str "potential-topic-" next-highlighted))]
+            (set! (.-scrollTop topics-to-add) (- (.-offsetTop new-topic-div) (.-offsetTop topics-to-add)))
+            (cond
+              (= key-code down-arrow-key-code)
+              (om/set-state! owner :highlighted-topic next-highlighted)
               (= key-code up-arrow-key-code)
-              (= key-code enter-key-code)
-              (= key-code esc-key-code))
-      (.stopPropagation e)
-      (.preventDefault e)
-      (cond
-        (= key-code enter-key-code)
-        ; enter key: select topic
-        (when-let [topic (om/get-state owner :highlighted-topic)]
-          (add-topic-click owner options topic))
-        (= key-code esc-key-code)
-        (dismiss-popover options)
-        :else
-        ; arrow key: change focus and scroll the parent div
-        (let [cur-highlighted  (om/get-state owner :highlighted-topic)
-              idx-fn           (cond
-                                 (= key-code down-arrow-key-code) inc
-                                 (= key-code up-arrow-key-code) dec)
-              min-idx          0
-              max-idx          (dec (count unactive-topics))
-              next-highlighted (min (max (idx-fn cur-highlighted) min-idx) max-idx)
-              topics-to-add    (om/get-ref owner "topics-to-add")
-              new-topic-div    (om/get-ref owner (str "potential-topic-" next-highlighted))]
-          (set! (.-scrollTop topics-to-add) (- (.-offsetTop new-topic-div) (.-offsetTop topics-to-add)))
-          (cond
-            (= key-code down-arrow-key-code)
-            (om/set-state! owner :highlighted-topic next-highlighted)
-            (= key-code up-arrow-key-code)
-            (om/set-state! owner :highlighted-topic next-highlighted)))))))
+              (om/set-state! owner :highlighted-topic next-highlighted))))))))
 
 (defn history-nav [options]
   ((:dismiss-popover options)))
+
+(defn add-custom-topic [owner e]
+  (.preventDefault e)
+  (.stopPropagation e)
+  (om/set-state! owner :adding-custom-topic true))
+
+(defn custom-topic-key-down [owner e]
+  (.stopPropagation e)
+  (let [key-code (.-keyCode e)]
+    (cond
+      (and (= key-code enter-key-code)
+           (pos? (count (om/get-state owner :custom-topic-title))))
+      ; TODO: add title
+      (let [topic-name (str "custom-" (utils/my-uuid))
+            topic-title (om/get-state owner :custom-topic-title)
+            company-data (dis/company-data)
+            sections (:sections company-data)
+            new-sections (update-in sections [:progress] #(conj % topic-name))]
+        (api/patch-sections new-sections))
+      (= key-code esc-key-code)
+      ; ESC: exit adding topic
+      (om/set-state! owner :adding-custom-topic false))))
 
 (defcomponent add-topic-popover [{:keys [all-topics active-topics-list column show-above archived-topics] :as data} owner options]
 
@@ -121,7 +148,12 @@
     (events/unlistenByKey (om/get-state owner :kb-listener))
     (events/unlistenByKey (om/get-state owner :nav-listener)))
 
-  (render-state [_ {:keys [active-topics unactive-topics highlighted-topic archived-topics-list]}]
+  (did-update [_ _ prev-state]
+    (when (and (om/get-state owner :adding-custom-topic)
+               (not (:adding-custom-topic prev-state)))
+      (.focus (.findDOMNode js/ReactDOM (om/get-ref owner "add-custom-topic-input")))))
+
+  (render-state [_ {:keys [active-topics unactive-topics highlighted-topic archived-topics-list adding-custom-topic custom-topic-title]}]
     (dom/div {:class (utils/class-set {:add-topic-popover true
                                        (str "column-" column) true
                                        :show-above show-above})
@@ -154,4 +186,15 @@
                             :ref (str "potential-topic-archived-" idx)
                             :onMouseOver #(om/set-state! owner :highlighted-topic topic)
                             :onClick #(add-topic-click owner options topic)}
-                topic-title))))))))
+                topic-title))))
+        (if-not adding-custom-topic
+          (dom/div {:class "add-custom-topic"
+                    :on-click (partial add-custom-topic owner)} "+ Or Add New Topic")
+          (dom/div {:class "add-custom-topic-container"}
+            (dom/input #js {:type "text"
+                            :className "add-custom-topic-input"
+                            :ref "add-custom-topic-input"
+                            :value custom-topic-title
+                            :onChange #(om/set-state! owner :custom-topic-title (.-value (.-target %)))
+                            :onKeyDown  (partial custom-topic-key-down owner)
+                            :placeholder "Topic title"})))))))
