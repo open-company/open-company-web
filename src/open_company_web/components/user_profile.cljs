f73d0aad2899afd3e9bff9c5d9bd04102ffa880f
bago2k4 <iacopo@rc0.eu>
2016-06-06 21:30:21 +0200

    user profile new style

--- src/open_company_web/components/user_profile.cljs
+++ src/open_company_web/components/user_profile.cljs
@@ -2,40 +2,27 @@
   (:require [om.core :as om :include-macros true]
             [om-tools.core :as om-core :refer-macros (defcomponent)]
             [om-tools.dom :as dom :include-macros true]
-            [open-company-web.components.menu :refer (menu)]
-            [open-company-web.components.navbar :refer (navbar)]
             [open-company-web.router :as router]
             [open-company-web.lib.jwt :as jwt]
-            [open-company-web.lib.utils :as utils]
-            [open-company-web.lib.cookies :as cook]))
+            [open-company-web.components.footer :refer (footer)]
+            [open-company-web.components.ui.back-to-dashboard-btn :refer (back-to-dashboard-btn)]))
 
 (defcomponent user-profile [data owner]
   (render [_]
-    (dom/div {:class "user-profile row"}
-      (om/build menu data)
-      (dom/div {:class "page"}
-        (om/build navbar {})
-        (dom/div {:class "navbar-offset container-fluid"}
-          (dom/div {:class "col-md-12 main"}
-            (dom/div {:class "panel panel-default"}
-              (dom/div {:class "panel-heading"}
-                (dom/h4 {} "User profile")
-                "Your user profile comes from Slack, you can update your profile in Slack.")
-              (dom/div {:class "panel-body"}
-                (dom/span {:class "user-profile-label"} "Name:")
-                (dom/span {:class "user-profile-value"} (jwt/get-key :real-name))
-                (dom/br)(dom/br)
-                (dom/span {:class "user-profile-label"} "Organization:")
-                (dom/span {:class "user-profile-value"} (jwt/get-key :org-name))
-                (dom/br)(dom/br)
-                (dom/span {:class "user-profile-label"} "Email:")
-                (dom/span {:class "user-profile-value"} (jwt/get-key :email))
-                (dom/br)(dom/br)
-                (dom/span {:class "user-profile-label"} "Avatar:")
-                (when (jwt/get-key :avatar)
-                  (dom/img {:class "user-avatar" :src (jwt/get-key :avatar)}))
-                (dom/br)(dom/br)
-                (dom/button {:class "btn btn-danger"
-                             :on-click (fn [e]
-                                         (router/history-back!))}
-                            "Done")))))))))
+    (dom/div {:class "user-profile fullscreen-page"}
+      (om/build back-to-dashboard-btn {})
+      (dom/div {:class "user-profile-internal"}
+        (dom/div {:class "user-profile-title"} "User Info")
+        (dom/div {:class "user-profile-content"}
+          (dom/div {:class "left-column"}
+            (dom/div {:class "user-profile-name-title"} "NAME")
+            (dom/div {:class "user-profile-name"} (jwt/get-key :real-name))
+            (dom/div {:class "user-profile-org-title"} "ORGANIZATION")
+            (dom/div {:class "user-profile-org"} (jwt/get-key :org-name))
+            (dom/div {:class "user-profile-email-title"} "EMAIL")
+            (dom/div {:class "user-profile-email"} (jwt/get-key :email)))
+          (dom/div {:class "right-column"}
+            (dom/div {:class "user-profile-avatar-title"} "AVATAR")
+            (when (jwt/get-key :avatar)
+              (dom/img {:class "user-profile-avatar" :src (jwt/get-key :avatar)})))))
+      (om/build footer data))))
