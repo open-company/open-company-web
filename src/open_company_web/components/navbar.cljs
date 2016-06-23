42bf2744508d1733f3be7d95b584020771bcf8d7
bago2k4 <iacopo@rc0.eu>
2016-06-23 14:53:50 +0200

    remove sharing mode from navbar

--- src/open_company_web/components/navbar.cljs
+++ src/open_company_web/components/navbar.cljs
@@ -2,9 +2,6 @@
   (:require [om.core :as om :include-macros true]
             [om-tools.core :as om-core :refer-macros (defcomponent)]
             [om-tools.dom :as dom :include-macros true]
-            [cljs.core.async :refer (put!)]
-            [dommy.core :as dommy :refer-macros (sel1)]
-            [open-company-web.router :as router]
             [open-company-web.dispatcher :as dis]
             [open-company-web.urls :as oc-urls]
             [open-company-web.lib.jwt :as jwt]
@@ -15,11 +12,6 @@
             [open-company-web.components.ui.login-button :refer (login-button)]
             [open-company-web.components.ui.small-loading :refer (small-loading)]
             [open-company-web.components.ui.company-avatar :refer (company-avatar)]))
-
-(defn close-preview-clicked [e]
-  (.preventDefault e)
-  (.stopPropagation e)
-  (router/nav! (oc-urls/company)))
 
 (defn menu-click [owner e]
   (when e
@@ -33,25 +25,6 @@
                           (* 20 (dec columns-num))      ; cards right margin
                           (when (> columns-num 1) 60))] ; x margins if needed
       (dom/nav {:class "oc-navbar group"}
-        (when (:su-preview data)
-          (dom/div {:class "su-snapshot-preview"}
-            (dom/div {:class "su-snapshot-preview-internal group"
-                      :style #js {:width (str header-width "px")}}
-              (when (> columns-num 1)
-                (dom/div {:class "su-snapshot-buttons group"}
-                  (dom/button {:class "ready-slack-button"
-                             :on-click (:share-slack-cb options)}
-                    (dom/i {:class "fa fa-slack"})
-                    (dom/label {} "SHARE ON SLACK"))
-                  (dom/button {:class "ready-mail-button"
-                             :on-click (:share-link-cb options)}
-                    (if email-loading
-                      (om/build small-loading {:animating true})
-                      (icon :link-72 {:size 20 :stroke "4" :color "rgba(78, 90, 107, 0.7)" :accent-color "rgba(78, 90, 107, 0.7)"}))
-                    (dom/label {} "SHARE URL"))))
-              (dom/button {:class "close-preview"
-                           :on-click close-preview-clicked}
-                (icon :simple-remove {:stroke "4" :color "rgba(255, 255, 255, 0.8)" :accent-color "rgba(255, 255, 255, 0.8)"})))))
         (dom/div {:class "oc-navbar-header"
                   :style #js {:width (str header-width "px")}}
           (om/build company-avatar data)
