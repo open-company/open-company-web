e789c9c5feaea9debd8c2872f5d9242fe75a7635
bago2k4 <iacopo@rc0.eu>
2016-06-24 20:17:17 +0200

    remove all topic navigation stuff, recalculate topics when data gets updated from server, initialize su sections with all sections if it's empty

--- src/open_company_web/components/su_snapshot_preview.cljs
+++ src/open_company_web/components/su_snapshot_preview.cljs
@@ -16,6 +16,7 @@
             [open-company-web.components.ui.icon :as i]
             [open-company-web.components.topics-columns :refer (topics-columns)]
             [open-company-web.components.su-preview-dialog :refer (su-preview-dialog)]
+            [open-company-web.components.tooltip :refer (tooltip)]
             [cljs-dynamic-resources.core :as cdr]
             [goog.events :as events]
             [goog.events.EventType :as EventType]
@@ -36,32 +37,6 @@
         topics (om/get-state owner :su-topics)]
     (api/patch-stakeholder-update {:title (or title "")
                                    :sections topics})))
-
-(defn switch-topic [owner is-left?]
-  (when (and (om/get-state owner :topic-navigation)
-             (om/get-state owner :selected-topic)
-             (nil? (om/get-state owner :tr-selected-topic)))
-    (let [selected-topic (om/get-state owner :selected-topic)
-          company-data   (dis/company-data (om/get-props owner))
-          topics         (:sections (:stakeholder-update company-data))
-          current-idx    (.indexOf (vec topics) selected-topic)]
-      (if is-left?
-        ;prev
-        (let [prev-idx (mod (dec current-idx) (count topics))
-              prev-topic (get (vec topics) prev-idx)]
-          (om/set-state! owner :tr-selected-topic prev-topic))
-        ;next
-        (let [next-idx (mod (inc current-idx) (count topics))
-              next-topic (get (vec topics) next-idx)]
-          (om/set-state! owner :tr-selected-topic next-topic))))))
-
-(defn kb-listener [owner e]
-  (let [key-code (.-keyCode e)]
-    (when (= key-code 39)
-      ;next
-      (switch-topic owner false))
-    (when (= key-code 37)
-      (switch-topic owner true))))
 
 (defn focus-title [owner]
   (when-not (om/get-state owner :title-focused)
@@ -86,102 +61,50 @@
                                                     :link-posted false})))
 
 (defn setup-sortable [owner options]
-  (when (and (om/get-state owner :did-mount)
-             (om/get-state owner :sortable-loaded))
-    (when-let [list-node (sel1 [:div.topics-column])]
-      (.create js/Sortable list-node #js {:handle ".topic-internal"
-                                          :onStart #(dommy/add-class! list-node :dragging)
-                                          :onEnd (fn [_]
-                                                    (let [topic-nodes (sel [:div.topic-row])
-                                                          topics      (vec (for [t topic-nodes] (.-topic (.-dataset t))))]
-                                                      (om/set-state! owner :su-topics topics))
-                                                    (dommy/remove-class! list-node :dragging))}))))
+    (when-let [list-node (js/jQuery (sel1 [:div.topics-column]))]
+      (.sortable list-node #js {:cursor "move"
+                                :opacity 1})))
 
 (defn add-su-section [owner topic]
   (om/update-state! owner :su-topics #(conj % topic)))
 
-(defn top-position [el]
-  (loop [yPos 0
-         element el]
-    (if element
-      (recur (+ yPos (.-offsetTop element))
-             (.-offsetParent element))
-      yPos)))
-
-(defn mouse-moved [owner e]
-  (let [y-pos (+ (.-clientY e) (.-scrollTop (.-body js/document)))
-        all-topics (sel [:div.topic-row])
-        hover-topic (loop [idx 0
-                           topic (get all-topics idx)
-                           last-topic nil]
-                      ; (println "   - idx:" idx)
-                      ; (println "   - topic:" (.-topic (.-dataset topic)) "-" (top-position topic))
-                      ; (println "   - last-topic:" last-topic)
-                      ; (println "   " (.-topic (.-dataset topic)) ": " y-pos "<" (top-position topic) "=" (< y-pos (top-position topic)))
-                      (if (< y-pos (top-position topic))
-                        last-topic
-                        (if (>= (inc idx) (count all-topics))
-                          nil
-                          (recur (inc idx)
-                                 (get all-topics (inc idx))
-                                 topic))))]
-
-    (println "hide all:" (count (sel [:div.share-remove-container])))
-    (for [topic (sel [:div.share-remove-container])]
-      (dommy/add-class! topic :hidden))
-    (when hover-topic
-      (println "show for:" (.-topic (.-dataset hover-topic)) "=>" (sel1 [(str "div#share-remove-" (.-topic (.-dataset hover-topic)))]))
-      (dommy/remove-class! (sel1 [(str "div#share-remove-" (.-topic (.-dataset hover-topic)))]) :hidden)
-      ; (om/set-state! owner :show-share-remove (.-topic (.-dataset hover-topic)))
-      )))
+(defn title-from-section-name [owner section]
+  (let [company-data (dis/company-data (om/get-props owner))]
+    (->> section keyword (get company-data) :title)))
 
 (defcomponent su-snapshot-preview [data owner options]
 
   (init-state [_]
-    (utils/add-channel "fullscreen-topic-save" (chan))
-    (utils/add-channel "fullscreen-topic-cancel" (chan))
-    (cdr/add-script! "/lib/Sortable.js/Sortable.js"
-                     (fn []
-                       (om/set-state! owner :sortable-loaded true)
-                       (setup-sortable owner options)))
-    (let [su-data (stakeholder-update-data data)]
+    (let [company-data (dis/company-data data)
+          su-data (stakeholder-update-data data)
+          su-sections (if (empty? (:sections su-data))
+                        (flatten (vals (:sections company-data)))
+                        (:sections su-data))]
       {:columns-num (responsive/columns-num)
-       :su-topics (:sections su-data)
+       :su-topics su-sections
        :title-focused false
        :title (if (clojure.string/blank? (:title su-data))  (utils/su-default-title) (:title su-data))
        :show-su-dialog false
        :link-loading false
        :slack-loading false
        :link-posting false
-       :link-posted false}))
+       :link-posted false
+       :su-tooltip-dismissed false}))
 
   (did-mount [_]
     (om/set-state! owner :did-mount true)
     (setup-sortable owner options)
     (events/listen js/window EventType/RESIZE #(om/set-state! owner :columns-num (responsive/columns-num)))
-    (focus-title owner)
-    (when-not (utils/is-test-env?)
-      (let [kb-listener (events/listen js/window EventType/KEYDOWN (partial kb-listener owner))
-            swipe-listener (js/Hammer (sel1 [:div#app]))];(.-body js/document))]
-        (om/set-state! owner :kb-listener kb-listener)
-        (om/set-state! owner :swipe-listener swipe-listener)
-        (.on swipe-listener "swipeleft" (fn [e] (switch-topic owner true)))
-        (.on swipe-listener "swiperight" (fn [e] (switch-topic owner false))))
-      (let [mouse-move-listener (events/listen js/window EventType/MOUSEMOVE (partial mouse-moved owner))]
-        )))
-
-  (will-unmount [_]
-    (utils/remove-channel "fullscreen-topic-save")
-    (utils/remove-channel "fullscreen-topic-cancel")
-    (when-not (utils/is-test-env?)
-      (events/unlistenByKey (om/get-state owner :kb-listener))
-      (let [swipe-listener (om/get-state owner :swipe-listener)]
-        (.off swipe-listener "swipeleft")
-        (.off swipe-listener "swiperight"))))
+    (focus-title owner))
 
   (will-receive-props [_ next-props]
     (when-not (= (dis/company-data data) (dis/company-data next-props))
-      (om/set-state! owner :su-topics (:sections (stakeholder-update-data next-props))))
+      (let [company-data (dis/company-data next-props)
+            su-data      (stakeholder-update-data next-props)
+            su-sections  (if (empty? (:sections su-data))
+                           (flatten (vals (:sections company-data)))
+                           (:sections su-data))]
+        (om/set-state! owner :su-topics su-sections)))
     ; share via link
     (when (om/get-state owner :link-loading)
       (if-not (om/get-state owner :link-posting)
@@ -204,16 +127,23 @@
                            slack-loading
                            link-posting
                            link-posted
-                           su-topics]}]
+                           su-topics
+                           su-tooltip-dismissed]}]
     (let [company-data (dis/company-data data)
           su-data      (stakeholder-update-data data)
           card-width   (responsive/calc-card-width)
           ww           (.-clientWidth (sel1 js/document :body))
-          total-width  (if (> ww 413) (str card-width "px") "auto")
+          total-width  (if (> ww 413) (str (+ card-width 100) "px") "auto")
           su-subtitle  (str "— " (utils/date-string (js/Date.) true))
-          topics-to-add (reduce utils/vec-dissoc (flatten (vals (:sections company-data))) su-topics)]
+          su-sections  (:sections su-data)
+          topics-to-add (sort #(compare (title-from-section-name owner %1) (title-from-section-name owner %2)) (reduce utils/vec-dissoc (flatten (vals (:sections company-data))) su-topics))]
       (dom/div {:class (utils/class-set {:su-snapshot-preview true
                                          :main-scroll true})}
+        (when (and (seq company-data)
+                   (empty? (:sections su-data))
+                   (not su-tooltip-dismissed))
+          (om/build tooltip {:cta "THIS IS A PREVIEW OF YOUR SNAPSHOT. DRAG TOPICS TO REORDER, OR CLICK THE X TO REMOVE A TOPIC."}
+                            {:opts {:dismiss-tooltip #(om/set-state! owner :su-tooltip-dismissed true)}}))
         (om/build menu data)
         (dom/div {:class "page snapshot-page"}
           (dom/div {:class "su-snapshot-header"}
@@ -265,8 +195,9 @@
               (dom/div {:class "su-preview-add-section"
                         :style #js {:width total-width}}
                 (dom/div {:class "add-header"} "ADD TOPICS")
-                (for [topic topics-to-add]
+                (for [topic topics-to-add
+                      :let [title (->> topic keyword (get company-data) :title)]]
                   (dom/div {:class "add-section"
                             :on-click #(add-su-section owner topic)}
                     (i/icon :check-square-09 {:accent-color "transparent" :size 16 :color "black"})
-                    (dom/div {:class "section-name"} (name topic))))))))))))
+                    (dom/div {:class "section-name"} title)))))))))))
