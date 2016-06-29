51589a35da420208a8ba3802f774a45ad81ee30f
bago2k4 <iacopo@rc0.eu>
2016-06-29 12:46:26 +0200

    avoid to show log in screen, then no companies then the companies list when there is one

--- src/open_company_web/components/list_companies.cljs
+++ src/open_company_web/components/list_companies.cljs
@@ -22,20 +22,21 @@
 
   (render-state [_ {:keys [columns-num]}]
     (utils/update-page-title "OpenCompany - Startup Transparency Made Simple")
-    (let [company-list (:companies data)
-          card-width (responsive/calc-card-width)]
-      (dom/div {:class "list-companies"}
-        (om/build menu data)
-        (dom/div {:class "page"}
-          (om/build navbar {:card-width card-width
-                            :sharing-mode false
-                            :columns-num columns-num
-                            :menu-open menu-open
-                            :auth-settings (:auth-settings data)})
-          (dom/div {:class "navbar-offset group"}
-            (if (:loading data)
-              (dom/h4 "Loading companies...")
-              (if (pos? (count company-list))
-                (dom/ul {:class "companies"}
-                  (om/build-all list-page-item company-list))
-                (dom/h2 "No companies found.")))))))))
+    (when (:companies data)
+      (let [company-list (:companies data)
+            card-width (responsive/calc-card-width)]
+        (dom/div {:class "list-companies"}
+          (om/build menu data)
+          (dom/div {:class "page"}
+            (om/build navbar {:card-width card-width
+                              :sharing-mode false
+                              :columns-num columns-num
+                              :menu-open menu-open
+                              :auth-settings (:auth-settings data)})
+            (dom/div {:class "navbar-offset group"}
+              (if (:loading data)
+                (dom/h4 "Loading companies...")
+                (if (pos? (count company-list))
+                  (dom/ul {:class "companies"}
+                    (om/build-all list-page-item company-list))
+                  (dom/h2 "No companies found."))))))))))
