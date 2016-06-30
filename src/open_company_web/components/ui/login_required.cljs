5910d4ca9898298449239a430cb99b0c15028798
bago2k4 <iacopo@rc0.eu>
2016-06-30 14:38:29 +0200

    fix login-required component to reflect new login required mocks

--- src/open_company_web/components/ui/login_required.cljs
+++ src/open_company_web/components/ui/login_required.cljs
@@ -3,24 +3,35 @@
             [om-tools.core :as om-core :refer-macros (defcomponent)]
             [om-tools.dom :as dom :include-macros true]
             [open-company-web.lib.jwt :as jwt]
-            [open-company-web.components.ui.login-button :as login]))
+            [open-company-web.components.ui.login-button :as login]
+            [open-company-web.components.ui.back-to-dashboard-btn :refer (back-to-dashboard-btn)]))
 
 (defcomponent login-required [{:keys [welcome] :as data} owner]
   (render [_]
-    (dom/div {:class "max-width-3 p4 mx-auto center mb4"}
-      (if (jwt/jwt)
-        (dom/p {:class "mb2"}
-          "Sorry, you don't have access to this company's dashboard."
-          (dom/br)
-          (dom/br)
-          (dom/small
-              (dom/a {:href "#"
-                      :on-click #(login/login! (:extended-scopes-url (:auth-settings data)) %)}
-               "Sign in with a different Slack account ")
-            "to access different dashboards."))
-        (dom/div
-          (dom/p {:class "mb2"}
-            (if welcome
-              "OpenCompany, See the big picture."
-              "Please log in to view this dashboard."))
-          (om/build login/login-button data))))))
+    (dom/div {:class "login-required"}
+      (when-not welcome
+        (om/build back-to-dashboard-btn {:button-cta "OPENCOMPANY.COM"}))
+      (dom/div {:class "center mx-auto max-width-3"}
+        (if (jwt/jwt)
+          (dom/div {}
+            (dom/p {:class "login-rquired-cta"}
+              "Sorry, you don't have access to this company's dashboard."
+              (dom/br)
+              (dom/br)
+              (dom/small
+                  (dom/a {:href "#"
+                          :on-click #(login/login! (:extended-scopes-url (:auth-settings data)) %)}
+                   "Sign in with a different Slack account ")
+                "to access different dashboards.")))
+          (dom/div
+            (dom/p {:class "login-rquired-cta"}
+              (if welcome
+                "OpenCompany, See the Big Picture"
+                "Please Sign In to Access This Screen"))
+            (dom/a {:href "#"
+                    :class "login-button"
+                    :on-click #(login/login! (:extended-scopes-url (:auth-settings data)) %)}
+              (dom/img {:src "https://api.slack.com/img/sign_in_with_slack.png"}))))
+        (dom/div {:class "logo-container"}
+          (dom/img {:class "logo-gold" :src "/img/oc-logo-gold.svg"})
+          (dom/div {:class "logo-cta"} "OpenCompany makes it easy to see the big picture. Companies are stronger when everyone knows what matters most."))))))
