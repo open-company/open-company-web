6b56ed11cb6e00f28eb68fd0db28a5bc4fb2438d
bago2k4 <iacopo@rc0.eu>
2016-07-04 19:55:43 +0200

    new login component

---
+++ src/open_company_web/components/login.cljs
@@ -0,0 +1,19 @@
+(ns open-company-web.components.login
+  (:require [om.core :as om :include-macros true]
+            [om-tools.core :as om-core :refer-macros (defcomponent)]
+            [om-tools.dom :as dom :include-macros true]
+            [open-company-web.lib.jwt :as jwt]
+            [open-company-web.lib.responsive :as responsive]
+            [open-company-web.components.footer :refer (footer)]
+            [open-company-web.components.ui.login-required :refer (login-required)]))
+
+(defcomponent login [data owner]
+
+  (render [_]
+    (let [card-width  (responsive/calc-card-width)]
+      (dom/div {:class "login fullscreen-page"}
+        (dom/div {:class "login-internal"}
+          (om/build login-required data))
+        (om/build footer {:su-preview false
+                          :card-width card-width
+                          :columns-num (responsive/columns-num)})))))
