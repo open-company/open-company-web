0bfa53af552ee6b3dd6906c4f58d325f7dcfc04e
Sean Johnson <sean@snootymonkey.com>
2016-07-04 09:07:53 -0400

    Normalize the 3 variants of the login page.

--- src/open_company_web/components/ui/login_required.cljs
+++ src/open_company_web/components/ui/login_required.cljs
@@ -12,26 +12,18 @@
       (when-not welcome
         (om/build back-to-dashboard-btn {:button-cta "OPENCOMPANY.COM"}))
       (dom/div {:class "center mx-auto max-width-3"}
-        (if (jwt/jwt)
-          (dom/div {}
-            (dom/p {:class "login-rquired-cta"}
-              "Sorry, you don't have access to this company's dashboard."
-              (dom/br)
-              (dom/br)
-              (dom/small
-                  (dom/a {:href "#"
-                          :on-click #(login/login! (:extended-scopes-url (:auth-settings data)) %)}
-                   "Sign in with a different Slack account ")
-                "to access different dashboards.")))
-          (dom/div
-            (dom/p {:class "login-rquired-cta"}
-              (if welcome
-                "OpenCompany, See the Big Picture"
-                "Please Sign In to Access This Screen"))
+        (dom/div (dom/p {:class "login-rquired-cta"}
+          (cond 
+            (jwt/jwt)
+              "Sorry, you don't have access to this company dashboard. Please sign in with a different Slack account."
+            welcome
+              "OpenCompany, See the Big Picture"
+            :else
+              "Please sign in to access this company dashboard."))
             (dom/a {:href "#"
                     :class "login-button"
                     :on-click #(login/login! (:extended-scopes-url (:auth-settings data)) %)}
-              (dom/img {:src "https://api.slack.com/img/sign_in_with_slack.png"}))))
-        (dom/div {:class "logo-container"}
-          (dom/img {:class "logo-gold" :src "/img/oc-logo-gold.svg"})
-          (dom/div {:class "logo-cta"} "OpenCompany makes it easy to see the big picture. Companies are stronger when everyone knows what matters most."))))))
+              (dom/img {:src "https://api.slack.com/img/sign_in_with_slack.png"})))
+          (dom/div {:class "logo-container"}
+            (dom/img {:class "logo-gold" :src "/img/oc-logo-gold.svg"})
+            (dom/div {:class "logo-cta"} "OpenCompany makes it easy to see the big picture. Companies are strongest when everyone knows what matters most."))))))
