8feca464c3bfbaa17f9983ac2c8243d3af280754
bago2k4 <iacopo@rc0.eu>
2016-07-05 12:07:11 +0200

    pass optional click callback to execute when the back button is clicked

--- src/open_company_web/components/ui/back_to_dashboard_btn.cljs
+++ src/open_company_web/components/ui/back_to_dashboard_btn.cljs
@@ -4,18 +4,23 @@
             [om-tools.dom :as dom :include-macros true]
             [open-company-web.router :as router]
             [open-company-web.dispatcher :as dis]
-            [open-company-web.urls :as oc-urls]))
+            [open-company-web.urls :as oc-urls]
+            [open-company-web.lib.utils :as utils]))
 
-(defn btn-clicked [e]
-  (let [company-slug (or (router/current-company-slug) (dis/last-company-slug))
-        redirect-url (if company-slug
-                        (oc-urls/company company-slug)
-                        oc-urls/home)]
-    (router/nav! redirect-url)))
+(defn btn-clicked [owner e]
+  (utils/event-stop e)
+  (let [click-cb (om/get-props owner :click-cb)]
+    (if (fn? click-cb)
+      (click-cb)
+      (let [company-slug (or (router/current-company-slug) (dis/last-company-slug))
+            redirect-url (if company-slug
+                            (oc-urls/company company-slug)
+                            oc-urls/home)]
+        (router/nav! redirect-url)))))
 
 (defcomponent back-to-dashboard-btn [data owner]
   (render [_]
     (let [button-cta (or (:button-cta data) "BACK TO DASHBOARD")]
       (dom/div {:class "back-to-dashboard-row"}
         (dom/button {:class "back-to-dashboard btn-reset btn-outline"
-                     :on-click (partial btn-clicked)} (str "← " button-cta))))))
+                     :on-click (partial btn-clicked owner)} (str "← " button-cta))))))
