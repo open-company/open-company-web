374c8220800b70be2e44cb415dabca54476dbf57
Martin Klepsch <martinklepsch@googlemail.com>
2016-07-06 09:51:36 +0200

    rum-ify user-avatar component

--- test/test/open_company_web/components/ui/user_avatar.cljs
+++ test/test/open_company_web/components/ui/user_avatar.cljs
@@ -2,10 +2,9 @@
   (:require [cljs.test :refer-macros [deftest async testing is are use-fixtures]]
             [cljs-react-test.simulate :as sim]
             [cljs-react-test.utils :as tu]
-            [om.core :as om :include-macros true]
+            [rum.core :as rum]
             [dommy.core :as dommy :refer-macros [sel1 sel]]
             [open-company-web.components.ui.user-avatar :refer [user-avatar]]
-            [om.dom :as dom :include-macros true]
             [open-company-web.router :as router]))
 
 (enable-console-print!)
@@ -13,15 +12,12 @@
 ; dynamic mount point for components
 (def ^:dynamic c)
 
-(def test-atom {})
-
 (deftest test-user-avatar-component
   (testing "User avatar component"
     (router/set-route! ["companies" "test"]
                        {:slug "test"})
     (let [c (tu/new-container!)
-          app-state (atom test-atom)
-          _ (om/root user-avatar app-state {:target c})
+          _ (rum/mount (user-avatar nil) c)
           user-avatar-node (sel1 c [:div.user-avatar])]
       (is (not (nil? user-avatar-node)))
       (tu/unmount! c))))
