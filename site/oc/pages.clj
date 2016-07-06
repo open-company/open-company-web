ed22f3bf7ad4d90411363c9f6b8babeb99d5df46
bago2k4 <iacopo@rc0.eu>
2016-07-06 17:01:26 +0200

    load medium-editor css when page loads

--- site/oc/pages.clj
+++ site/oc/pages.clj
@@ -165,6 +165,9 @@
           [:link {:type "text/css" :rel "stylesheet" :href "/css/emojione/autocomplete.css?oc_deploy_key"}]
           ;; Google fonts Domine and OpenSans
           [:link {:type "text/css", :rel "stylesheet", :href "https://fonts.googleapis.com/css?family=Open+Sans:400,700,600,300,800|Domine:400,700"}]]
+          ;;  Medium Editor css
+          [:link {:type "text/css", :rel "stylesheet", :href "/css/medium-editor/medium-editor.css?oc_deploy_key"}]]
+          [:link {:type "text/css", :rel "stylesheet", :href "/css/medium-editor/default.css?oc_deploy_key"}]]
    :body [:body
           [:div#app [:div.oc-loading.active [:div.oc-loading-internal]]]
           [:div#oc-loading]
