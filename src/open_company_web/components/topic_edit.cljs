1b91be0c88d310dd94ca718454f2404335ee40d8
bago2k4 <iacopo@rc0.eu>
2016-07-07 12:24:28 +0200

    add char counter, fix buttons positions

--- src/open_company_web/components/topic_edit.cljs
+++ src/open_company_web/components/topic_edit.cljs
@@ -15,6 +15,13 @@
             [open-company-web.components.ui.filestack-uploader :refer (filestack-uploader)]
             [cljsjs.medium-editor])) ; pulled in for cljsjs externs
 
+(def title-max-length 20)
+(def headline-max-length 100)
+(def snippet-max-length 500)
+(def title-alert-limit 3)
+(def headline-alert-limit 10)
+(def snippet-alert-limit 50)
+
 (defn scroll-to-topic-top [topic]
   (let [body-scroll (.-scrollTop (.-body js/document))
         topic-scroll-top (utils/offset-top topic)]
@@ -32,12 +39,56 @@
         topic (om/get-props owner :section)]
     (when-let [headline-el    (sel1 [(str "div#foce-headline-" section-name)])]
       (let [headline-editor (new js/MediumEditor headline-el (clj->js (utils/medium-editor-options "Headline")))]
-        (.subscribe headline-editor "editableInput" (fn [event editable] (dis/dispatch! [:foce-input :headline (.-innerHTML headline-el)])))))
+        (.subscribe headline-editor
+                    "editableInput"
+                    (fn [event editable]
+                      (dis/dispatch! [:foce-input :headline (.-innerHTML headline-el)])
+                      (let [v (.-innerText headline-el)
+                            remaining-chars (- headline-max-length (count v))]
+                        (om/set-state! owner :char-count remaining-chars)
+                        (om/set-state! owner :char-count-alert (< remaining-chars headline-alert-limit)))))))
     (when-let [snippet-el (sel1 [(str "div#foce-snippet-" section-name)])]
       (let [snippet-placeholder (if (:placeholder topic-data) (utils/get-topic-body topic-data topic) "")
             snippet-editor      (new js/MediumEditor snippet-el (clj->js (utils/medium-editor-options snippet-placeholder)))]
-        (.subscribe snippet-editor "editableInput" (fn [event editable] (dis/dispatch! [:foce-input :snippet (.-innerHTML snippet-el)])))))
+        (.subscribe snippet-editor
+                    "editableInput"
+                    (fn [event editable]
+                      (dis/dispatch! [:foce-input :snippet (.-innerHTML snippet-el)])
+                      (let [v (.-innerText snippet-el)
+                            remaining-chars (- snippet-max-length (count v))]
+                        (om/set-state! owner :char-count remaining-chars)
+                        (om/set-state! owner :char-count-alert (< remaining-chars snippet-alert-limit)))))))
     (js/emojiAutocomplete)))
+
+(defn check-headline-count [owner e]
+  (when-let [headline (sel1 (str "div#foce-headline-" (name (dis/foce-section-key))))]
+    (let [headline-value (.-innerText headline)]
+      (when (and (not= (.-keyCode e) 8)
+                 (not= (.-keyCode e) 16)
+                 (not= (.-keyCode e) 17)
+                 (not= (.-keyCode e) 40)
+                 (not= (.-keyCode e) 38)
+                 (not= (.-keyCode e) 13)
+                 (not= (.-keyCode e) 27)
+                 (not= (.-keyCode e) 37)
+                 (not= (.-keyCode e) 39)
+                 (>= (count headline-value) headline-max-length))
+        (.preventDefault e)))))
+
+(defn check-snippet-count [owner e]
+  (when-let [snippet (sel1 (str "div#foce-snippet-" (name (dis/foce-section-key))))]
+    (let [snippet-value (.-innerText snippet)]
+      (when (and (not= (.-keyCode e) 8)
+                 (not= (.-keyCode e) 16)
+                 (not= (.-keyCode e) 17)
+                 (not= (.-keyCode e) 40)
+                 (not= (.-keyCode e) 38)
+                 (not= (.-keyCode e) 13)
+                 (not= (.-keyCode e) 27)
+                 (not= (.-keyCode e) 37)
+                 (not= (.-keyCode e) 39)
+                 (>= (count snippet-value) snippet-max-length))
+        (.preventDefault e)))))
 
 (defcomponent topic-edit [{:keys [currency
                                   prev-rev
@@ -46,12 +97,14 @@
   (init-state [_]
     (let [topic-data          (dis/foce-section-data)]
       {:initial-headline (:headline topic-data)
-       :initial-snippet  (:snippet topic-data)}))
+       :initial-snippet  (:snippet topic-data)
+       :char-count nil
+       :char-count-alert false}))
 
   (did-mount [_]
     (utils/after 1000 #(setup-edit owner)))
 
-  (render-state [_ {:keys [initial-headline initial-snippet]}]
+  (render-state [_ {:keys [initial-headline initial-snippet char-count char-count-alert]}]
     (let [section             (dis/foce-section-key)
           topic-data          (dis/foce-section-data)
           section-kw          (keyword section)
@@ -92,33 +145,60 @@
         ;; Topic title
         (dom/input {:class "topic-title"
                     :value (:title topic-data)
+                    :max-length title-max-length
                     :type "text"
-                    :on-change #(dis/dispatch! [:foce-input :title (.. % -target -value)])})
+                    :on-blur #(om/set-state! owner :char-count nil)
+                    :on-change #(let [v (.. % -target -value)
+                                      remaining-chars (- title-max-length (count v))]
+                                  (dis/dispatch! [:foce-input :title v])
+                                  (om/set-state! owner :char-count remaining-chars)
+                                  (om/set-state! owner :char-count-alert (< remaining-chars title-alert-limit)))})
+        (dom/button {:class "topic-pencil-button btn-reset"
+                     :on-click (partial pencil-click owner options)}
+          (i/icon :pencil {:size 16
+                           :color gray-color
+                           :accent-color gray-color}))
         ;; Topic headline
         (dom/div {:class "topic-headline-inner emoji-autocomplete"
                   :id (str "foce-headline-" (name section))
+                  :on-key-up   #(check-headline-count owner %)
+                  :on-key-down #(check-headline-count owner %)
+                  :on-focus    #(check-headline-count owner %)
+                  :onBlur #(do
+                             (check-headline-count owner %)
+                             (om/set-state! owner :char-count nil))
                   :dangerouslySetInnerHTML #js {"__html" initial-headline}})
         (dom/div #js {:className "topic-body topic-snippet emoji-autocomplete"
-                      :ref "topic-snippet"
                       :id (str "foce-snippet-" (name section))
+                      :ref "topic-snippet"
+                      :onKeyUp   #(check-snippet-count owner %)
+                      :onKeyDown #(check-snippet-count owner %)
+                      :onFocus    #(check-snippet-count owner %)
+                      :onBlur #(do
+                                 (check-snippet-count owner %)
+                                 (om/set-state! owner :char-count nil))
                       :dangerouslySetInnerHTML #js {"__html" initial-snippet}})
-        (dom/div {:class "topic-foce-buttons"}
-          (when-not image-header
-            (dom/button {:class "btn-reset camera"}
-              (i/icon :camera-20 {:size 16
-                                  :color (oc-colors/get-color-by-kw :oc-gray-5)
-                                  :accent-color (oc-colors/get-color-by-kw :oc-gray-5)})))
-          (dom/button {:class "btn-reset add-image"}
+        (dom/div {:class "topic-foce-buttons group"}
+          (dom/button {:class "btn-reset add-content"
+                       :title "Add more content"
+                       :data-toggle "tooltip"
+                       :data-placeholder "top"}
             (i/icon :simple-add {:size 16
                                  :color (oc-colors/get-color-by-kw :oc-gray-5)
                                  :accent-color (oc-colors/get-color-by-kw :oc-gray-5)}))
-          (dom/button {:class "topic-pencil-button btn-reset"
-                       :on-click (partial pencil-click owner options)}
-            (i/icon :pencil {:size 16
-                             :color gray-color
-                             :accent-color gray-color})))
-        (dom/div {:class "topic-foce-footer"}
-          (dom/button {:class "btn-reset btn-outline"
-                       :on-click #(do (utils/event-stop %) (dis/dispatch! [:start-foce nil]))} "CANCEL")
-          (dom/button {:class "btn-reset btn-solid"
-                       :disabled false} "SAVE"))))))
+          (when-not image-header
+            (dom/button {:class "btn-reset camera"
+                         :title "Add an image"
+                         :data-toggle "tooltip"
+                         :data-placeholder "top"}
+              (i/icon :camera-20 {:size 16
+                                  :color (oc-colors/get-color-by-kw :oc-gray-5)
+                                  :accent-color (oc-colors/get-color-by-kw :oc-gray-5)}))))
+        (dom/div {:class "topic-foce-footer group"}
+          (dom/div {:class "topic-foce-footer-left"}
+            (dom/label {:class (str "char-counter" (when char-count-alert " char-count-alert"))} char-count))
+          (dom/div {:class "topic-foce-footer-right"}
+            (dom/button {:class "btn-reset btn-solid"
+                         :disabled false} "SAVE")
+            (dom/button {:class "btn-reset btn-outline"
+                         :on-click #(do (utils/event-stop %) (dis/dispatch! [:start-foce nil]))} "CANCEL")))))))
