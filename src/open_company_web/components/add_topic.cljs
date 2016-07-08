8f0d3f2ac93885df4445b291a9052229e63e88cf
bago2k4 <iacopo@rc0.eu>
2016-07-08 16:51:23 +0200

    new add topic component

---
+++ src/open_company_web/components/add_topic.cljs
@@ -0,0 +1,54 @@
+(ns open-company-web.components.add-topic
+  (:require [om.core :as om :include-macros true]
+            [om-tools.core :as om-core :refer-macros [defcomponent]]
+            [om-tools.dom :as dom :include-macros true]
+            [dommy.core :refer-macros (sel1)]
+            [open-company-web.router :as router]
+            [open-company-web.caches :as caches]
+            [open-company-web.components.ui.add-topic-popover :refer (add-topic-popover)]))
+
+(defn add-topic-click [owner]
+  (println "add-topic-click")
+  (when-not (om/get-state owner :show-add-topic-popover)
+    (om/set-state! owner :show-add-topic-popover true)))
+
+(defn show-popover-above? [owner]
+  (let [scroll             (.-scrollTop (.-body js/document))
+        win-height        (- (.-clientHeight (.-documentElement js/document)) (.-clientHeight (sel1 [:nav.oc-navbar])) 4)
+        popover-offsettop (.-offsetTop (om/get-ref owner "topic"))
+        add-topic-pos     (- popover-offsettop scroll)]
+    (> add-topic-pos (/ win-height 2))))
+
+(defn get-all-sections [slug]
+  (println "aa" @caches/new-sections)
+  (let [categories-data (:categories (slug @caches/new-sections))
+        all-category-sections (apply concat
+                                     (for [category categories-data]
+                                       (let [cat-name (:name category)
+                                             sections (:sections category)]
+                                         (map #(assoc % :category cat-name) sections))))]
+    (apply merge
+           (map #(hash-map (keyword (:section-name %)) %) all-category-sections))))
+
+(defcomponent add-topic [{:keys [active-topics archived-topics column] :as data} owner options]
+
+  (init-state [_]
+    {:show-add-topic-popover false})
+
+  (render-state [_ {:keys [show-add-topic-popover]}]
+    (dom/div #js {:className (str "topic group add-topic" (when show-add-topic-popover " active"))
+                  :onClick #(add-topic-click owner)
+                  :ref "topic"}
+      (dom/div {:class "topic-title"} "+ ADD TOPIC")
+      (when show-add-topic-popover
+        (let [slug (keyword (router/current-company-slug))
+              all-sections (get-all-sections slug)
+              update-active-topics (:update-active-topics options)
+              list-data {:all-topics all-sections
+                         :active-topics-list active-topics
+                         :archived-topics archived-topics
+                         :show-above (show-popover-above? owner)
+                         :column column}
+              list-opts {:did-change-active-topics update-active-topics
+                         :dismiss-popover #(om/set-state! owner :show-add-topic-popover false)}]
+          (om/build add-topic-popover list-data {:opts list-opts}))))))
