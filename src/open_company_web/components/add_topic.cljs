dc2cf629e615f863312bf7b9a4a7723ac24c6d3d
Martin Klepsch <martinklepsch@googlemail.com>
2016-07-30 01:35:29 +0200

    simplify add topic component

--- src/open_company_web/components/add_topic.cljs
+++ src/open_company_web/components/add_topic.cljs
@@ -1,55 +1,90 @@
 (ns open-company-web.components.add-topic
-  (:require [om.core :as om :include-macros true]
-            [om-tools.core :as om-core :refer-macros [defcomponent]]
-            [om-tools.dom :as dom :include-macros true]
-            [dommy.core :refer-macros (sel1)]
+  (:require [rum.core :as rum]
+            [clojure.set :as cs]
+            [clojure.string :as string]
+            [open-company-web.lib.utils :as utils]
             [open-company-web.router :as router]
-            [open-company-web.caches :as caches]
-            [open-company-web.components.ui.add-topic-popover :refer (add-topic-popover)]))
+            [open-company-web.dispatcher :as dis]
+            [open-company-web.caches :as caches]))
 
-(defn add-topic-click [owner]
-  (om/set-state! owner :show-add-topic-popover true))
+;; This should be defined as a derivative specifically suited to rendering the
+;; topic adding component
+(defn get-all-sections
+  "Return all sections for the current company no matter it's state (archived, active, inactive)
+   e.g {:section a-string :title a-string :category a-string*}"
+  []
+  (let [company-data (dis/company-data)
+        slug         (keyword (router/current-company-slug))
+        categories   (:categories company-data)]
+    (into
+     (:archived company-data)
+     (for [cat (:categories (get @caches/new-sections slug))
+           sec (:sections cat)]
+       (-> sec
+           (assoc :section (:section-name sec))
+           (select-keys [:title :section])
+           (assoc :category (:name cat)))))))
 
-(defn show-popover-above? [owner]
-  (let [scroll             (.-scrollTop (.-body js/document))
-        win-height        (- (.-clientHeight (.-documentElement js/document)) (.-clientHeight (sel1 [:nav.oc-navbar])) 4)
-        popover-offsettop (.-offsetTop (om/get-ref owner "topic"))
-        add-topic-pos     (- popover-offsettop scroll)]
-    (> add-topic-pos (/ win-height 2))))
+(rum/defcs custom-topic-input
+  < (rum/local "" ::topic-title)
+  [s submit-fn]
+  [:div.mt1.flex
+   [:input.npt.mr1.p1.flex-auto
+    {:type "text",
+     :value @(::topic-title s)
+     :on-change #(reset! (::topic-title s) (.. % -target -value))
+     :style {:font-size "16px"}
+     :placeholder "Custom topic title"}]
+   [:button.btn-reset.btn-outline
+    {:disabled (string/blank? @(::topic-title s))
+     :on-click #(let [topic-name     (str "custom-" (utils/my-uuid))
+                      new-topic-data {:title @(::topic-title s)
+                                      :headline ""
+                                      :body ""
+                                      :section topic-name
+                                      :placeholder true}]
+                  (submit-fn :progress topic-name new-topic-data))} "Add"]])
 
-(defn get-all-sections [slug]
-  (let [categories-data (:categories (slug @caches/new-sections))
-        all-category-sections (apply concat
-                                     (for [category categories-data]
-                                       (let [cat-name (:name category)
-                                             sections (:sections category)]
-                                         (map #(assoc % :category cat-name) sections))))]
-    (apply merge
-           (map #(hash-map (keyword (:section-name %)) %) all-category-sections))))
+(defn chunk-topics
+  "Partition the provided sequences as if they were one with `::archived` inbetween"
+  [inactive archived]
+  (let [w-marker   (cond-> inactive (seq archived) (conj ::archived))
+        items      (into w-marker archived)
+        chunk-size (inc (quot (count items) 2))]
+    (partition-all chunk-size items)))
 
-(defn dismiss-popover [owner]
-  (om/set-state! owner :show-add-topic-popover false))
-
-(defcomponent add-topic [{:keys [active-topics archived-topics column] :as data} owner options]
-
-  (init-state [_]
-    {:show-add-topic-popover false})
-
-  (render-state [_ {:keys [show-add-topic-popover]}]
-    (dom/div #js {:className (str "topic group add-topic" (when show-add-topic-popover " active"))
-                  :onClick #(add-topic-click owner)
-                  :ref "topic"}
-      (dom/div {:class "topic-title"} "+ ADD TOPIC")
-      (when show-add-topic-popover
-        (let [slug (keyword (router/current-company-slug))
-              all-sections (get-all-sections slug)
-              update-active-topics (:update-active-topics options)
-              list-data {:topic-order (:new-section-order (slug @caches/new-sections))
-                         :all-topics all-sections
-                         :active-topics-list active-topics
-                         :archived-topics archived-topics
-                         :show-above (show-popover-above? owner)
-                         :column column}
-              list-opts {:did-change-active-topics update-active-topics
-                         :dismiss-popover #(dismiss-popover owner)}]
-          (om/build add-topic-popover list-data {:opts list-opts}))))))
+(rum/defcs add-topic
+  < (rum/local false ::expanded?)
+  [s {:keys [active-topics archived-topics column update-active-topics]}]
+  (if @(::expanded? s)
+    (let [all-sections (into {} (for [s (get-all-sections)]
+                                  [(keyword (:section s)) s]))
+          inactive-not-archived (filterv (complement (cs/union (set active-topics) (set archived-topics)))
+                                         (keys all-sections))
+          chunked (chunk-topics inactive-not-archived archived-topics)]
+      [:div.card.p--card
+       [:div.open-sans.small-caps.bold.mb2.gray5
+        [:span.mr1 "Suggested Topics"]
+        [:span.dimmed-gray.btn-reset
+         {:on-click #(reset! (::expanded? s) false)}
+         "Cancel"]]
+       [:div.mxn2.clearfix
+        (for [col chunked]
+          [:div.col.col-6.px2
+           {:key (first col)}
+           (for [topic col]
+             (if (= ::archived topic)
+               [:span.block.open-sans.small-caps.bold.mb1.mt2 {:key topic} "Archived"]
+               (let [topic-full (get all-sections topic)]
+                 [:div.mb1.btn-reset.yellow-line-hover-child
+                  {:key topic
+                   :on-click #(do (update-active-topics (or (:category topic-full) :progress) (:section topic-full))
+                                  (reset! (::expanded? s) false))}
+                  [:span.child
+                   (:title topic-full)
+                   (:section-name topic-full)]])))])]
+       (custom-topic-input #(do (update-active-topics %1 %2 %3)
+                                (reset! (::expanded? s) false)))])
+    [:div.p3.border.center.btn-reset
+     {:on-click #(reset! (::expanded? s) true)}
+     [:span.small-caps.bold "+ Add Topic"]]))
