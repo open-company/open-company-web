1cea06a66f3903601688a16da4eb266417c99398
bago2k4 <iacopo@rc0.eu>
2016-08-06 20:40:51 +0200

    close the menu clicking on the page content

--- src/open_company_web/components/menu.cljs
+++ src/open_company_web/components/menu.cljs
@@ -52,12 +52,27 @@
     (let [listener-key (events/listen page EventType/TRANSITIONEND #(on-transition-end owner body))]
       (om/set-state! owner :transition-end-listener listener-key))))
 
+(defn body-clicked [owner e]
+  ; if the menu is open and the user click on the content (not the menu)
+  ; close the menu
+  (when (and (dommy/has-class? (.-body js/document) :menu-visible)
+             (utils/event-inside? e (sel1 [:div.page])))
+    (utils/event-stop e)
+    (toggle-menu owner true)))
+
 (defcomponent menu [data owner options]
 
   (did-mount [_]
     (when (:menu-open data)
       (let [body (sel1 [:body])]
-        (dommy/add-class! body :menu-visible))))
+        (dommy/add-class! body :menu-visible)))
+    (let [body-click-listener (events/listen (.-body js/document)
+                                             EventType/CLICK
+                                             #(body-clicked owner %))]
+      (om/set-state! owner :body-click-listener body-click-listener)))
+
+  (will-unmount [_]
+    (events/unlistenByKey (om/get-state owner :body-click-listener)))
 
   (will-receive-props [_ next-props]
     (cond
