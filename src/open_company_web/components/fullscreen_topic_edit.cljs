813a23803eb52be154f66ffaad7aa7a1d7a804c9
Sean Johnson <sean@snootymonkey.com>
2016-08-09 08:50:36 -0400

    Fix error.

--- src/open_company_web/components/fullscreen_topic_edit.cljs
+++ src/open_company_web/components/fullscreen_topic_edit.cljs
@@ -670,7 +670,7 @@
                           :dangerouslySetInnerHTML topic-body})
             (dom/div {:class "topc-edit-top-box-footer"}
               (dom/div {:class "fullscreen-topic-emoji-picker left mr2"
-                        :style {:display (if (nil? file-upload-ui) "block" "none")}}
+                        :style {:display (if (nil? (sel1 [:div#file-upload-ui])) "block" "none")}}
                 (emoji-picker {:add-emoji-cb (fn [editor emoji]
                                                (when (= editor (sel1 (str "div#topic-edit-body-" (name topic))))
                                                  (force-hide-placeholder owner)))
