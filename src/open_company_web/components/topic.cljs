206cbcdc24557898175eb1ed8d685f2d89d97044
bago2k4 <iacopo@rc0.eu>
2016-09-14 10:40:09 +0200

    move tooltip above remove button and below drag button, fix text wrapping

--- src/open_company_web/components/topic.cljs
+++ src/open_company_web/components/topic.cljs
@@ -246,7 +246,7 @@
                     :id (str "share-remove-" (name section))}
             (dom/button {:class "btn-reset share-remove"
                          :data-toggle "tooltip"
-                         :data-placement "right"
+                         :data-placement "top"
                          :title "Remove topic from this update."
                          :on-click #(when (contains? options :share-remove-click) ((:share-remove-click options) (name section)))}
               (i/icon :simple-remove {:color "rgba(78, 90, 107, 0.5)" :size 12 :stroke 4 :accent-color "rgba(78, 90, 107, 0.5)"}))))
@@ -255,7 +255,7 @@
                     :id (str "share-dnd-" (name section))}
             (dom/button {:class "btn-reset share-dnd"
                          :data-toggle "tooltip"
-                         :data-placement "right"
+                         :data-placement "bottom"
                          :title "Drag and drop topic to reorder."}
               (dom/i {:class "fa fa-arrows"}))))
         (dom/div #js {:className "topic-anim group"
