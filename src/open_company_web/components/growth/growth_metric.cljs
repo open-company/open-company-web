adaed6e5829d63a5eaa57518bef8db3daac72fc3
Sean Johnson <sean@snootymonkey.com>
2016-09-14 16:43:17 -0400

    Remove jitter from missing period(s) in growth data.

--- src/open_company_web/components/growth/growth_metric.cljs
+++ src/open_company_web/components/growth/growth_metric.cljs
@@ -62,7 +62,7 @@
                              :sub-label-color (occ/get-color-by-kw :oc-gray-5)
                              :labels {:value {:position :bottom
                                               :order 1
-                                              :value-presenter #(:label %2)
+                                              :value-presenter #(or (:label %2) "-")
                                               :value (occ/get-color-by-kw :oc-gray-5) 
                                               :label-presenter #(:sub-label %2)
                                               :label-color (occ/get-color-by-kw :oc-gray-5)}}
