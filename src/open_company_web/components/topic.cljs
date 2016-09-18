2c2b4fef5b29825a683aa1a3d3428a318f286ad8
bago2k4 <iacopo@rc0.eu>
2016-09-18 15:36:23 +0200

    add draggable-topic class only if the user have write access

--- src/open_company_web/components/topic.cljs
+++ src/open_company_web/components/topic.cljs
@@ -226,8 +226,8 @@
         (api/load-revision next-rev slug section-kw))
       (dom/div #js {:className (utils/class-set {:topic true
                                                  :group true
-                                                 :draggable-topic (:pin topic-data)
-                                                 :not-draggable-topic (not (:pin topic-data))
+                                                 :draggable-topic (and (not (:read-only-company data)) (:pin topic-data))
+                                                 :not-draggable-topic (or (:read-only-company data) (not (:pin topic-data)))
                                                  :no-foce (and foce-active (not is-foce))
                                                  :sharing-selected (and sharing-mode share-selected)})
                     :style #js {:width (if (responsive/is-mobile?) "auto" (str card-width "px"))
