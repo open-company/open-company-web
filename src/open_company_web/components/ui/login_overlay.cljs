3cc1291e8dac5d3b76eeedae830fbcc3fae7cc76
bago2k4 <iacopo@rc0.eu>
2016-09-20 14:36:26 +0200

    add login-with-email compnent and style

--- src/open_company_web/components/ui/login_overlay.cljs
+++ src/open_company_web/components/ui/login_overlay.cljs
@@ -9,9 +9,12 @@
   (utils/event-stop e)
   (dis/dispatch! [:show-login-overlay false]))
 
-(rum/defcs login-overlay < rum/reactive 
-                           {:will-mount (fn [s] (dommy/add-class! (sel1 [:body]) :no-scroll) s)
-                            :will-unmount (fn [s] (dommy/remove-class! (sel1 [:body]) :no-scroll) s)}
+(def dont-scroll
+  {:will-mount (fn [s] (dommy/add-class! (sel1 [:body]) :no-scroll) s)
+   :will-unmount (fn [s] (dommy/remove-class! (sel1 [:body]) :no-scroll) s)})
+
+(rum/defcs login-with-slack < rum/reactive
+                              dont-scroll
   [state]
   [:div.login-overlay-container.group
     {:on-click (partial close-overlay)}
@@ -21,5 +24,32 @@
       [:button.btn-reset.mt2.login-button
         {:on-click #(login/login! (:extended-scopes-url (:auth-settings @dis/app-state)) %)}
         [:img {:src "https://api.slack.com/img/sign_in_with_slack.png"}]]
+      [:div.login-overlay-footer.p2.mt1.group
+        [:a.left {:on-click #(dis/dispatch! [:show-login-overlay :email])} "DON’T HAVE AN ACCOUNT? SIGN UP NOW"]]]])
+
+(rum/defcs login-with-email < rum/reactive
+                              dont-scroll
+  [state]
+  [:div.login-overlay-container.group
+    {:on-click (partial close-overlay)}
+    [:div.login-overlay.login-with-email.group
+      {:on-click #(utils/event-stop %)}
+      [:button.close {:on-click (partial close-overlay)} [:i.fa.fa-times]]
+      [:div.p2.group
+        [:div.sing-in-cta.mb3 "Sign in"]
+        [:form.sign-in-form
+          [:div.sign-in-label-container
+            [:label.sign-in-label "EMAIL"]]
+          [:div.sign-in-field-container
+            [:input.sign-in-field {:value "" :type "text" :name "email"}]]
+          [:div.sign-in-label-container
+            [:label.sign-in-label "PASSWORD"]]
+          [:div.sign-in-field-container
+            [:input.sign-in-field {:value "" :type "password" :name "pswd"}]]
+          [:div.group.pb2.my3
+            [:div.left.forgot-password
+              [:a {} "FORGOT PASSWORD?"]]
+            [:div.right
+              [:button.btn-reset.btn-solid "SIGN IN"]]]]]
       [:div.login-overlay-footer.p2.mt1.group
         [:a.left {} "DON’T HAVE AN ACCOUNT? SIGN UP NOW"]]]])
