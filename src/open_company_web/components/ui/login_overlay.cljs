e6cc3d599a583b7e505730a492285d77ab72897f
bago2k4 <iacopo@rc0.eu>
2016-09-20 19:36:47 +0200

    add password reset component, add style for password reset, change all titles and labels font family and weight to headers font

--- src/open_company_web/components/ui/login_overlay.cljs
+++ src/open_company_web/components/ui/login_overlay.cljs
@@ -13,21 +13,27 @@
   {:will-mount (fn [s] (dommy/add-class! (sel1 [:body]) :no-scroll) s)
    :will-unmount (fn [s] (dommy/remove-class! (sel1 [:body]) :no-scroll) s)})
 
-(rum/defcs login-with-slack < rum/reactive
-                              dont-scroll
+(rum/defcs login-signup-with-slack < rum/reactive
+                                     dont-scroll
   [state]
   [:div.login-overlay-container.group
     {:on-click (partial close-overlay)}
     [:div.login-overlay.login-with-slack.center
       {:on-click #(utils/event-stop %)}
+      [:div.group
+        (cond
+          (= (:show-login-overlay (rum/react dis/app-state)) :signup-with-slack)
+          [:div.sign-in-cta.m2.left "Sign Up"]
+          :else
+          [:div])]
       [:button.close {:on-click (partial close-overlay)} [:i.fa.fa-times]]
       [:button.btn-reset.mt2.login-button
         {:on-click #(login/login! (:extended-scopes-url (:auth-settings @dis/app-state)) %)}
         [:img {:src "https://api.slack.com/img/sign_in_with_slack.png"}]]
       [:div.login-with-email
-        [:a {:on-click #(do (utils/event-stop %) (dis/dispatch! [:show-login-overlay :email]))} "OR SIGN IN VIA EMAIL"]]
+        [:a {:on-click #(do (utils/event-stop %) (dis/dispatch! [:show-login-overlay :login-with-email]))} "OR SIGN IN VIA EMAIL"]]
       [:div.login-overlay-footer.p2.mt1.group
-        [:a.left {:on-click #(dis/dispatch! [:show-login-overlay :sign-up])} "DON’T HAVE AN ACCOUNT? SIGN UP NOW"]]]])
+        [:a.left {:on-click #(dis/dispatch! [:show-login-overlay :signup-with-slack])} "DON’T HAVE AN ACCOUNT? SIGN UP NOW"]]]])
 
 (rum/defcs login-with-email < rum/reactive
                               dont-scroll
@@ -50,8 +56,32 @@
             [:input.sign-in-field {:value "" :type "password" :name "pswd"}]]
           [:div.group.pb2.my3
             [:div.left.forgot-password
-              [:a {} "FORGOT PASSWORD?"]]
+              [:a {:on-click #(dis/dispatch! [:show-login-overlay :password-reset])} "FORGOT PASSWORD?"]]
             [:div.right
               [:button.btn-reset.btn-solid "SIGN IN"]]]]]
       [:div.login-overlay-footer.p2.mt1.group
-        [:a.left {} "DON’T HAVE AN ACCOUNT? SIGN UP NOW"]]]])
+        [:a.left {:on-click #(do (utils/event-stop %) (dis/dispatch! [:show-login-overlay :signup-with-email]))} "DON’T HAVE AN ACCOUNT? SIGN UP NOW"]]]])
+
+(rum/defcs password-reset < rum/reactive
+                            dont-scroll
+  [state]
+  [:div.login-overlay-container.group
+    {:on-click (partial close-overlay)}
+    [:div.login-overlay.password-reset
+      {:on-click #(utils/event-stop %)}
+      [:button.close {:on-click (partial close-overlay)} [:i.fa.fa-times]]
+      [:div.p2.group
+        [:div.password-reset-cta.mb3 "Password Reset"]
+        [:form.password-reset-form
+          [:div.password-reset-label-container
+            [:label.password-reset-label "PLEASE ENTER YOUR EMAIL ADDRESS"]]
+          [:div.password-reset-field-container
+            [:input.password-reset-field {:value "" :type "text" :name "email"}]]
+          [:div.group.pb2.mt3
+            [:div.right.ml1
+              [:button.btn-reset.btn-solid
+                "RESET PASSWORD"]]
+            [:div.right
+              [:button.btn-reset.btn-outline
+                {:on-click #(dis/dispatch! [:show-login-overlay nil])}
+                "CANCEL"]]]]]]])
