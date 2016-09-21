036887d5359de3b5a2ea8cbd7351577b19ccb840
bago2k4 <iacopo@rc0.eu>
2016-09-21 15:38:19 +0200

    fix css and html to reflect new company name setup design

--- src/open_company_web/components/company_editor.cljs
+++ src/open_company_web/components/company_editor.cljs
@@ -4,8 +4,10 @@
             [om-tools.dom :as dom :include-macros true]
             [open-company-web.lib.jwt :as jwt]
             [open-company-web.lib.oc-colors :as occ]
+            [open-company-web.lib.responsive :as responsive]
             [open-company-web.components.ui.navbar :refer (navbar)]
             [open-company-web.components.ui.small-loading :as loading]
+            [open-company-web.components.ui.footer :refer (footer)]
             [open-company-web.dispatcher :as dis]
             [open-company-web.lib.utils :as utils]))
 
@@ -34,44 +36,24 @@
       (utils/after 1 #(dis/dispatch! [:input [:company-editor :name] (jwt/get-key :org-name)]))))
 
   (render-state [_ {:keys [loading]}]
-    (dom/div {:class "company-editor"
-              :style {:border-top (str "4px solid " (occ/get-color-by-kw :yellow))}}
-      (dom/div {:class "col-12 p3"}
-        (dom/button {:on-click #(dis/dispatch! [:logout])
-                     :class "btn-reset btn-outline"}
-          "Back to Opencompany.com"))
-      (dom/div {:class "container"}
+    (dom/div {:class "company-editor"}
+      (dom/div {:class (str "group fullscreen-page "  (if (jwt/jwt) "with-small-footer" "with-footer"))}
         (dom/div {:class "col-md-7 col-md-offset-2 p0"}
-          (dom/h2 {:class "domine mb3"} "Get Started"))
+          (dom/h2 {:class "domine mb3"} "Company Setup"))
         (dom/div {:class "col-md-7 col-md-offset-2 bg-gray p3"}
           (dom/form {:on-submit (partial create-company-clicked owner)}
             (dom/div {:class "form-group"}
-              (dom/label {:class "small-caps h6 bold block"} "Company Name")
+              (dom/label {:class "small-caps h6 bold block"} "COMPANY NAME")
               (dom/input {:type "text"
                           :class "domine h4 p2 bg-white-05 md-col-9 border-none"
+                          :style #js {:width "100%"}
                           :placeholder "Simple name without the Inc., LLC, etc."
                           :value (-> data :company-editor :name)
-                          :on-change #(dis/dispatch! [:input [:company-editor :name] (.. % -target -value)])}))
-
-            ;; Slug preview/input
-            #_(dom/div {:class "md-col-9 py2 overflow-scroll"}
-                (dom/label {:class "caps h6 bold block"} "Slug")
-                (dom/span {:class "domine h4 pl2 py2 bg-white-05"} "opencompany.com/")
-                (dom/input {:type "text"
-                            :class "domine h4 pr2 py2 bg-white-05 border-none"
-                            :placeholder "..."
-                            :value (or (-> data :company-editor :slug) (-> data :company-editor :name))
-                            :on-change #(dis/dispatch! [:input [:company-editor :slug] (.. % -target -value)])})))
-          ;; Description
-          #_(dom/div {:class "form-group"}
-              (dom/label {:class "col"} "Description")
-              (dom/input {:type "text"
-                          :class "form-control"
-                          :placeholder "Simple company description or tagline (e.g. 'A messaging app for teams')"
-                          :value (-> data :company-editor :description)
-                          :on-change #(dis/dispatch! [:input [:company-editor :description] (.. % -target -value)])}))
-            (dom/button {:class "btn-reset btn-solid"
+                          :on-change #(dis/dispatch! [:input [:company-editor :name] (.. % -target -value)])})))
+            (dom/button {:class "btn-reset btn-solid right"
                          :on-click (partial create-company-clicked owner)}
                         (when loading
                           (loading/small-loading {:class "left mt1"}))
-                        (dom/label {:class (str "mt1" (when loading " ml2"))} "Setup Your Company")))))))
+                        (dom/label {:class (str "mt1" (when loading " ml2"))} "NEXT →"))))
+      (om/build footer {:columns-num (responsive/columns-num)
+                        :card-width (responsive/calc-card-width)}))))
