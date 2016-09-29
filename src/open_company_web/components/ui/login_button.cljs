557612ea51d8eb15357c3ef91a485eddc507b9fe
bago2k4 <iacopo@rc0.eu>
2016-09-29 15:36:10 +0200

    remove unused deps, add login-overlays handle, show login overlay when the user click the login button

--- src/open_company_web/components/ui/login_button.cljs
+++ src/open_company_web/components/ui/login_button.cljs
@@ -1,16 +1,8 @@
 (ns open-company-web.components.ui.login-button
   (:require [rum.core :as rum]
-            [open-company-web.api :as api]
-            [open-company-web.urls :as oc-urls]
-            [open-company-web.lib.cookies :as cook]
-            [open-company-web.lib.utils :as utils]
-            [open-company-web.router :as router]
             [open-company-web.dispatcher :as dis]
-            [open-company-web.local-settings :as ls]))
-
-(defn login! [auth-url e]
-  (.preventDefault e)
-  (dis/dispatch! [:login-with-slack auth-url]))
+            [open-company-web.lib.utils :as utils]
+            [open-company-web.components.ui.login-overlay :refer (login-overlays-handler)]))
 
 (rum/defcs login-button < rum/reactive
                           {:will-mount (fn [s]
@@ -18,7 +10,8 @@
                                           (dis/dispatch! [:get-auth-settings]))
                                         s)}
   [s auth-settings]
-  [:button.btn-reset
-    {:on-click #(login! (:extended-scopes-url (:slack auth-settings)) %)}
-    (when (:auth-settings (rum/react dis/app-state))
-      [:img {:src "https://api.slack.com/img/sign_in_with_slack.png"}])])
+  [:div.login-button
+    (login-overlays-handler (rum/react dis/app-state))
+    [:button.btn-reset.signup-signin
+      {:on-click #(dis/dispatch! [:show-login-overlay :login-with-slack])}
+      "Sign In / Sign Up"]])
