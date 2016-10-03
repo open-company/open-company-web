747af586572b8bcc4532dd089d2d3a2c85ac932c
bago2k4 <iacopo@rc0.eu>
2016-10-03 17:39:16 +0200

    user invitation component

---
+++ src/open_company_web/components/user_invitation.cljs
@@ -0,0 +1,57 @@
+(ns open-company-web.components.user-invitation
+  (:require [rum.core :as rum]
+            [open-company-web.dispatcher :as dis]
+            [open-company-web.lib.utils :as utils]))
+
+(rum/defc invite-row
+  [invitation]
+  [:tr
+    [:td [:div.value (:real-name invitation)]]
+    [:td [:div.value (:email invitation)]]
+    [:td [:div (clojure.string/upper-case (:status invitation))]]
+    [:td
+      (cond
+        (= (clojure.string/lower-case (:status invitation)) "pending")
+        [:div
+          [:button.btn-reset
+            {:data-placement "top"
+             :data-toggle "tooltip"
+             :data-container "body"
+             :title "RESEND INVITE"
+             :on-click #(dis/dispatch! [:user-invitation-action (:user-id invitation) "invite"])}
+            [:i.fa.fa-share]]
+          [:button.btn-reset
+            {:data-placement "top"
+             :data-toggle "tooltip"
+             :data-container "body"
+             :title "CANCEL INVITE"
+             :on-click #(dis/dispatch! [:user-invitation-action (:user-id invitation) "delete"])}
+            [:i.fa.fa-times]]]
+        (= (clojure.string/lower-case (:status invitation)) "joined")
+        [:div
+          [:button.btn-reset
+            {:data-placement "top"
+             :data-toggle "tooltip"
+             :data-container "body"
+             :title "REMOVE USER"
+             :on-click #(dis/dispatch! [:user-invitation-action (:user-id invitation) "delete"])}
+            [:i.fa.fa-trash-o]]]
+        :else
+        [:div])]])
+
+(rum/defc user-invitation < {:did-mount (fn [s]
+                                        (when-not (utils/is-test-env?)
+                                          (.tooltip (js/$ "[data-toggle=\"tooltip\"]")))
+                                        s)}
+  [invitations]
+  [:div.my3.um-invitations-box.container.col-12.group
+    [:table.table
+      [:thead
+        [:tr
+          [:th "NAME"]
+          [:th "EMAIL"]
+          [:th "STATUS"]
+          [:th "ACTIONS"]]]
+      [:tbody
+        (for [invitation invitations]
+          (rum/with-key (invite-row invitation) (str "invitation-tr-" (:user-id invitation))))]]])
