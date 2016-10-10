d02ae353d0b0c842f3b211a91b6927cd2b9a3e09
bago2k4 <iacopo@rc0.eu>
2016-10-10 22:37:25 +0200

    show error message if the user invited is already present in the users list, reset error message when editing the email field

--- src/open_company_web/components/user_management.cljs
+++ src/open_company_web/components/user_management.cljs
@@ -32,24 +32,31 @@
     [:div.my4.um-invite.group
       [:div.um-invite-label
         "INVITE USER BY EMAIL ADDRESS"]
-       [:div
-         [:input.left.um-invite-field.email
-           {:name "um-invite"
-            :type "email"
-            :autoCapitalize "none"
-            :value (:email (:um-invite (rum/react dis/app-state)))
-            :pattern "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$"
-            :on-change #(dis/dispatch! [:input [:um-invite :email] (.. % -target -value)])
-            :placeholder "Email address"}]
-        [:button.right.btn-reset.btn-solid.um-invite-send
-           {:disabled (not (utils/valid-email? (:email (:um-invite (rum/react dis/app-state)))))
-            :on-click #(let [email (:email (:um-invite @dis/app-state))]
-                         (if (utils/valid-email? email)
-                           (dis/dispatch! [:invite-by-email email])
-                           (dis/dispatch! [:input [:invite-by-email-error] true])))}
-          "SEND INVITE"]
+        [:div
+          [:div.group
+            [:input.left.um-invite-field.email
+              {:name "um-invite"
+               :type "email"
+               :autoCapitalize "none"
+               :value (:email (:um-invite (rum/react dis/app-state)))
+               :pattern "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$"
+               :on-change #(dis/dispatch! [:invite-by-email-change (.. % -target -value)])
+               :placeholder "Email address"}]
+            [:button.right.btn-reset.btn-solid.um-invite-send
+              {:disabled (not (utils/valid-email? (:email (:um-invite (rum/react dis/app-state)))))
+               :on-click #(let [email (:email (:um-invite @dis/app-state))]
+                            (if (utils/valid-email? email)
+                              (dis/dispatch! [:invite-by-email email])
+                              (dis/dispatch! [:input [:invite-by-email-error] true])))}
+             "SEND INVITE"]]
         (when (:invite-by-email-error (rum/react dis/app-state))
-          [:span.small-caps.red.mt1.left "An error occurred, please try again."])]]
+          [:div
+            (cond
+              (and (= (:invite-by-email-error (rum/react dis/app-state)) :user-exists)
+                   (:email (:um-invite @dis/app-state)))
+              [:span.small-caps.red.mt1.left (str (:email (:um-invite @dis/app-state)) " is already a user.")]
+              :else
+              [:span.small-caps.red.mt1.left "An error occurred, please try again."])])]]
     [:div.my2.um-byemail-container.group.hidden
       [:div.group
         [:span.left.ml1.um-byemail-anyone-span
