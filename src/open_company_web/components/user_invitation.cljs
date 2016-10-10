d9f9ea7d8b45d7bf7ee5daac34474e18b1a5e248
bago2k4 <iacopo@rc0.eu>
2016-10-10 22:36:40 +0200

    change logic to show the action buttons, base the choise on the links and then the status

--- src/open_company_web/components/user_invitation.cljs
+++ src/open_company_web/components/user_invitation.cljs
@@ -17,43 +17,44 @@
         [:td (when-not (clojure.string/blank? (:real-name invitation)) [:div.value (:real-name invitation)])])
       [:td [:div (clojure.string/upper-case (:status invitation))]]
       [:td
-        (cond
-          (:loading invitation)
+        (if (:loading invitation)
+          ; if it's loading show the spinner
+          [:div (small-loading)]
           [:div
-            (small-loading)]
-          (= (clojure.string/lower-case (:status invitation)) "pending")
-          [:div
-            [:button.btn-reset.invite-row-action
-              {:data-placement "top"
-               :data-toggle "tooltip"
-               :data-container "body"
-               :title "RESEND INVITE"
-               :on-click #(let [company-data (dis/company-data)]
-                            (user-invitation-action
-                              invitation
-                              "invite"
-                              {:email (:email invitation)
-                               :company-name (:name company-data)
-                               :logo (or (:logo company-data) "")}))}
-              [:i.fa.fa-share]]
-            [:button.btn-reset.invite-row-action
-              {:data-placement "top"
-               :data-toggle "tooltip"
-               :data-container "body"
-               :title "CANCEL INVITE"
-               :on-click #(user-invitation-action invitation "delete")}
-              [:i.fa.fa-times]]]
-          (= (clojure.string/lower-case (:status invitation)) "active")
-          [:div
-            [:button.btn-reset.invite-row-action
-              {:data-placement "top"
-               :data-toggle "tooltip"
-               :data-container "body"
-               :title "REMOVE USER"
-               :on-click #(user-invitation-action invitation "delete")}
-              [:i.fa.fa-trash-o]]]
-          :else
-          [:div])]]))
+            ; if it has an invite link show a resend invite button
+            (when (utils/link-for (:links invitation) "invite")
+              [:button.btn-reset.invite-row-action
+                {:data-placement "top"
+                 :data-toggle "tooltip"
+                 :data-container "body"
+                 :title "RESEND INVITE"
+                 :on-click #(let [company-data (dis/company-data)]
+                              (user-invitation-action
+                                invitation
+                                "invite"
+                                {:email (:email invitation)
+                                 :company-name (:name company-data)
+                                 :logo (or (:logo company-data) "")}))}
+                [:i.fa.fa-share]])
+            ; if it has a delete link
+            (when (utils/link-for (:links invitation) "delete")
+              (if (= (clojure.string/lower-case (:status invitation)) "pending")
+                ; and it's pending show a cancel invite button
+                [:button.btn-reset.invite-row-action
+                  {:data-placement "top"
+                   :data-toggle "tooltip"
+                   :data-container "body"
+                   :title "CANCEL INVITE"
+                   :on-click #(user-invitation-action invitation "delete")}
+                  [:i.fa.fa-times]]
+                ; if it's not pending show a remove user button
+                [:button.btn-reset.invite-row-action
+                  {:data-placement "top"
+                   :data-toggle "tooltip"
+                   :data-container "body"
+                   :title "REMOVE USER"
+                   :on-click #(user-invitation-action invitation "delete")}
+                  [:i.fa.fa-trash-o]]))])]]))
 
 (rum/defc user-invitation < {:did-mount (fn [s]
                                         (when-not (utils/is-test-env?)
