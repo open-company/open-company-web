42084f9a0b5e27207239b5325e5e8bcd1d840c4e
bago2k4 <iacopo@rc0.eu>
2016-10-11 22:37:02 +0200

    avoid to render login-overlays in tests or the derivatives will crash them

--- src/open_company_web/components/list_companies.cljs
+++ src/open_company_web/components/list_companies.cljs
@@ -34,7 +34,8 @@
             card-width (responsive/calc-card-width)]
         (dom/div {:class "list-companies"}
           ;show login overlays if needed
-          (login-overlays-handler)
+          (when-not (utils/is-test-env?)
+            (login-overlays-handler))
           (om/build menu data)
           (dom/div {:class "page"}
             (om/build navbar {:card-width card-width
