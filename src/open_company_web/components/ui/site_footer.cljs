2069a44d0b011649728871e555bb8e3f32503307
bago2k4 <iacopo@rc0.eu>
2016-10-11 13:25:16 +0200

    fix site footer component

--- src/open_company_web/components/ui/site_footer.cljs
+++ src/open_company_web/components/ui/site_footer.cljs
@@ -17,12 +17,12 @@
             [:a {:href oc-urls/about} "About"]]
         [:li
           [:a.contact {:href (str "mailto:" oc-urls/contact-email)} "Contact"]]
-        [:li.mobile-only {style {:float "right" :marginRight "15px"}}
+        [:li.mobile-only {:style {:float "right" :marginRight "15px"}}
           [:a {:href "https://twitter.com/opencompanyhq"}
             [:i.fa.fa-2x.fa-twitter {:aria-hidden "true"}]]]]
       [:ul.nav.navbar-nav.navbar-right
         [:li
-          [:a {:href oc-urls/oc-twitter}>
+          [:a {:href oc-urls/oc-twitter}
             [:i.fa.fa-2x.fa-twitter {:aria-hidden "true"}]]]
         [:li
           [:a {:href "https://github.com/open-company"}
