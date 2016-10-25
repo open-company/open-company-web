2fc4e60d552acaf52474bed706c8776fe704b9d1
bago2k4 <iacopo@rc0.eu>
2016-10-25 18:31:37 +0200

    pass archive and edit cb to the sparkline component, call it when the correspondant button is clicked

--- src/open_company_web/components/growth/growth_sparklines.cljs
+++ src/open_company_web/components/growth/growth_sparklines.cljs
@@ -3,7 +3,7 @@
             [open-company-web.dispatcher :as dis]))
 
 (rum/defc growth-sparkline < {:did-mount (fn [s] (.tooltip (js/$ "[data-toggle=tooltip]")) s)}
-  [{:keys [metric-data metric-metadata]}]
+  [{:keys [metric-data metric-metadata archive-cb edit-cb]}]
   [:div.growth-sparkline.group
     {:id (str "growth-sparkline-" (:slug metric-metadata))}
     [:div.left-box
@@ -16,21 +16,25 @@
         {:data-placement "right"
          :data-container "body"
          :data-toggle "tooltip"
-         :title "Edit chart"}
+         :title "Edit chart"
+         :on-click #(edit-cb (:slug metric-metadata))}
         [:i.fa.fa-pencil]]
       [:button.btn-reset
         {:data-placement "right"
          :data-container "body"
          :data-toggle "tooltip"
-         :title "Remove chart"}
+         :title "Remove chart"
+         :on-click #(archive-cb (:slug metric-metadata))}
         [:i.fa.fa-times]]]])
 
 (rum/defc growth-sparklines
-  [{:keys [growth-data growth-metrics growth-metric-slugs] :as data}]
+  [{:keys [growth-data growth-metrics growth-metric-slugs archive-cb edit-cb] :as data}]
   [:div.growth-sparklines
     {:class (when (= (dis/foce-section-key) :growth) "editing")}
     (for [slug growth-metric-slugs]
       (rum/with-key
         (growth-sparkline {:metric-data (filter #(= (:slug %) slug) growth-data)
-                           :metric-metadata (get growth-metrics slug)})
+                           :metric-metadata (get growth-metrics slug)
+                           :archive-cb archive-cb
+                           :edit-cb edit-cb})
         (name slug)))])
