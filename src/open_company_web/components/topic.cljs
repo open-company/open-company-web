c0c33bf39fad2a34d45cf7851799979e306f7aa5
bago2k4 <iacopo@rc0.eu>
2016-10-27 17:13:34 +0200

    move also finances below headline like growth

--- src/open_company_web/components/topic.cljs
+++ src/open_company_web/components/topic.cljs
@@ -80,18 +80,9 @@
       (dom/div #js {:className "topic-internal group"
                     :key (str "topic-internal-" (name section))
                     :ref "topic-internal"}
-        (when (or (and is-growth-finances?
-                       (utils/data-topic-has-data section topic-data))
-                   image-header)
-          (dom/div {:class (utils/class-set {:card-header true
-                                             :card-image (not is-growth-finances?)})}
-            (cond
-              (= section "finances")
-              (om/build topic-finances {:section-data (utils/fix-finances topic-data)
-                                        :section section
-                                        :currency currency} {:opts chart-opts})
-              (and (not= section "finances") (not= section "growth"))
-              (om/build topic-image-header {:image-header image-header :image-size image-header-size} {:opts options}))))
+        (when image-header
+          (dom/div {:class "card-header card-image"}
+            (om/build topic-image-header {:image-header image-header :image-size image-header-size} {:opts options})))
         ;; Topic title
         (dom/div {:class "topic-dnd-handle group"}
           (dom/div {:class "topic-title"} (:title topic-data))
@@ -124,12 +115,18 @@
         (when-not (clojure.string/blank? (:headline topic-data))
           (om/build topic-headline topic-data))
 
-        (when (= section "growth")
-          (dom/div {:class (utils/class-set {:card-header true
-                                             :card-image (not is-growth-finances?)})}
-            (om/build topic-growth {:section-data topic-data
-                                    :section section
-                                    :currency currency} {:opts chart-opts})))
+        (when (and is-growth-finances?
+                   (utils/data-topic-has-data section topic-data))
+          (dom/div {:class ""}
+            (cond
+              (= section "growth")
+              (om/build topic-growth {:section-data topic-data
+                                      :section section
+                                      :currency currency} {:opts chart-opts})
+              (= section "finances")
+              (om/build topic-finances {:section-data (utils/fix-finances topic-data)
+                                        :section section
+                                        :currency currency} {:opts chart-opts}))))
 
         (dom/div #js {:className (str "topic-body" (when (:placeholder topic-data) " italic"))
                       :ref "topic-body"
