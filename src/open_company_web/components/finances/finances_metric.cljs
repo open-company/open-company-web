f8fe59de6d404a0b8c55509e3d0c5a2209f0e49f
bago2k4 <iacopo@rc0.eu>
2016-10-27 23:22:51 +0200

    first pass to finances metric component, twin of growth-metric component

---
+++ src/open_company_web/components/finances/finances_metric.cljs
@@ -0,0 +1,111 @@
+(ns open-company-web.components.finances.finances-metric
+  (:require [om.core :as om :include-macros true]
+            [om-tools.core :refer-macros (defcomponent)]
+            [om-tools.dom :as dom :include-macros true]
+            [open-company-web.lib.utils :as utils]
+            [open-company-web.lib.oc-lib :as oc-lib]
+            [open-company-web.components.ui.d3-chart :refer (d3-chart)]
+            [open-company-web.lib.finance-utils :as finance-utils]
+            [open-company-web.router :as router]
+            [open-company-web.lib.oc-colors :as occ]
+            [open-company-web.dispatcher :as dispatcher]
+            [cuerdas.core :as s]
+            [cljs-time.core :as t]
+            [cljs-time.format :as f]))
+
+(defn- format-delta
+  "Create a display fragment for a delta value."
+
+  ([delta]
+  (let [pos (when (pos? delta) "+")]
+    (dom/span {:class "domine"}
+      "("
+      pos
+      (if (zero? delta) "no change" (str (oc-lib/with-size-label delta) "%"))
+      ")")))
+
+  ([currency delta]
+    (dom/span {:class "domine"}
+      "("
+      (if (zero? delta) "no change" (oc-lib/with-currency currency (oc-lib/with-size-label delta) true))
+      ")")))
+
+(defn- finance-metric-delta [periods data-key currency]
+  (let [finance-metric (first periods)
+        period (utils/date-from-period (:period finance-metric))
+        date (when period (oc-lib/format-period "monthly" period))
+        value (get finance-metric data-key)
+        ;; Check for older periods contiguous to most recent
+        contiguous-periods (when (seq periods) (oc-lib/contiguous (map :period periods) :monthly))
+        prior-contiguous? (>= (count contiguous-periods) 2)
+        ;; Info on prior period
+        prior-metric (when prior-contiguous?
+                        (first (filter #(= (:period %) (second contiguous-periods)) periods)))
+        prior-value (when prior-metric (get prior-metric data-key))
+        metric-delta (when (and value prior-value) (- value prior-value))
+        metric-delta-percent (when metric-delta (* 100 (float (/ metric-delta prior-value))))
+        formatted-metric-delta (when metric-delta-percent (format-delta metric-delta-percent))]
+    ;; Format output
+    (dom/div {} date " " formatted-metric-delta)))
+
+(defn- label-from-set [data-set data-key currency-symbol]
+  (let [actual-val (data-key data-set)
+        metric-name (s/capital (s/human data-key))
+        period (utils/get-period-string (:period data-set))
+        fixed-cur-unit currency-symbol]
+    (when actual-val
+      (dom/span {:class "bold"} (str fixed-cur-unit (oc-lib/with-size-label actual-val) " " metric-name)))))
+
+(defn- sub-label [period metric-info]
+  (let [mname (:name metric-info)
+        interval (:interval metric-info)
+        label (if-not (clojure.string/blank? mname) (str mname " - ") "")]
+    (str label (utils/get-month period) " " (utils/get-year period))))
+
+(defcomponent finances-metric [{:keys [finances-data data-key currency charts-count] :as data} owner options]
+
+  (render [_]
+    (let [filled-metric-data (finance-utils/fill-gap-months finances-data)
+          actual-set (first finances-data)
+          period (utils/get-period-string (:period actual-set))
+          currency-symbol (utils/get-symbol-for-currency-code (:currency data))
+          actual-with-label (label-from-set actual-set data-key currency-symbol)
+          fixed-sorted-metric (vec (map #(merge % {:label (label-from-set % data-key currency-symbol)
+                                                   :sub-label (let [idx (.indexOf (vec (map :period (reverse finances-data))) (:period %))
+                                                                    periods (subvec (vec (reverse finances-data)) idx)]
+                                                               (finance-metric-delta periods data-key currency-symbol))}) finances-data))
+          chart-opts {:opts {:chart-type "unbordered-chart"
+                             :chart-height (or (:height (:chart-size options)) 100)
+                             :chart-width (:width (:chart-size options))
+                             :chart-keys [data-key]
+                             :interval "monthly"
+                             :x-axis-labels false
+                             :chart-colors (finance-utils/finances-key-colors)
+                             :chart-selected-colors (finance-utils/finances-key-colors)
+                             :chart-fill-polygons (or (:chart-fill-polygons options) false)
+                             :label-color (occ/get-color-by-kw :oc-gray-5)
+                             :sub-label-color (occ/get-color-by-kw :oc-gray-5)
+                             :sparklines-class "sparklines"
+                             :labels {:value {:position :bottom
+                                              :order 1
+                                              :value-presenter #(or (:label %2) "-")
+                                              :value (occ/get-color-by-kw :oc-blue-dark)
+                                              :label-presenter #(:sub-label %2)
+                                              :label-color (occ/get-color-by-kw :oc-gray-5)}}
+                             :hide-nav (:hide-nav options)}}]
+      (dom/div {:class (utils/class-set {:section true
+                                         data-key true
+                                         :fake-chart (:fake-chart data)
+                                         :read-only (:read-only data)})
+                :key data-key
+                :on-click (:start-editing-cb data)}
+
+        (when (and (pos? (count finances-data))
+                   (not= data-key :cash))
+          (dom/div {}
+            (om/build d3-chart {:chart-data fixed-sorted-metric
+                                :circle-radius (:circle-radius data)
+                                :circle-stroke (:circle-stroke data)
+                                :circle-fill (:circle-fill data)
+                                :line-stroke-width (:line-stroke-width data)
+                                :circle-selected-stroke (:circle-selected-stroke data)} chart-opts)))))))
