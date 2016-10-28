92209e24cfd62b9cac252d073a082bce8d3ea088
bago2k4 <iacopo@rc0.eu>
2016-10-28 11:20:32 +0200

    move sparklines below headline in lightbox too

--- src/open_company_web/components/fullscreen_topic.cljs
+++ src/open_company_web/components/fullscreen_topic.cljs
@@ -87,6 +87,14 @@
             (dom/div {:class "topic-header-image"}
               (dom/img {:src (:image-url topic-data)})))
           
+          ;; Title
+          (dom/div {:class "topic-title-container group"}
+            (dom/div {:class "topic-title left"} (:title topic-data)))
+
+          ;; Headline
+          (dom/div {:class "topic-headline"
+                    :dangerouslySetInnerHTML (utils/emojify (:headline topic-data))})
+
           ;; Chart
           (when (or (= topic "growth") (= topic "finances"))
             (dom/div {:class "topic-growth-finances"}
@@ -95,14 +103,7 @@
                 (om/build topic-growth chart-data {:opts chart-opts})
                 (= topic "finances")
                 (om/build topic-finances chart-data {:opts chart-opts}))))
-          
-          ;; Title
-          (dom/div {:class "topic-title-container group"}
-            (dom/div {:class "topic-title left"} (:title topic-data)))
-          
-          ;; Headline
-          (dom/div {:class "topic-headline"
-                    :dangerouslySetInnerHTML (utils/emojify (:headline topic-data))})
+
           ;; Body
           (dom/div {:class (str "topic-body" (when (:placeholder topic-data) " italic"))
                   :dangerouslySetInnerHTML (utils/emojify topic-body)})
