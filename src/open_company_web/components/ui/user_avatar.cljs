1999403d536ff828ec8a479499cbed633727d1a2
bago2k4 <iacopo@rc0.eu>
2016-11-07 12:09:45 +0100

    remove wrapper around user-avatar button to avoid breaking the bootstrap dropdown

--- src/open_company_web/components/ui/user_avatar.cljs
+++ src/open_company_web/components/ui/user_avatar.cljs
@@ -6,17 +6,18 @@
 
 (rum/defc user-avatar < rum/static
                         rum/reactive
-  [on-click]
+  [{:keys [classes]}]
   (let [has-avatar (not (clojure.string/blank? (get-in (rum/react dis/app-state) [:jwt :avatar])))]
-    [:div.user-avatar
-      [:button
-        {:type "button"
-         :class (str "user-avatar-button" (when has-avatar " no-image"))
-         :id "user-profile-dropdown"
-         :on-click #(on-click %)}
-        (if-not has-avatar
-          [:div.user-avatar-name.left (or (get-in (rum/react dis/app-state) [:jwt :first-name]) (get-in (rum/react dis/app-state) [:jwt :last-name]))]
-          [:img.user-avatar-img
-            {:src (jwt/get-key :avatar)
-             :title (get-in (rum/react dis/app-state) [:jwt :real-name])}])
-        [:i.fa.fa-ellipsis-v.right]]]))
+    [:button
+      {:type "button"
+       :class (str classes " user-avatar-button" (when has-avatar " no-image"))
+       :id "dropdown-toggle-menu"
+       :data-toggle "dropdown"
+       :aria-haspopup true
+       :aria-expanded false}
+      (if-not has-avatar
+        [:div.user-avatar-name.left (or (get-in (rum/react dis/app-state) [:jwt :first-name]) (get-in (rum/react dis/app-state) [:jwt :last-name]))]
+        [:img.user-avatar-img
+          {:src (jwt/get-key :avatar)
+           :title (get-in (rum/react dis/app-state) [:jwt :real-name])}])
+      [:i.fa.fa-ellipsis-v.right]]))
