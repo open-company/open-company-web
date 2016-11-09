67cf13afea651cbfb56753b7c545e3a329e5fe34
bago2k4 <iacopo@rc0.eu>
2016-11-09 15:33:41 +0100

    remove button-cta and tooltip from back to dashboard button

--- src/open_company_web/components/ui/back_to_dashboard_btn.cljs
+++ src/open_company_web/components/ui/back_to_dashboard_btn.cljs
@@ -10,23 +10,12 @@
   (.back (.-history js/window)))
 
 (rum/defc back-to-dashboard-btn < rum/static
-                                  {:did-mount (fn [s]
-                                                (when-not (utils/is-test-env?)
-                                                  (.tooltip (js/$ "[data-toggle=\"tooltip\"]")))
-                                                s)}
-  [{:keys [button-cta click-cb button-offset-left]
-    :or {button-cta "BACK TO DASHBOARD"
-         click-cb   btn-clicked
+  [{:keys [click-cb button-offset-left]
+    :or {click-cb   btn-clicked
          button-offset-left (- (/ (.-clientWidth (.-body js/document)) 2) 100)}}]
   (assert (fn? click-cb) "back-to-dashboard callback not fn?")
-  (let [button-cta (or button-cta "BACK TO DASHBOARD")]
-    [:div.back-to-dashboard-row.center
-     [:button.back-to-dashboard.btn-reset.btn-outline
-      {:on-click #(do (.preventDefault %) (click-cb))
-       :data-placement "left"
-       :data-container "body"
-       :data-toggle "tooltip"
-       :title button-cta
-       :style {:margin-left (str button-offset-left "px")}}
-      ; (str "← " button-cta)
-      (i/icon :simple-remove {:color "rgba(78, 90, 107, 0.8)" :size 24 :stroke 4 :accent-color "rgba(78, 90, 107, 1.0)"})]]))
+  [:div.back-to-dashboard-row.center
+   [:button.back-to-dashboard.btn-reset.btn-outline
+    {:on-click #(do (.preventDefault %) (click-cb))
+     :style {:margin-left (str button-offset-left "px")}}
+    (i/icon :simple-remove {:color "rgba(78, 90, 107, 0.8)" :size 24 :stroke 4 :accent-color "rgba(78, 90, 107, 1.0)"})]])
