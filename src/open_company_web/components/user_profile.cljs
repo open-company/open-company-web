64767693baa0e8fb8d3fe3f2662f3279e52d12be
bago2k4 <iacopo@rc0.eu>
2016-11-18 18:33:53 +0100

    User profile panel has the same width and spacing of the company settings and the invite and team management panel

--- src/open_company_web/components/user_profile.cljs
+++ src/open_company_web/components/user_profile.cljs
@@ -9,7 +9,7 @@
   (render [_]
     (dom/div {:class "user-profile fullscreen-page"}
       (back-to-dashboard-btn {:title "User Profile"})
-      (dom/div {:class "user-profile-internal"}
+      (dom/div {:class "user-profile-internal mx-auto my4"}
         (dom/div {:class "user-profile-content group"}
           (dom/div {:class "left-column"}
             (dom/div {:class "user-profile-name-title data-title"} "NAME")
@@ -25,9 +25,9 @@
             (dom/div {:class "user-profile-email"} (get-in data [:jwt :email])))
           (dom/div {:class "right-column"}
             (when (get-in data [:jwt :avatar])
-              (dom/div {:class "user-profile-avatar-title data-title"} "AVATAR")
-              (when (get-in data [:jwt :avatar])
-                (dom/img {:class "user-profile-avatar" :src (get-in data [:jwt :avatar])})))))
+              (dom/div {:class "user-profile-avatar-title data-title"} "AVATAR"))
+            (when (get-in data [:jwt :avatar])
+              (dom/img {:class "user-profile-avatar" :src (get-in data [:jwt :avatar])}))))
         (when (= (get-in data [:jwt :auth-source]) "slack")
           (dom/div {:class "user-profile-disclaimer"}
             "User information is from your Slack account.")))
