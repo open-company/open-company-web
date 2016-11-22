679477ef8de972d142f51ab02c7bb1adc9b269c8
bago2k4 <iacopo@rc0.eu>
2016-11-22 19:40:06 +0100

    Adapt the column and card width calculation to layout 2 columns even on mobile when the window width is bigger than 320 (customizable)

--- src/open_company_web/lib/responsive.cljs
+++ src/open_company_web/lib/responsive.cljs
@@ -4,39 +4,43 @@
             [goog.object :as gobj]
             [goog.userAgent :as userAgent]))
 
+(def big-web-min-width 684)
+; 2 columns 302 * 2 = 604 diff: 80 |30|col1|20|col2|30|
+; 1 column 420 diff: 263: |131|420|131|
+
+(def mobile-2-columns-breakpoint 320)
+
+(defn window-exceeds-breakpoint []
+  (> (.-clientWidth (.-body js/document)) mobile-2-columns-breakpoint))
+
 (defn columns-num []
   (let [win-width (.-clientWidth (.-body js/document))]
     (cond
       (>= win-width 1012)
       3
-      (>= win-width 684)
+      (>= win-width mobile-2-columns-breakpoint)
       2
       :else
       1)))
 
 ;; 3 Columns
-(def c3-min-win-width 1012)
-(def c3-max-win-width 1800)
-(def c3-min-card-width 302)
-(def c3-max-card-width 420)
+(def c3-max-win-width 1800) (def c3-max-card-width 420)
+(def c3-min-win-width 1012) (def c3-min-card-width 302)
 (def c3-win-card-diff (- (/ c3-max-win-width c3-max-card-width) (/ c3-min-win-width c3-min-card-width)))
 (def c3-win-diff (- c3-max-win-width c3-min-win-width))
 (def c3-min-card-delta (/ c3-min-win-width c3-min-card-width))
 
 ;; 2 Columns
-(def c2-min-win-width 684)
-(def c2-max-win-width 1011)
-(def c2-min-card-width 302)
-(def c2-max-card-width 420)
+(def c2-max-win-width 1011) (def c2-max-card-width 420)
+(def c2-padding (if (> (.-clientWidth (.-body js/document)) big-web-min-width) 80 60))
+(def c2-min-win-width mobile-2-columns-breakpoint) (def c2-min-card-width (/ (- mobile-2-columns-breakpoint c2-padding) 2))
 (def c2-win-card-diff (- (/ c2-max-win-width c2-max-card-width) (/ c2-min-win-width c2-min-card-width)))
 (def c2-win-diff (- c2-max-win-width c2-min-win-width))
 (def c2-min-card-delta (/ c2-min-win-width c2-min-card-width))
 
 ;; 1 Columns
-(def c1-min-win-width 414)
-(def c1-max-win-width 683)
-(def c1-min-card-width 396)
-(def c1-max-card-width 420)
+(def c1-max-win-width (dec mobile-2-columns-breakpoint)) (def c1-max-card-width (- (dec mobile-2-columns-breakpoint) 263))
+(def c1-min-win-width 414) (def c1-min-card-width 396)
 (def c1-win-card-diff (- (/ c1-max-win-width c1-max-card-width) (/ c1-min-win-width c1-min-card-width)))
 (def c1-win-diff (- c1-max-win-width c1-min-win-width))
 (def c1-min-card-delta (/ c1-min-win-width c1-min-card-width))
@@ -88,8 +92,6 @@
       (/ ww delta)))
 
 (def _mobile (atom -1))
-
-(def big-web-min-width 684)
 
 (defn set-browser-type! []
   (let [force-mobile-cookie (cook/get-cookie :force-browser-type)
@@ -144,8 +146,8 @@
 
 (defn total-layout-width-int [card-width columns-num]
   (- (* (+ card-width topic-total-x-padding) columns-num) ; width of each column less
-     20                                                   ; the container padding
-     40))                                                 ; the distance btw the columns
+     (if (is-mobile?) 20 10)                              ; the container padding
+     (if (is-mobile?) 40 0)))                             ; the distance btw the columns on big web
 
 (defn calc-update-width [columns-num]
   (let [card-width   (calc-card-width)
