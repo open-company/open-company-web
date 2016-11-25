0d87f1f4b418154062ebd8151a9b5f24564e5986
bago2k4 <iacopo@rc0.eu>
2016-11-25 16:40:38 +0100

    Do not show the second paragraph of the no updates message if the user has no write permissions.

--- src/open_company_web/components/prior_updates.cljs
+++ src/open_company_web/components/prior_updates.cljs
@@ -54,10 +54,11 @@
 
             [:div.message-container.pt2
               [:h3.message "No company updates have been shared."]
-              [:p.message
-                (if (>= (count (utils/filter-placeholder-sections (vec (map keyword (:sections company-data))) company-data)) 2)
-                  "As updates are shared by email, Slack or URL, they show up here so they are easy to find and read."
-                  "This is where you’ll find company updates once you’ve shared with others. Create some topics on your dashboard first, then you’ll be able to give it a try.")]]
+              (when-not (:read-only company-data)
+                [:p.message
+                  (if (>= (count (utils/filter-placeholder-sections (vec (map keyword (:sections company-data))) company-data)) 2)
+                    "As updates are shared by email, Slack or URL, they show up here so they are easy to find and read."
+                    "This is where you’ll find company updates once you’ve shared with others. Create some topics on your dashboard first, then you’ll be able to give it a try.")])]
 
             [:div.update-container.pt2
               (for [update updates]
