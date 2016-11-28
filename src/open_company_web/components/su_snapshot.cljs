afbc8e72c50ca8c6e860543f777d24f1105284a7
bago2k4 <iacopo@rc0.eu>
2016-11-28 18:56:32 +0100

    Remove Company's Update title from SU page.

--- src/open_company_web/components/su_snapshot.cljs
+++ src/open_company_web/components/su_snapshot.cljs
@@ -130,7 +130,7 @@
           (when (and prior-list mobile?)
             (dom/div {:class "su-mobile-navigation"
                       :on-click #(.back (.-history js/window))}
-              (dom/div {:class "su-mobile-navigation-title"} (str (:name company-data) "'s Update"))
+              (dom/div {:class "su-mobile-navigation-title"})
               (dom/button {:class "btn-reset close-su-btn"}
                 (dom/i {:class "fa fa-angle-left"}))))
 
