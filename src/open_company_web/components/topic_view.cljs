e4df368a68f50357977934c7f535251adb911085
bago2k4 <iacopo@rc0.eu>
2016-12-02 15:51:51 +0100

    Add some margin btw the topics list and the topic view, get and set the topic view selected topic from the global app staet.

--- src/open_company_web/components/topic_view.cljs
+++ src/open_company_web/components/topic_view.cljs
@@ -7,22 +7,20 @@
 
 (defcomponent topic-view [{:keys [card-width
                                   columns-num
-                                  topic-name
+                                  selected-topic-view
                                   company-data] :as data} owner options]
   (render [_]
     (let [topic-view-width (responsive/topic-view-width card-width columns-num)
-          topic-data (->> topic-name keyword (get company-data))]
-      (js/console.log "topic-view/render" data)
-      (js/console.log "   topic-data" topic-data)
-      (js/console.log "   topic-view-width" topic-view-width)
+          topic-data (->> selected-topic-view keyword (get company-data))]
       (dom/div {:class "topic-view"
-                :style {:width (str topic-view-width "px")}}
+                :style {:width (str (- topic-view-width 20) "px")}}
         (dom/div {:class "topic-view-internal"
                   :style {:width (str card-width "px")}}
-          (om/build topic {:section topic-name
+          (om/build topic {:section selected-topic-view
                            :section-data topic-data
-                           :card-width (- topic-view-width 200)
+                           :card-width (- topic-view-width 180)
                            :foce-data-editing? (:foce-data-editing? data)
                            :read-only-company (:read-only company-data)
-                           :currency (:currency company-data)}
-                           {:opts {:section-name topic-name}}))))))
+                           :currency (:currency company-data)
+                           :is-topic-view true}
+                           {:opts {:section-name selected-topic-view}}))))))
