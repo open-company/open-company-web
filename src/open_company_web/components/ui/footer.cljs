acce3593edc454a5ba93b2d98bf7f394472ac2ca
bago2k4 <iacopo@rc0.eu>
2016-12-07 14:54:04 +0100

    Add title to the links so they explain what they do. Mostly needed for the contact link so the user doens't think it's contacting the company his looking to.

--- src/open_company_web/components/ui/footer.cljs
+++ src/open_company_web/components/ui/footer.cljs
@@ -22,8 +22,8 @@
           (dom/a {:class "oc-logo" :href oc-urls/home} (dom/img {:src "/img/oc-wordmark.svg"}))
           ; (dom/a {:class "footer-link" :href oc-urls/about} (str "ABOUT"  (when-not (responsive/is-mobile-size?) " US")))
           (dom/div {:class "footer-bottom-right"}
-            (dom/a {:class "contact" :href oc-urls/contact-mail-to :alt "Contact OpenCompany"} "CONTACT")
-            (dom/a {:class "twitter" :target "_blank" :href oc-urls/oc-twitter :alt "twitter"}
+            (dom/a {:class "contact" :href oc-urls/contact-mail-to :title "Contact OpenCompany" :alt "Contact OpenCompany"} "CONTACT")
+            (dom/a {:class "twitter" :target "_blank" :href oc-urls/oc-twitter :title "OpenCompany on Twitter" :alt "twitter"}
               (dom/img {:src "/img/twitter.svg"}))
-            (dom/a {:class "github" :target "_blank" :href oc-urls/oc-github :alt "github"}
+            (dom/a {:class "github" :target "_blank" :href oc-urls/oc-github :title "OpenCompany on GitHub" :alt "github"}
               (dom/img {:src "/img/github.svg"}))))))))
