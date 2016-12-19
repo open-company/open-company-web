aa6eff389a61f27293c82da9643ff080812ba9f4
bago2k4 <iacopo@rc0.eu>
2016-12-19 14:29:18 +0100

    Tooltip wrapper improvements.

--- src/open_company_web/lib/tooltip.cljs
+++ src/open_company_web/lib/tooltip.cljs
@@ -1,19 +1,33 @@
-(ns open-company-web.lib.tooltip)
+(ns open-company-web.lib.tooltip
+  "Cljs wrapper for Tooltip js object that is in /lib/tooltip/tooltip.js
+   Docs at https://github.com/darsain/tooltip/wiki"
+  (:require [defun.core :refer (defun)]))
 
-;; Cljs wrapper for Tooltip js object that is in /lib/tooltip/tooltip.js
-;; Docs at https://github.com/darsain/tooltip/wiki
+(defn tooltip
+  "Create a tooltip, attach it to the passed element at the gived position and return it"
+  [content el config]
+  (when el
+    (.position
+      (js/Tooltip. content (clj->js (merge {:baseClass "js-tooltip"
+                                            :effect "fade in"
+                                            :auto true
+                                            :place "bottom"}
+                                           config)))
+      el)))
 
-(defn tooltip [content el position]
-  (.position
-    (js/Tooltip. content #js {:baseClass "js-tooltip"
-                              :effect "fade in"
-                              :auto true
-                              :place position})
-    el))
-
-(defn show [tip]
-  (.show tip)
-  (.on (js/$ (.-body js/document)) "click" #(.hide tip)))
-
-(defn hide [tip]
+(defn hide
+  "Programmatically hide the passed tooltip"
+  [tip]
   (.hide tip))
+
+(defun show
+  "Show the passed tooltip. Optionally disable the hide on click."
+  ([tip] (show tip true))
+  ([tip false] (.show tip))
+  ([tip true]
+    (let [$body (js/$ (.-body js/document))]
+      (.show tip)
+      (.on $body "click" (fn []
+                           (js/console.log "Body click!")
+                           (hide tip)
+                           (.off $body "click"))))))
