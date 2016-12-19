837c32aefa4f137971e1abfa020d464c6ae27244
bago2k4 <iacopo@rc0.eu>
2016-12-19 12:27:31 +0100

    Refactor user-management component to use only one derivative that is built adhoc for this component.

--- src/open_company_web/components/user_management.cljs
+++ src/open_company_web/components/user_management.cljs
@@ -17,10 +17,9 @@
             [open-company-web.components.user-invitation :refer (user-invitation)]
             [open-company-web.components.ui.back-to-dashboard-btn :refer (back-to-dashboard-btn)]))
 
-(rum/defcs user-management < (rum/local false ::public)
-                             (rum/local false ::has-changes)
+(rum/defcs user-management < rum/static
                              rum/reactive
-                             (drv/drv :company-data)
+                             (drv/drv :user-management)
                              {:before-render (fn [s]
                                                (when (and (:auth-settings @dis/app-state)
                                                           (not (:enumerate-users-requested @dis/app-state)))
@@ -29,62 +28,62 @@
                              :did-mount (fn [s]
                                           (when-not (utils/is-test-env?)
                                             (dis/dispatch! [:input [:um-invite :email] ""]))
-                                          (let [public (::public s)]
-                                            (reset! public (not (not (:public (dis/company-data))))))
                                           s)}
-  [{:keys [::has-changes ::public] :as s}]
-  [:div.user-management.mx-auto.p3.my4.group
-    [:div
-      [:div.mb3.um-invite.group
-        [:div.um-invite-label
-          "INVITE TEAM MEMBERS"]
-          [:div
-            [:div.group
-              [:input.left.um-invite-field.email
-                {:name "um-invite"
-                 :type "email"
-                 :autoCapitalize "none"
-                 :value (:email (:um-invite (rum/react dis/app-state)))
-                 :pattern "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$"
-                 :on-change #(dis/dispatch! [:invite-by-email-change (.. % -target -value)])
-                 :placeholder "Email address"}]
-              [:button.right.btn-reset.btn-solid.um-invite-send
-                {:disabled (not (utils/valid-email? (:email (:um-invite (rum/react dis/app-state)))))
-                 :on-click #(let [email (:email (:um-invite @dis/app-state))]
-                              (if (utils/valid-email? email)
-                                (dis/dispatch! [:invite-by-email email])
-                                (dis/dispatch! [:input [:invite-by-email-error] true])))}
-               "SEND INVITE"]]
-          (when (:invite-by-email-error (rum/react dis/app-state))
+  [s]
+  (let [{:keys [um-invite enumerate-users invite-by-email-error] :as user-man} (drv/react s :user-management)
+        ro-user-man @(drv/get-ref s :user-management)]
+    [:div.user-management.mx-auto.p3.my4.group
+      [:div
+        [:div.mb3.um-invite.group
+          [:div.um-invite-label
+            "INVITE TEAM MEMBERS"]
             [:div
-              (cond
-                (and (= (:invite-by-email-error (rum/react dis/app-state)) :user-exists)
-                     (:email (:um-invite @dis/app-state)))
-                [:span.small-caps.red.mt1.left (str (:email (:um-invite @dis/app-state)) " is already a user.")]
-                :else
-                [:span.small-caps.red.mt1.left "An error occurred, please try again."])])]]
-      [:div.um-invite.group
-        [:div.um-invite-label
-            "TEAM MEMBERS"]
-        [:div.um-invite-label-2
-          "The following people can view, edit and share information:"]
-        (when (jwt/is-slack-org?)
+              [:div.group
+                [:input.left.um-invite-field.email
+                  {:name "um-invite"
+                   :type "email"
+                   :autoCapitalize "none"
+                   :value (:email um-invite)
+                   :pattern "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$"
+                   :on-change #(dis/dispatch! [:invite-by-email-change (.. % -target -value)])
+                   :placeholder "Email address"}]
+                [:button.right.btn-reset.btn-solid.um-invite-send
+                  {:disabled (not (utils/valid-email? (:email um-invite)))
+                   :on-click #(let [email (:email (:um-invite ro-user-man))]
+                                (if (utils/valid-email? email)
+                                  (dis/dispatch! [:invite-by-email email])
+                                  (dis/dispatch! [:input [:invite-by-email-error] true])))}
+                 "SEND INVITE"]]
+            (when invite-by-email-error
+              [:div
+                (cond
+                  (and (= invite-by-email-error :user-exists)
+                       (:email um-invite))
+                  [:span.small-caps.red.mt1.left (str (:email um-invite) " is already a user.")]
+                  :else
+                  [:span.small-caps.red.mt1.left "An error occurred, please try again."])])]]
+        [:div.um-invite.group
+          [:div.um-invite-label
+              "TEAM MEMBERS"]
           [:div.um-invite-label-2
-            "Members of your " [:img {:src "/img/Slack_Icon.png" :width 14 :height 14}] " Slack team (not guests)."])
-        (when (pos? (count (:enumerate-users (rum/react dis/app-state))))
-          (user-invitation (:enumerate-users (rum/react dis/app-state))))]
-      (comment
-        [:div.my2.um-byemail-container.group
-          [:div.group
-            [:span.left.ml1.um-byemail-anyone-span
-              "ANYONE WITH THIS EMAIL DOMAIN HAS USER ACCESS"]]
-          [:div.mt2.um-byemail.group
-            [:span.left.um-byemail-at "@"]
-            [:input.left.um-byemail-email
-              {:type "text"
-               :name "um-byemail-email"
-               :placeholder "your email domain without @"}]
-            [:button.left.um-byemail-save.btn-reset.btn-outline "SAVE"]]])]])
+            "The following people can view, edit and share information:"]
+          (when (jwt/is-slack-org?)
+            [:div.um-invite-label-2
+              "Members of your " [:img {:src "/img/Slack_Icon.png" :width 14 :height 14}] " Slack team (not guests)."])
+          (when (pos? (count enumerate-users))
+            (user-invitation enumerate-users))]
+        (comment
+          [:div.my2.um-byemail-container.group
+            [:div.group
+              [:span.left.ml1.um-byemail-anyone-span
+                "ANYONE WITH THIS EMAIL DOMAIN HAS USER ACCESS"]]
+            [:div.mt2.um-byemail.group
+              [:span.left.um-byemail-at "@"]
+              [:input.left.um-byemail-email
+                {:type "text"
+                 :name "um-byemail-email"
+                 :placeholder "your email domain without @"}]
+              [:button.left.um-byemail-save.btn-reset.btn-outline "SAVE"]]])]]))
 
 (defcomponent user-management-wrapper [data owner]
   (render [_]
