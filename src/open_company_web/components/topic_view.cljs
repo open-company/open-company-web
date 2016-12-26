2d5d0fc59ec9af70e989c74d92a3aa3add849561
bago2k4 <iacopo@rc0.eu>
2016-12-26 18:27:39 +0100

    Show a spinner if the data of the selected topic are still loading. Added a message when the topic is not found in the company data.

--- src/open_company_web/components/topic_view.cljs
+++ src/open_company_web/components/topic_view.cljs
@@ -13,6 +13,7 @@
             [open-company-web.components.topic :refer (topic)]
             [open-company-web.components.topic-edit :refer (topic-edit)]
             [open-company-web.components.ui.popover :refer (add-popover hide-popover)]
+            [open-company-web.components.ui.loading :refer (loading)]
             [cuerdas.core :as s]))
 
 (defn- remove-topic-click [owner e]
@@ -99,7 +100,9 @@
           is-custom-section (s/starts-with? (:section topic-data) "custom-")
           revisions (:revisions-data topic-data)
           is-new-foce (and (= foce-key section-kw) (nil? (:created-at foce-data)))
-          is-another-foce (and (not (nil? foce-key)) (not (nil? (:created-at foce-data))))]
+          is-another-foce (and (not (nil? foce-key)) (not (nil? (:created-at foce-data))))
+          loading-topic-data (and (contains? company-data section-kw)
+                                  (:loading topic-data))]
       (dom/div {:class "topic-view-container group"
                 :style {:width (if (responsive/is-tablet-or-mobile?) "100%" (str topic-card-width "px"))
                         :margin-right (if (responsive/is-tablet-or-mobile?) "0px" (str (max 0 (- topic-view-width topic-card-width 50)) "px"))}
@@ -110,63 +113,51 @@
               (dom/div {:class "topic-view-navbar-close left"
                         :on-click #(router/nav! (oc-urls/company))} "<")
               (dom/div {:class "topic-view-navbar-title left"} (:title topic-data))))
-          (dom/div {:class "topic-view-internal"
-                    :style {:width (if (responsive/is-tablet-or-mobile?) "auto" (str topic-card-width "px"))}}
-            (when (and (not (:read-only company-data))
-                       (not (responsive/is-tablet-or-mobile?)))
-              (dom/div {:class (str "fake-textarea " (when is-another-foce "disabled"))}
-                (if is-new-foce
-                  (dom/div {:class "topic topic-edit"
-                            :style {:width (str (- topic-card-width 120) "px")}}
-                    (om/build topic-edit {:is-stakeholder-update false
-                                          :currency (:currency company-data)
-                                          :card-width (- topic-card-width 120)
-                                          :columns-num columns-num
-                                          :is-topic-view true
-                                          :foce-data-editing? foce-data-editing?
-                                          :foce-key foce-key
-                                          :show-archive-button false
-                                          :foce-data foce-data}
-                                         {:opts options
-                                          :key (str "topic-foce-" selected-topic-view "-new")}))
-                  (let [initial-data (utils/new-section-initial-data selected-topic-view (:title topic-data) topic-data)
-                        with-data (if (#{:growth :finances} section-kw) (assoc initial-data :data (:data topic-data)) initial-data)
-                        with-metrics (if (= :growth section-kw) (assoc with-data :metrics (:metrics topic-data)) with-data)]
-                    (dom/div {:class "fake-textarea-internal"
-                              :on-click #(dis/dispatch! [:start-foce section-kw with-metrics])
-                              :style {:width (str (- topic-card-width 100) "px")}}
-                      "Write something new here...")))))
-            ;; Render the topic from the company data only until the revisions are loaded.
-            (when (and (not revisions)
-                       (not (:placeholder topic-data)))
-              (dom/div {:class "revision-container group"}
-                (when (and (not (:read-only company-data))
-                           (not (responsive/is-tablet-or-mobile?)))
-                  (dom/hr {:class "separator-line"
-                           :style {:width (if (responsive/is-tablet-or-mobile?) "auto" (str (- topic-card-width 80) "px"))}}))
-                (om/build topic {:section selected-topic-view
-                                 :section-data topic-data
-                                 :card-width (- topic-card-width 60)
-                                 :is-stakeholder-update false
-                                 :read-only-company (:read-only company-data)
-                                 :currency (:currency company-data)
-                                 :is-topic-view true
-                                 :foce-data-editing? foce-data-editing?
-                                 :foce-key foce-key
-                                 :foce-data foce-data
-                                 :show-editing false}
-                                 {:opts {:section-name selected-topic-view}})))
-            (for [idx (range (count revisions))
-                  :let [rev (get revisions idx)]]
-              (when rev
+          (cond
+            (nil? topic-data)
+            (dom/div {:class "topic-view-internal"}
+              (dom/div {:class "topic-404"} "No data found."))
+            (or (:loading company-data)
+                loading-topic-data)
+            (dom/div {:class "topic-view-internal loading group"}
+              (om/build loading {:loading true}))
+            :else
+            (dom/div {:class "topic-view-internal"
+                      :style {:width (if (responsive/is-tablet-or-mobile?) "auto" (str topic-card-width "px"))}}
+              (when (and (not (:read-only company-data))
+                         (not (responsive/is-tablet-or-mobile?)))
+                (dom/div {:class (str "fake-textarea " (when is-another-foce "disabled"))}
+                  (if is-new-foce
+                    (dom/div {:class "topic topic-edit"
+                              :style {:width (str (- topic-card-width 120) "px")}}
+                      (om/build topic-edit {:is-stakeholder-update false
+                                            :currency (:currency company-data)
+                                            :card-width (- topic-card-width 120)
+                                            :columns-num columns-num
+                                            :is-topic-view true
+                                            :foce-data-editing? foce-data-editing?
+                                            :foce-key foce-key
+                                            :show-archive-button false
+                                            :foce-data foce-data}
+                                           {:opts options
+                                            :key (str "topic-foce-" selected-topic-view "-new")}))
+                    (let [initial-data (utils/new-section-initial-data selected-topic-view (:title topic-data) topic-data)
+                          with-data (if (#{:growth :finances} section-kw) (assoc initial-data :data (:data topic-data)) initial-data)
+                          with-metrics (if (= :growth section-kw) (assoc with-data :metrics (:metrics topic-data)) with-data)]
+                      (dom/div {:class "fake-textarea-internal"
+                                :on-click #(dis/dispatch! [:start-foce section-kw with-metrics])
+                                :style {:width (str (- topic-card-width 100) "px")}}
+                        "Write something new here...")))))
+              ;; Render the topic from the company data only until the revisions are loaded.
+              (when (and (not revisions)
+                         (not (:placeholder topic-data)))
                 (dom/div {:class "revision-container group"}
-                  (when-not (and (= idx 0)
-                                 (or (responsive/is-tablet-or-mobile?)
-                                     (:read-only company-data)))
+                  (when (and (not (:read-only company-data))
+                             (not (responsive/is-tablet-or-mobile?)))
                     (dom/hr {:class "separator-line"
-                             :style {:width (if (responsive/is-tablet-or-mobile?) "auto" (str (- topic-card-width 60) "px"))}}))
+                             :style {:width (if (responsive/is-tablet-or-mobile?) "auto" (str (- topic-card-width 80) "px"))}}))
                   (om/build topic {:section selected-topic-view
-                                   :section-data rev
+                                   :section-data topic-data
                                    :card-width (- topic-card-width 60)
                                    :is-stakeholder-update false
                                    :read-only-company (:read-only company-data)
@@ -175,14 +166,36 @@
                                    :foce-data-editing? foce-data-editing?
                                    :foce-key foce-key
                                    :foce-data foce-data
-                                   :show-archive-button (= (count revisions) 1)
-                                   :show-editing true}
-                                   {:opts {:section-name selected-topic-view}
-                                    :key (str "topic-"
-                                          (when foce-key
-                                            "foce-")
-                                          selected-topic-view "-" (:updated-at rev))}))))))
-        (when (and (not (responsive/is-tablet-or-mobile?))
+                                   :show-editing false}
+                                   {:opts {:section-name selected-topic-view}})))
+              (for [idx (range (count revisions))
+                    :let [rev (get revisions idx)]]
+                (when rev
+                  (dom/div {:class "revision-container group"}
+                    (when-not (and (= idx 0)
+                                   (or (responsive/is-tablet-or-mobile?)
+                                       (:read-only company-data)))
+                      (dom/hr {:class "separator-line"
+                               :style {:width (if (responsive/is-tablet-or-mobile?) "auto" (str (- topic-card-width 60) "px"))}}))
+                    (om/build topic {:section selected-topic-view
+                                     :section-data rev
+                                     :card-width (- topic-card-width 60)
+                                     :is-stakeholder-update false
+                                     :read-only-company (:read-only company-data)
+                                     :currency (:currency company-data)
+                                     :is-topic-view true
+                                     :foce-data-editing? foce-data-editing?
+                                     :foce-key foce-key
+                                     :foce-data foce-data
+                                     :show-archive-button (= (count revisions) 1)
+                                     :show-editing true}
+                                     {:opts {:section-name selected-topic-view}
+                                      :key (str "topic-"
+                                            (when foce-key
+                                              "foce-")
+                                            selected-topic-view "-" (:updated-at rev))})))))))
+        (when (and (not loading-topic-data)
+                   (not (responsive/is-tablet-or-mobile?))
                    (not (:read-only company-data))
                    (> (count revisions) 1))
           (dom/button {:class "btn-reset btn-link archive-topic"
