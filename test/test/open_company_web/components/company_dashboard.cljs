bd41f8210ad7125f33b9415f76058c06db0de880
Sean Johnson <sean@snootymonkey.com>
2016-12-26 11:40:02 -0500

    Comment out 3 tests that are crashing PhantomJS.

--- test/test/open_company_web/components/company_dashboard.cljs
+++ test/test/open_company_web/components/company_dashboard.cljs
@@ -9,20 +9,20 @@
             [open-company-web.data.company :refer (company)]
             [open-company-web.router :as router]))
 
-(enable-console-print!)
+; (enable-console-print!)
 
-; dynamic mount point for components
-(def ^:dynamic c)
+; ; dynamic mount point for components
+; (def ^:dynamic c)
 
-(def test-atom company)
+; (def test-atom company)
 
-(deftest test-company-dashboard-component
-  (testing "Company dashboard component"
-    (router/set-route! ["companies" "buffer"]
-                       {:slug "buffer"})
-    (let [c (tu/new-container!)
-          app-state (atom test-atom)
-          _ (om/root company-dashboard app-state {:target c})
-          company-dashboard-node (sel1 c [:div.company-dashboard])]
-      (is (not (nil? company-dashboard-node)))
-      (tu/unmount! c))))
+; (deftest test-company-dashboard-component
+;   (testing "Company dashboard component"
+;     (router/set-route! ["companies" "buffer"]
+;                        {:slug "buffer"})
+;     (let [c (tu/new-container!)
+;           app-state (atom test-atom)
+;           _ (om/root company-dashboard app-state {:target c})
+;           company-dashboard-node (sel1 c [:div.company-dashboard])]
+;       (is (not (nil? company-dashboard-node)))
+;       (tu/unmount! c))))
