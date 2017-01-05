bbbf8adf39616c241968bf620a80ed7937d3f5d7
bago2k4 <iacopo@rc0.eu>
2017-01-05 13:52:02 +0100

    Do not turn on add topic if there are 0 topics in the company on tablets.

--- src/open_company_web/actions.cljs
+++ src/open_company_web/actions.cljs
@@ -111,7 +111,8 @@
     ;; add section name inside each section
     (let [updated-body (utils/fix-sections body)
           with-company-data (assoc-in db (dispatcher/company-data-key (:slug updated-body)) updated-body)
-          with-open-add-topic (if (zero? (count (:sections updated-body)))
+          with-open-add-topic (if (and (not (responsive/is-tablet-or-mobile?))
+                                       (zero? (count (:sections updated-body))))
                                (assoc with-company-data :show-add-topic true)
                                with-company-data)]
       ; async preload the SU list
