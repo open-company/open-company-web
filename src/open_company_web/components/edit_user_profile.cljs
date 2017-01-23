6b90e1888e183e6995c7f718366d556b007293c1
bago2k4 <iacopo@rc0.eu>
2017-01-23 15:17:48 +0100

    Style clean of the profile edit.

--- src/open_company_web/components/edit_user_profile.cljs
+++ src/open_company_web/components/edit_user_profile.cljs
@@ -4,7 +4,8 @@
             [om-tools.dom :as dom :include-macros true]
             [open-company-web.lib.responsive :as responsive]
             [open-company-web.components.ui.footer :refer (footer)]
-            [open-company-web.components.ui.back-to-dashboard-btn :refer (back-to-dashboard-btn)]))
+            [open-company-web.components.ui.back-to-dashboard-btn :refer (back-to-dashboard-btn)]
+            [open-company-web.components.ui.user-avatar :refer (user-avatar-image)]))
 
 (defcomponent edit-user-profile [data owner]
   (render [_]
@@ -13,21 +14,18 @@
       (dom/div {:class "edit-user-profile-internal mx-auto my4"}
         (dom/div {:class "edit-user-profile-content group"}
           (dom/div {:class "left-column"}
-            (dom/div {:class "user-profile-name-title data-title"} "FIRST NAME")
-            (dom/input {:class "user-profile-name"
+            (dom/div {:class "edit-user-profile-title data-title"} "FIRST NAME")
+            (dom/input {:class "edit-user-profile"
                         :value (or (get-in data [:jwt :first-name]) "")})
-            (dom/div {:class "user-profile-name-title data-title"} "LAST NAME")
-            (dom/input {:class "user-profile-name"
+            (dom/div {:class "edit-user-profile-title data-title"} "LAST NAME")
+            (dom/input {:class "edit-user-profile"
                         :value (or (get-in data [:jwt :last-name]) "")})
-            (dom/div {:class "edit-user-profile-email-title data-title"} "EMAIL")
-            (dom/input {:class "user-profile-email"
+            (dom/div {:class "edit-user-profile-title data-title"} "EMAIL")
+            (dom/input {:class "edit-user-profile"
                         :value (or (get-in data [:jwt :email]) "")}))
           (dom/div {:class "right-column"}
-            (let [avatar (get-in data [:jwt :avatar])]
-              (when avatar
-                (dom/div {:class "edit-user-profile-avatar-title data-title"} "AVATAR"))
-              (when avatar
-                (dom/img {:class "edit-user-profile-avatar" :src avatar}))))))
+            (dom/div {:class "edit-user-profile-title data-title"} "AVATAR")
+            (user-avatar-image))))
       (let [columns-num (responsive/columns-num)
             card-width (responsive/calc-card-width)]
         (om/build footer (assoc data :footer-width (responsive/total-layout-width-int card-width columns-num)))))))
