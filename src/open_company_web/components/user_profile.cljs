be6048b87532c6a1a363661838a1f736a6ae0346
bago2k4 <iacopo@rc0.eu>
2017-02-01 17:14:59 +0100

    Use avatar-url instead of avatar for the user avatar. Call the right refresh action.

--- src/open_company_web/components/user_profile.cljs
+++ src/open_company_web/components/user_profile.cljs
@@ -23,15 +23,15 @@
             (dom/div {:class "user-profile-email-title data-title"} "EMAIL")
             (dom/div {:class "user-profile-email"} (get-in data [:jwt :email])))
           (dom/div {:class "right-column"}
-            (when (get-in data [:jwt :avatar])
+            (when (get-in data [:jwt :avatar-url])
               (dom/div {:class "user-profile-avatar-title data-title"} "AVATAR"))
-            (when (get-in data [:jwt :avatar])
-              (dom/img {:class "user-profile-avatar" :src (get-in data [:jwt :avatar])}))))
+            (when (get-in data [:jwt :avatar-url])
+              (dom/img {:class "user-profile-avatar" :src (get-in data [:jwt :avatar-url])}))))
         (dom/div {:class "user-profile-disclaimer"}
           "User information is from your Slack account."
           (dom/div {:class "right"}
             (dom/button {:class "btn-reset btn-link"
-                         :on-click #(dis/dispatch! [:login-with-slack])} "Refresh"))))
+                         :on-click #(dis/dispatch! [:refresh-slack-user])} "Refresh"))))
       (let [columns-num (responsive/columns-num)
             card-width (responsive/calc-card-width)]
         (om/build footer (assoc data :footer-width (responsive/total-layout-width-int card-width columns-num)))))))
