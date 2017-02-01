aadcdcfd4a76bbbc4f2e85b2ee0a1e65eebc04d0
bago2k4 <iacopo@rc0.eu>
2017-02-01 17:06:00 +0100

    Fix icons of the user roles dialog

--- src/open_company_web/components/team_disclaimer_popover.cljs
+++ src/open_company_web/components/team_disclaimer_popover.cljs
@@ -10,9 +10,9 @@
       [:div.team-disclaimer-row
         [:span.title [:i.fa.fa-user] " Viewer"] "- read only access to the dashboard"]
       [:div.team-disclaimer-row
-        [:span.title [:i.fa.fa-user] " Author"] "- edit entries, add new entries, add and archive topics, share company updates"]
+        [:span.title [:i.fa.fa-pencil] " Author"] "- edit entries, add new entries, add and archive topics, share company updates"]
       [:div.team-disclaimer-row
-        [:span.title [:i.fa.fa-user] " Admin"] "- company settings, payments, manage team members"]
+        [:span.title [:i.fa.fa-gear] " Admin"] "- company settings, payments, manage team members"]
       [:div.center
         [:button.center.btn-reset.btn-solid.got-it-btn
           {:on-click #(hide-popover-cb)}
