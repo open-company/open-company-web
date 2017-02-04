2e0a67098e27d8e6e074a422b117dd865217a10d
bago2k4 <iacopo@rc0.eu>
2017-02-04 15:47:17 +0100

    Use :current-user-data ref instead of :jwt for the editable user profile data.

--- src/open_company_web/components/ui/user_avatar.cljs
+++ src/open_company_web/components/ui/user_avatar.cljs
@@ -9,39 +9,39 @@
 
 (rum/defcs avatar-with-initials < rum/static
                                  rum/reactive
-                                 (drv/drv :jwt)
+                                 (drv/drv :current-user-data)
   [s]
   [:div.user-avatar-name
-    (when (or (:first-name (drv/react s :jwt))
-              (:last-name (drv/react s :jwt)))
-      (let [first-name-initial (or (first (:first-name (drv/react s :jwt))) "")
-            last-name-initial (or (first (:last-name (drv/react s :jwt))) "")
+    (when (or (:first-name (drv/react s :current-user-data))
+              (:last-name (drv/react s :current-user-data)))
+      (let [first-name-initial (or (first (:first-name (drv/react s :current-user-data))) "")
+            last-name-initial (or (first (:last-name (drv/react s :current-user-data))) "")
             avatar-name (clojure.string/upper-case (str first-name-initial
                                                         last-name-initial))]
         [:span.user-avatar-name-span avatar-name]))])
 
 (rum/defcs user-avatar-image < rum/static
                                rum/reactive
-                               (drv/drv :jwt)
+                               (drv/drv :current-user-data)
   [s avatar-url]
   (if-not (clojure.string/blank? avatar-url)
     [:img.user-avatar-img
       {:src avatar-url
-       :title (:real-name (drv/react s :jwt))}]
+       :title (:real-name (drv/react s :current-user-data))}]
     (avatar-with-initials)))
 
 (rum/defcs user-avatar < rum/static
                          rum/reactive
-                         (drv/drv :jwt)
+                         (drv/drv :current-user-data)
   [s {:keys [classes click-cb]}]
   (let [not-mobile? (not (responsive/is-mobile-size?))]
     [:button.user-avatar-button.group
       {:type "button"
-       :class (str classes (when (clojure.string/blank? (:avatar-url (drv/react s :jwt))) " no-image"))
+       :class (str classes (when (clojure.string/blank? (:avatar-url (drv/react s :current-user-data))) " no-image"))
        :id "dropdown-toggle-menu"
        :data-toggle (when not-mobile? "dropdown")
        :on-click (when (fn? click-cb) (click-cb))
        :aria-haspopup true
        :aria-expanded false}
-      (user-avatar-image (:avatar-url (drv/react s :jwt)))
+      (user-avatar-image (:avatar-url (drv/react s :current-user-data)))
       [:img {:src "/img/vert-ellipsis.svg" :width 5 :height 24}]]))
