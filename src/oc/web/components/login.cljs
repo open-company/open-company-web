f0ff4038abd87d71afa13f080fb00a763c54896c
bago2k4 <iacopo@rc0.eu>
2017-02-09 15:39:44 +0100

    Make sure the signin and signup buttons work.

--- src/oc/web/components/login.cljs
+++ src/oc/web/components/login.cljs
@@ -2,11 +2,13 @@
   (:require [rum.core :as rum]
             [oc.web.lib.responsive :as responsive]
             [oc.web.components.ui.footer :refer (footer)]
-            [oc.web.components.ui.login-required :refer (login-required)]))
+            [oc.web.components.ui.login-required :refer (login-required)]
+            [oc.web.components.ui.login-overlay :refer (login-overlays-handler)]))
 
 (rum/defc login < rum/static
   [{:keys [welcome]}]
   [:div.login.fullscreen-page
+    (login-overlays-handler)
     [:div.login-internal
       (login-required {:welcome welcome})]
     (footer (responsive/total-layout-width-int (responsive/calc-card-width) (responsive/columns-num)))])
