6a58a66abf3637c24ac916424a4c4fc652564296
bago2k4 <iacopo@rc0.eu>
2017-03-09 12:13:14 +0100

    Avoid api key not found for filestack errors.

--- src/oc/web/components/edit_user_profile.cljs
+++ src/oc/web/components/edit_user_profile.cljs
@@ -77,7 +77,8 @@
 
   (did-mount [_]
     (when-not (responsive/is-tablet-or-mobile?)
-      (.tooltip (js/$ "[data-toggle=\"tooltip\"]"))))
+      (.tooltip (js/$ "[data-toggle=\"tooltip\"]"))
+      (js/filepicker.setKey ls/filestack-key)))
 
   (did-update [_ _ _]
     (when-not (utils/is-test-env?)
