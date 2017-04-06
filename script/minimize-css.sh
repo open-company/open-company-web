5e43b72b932ac6a3cf099bf8f54d737ec29becd8
bago2k4 <iacopo@rc0.eu>
2017-04-06 14:26:51 +0200

    Fix minimize css script.

--- script/minimize-css.sh
+++ script/minimize-css.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-cssnano < $1/target/public/css/normalize.css \
-< $1/target/public/css/app.main.css \
-< $1/target/public/css/emojione/autocomplete.css \
-< $1/target/public/css/medium-editor/medium-editor.css \
-< $1/target/public//css/medium-editor/default.css \
-< $1/target/public/css/emojione.css \
-< $1/target/public/css/emojione-picker.css \
-< $1/target/public/css/emojione.sprites.css \
->> $2
+cssnano < $1/target/public/css/normalize.css >> $2
+cssnano < $1/target/public/css/app.main.css >> $2
+cssnano < $1/target/public/css/emojione/autocomplete.css >> $2
+cssnano < $1/target/public/css/medium-editor/medium-editor.css >> $2
+cssnano < $1/target/public//css/medium-editor/default.css >> $2
+cssnano < $1/target/public/css/emojione.css >> $2
+cssnano < $1/target/public/css/emojione-picker.css >> $2
+cssnano < $1/target/public/css/emojione.sprites.css >> $2
