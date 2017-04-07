29fafba0a7fcc02b10f229242155d689e1cd3e4f
bago2k4 <iacopo@rc0.eu>
2017-04-07 14:55:44 +0200

    Fix compile assets script.

--- script/compile_assets.sh
+++ script/compile_assets.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-java -jar ~/closure_compiler/closure-compiler-v$1.jar /
+java -jar ~/closure_compiler/closure-compiler-v$1.jar \
 -W QUIET \
---js_output_file resources/public/js/oc_assets.js \
---create_source_map public/js/oc_assets.js.map \
---js resources/public/lib/print_ascii.js \
---js resources/public/lib/truncate/jquery.truncate.js \
---js resources/public/lib/scrollTo/scrollTo.min.js \
---js resources/public/js/emojione/autocomplete.js \
---js resources/public/lib/js-utils/svg-utils.js \
---js resources/public/lib/js-utils/pasteHtmlAtCaret.js \
---js resources/public/lib/cleanHTML/cleanHTML.js \
---js resources/public/lib/MediumEditorAutolist/autolist.js \
---js resources/public/lib/select2/js/select2.js \
---output_wrapper '%output%\n//# sourceMappingURL=$2/js/oc_assets.js.map'
+--js_output_file $2/resources/public/js/oc_assets.js \
+--create_source_map $2/public/js/oc_assets.js.map \
+--js $2/resources/public/lib/print_ascii.js \
+--js $2/resources/public/lib/truncate/jquery.truncate.js \
+--js $2/resources/public/lib/scrollTo/scrollTo.min.js \
+--js $2/resources/public/js/emojione/autocomplete.js \
+--js $2/resources/public/lib/js-utils/svg-utils.js \
+--js $2/resources/public/lib/js-utils/pasteHtmlAtCaret.js \
+--js $2/resources/public/lib/cleanHTML/cleanHTML.js \
+--js $2/resources/public/lib/MediumEditorAutolist/autolist.js \
+--js $2/resources/public/lib/select2/js/select2.js \
+--output_wrapper '%output%\n//# sourceMappingURL=$3/js/oc_assets.js.map'
