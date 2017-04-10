45e4a1c2526e9969151ab7a3aa6281ae001bb59d
bago2k4 <iacopo@rc0.eu>
2017-04-10 22:55:00 +0200

    Turn off zindex optimization for cssnano.

--- script/minimize-css.sh
+++ script/minimize-css.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cssnano < $1/target/public/css/normalize.css >> $2
-cssnano < $1/target/public/css/app.main.css >> $2
-cssnano < $1/target/public/css/emojione/autocomplete.css >> $2
-cssnano < $1/target/public/css/medium-editor/medium-editor.css >> $2
-cssnano < $1/target/public//css/medium-editor/default.css >> $2
-cssnano < $1/target/public/css/emojione.css >> $2
-cssnano < $1/target/public/css/emojione-picker.css >> $2
-cssnano < $1/target/public/css/emojione.sprites.css >> $2
+cssnano --no-zindex < $1/target/public/css/normalize.css >> $2
+cssnano --no-zindex < $1/target/public/css/app.main.css >> $2
+cssnano --no-zindex < $1/target/public/css/emojione/autocomplete.css >> $2
+cssnano --no-zindex < $1/target/public/css/medium-editor/medium-editor.css >> $2
+cssnano --no-zindex < $1/target/public//css/medium-editor/default.css >> $2
+cssnano --no-zindex < $1/target/public/css/emojione.css >> $2
+cssnano --no-zindex < $1/target/public/css/emojione-picker.css >> $2
+cssnano --no-zindex < $1/target/public/css/emojione.sprites.css >> $2
