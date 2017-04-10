12ed150185dfb8cd57501cd22d4972931a3f7842
bago2k4 <iacopo@rc0.eu>
2017-04-10 14:00:51 +0200

    Do not create the assets source map file in the js folder.

--- script/compile_assets.sh
+++ script/compile_assets.sh
@@ -4,7 +4,7 @@ cd $2/resources/public/ && \
 java -jar ~/closure_compiler/closure-compiler-v$1.jar \
 -W QUIET \
 --js_output_file oc_assets.js \
---create_source_map js/oc_assets.js.map \
+--create_source_map oc_assets.js.map \
 --js lib/print_ascii.js \
 --js lib/truncate/jquery.truncate.js \
 --js lib/scrollTo/scrollTo.min.js \
