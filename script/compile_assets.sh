4c9ccd10622c129c7f1d42dc0287433780e99b48
bago2k4 <iacopo@rc0.eu>
2017-04-10 12:40:04 +0200

    Run the compile js assets script from the target public folder directly to avoid absolute links.

--- script/compile_assets.sh
+++ script/compile_assets.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
+cd $2/resources/public/ \
 java -jar ~/closure_compiler/closure-compiler-v$1.jar \
 -W QUIET \
---js_output_file $2/resources/public/js/oc_assets.js \
---create_source_map $2/resources/public/js/oc_assets.js.map \
+--js_output_file js/oc_assets.js \
+--create_source_map js/oc_assets.js.map \
 --module_output_path_prefix "../" \
---js $2/resources/public/lib/print_ascii.js \
---js $2/resources/public/lib/truncate/jquery.truncate.js \
---js $2/resources/public/lib/scrollTo/scrollTo.min.js \
---js $2/resources/public/js/emojione/autocomplete.js \
---js $2/resources/public/lib/js-utils/svg-utils.js \
---js $2/resources/public/lib/js-utils/pasteHtmlAtCaret.js \
---js $2/resources/public/lib/cleanHTML/cleanHTML.js \
---js $2/resources/public/lib/MediumEditorAutolist/autolist.js \
---js $2/resources/public/lib/select2/js/select2.js \
+--js lib/print_ascii.js \
+--js lib/truncate/jquery.truncate.js \
+--js lib/scrollTo/scrollTo.min.js \
+--js js/emojione/autocomplete.js \
+--js lib/js-utils/svg-utils.js \
+--js lib/js-utils/pasteHtmlAtCaret.js \
+--js lib/cleanHTML/cleanHTML.js \
+--js lib/MediumEditorAutolist/autolist.js \
+--js lib/select2/js/select2.js \
 --output_wrapper "%output%
 //# sourceMappingURL=$3/js/oc_assets.js.map"
