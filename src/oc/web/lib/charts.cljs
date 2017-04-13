e4aaa2bf4eda446af274be9d50fcca3a0d5b322a
bago2k4 <iacopo@rc0.eu>
2017-04-14 00:53:34 +0200

    Load the scripts with source before evaluating the inline javascript of the chart.

--- src/oc/web/lib/charts.cljs
+++ src/oc/web/lib/charts.cljs
@@ -13,17 +13,16 @@
             non-src-scripts (js/$ "script[type='text/javascript']:not([src])" chart-dom)
             src-scripts (js/$ "script[src]" chart-dom)]
 
+          (dotimes [idx (.-length src-scripts)]
+            (let [src (.-src (.get src-scripts idx))
+                  splitted (clojure.string/split src #"/")
+                  final-src (str "https://docs.google.com/" (clojure.string/join "/" (subvec splitted 3)) "?no-cache=" (rand 4))]
+              (.append (js/$ "body") (js/$ (str "<script type='text/javascript' src='" final-src "' />")))))
+
           (dotimes [idx (.-length non-src-scripts)]
             (let [script-js-str (.-innerHTML (.get non-src-scripts idx))
                   rp (js/RegExp "(\"width\":\\d+)" "gi")
                   fixed-string (.replace script-js-str rp (str "\"width\":" card-width))
                   r-chart (js/RegExp "safeDraw\\(document.getElementById\\('c'\\)\\)" "gi")
                   fixed-string-c (.replace fixed-string r-chart (str "safeDraw(document.getElementById('chart-" chart-url "'))"))]
-              (.call js/eval js/window fixed-string-c)))
-
-          (dotimes [idx (.-length src-scripts)]
-            (let [src (.-src (.get src-scripts idx))
-                  splitted (clojure.string/split src #"/")
-                  final-src (str "https://docs.google.com/" (clojure.string/join "/" (subvec splitted 3)) "?no-cache=" (rand 4))]
-              (.append (js/$ "body") (js/$ (str "<script type='text/javascript' src='" final-src "' />")))))
-        ))))
+              (.call js/eval js/window fixed-string-c)))))))
