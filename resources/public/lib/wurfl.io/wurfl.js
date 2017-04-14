ff1104cc66640373b0f9c48637e830a77c642817
bago2k4 <iacopo@rc0.eu>
2017-04-14 13:50:30 +0200

    Add a local copy of wurfl.io, load it from lib on local dev.

---
+++ resources/public/lib/wurfl.io/wurfl.js
@@ -0,0 +1,6 @@
+// Copyright 2017 - ScientiaMobile, Inc., Reston, VA
+// WURFL Device Detection
+// Terms of service:
+// http://web.wurfl.io/license
+
+eval(function(p,a,c,k,e,d){e=function(c){return c};if(!''.replace(/^/,String)){while(c--){d[c]=k[c]||c}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('8 7={"6":5,"4":"3 2","1":"0"};',9,9,'Desktop|form_factor|Chrome|Google|complete_device_name|false|is_mobile|WURFL|var'.split('|'),0,{}))
