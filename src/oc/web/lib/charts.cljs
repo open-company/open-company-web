cdf0a90e0d1b13dcc7caad2cdb79510167368139
Sean Johnson <sean@snootymonkey.com>
2017-04-16 06:14:22 -0400

    Update to use our forward proxy to Google Sheets rather than crossorigin.me.

--- src/oc/web/lib/charts.cljs
+++ src/oc/web/lib/charts.cljs
@@ -5,10 +5,23 @@
             [taoensso.timbre :as timbre]
             [oc.web.lib.utils :as utils]))
 
-(defn load-chart-url [chart-url card-width]
-  (let [cors-io-url (str "https://crossorigin.me/" chart-url)]
+(defn load-chart-url
+  "
+  Load a (presumably) public Google Sheets chart URL via our proxy (to avoid CORs issues) and extract
+  the parts we need to display the chart. Then display it.
+
+  Public Google Sheets chart URLs look like:
+  https://docs.google.com/spreadsheets/d/1X5Ar6_JJ3IviO64-cJ0DeklFuS42BSdXxZV6x5W0qOc/pubchart?oid=1033950253&format=interactive
+
+  Our proxy request URLs look like:
+  /_/sheets-proxy/1X5Ar6_JJ3IviO64-cJ0DeklFuS42BSdXxZV6x5W0qOc/pubchart?oid=1033950253&format=interactive
+  "
+  [chart-url card-width]
+  
+  (let [url-fragment (last (clojure.string/split chart-url #"/spreadsheets/d/"))
+        chart-proxy-url (str "/_/sheets-proxy/" url-fragment)]
     (go
-      (let [resp (<! (http/get cors-io-url {:with-credentials? false}))
+      (let [resp (<! (http/get chart-proxy-url {:with-credentials? false})) ; get the chart via the proxy
             chart-dom (js/$ (str "<div>" (:body resp) "</div>"))
             non-src-scripts (js/$ "script[type='text/javascript']:not([src])" chart-dom)
             src-scripts (js/$ "script[src]" chart-dom)]
