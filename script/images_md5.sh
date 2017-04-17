0981ce09023e3cf370ff4bc00b7915278f718bee
bago2k4 <iacopo@rc0.eu>
2017-04-17 19:39:45 +0200

    Do not count the file timestamp for the md5sum since the files are all copied during the build.

--- script/images_md5.sh
+++ script/images_md5.sh
@@ -3,7 +3,7 @@
 for f in $(find ./target/public/img/ -type f)
 do
   echo $f >> /tmp/partial_$1.txt
-  (stat -c %Y $f; echo $f; cat $f) | md5sum >> /tmp/partial_$1.txt
+  (echo $f; cat $f) | md5sum >> /tmp/partial_$1.txt
 done
 
 cat /tmp/partial_$1.txt | md5sum
