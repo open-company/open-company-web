805a62b64cbc31d621e0854e69a903e1d12c94e5
bago2k4 <iacopo@rc0.eu>
2017-04-18 16:23:35 +0200

    Create the chart node id from the topic slug, :created-at and the chart url. Add a retry check that restart the render chart script if the node is empty after the script was launched, Retry 5 times top with a growing delay.

--- src/oc/web/components/chart.cljs
+++ src/oc/web/components/chart.cljs
@@ -1,13 +1,37 @@
 (ns oc.web.components.chart
   (:require [rum.core :as rum]
             [oc.web.lib.charts :as c]
-            [oc.web.lib.utils :as utils]))
+            [oc.web.lib.utils :as utils]
+            [cljs-hash.goog :as gh]))
+
+(defn chart-uid [topic-data]
+  (when (:chart-url topic-data)
+    (str (:topic topic-data) "-" (:created-at topic-data) "-" (gh/hash :md5 (:chart-url topic-data)))))
+
+(declare render-chart)
+
+(defn check-chart
+  "After the render-chart function check if the chart node is empty, if so that means something went wrong
+   and we need to retry the mount. Increase the retry variable, retry at most 5 times with a growing delay."
+  [s]
+  (let [retry @(::retry s)
+        topic-data (first (:rum/args s))
+        chart-id (chart-uid topic-data)]
+    (when-let [chart-node (.getElementById js/document chart-id)]
+      (when (and (empty? (.trim (.-innerHTML chart-node)))
+                 (< retry 5))
+        (reset! (::retry s) (* retry 2))
+        (render-chart s)))))
 
 (defn render-chart [s]
-  (let [chart-url (first (:rum/args s))]
-    (when (and (not (empty? chart-url))
+  (let [topic-data (first (:rum/args s))
+        chart-url (:chart-url topic-data)
+        chart-id (chart-uid topic-data)]
+    (when (and (not (empty? topic-data))
+               (not (empty? chart-url))
                (not= @(::last-url s) chart-url))
-      (c/load-chart-url chart-url (second (:rum/args s))))))
+      (c/load-chart-url chart-url chart-id (second (:rum/args s)))
+      (utils/after (* @(::retry s) 1500) #(check-chart s)))))
 
 (defn chart-did-mount [s]
   (render-chart s)
@@ -18,11 +42,15 @@
   s)
 
 (rum/defcs chart < (rum/local "" ::last-url)
+                   (rum/local 1 ::retry)
                    {:did-remoout chart-did-remount
                     :did-mount chart-did-mount
                     :did-update chart-did-mount}
 
-  [s gchart-link card-width]
-  [:div.chart
-    {:key gchart-link
-     :id (str "chart-" gchart-link)}])
+  [s topic-data card-width]
+  (when (not (empty? (:chart-url topic-data)))
+    (let [chart-id (chart-uid topic-data)]
+      [:div.chart
+        {:key chart-id
+         :data-chart-src (:chart-url topic-data)
+         :id chart-id}])))
