0ab0ce57078525be909e68cc7bfef67a7e4e9d6e
bago2k4 <iacopo@rc0.eu>
2017-04-20 15:16:19 +0200

    Add some arbitrary js to calc the width and height of the viewport to render the chart and to redraw the chart when the viewport changes.

--- site/oc/server.clj
+++ site/oc/server.clj
@@ -23,6 +23,22 @@
 (defn server-error []
   (assoc (res/resource-response "/500.html" {:root "public"}) :status 500))
 
+(defn inject-js []
+  (str "<script type=\"text/javascript\">
+function getViewportWidth(){
+  return Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+}
+function getViewportHeight(){
+  return Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+}
+document.addEventListener(\"DOMContentLoaded\", function(event) {
+  window.addEventListener(\"resize\", function(event) {
+    if(window.onNumberFormatApiLoad !== undefined){
+      onNumberFormatApiLoad();
+    }
+  });
+});</script>"))
+
 (defn proxy-sheets-pass
   "Proxy requests to Google Sheets (needed for CORs). Done by nginx in production."
   [sheet-path params]
@@ -38,7 +54,7 @@
         (do (println body) {:status status :body body})
         {:status 200 :body body :headers {"Content-Type" "text/html"}}))))
 
-(defn fix-script-string [s chart-id chart-width chart-height]
+(defn fix-script-string [s chart-id]
   (let [r0 #"(?i)(\"width\":\d+)" ;(js/RegExp  "gi")
         r01 #"(?i)(\"height\":\d+)" ;(js/RegExp  "gi")
         r1 #"(?i)safeDraw\(document.getElementById\('c'\)\)" ;(js/RegExp  "gi")
@@ -47,17 +63,17 @@
         r3 #"(?i)\"containerId\":\"embed_\d+\"" ;(js/RegExp  "gi")
         r4 #"(?i)posObj\('\d+', 'embed_\d+', 0, 0, 0, 0\);};" ;(js/RegExp  "gi")
         ;; Replace all regexp
-        fixed-string (clojure.string/replace s r0 (str "\"width\":" chart-width))
-        fixed-string-01 (clojure.string/replace fixed-string r01 (str "\"height\":" chart-height))
+        fixed-string (clojure.string/replace s r0 (str "\"width\": getViewportWidth()"))
+        fixed-string-01 (clojure.string/replace fixed-string r01 (str "\"height\": getViewportHeight()"))
         fixed-string-1 (clojure.string/replace fixed-string-01 r1 (str "safeDraw(document.getElementById('" chart-id "'))"))
         fixed-string-2 (clojure.string/replace fixed-string-1 r2 (str "activeSheetId = '" chart-id "'; switchToSheet('" chart-id "');"))
         fixed-string-3 (clojure.string/replace fixed-string-2 r3 (str "\"containerId\":\"" chart-id "\""))
         fixed-string-4 (clojure.string/replace fixed-string-3 r4 (str "posObj('" chart-id "', '" chart-id "', 0, 0, 0, 0);};"))]
     fixed-string-4))
 
-(defn get-script-tag [s chart-id chart-width chart-height]
+(defn get-script-tag [s chart-id]
   (if (empty? (:src (:attrs s)))
-    (str "<script type=\"text/javascript\">" (fix-script-string (apply str (:content s)) chart-id chart-width chart-height) "</script>")
+    (str "<script type=\"text/javascript\">" (fix-script-string (apply str (:content s)) chart-id) "</script>")
     (str "<script type=\"text/javascript\" src=\"/_/sheets-proxy-pass" (:src (:attrs s)) "\"></script>")))
 
 (defn proxy-sheets
@@ -75,8 +91,8 @@
         (do (println body) {:status status :body body})
         (let [parsed-html (h/as-hickory (h/parse body))
               scripts (s/select (s/tag :script) parsed-html)
-              script-strings (apply str (map #(get-script-tag % (get params "chart-id") (get params "chart-width") (get params "chart-height")) scripts))
-              output-html (str "<html><head><style type=\"text/css\">html,body{margin:0;padding:0;border:none;overflow:hidden;}</style></head><body>" script-strings "<div id=\"" (get params "chart-id") "\"></div></body></html>")]
+              script-strings (apply str (map #(get-script-tag % (get params "chart-id")) scripts))
+              output-html (str "<html><head>" (inject-js) "<style type=\"text/css\">html,body{margin:0;padding:0;border:none;overflow:hidden;}</style></head><body>" script-strings "<div id=\"" (get params "chart-id") "\"></div></body></html>")]
           {:status 200 :body output-html})))))
 
 (defroutes resources
