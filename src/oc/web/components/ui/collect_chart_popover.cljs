37123fea2276adb38ba477e7665da95cbff014a5
bago2k4 <iacopo@rc0.eu>
2017-05-02 17:19:02 +0200

    Enable save button when the chart url is added.

--- src/oc/web/components/ui/collect_chart_popover.cljs
+++ src/oc/web/components/ui/collect_chart_popover.cljs
@@ -14,7 +14,9 @@
                                                  s)}
   [s]
   (let [topic-data (drv/react s :foce-data)
-        hide-popover-cb (:hide-popover-cb (first (:rum/args s)))]
+        props (first (:rum/args s))
+        hide-popover-cb (:hide-popover-cb props)
+        collect-chart-cb (:collect-chart-cb props)]
     [:div.oc-popover-container-internal
       [:div.collect-chart-popover.oc-popover
         [:button.btn-reset.close-button
@@ -41,5 +43,7 @@
             {:disabled (not (utils/check-google-chart-url @(::chart-url s)))
              :on-click (fn [e]
                          (dis/dispatch! [:foce-input [:chart-url (utils/clean-google-chart-url @(::chart-url s))]])
+                         (when (fn? collect-chart-cb)
+                           (collect-chart-cb e))
                          (hide-popover-cb e))}
             "ADD CHART"]]]]))
