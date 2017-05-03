4ae71b7694f3c746e11d749f91053247ed208eba
bago2k4 <iacopo@rc0.eu>
2017-05-03 13:27:48 +0200

    Make the chart area square using the same size as of the card width for the chart height.

--- src/oc/web/components/chart.cljs
+++ src/oc/web/components/chart.cljs
@@ -17,5 +17,5 @@
         {:key chart-id
          :data-chart-src (:chart-url topic-data)}
         [:iframe
-          {:style {:border "none" :margin "0" :padding "0" :width (str card-width "px") :height "350px"}
+          {:style {:border "none" :margin "0" :padding "0" :width (str card-width "px") :height (str card-width "px")}
            :src chart-proxy-url}]])))
