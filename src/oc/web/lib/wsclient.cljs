9689784bc8f28f07df5857867f21bd548d4fc8ea
bago2k4 <iacopo@rc0.eu>
2017-05-09 19:44:15 +0200

    Rewrite how the ws client connects and handle events.

--- src/oc/web/lib/wsclient.cljs
+++ src/oc/web/lib/wsclient.cljs
@@ -4,53 +4,81 @@
             [om.dom :as dom :include-macros true]
             [sablono.core :as html :refer-macros [html]]
             [taoensso.sente :as s]
-            [cljs.core.async :as async :refer [<! >! chan]]))
+            [cljs.core.async :as async :refer [<! >! chan]]
+            [taoensso.encore :as encore :refer-macros (have)]))
 
 (def ws-port 3000)
 
 (def ws-server "localhost")
 
-(let [{:keys [chsk ch-recv send-fn state]}
-      (s/make-channel-socket! "/chsk" {:type :auto :host "localhost:3000"})]
-  (def chsk       chsk)
-  (def ch-chsk    ch-recv)
-  (def chsk-send! send-fn)
-  (def chsk-state chsk-state))
+(def ch-chsk (atom nil))
+(def chsk-send! (atom nil))
 
-(defmulti handle-event
-  "Handle events based on the event ID."
-  (fn [[ev-id ev-arg] app owner] ev-id))
+;; Event handlers
 
-;; Process the server's reply by updating the application state:
+(defmulti -event-msg-handler
+  "Multimethod to handle Sente `event-msg`s"
+  :id ; Dispatch on event-id
+  )
 
-(defmethod handle-event :test/reply
-  [[_ msg] app owner]
-  (js/console.log "handle-event/:test/reply"))
+(defn event-msg-handler
+  "Wraps `-event-msg-handler` with logging, error catching, etc."
+  [{:as ev-msg :keys [id ?data event]}]
+  (-event-msg-handler ev-msg))
 
-;; Ignore unknown events (we just print to the console):
+(defmethod -event-msg-handler :default
+  [{:as ev-msg :keys [event]}]
+  ; Default/fallback case (no other matching handler)
+  (js/console.log (encore/format "Unhandled event: %s" event)))
 
-(defmethod handle-event :default
-  [event app owner]
-  (js/console.log "handle-event/:default"))
+(defmethod -event-msg-handler :chsk/state
+  [{:as ev-msg :keys [?data]}]
+  (let [[old-state-map new-state-map] (have vector? ?data)]
+    (if (:first-open? new-state-map)
+      (js/console.log (encore/format "Channel socket successfully established!: %s" new-state-map))
+      (js/console.log (encore/format "Channel socket state change: %s" new-state-map)))))
 
-;; Remember the session state in the application component's local state:
+(defmethod -event-msg-handler :chsk/recv
+  [{:as ev-msg :keys [?data]}]
+  (js/console.log (encore/format "Push event from server: %s" ?data)))
 
-(defmethod handle-event :session/state
-  [[_ state] app owner]
-  (js/console.log "handle-event/:session/state"))
+(defmethod -event-msg-handler :chsk/handshake
+  [{:as ev-msg :keys [?data]}]
+  (let [[?uid ?csrf-token ?handshake-data] ?data]
+    (js/console.log (encore/format "Handshake: %s" ?data))))
+
+;; Session test
 
 (defn test-session
   "Ping the server to update the sesssion state."
   []
-  (chsk-send! [:session/status]))
+  (@chsk-send! [:session/status]))
 
-(defn event-loop
-  "Handle inbound events."
-  []
-  (go (loop [[op arg] (:event (<! ch-chsk))]
-        (js/console.log "wsclient receive -" op)
-        (case op
-          :chsk/recv (handle-event arg)
-          ;; we ignore other Sente events
-          (test-session))
-        (recur (:event (<! ch-chsk))))))
+;;;; Sente event router (our `event-msg-handler` loop)
+
+(defonce router_ (atom nil))
+(defn  stop-router! [] (when-let [stop-f @router_] (stop-f)))
+(defn start-router! []
+  (stop-router!)
+  (reset! router_
+    (s/start-client-chsk-router!
+      @ch-chsk event-msg-handler)))
+
+(defn reconnect [uid]
+  (js/console.log "wsclient/reconnect" uid)
+  (let [{:keys [chsk ch-recv send-fn state] :as x}
+          (s/make-channel-socket! "/chsk" {:type :auto
+                                           :host (encore/format "%s:%d" ws-server ws-port)
+                                           :packer :edn
+                                           :uid uid})]
+    (js/console.log "   - x" x)
+    (js/console.log "   - chsk" chsk)
+    (js/console.log "   - ch-recv" ch-recv)
+    (js/console.log "   - send-fn" send-fn)
+    (js/console.log "   - state" state)
+    ; (reset! chsk chsk)
+    (reset! ch-chsk ch-recv)
+    (reset! chsk-send! send-fn)
+    ; (reset! chsk-state state)
+    ; (if (:is-open?))
+    (start-router!)))
