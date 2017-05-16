ab9c01ce8502c14cd35c9d7b569c8de0f22d46a9
bago2k4 <iacopo@rc0.eu>
2017-05-16 21:30:26 +0200

    Add actions to load and dispatch the loaded comments.

--- src/oc/web/actions.cljs
+++ src/oc/web/actions.cljs
@@ -927,3 +927,17 @@
            (= (:entry-uuid (:comments-open db)) entry-uuid))
     (dissoc db :comments-open)
     (assoc db :comments-open {:topic-slug topic-slug :entry-uuid entry-uuid})))
+
+(defmethod dispatcher/action :comments-get
+  [db [_ entry-uuid]]
+  (js/console.log "comments-get" entry-uuid)
+  (api/get-comments entry-uuid)
+  (let [org-slug (router/current-org-slug)
+        board-slug (router/current-board-slug)
+        topic-slug (router/current-topic-slug)]
+    (assoc-in db (conj (dispatcher/comments-key org-slug board-slug topic-slug entry-uuid) :loading) true)))
+
+(defmethod dispatcher/action :comments-get/finish
+  [db [_ {:keys [success error body entry-uuid]}]]
+  (let [comments-key (dispatcher/comments-key (router/current-org-slug) (router/current-board-slug) (router/current-topic-slug) entry-uuid)]
+    (assoc-in db comments-key body)))
