dbae0805460725308d1ad4745b603381a7315880
bago2k4 <iacopo@rc0.eu>
2017-05-17 16:30:23 +0200

    Remove hardcoded comments data. Fix key to render the list of comments. Use time-since for the comment date.

--- src/oc/web/components/comments.cljs
+++ src/oc/web/components/comments.cljs
@@ -5,49 +5,6 @@
             [oc.web.dispatcher :as dis]
             [oc.web.components.ui.small-loading :refer (small-loading)]))
 
-(def comment-author-me {
-  :name "Iacopo Carraro"
-  :user-id "3af0-4014-a3c6"
-  :avatar-url "https://avatars.slack-edge.com/2017-02-02/136114833346_3758034af26a3b4998f4_512.jpg"
-})
-
-(def comment-author-2 {
-  :name "Sean Johnson"
-  :user-id "4fb0-1536-a8c0"
-  :avatar-url "https://ca.slack-edge.com/T06SBMH60-U06SBTXJR-gf5b8fc1affa-1024"
-})
-
-(def comments-data {
-  :entry-uuid "1234-1234-1234-1234"
-  :id "1234"
-  :topic-slug "product"
-  :body "Body"
-  :headline "Headline"
-  :created-at "2017-03-23T11:21:39.000Z"
-  :updated-at "2017-03-23T12:21:39.000Z"
-  :author comment-author-me
-  :links []
-  :comments [{
-    :comment-id "fedc-ba98-7654-3210"
-    :body "Comment #1 body"
-    :created-at "2017-04-21T12:12:12.000Z"
-    :updated-at "2017-04-21T12:12:12.000Z"
-    :author comment-author-me
-  } {
-    :comment-id "fedc-ba98-7654-1230"
-    :body "Comment #2 body"
-    :created-at "2017-04-30T12:12:12.000Z"
-    :updated-at "2017-04-30T12:12:12.000Z"
-    :author comment-author-2
-  } {
-    :comment-id "fedc-ba98-7654-ab21"
-    :body "Comment #3 body"
-    :created-at "2017-05-01T12:12:12.000Z"
-    :updated-at "2017-05-01T12:12:12.000Z"
-    :author comment-author-me
-  }]
-})
-
 (rum/defc comment-row
   [c]
   (let [author (:author c)]
@@ -58,7 +15,7 @@
         [:div.comment-author.left
           (:name author)]
         [:div.comment-timestamp.left
-          (utils/date-string (utils/js-date (:created-at c)))]]
+          (utils/time-since (:created-at c))]]
       [:div.comment-body.group
         (:body c)]
       [:div.comment-footer.group]]))
@@ -104,5 +61,5 @@
         [:div.comments
           (when (pos? (count entry-comments))
             (for [c entry-comments]
-              (rum/with-key (comment-row c) (str "entry-" (:entry-uuid (:show-comments comments-data)) "comment-" (:comment-id c)))))
+              (rum/with-key (comment-row c) (str "entry-" (:entry-uuid (:show-comments comments-data)) "-comment-" (:created-at c)))))
           (add-comment)]))))
