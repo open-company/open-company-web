dd82897943c3da1441c83cdc78b4d4265fc81c52
bago2k4 <iacopo@rc0.eu>
2017-05-25 16:16:15 +0200

    Make sure the connection is not attempted again if the same board is loaded again. Close the connection and start another when the user switch board.

--- src/oc/web/lib/wsclient.cljs
+++ src/oc/web/lib/wsclient.cljs
@@ -4,29 +4,32 @@
             [om.dom :as dom :include-macros true]
             [sablono.core :as html :refer-macros [html]]
             [taoensso.sente :as s]
+            [taoensso.timbre :as timbre]
             [cljs.core.async :as async :refer [<! >! chan]]
             [taoensso.encore :as encore :refer-macros (have)]
             [oc.web.lib.jwt :as j]
-            [oc.web.local-settings :as ls]))
+            [oc.web.local-settings :as ls]
+            [goog.Uri :as guri]))
 
 (def ws-port 3002)
 
 (def ws-server "localhost")
 
+(def current-board-path (atom nil))
 (def channelsk (atom nil))
 (def ch-chsk (atom nil))
+(def ch-state (atom nil))
 (def chsk-send! (atom nil))
 
 ;; Auth
 
 (defn should-disconnect? [rep]
   (when-not (:valid rep)
-    (js/console.log "disconnecting client due to invalid JWT!")
-    (js/console.log rep)
+    (timbre/warn "disconnecting client due to invalid JWT!" rep)
     (s/chsk-disconnect! @channelsk)))
 
 (defn post-handshake-auth []
-  (js/console.log "wsc/post-handshake-auth")
+  (timbre/debug "Trying post handshake jwt auth")
   (@chsk-send! [:auth/jwt {:jwt (j/jwt)}] 1000 should-disconnect?))
 
 ;; Event handlers
@@ -44,24 +47,24 @@
 (defmethod -event-msg-handler :default
   [{:as ev-msg :keys [event]}]
   ; Default/fallback case (no other matching handler)
-  (js/console.log (encore/format "Unhandled event: %s" event)))
+  (timbre/warn "Unhandled event: " event))
 
 (defmethod -event-msg-handler :chsk/state
   [{:as ev-msg :keys [?data]}]
   (let [[old-state-map new-state-map] (have vector? ?data)]
     (if (:first-open? new-state-map)
-      (js/console.log (encore/format "Channel socket successfully established!: %s" new-state-map))
-      (js/console.log (encore/format "Channel socket state change: %s" new-state-map)))))
+      (timbre/debug "Channel socket successfully established!: " new-state-map)
+      (timbre/debug "Channel socket state change: " new-state-map))))
 
 (defmethod -event-msg-handler :chsk/recv
   [{:as ev-msg :keys [?data]}]
-  (js/console.log (encore/format "Push event from server: %s" ?data)))
+  (timbre/debug "Push event from server: " ?data))
 
 (defmethod -event-msg-handler :chsk/handshake
   [{:as ev-msg :keys [?data]}]
   (post-handshake-auth)
   (let [[?uid ?csrf-token ?handshake-data] ?data]
-    (js/console.log "Handshake:" ?uid ?csrf-token ?handshake-data)))
+    (timbre/debug "Handshake:" ?uid ?csrf-token ?handshake-data)))
 
 ;; Session test
 
@@ -73,23 +76,44 @@
 ;;;; Sente event router (our `event-msg-handler` loop)
 
 (defonce router_ (atom nil))
-(defn  stop-router! [] (when-let [stop-f @router_] (stop-f)))
+
+(defn  stop-router! []
+  (when-let [stop-f @router_]
+    (stop-f)
+    (timbre/info "Connection closed")))
+
 (defn start-router! []
   (stop-router!)
   (reset! router_
     (s/start-client-chsk-router!
-      @ch-chsk event-msg-handler)))
+      @ch-chsk event-msg-handler))
+  (timbre/info "Connection estabilished"))
 
-(defn reconnect [uid]
-  (js/console.log "wsclient/reconnect" uid)
-  (let [{:keys [chsk ch-recv send-fn state] :as x}
-          (s/make-channel-socket! "/interaction-socket/boards/1234-abcd-1234" {:type :auto
-                                           :host (encore/format "%s:%d" ws-server ws-port)
-                                           :protocol (if ls/jwt-cookie-secure :https :http)
-                                           :packer :edn
-                                           :uid uid
-                                           :params {:user-id uid}})]
-    (reset! channelsk chsk)
-    (reset! ch-chsk ch-recv)
-    (reset! chsk-send! send-fn)
-    (start-router!)))
+(defn reconnect [ws-link uid]
+  (let [ws-uri (guri/parse (:href ws-link))
+        ws-domain (str (.getDomain ws-uri) (when (.getPort ws-uri) (str ":" (.getPort ws-uri))))
+        ws-board-path (.getPath ws-uri)]
+    (when (or (not @ch-state)
+              (not (:open? @@ch-state))
+              (not= @current-board-path ws-board-path))
+      (timbre/debug "Reconnect for" (:href ws-link) "and" uid "current state:" @ch-state "current board:" @current-board-path)
+      ; if the path is different it means
+      (when (and @ch-state
+                 (:open? @@ch-state))
+        (timbre/info "Closing previous connection for" @current-board-path)
+        (stop-router!)
+        (reset! router_ nil))
+      (timbre/info "Attempting connection to" ws-domain "for board" ws-board-path)
+      (let [{:keys [chsk ch-recv send-fn state] :as x} (s/make-channel-socket! ws-board-path
+                                                        {:type :auto
+                                                         :host ws-domain
+                                                         :protocol (if ls/jwt-cookie-secure :https :http)
+                                                         :packer :edn
+                                                         :uid uid
+                                                         :params {:user-id uid}})]
+          (reset! current-board-path ws-board-path)
+          (reset! channelsk chsk)
+          (reset! ch-chsk ch-recv)
+          (reset! chsk-send! send-fn)
+          (reset! ch-state state)
+          (start-router!)))))
