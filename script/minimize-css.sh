afdb7650e45010636d9c2baacef4562a9c873fc2
bago2k4 <iacopo@rc0.eu>
2017-06-08 13:15:57 +0200

    Compact all our css into one file before passing it to cssnano or the prefixies will be overwritten.

--- script/minimize-css.sh
+++ script/minimize-css.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-cssnano --no-zindex < $1/target/public/css/normalize.css >> $2
-cssnano --no-zindex < $1/target/public/css/app.main.css >> $2
-cssnano --no-zindex < $1/target/public/css/emojione/autocomplete.css >> $2
-cssnano --no-zindex < $1/target/public/css/medium-editor/medium-editor.css >> $2
-cssnano --no-zindex < $1/target/public//css/medium-editor/default.css >> $2
-cssnano --no-zindex < $1/target/public/css/emojione.css >> $2
-cssnano --no-zindex < $1/target/public/css/emojione-picker.css >> $2
-cssnano --no-zindex < $1/target/public/css/emojione.sprites.css >> $2
+cat $1/target/public/css/normalize.css > $2.temp
+cat $1/target/public/css/app.main.css >> $2.temp
+cat $1/target/public/css/emojione/autocomplete.css >> $2.temp
+cat $1/target/public/css/medium-editor/medium-editor.css >> $2.temp
+cat $1/target/public//css/medium-editor/default.css >> $2.temp
+cat $1/target/public/css/emojione.css >> $2.temp
+cat $1/target/public/css/emojione-picker.css >> $2.temp
+cat $1/target/public/css/emojione.sprites.css >> $2.temp
+
+cssnano --no-zindex < $2.temp > $2
