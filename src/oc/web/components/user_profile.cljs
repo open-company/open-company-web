42e89361f0e9966517880b0bbbca7b5804b6a273
bago2k4 <iacopo@rc0.eu>
2017-06-09 20:07:20 +0200

    Change user profile component to reflect the new UI and add editable fields for all the user info.

--- src/oc/web/components/user_profile.cljs
+++ src/oc/web/components/user_profile.cljs
@@ -3,50 +3,125 @@
             [org.martinklepsch.derivatives :as drv]
             [oc.web.dispatcher :as dis]
             [oc.web.lib.utils :as utils]
-            [oc.web.lib.responsive :as responsive]
-            [oc.web.components.ui.footer :refer (footer)]
             [oc.web.components.ui.small-loading :refer (small-loading)]
-            [oc.web.components.ui.back-to-dashboard-btn :refer (back-to-dashboard-btn)]))
+            [oc.web.components.ui.user-avatar :refer (user-avatar-image)]
+            [cljsjs.moment-timezone]))
+
+(defn change! [k v]
+  (dis/dispatch! [:input [:edit-user-profile k] v])
+  (dis/dispatch! [:input [:edit-user-profile :has-changes] true]))
 
 (rum/defcs user-profile < rum/reactive
                           (rum/local false ::loading)
                           (rum/local false ::show-success)
-                          (drv/drv :current-user-data)
-                          {:did-remount (fn [old-state new-state]
+                          (drv/drv :edit-user-profile)
+                          {:init (fn [s _]
+                                   (dis/dispatch! [:user-profile-reset])
+                                   s)
+                           :did-remount (fn [old-state new-state]
                                           (when @(::loading new-state)
                                             (reset! (::show-success new-state) true)
                                             (reset! (::loading new-state) false)
                                             (utils/after 2000 (fn [] (reset! (::show-success new-state) false))))
                                           new-state)}
   [s]
-  [:div.user-profile.fullscreen-page
-    (back-to-dashboard-btn {:title "User Profile"})
-    [:div.user-profile-internal.mx-auto.my4
-      [:div.user-profile-content.group
-        [:div.left-column
-          [:div.user-profile-name-title.data-title "FIRST NAME"]
-          [:div.user-profile-name (:first-name (drv/react s :current-user-data))]
-          [:div.user-profile-name-title.data-title "LAST NAME"]
-          [:div.user-profile-name (:last-name (drv/react s :current-user-data))]
-          [:div.user-profile-name-title.data-title "SLACK ORGANIZATION"]
-          [:div.user-profile-name (:slack-org-name (drv/react s :current-user-data))]
-          [:div.user-profile-name-title.data-title "EMAIL"]
-          [:div.user-profile-name (:email (drv/react s :current-user-data))]]
-        [:div.right-column
-          (when (:avatar-url (drv/react s :current-user-data))
-            [:div.user-profile-avatar-title.data-title "AVATAR"])
-          (when (:avatar-url (drv/react s :current-user-data))
-            [:img.user-profile-avatar {:src (:avatar-url (drv/react s :current-user-data))}])]]
-      [:div.user-profile-disclaimer
-        [:span.left "User information is from your Slack account."]
-        [:button.user-profile-refresh.btn-reset.btn-link.left
-          {:class (if @(::show-success s) "success" "")
-           :on-click #(do
-                        (reset! (::loading s) true)
-                        (dis/dispatch! [:refresh-slack-user]))}
-          (if @(::show-success s)
-            "SAVED!"
-            (if @(::loading s)
-              (small-loading)
-              "Refresh"))]]]
-    (footer (responsive/total-layout-width-int (responsive/calc-card-width) (responsive/columns-num)))])
+  (let [user-profile-data (drv/react s :edit-user-profile)
+        current-user-data (:user-data user-profile-data)
+        error (:error user-profile-data)
+        timezones (.names (.-tz js/moment))]
+    [:div.user-profile.fullscreen-page
+      [:div.user-profile-header {} "Your Profile"]
+      [:div.user-profile-internal
+        [:div.user-profile-content.group
+          [:div.user-profile-avatar-box.group
+            [:div.user-profile-avatar
+              (user-avatar-image current-user-data)]
+            [:div.user-profile-avatar-change
+              [:button.mlb-reset.mlb-link.upload-photo
+                {:on-click #()}
+                [:span.user-avatar-upload-cta "Upload Photo"]
+                [:span.user-avatar-upload-description
+                  "A 160x160 transparent Gif or PNG works best"]]]]
+          ; Left column
+          [:div.user-profile-column-left
+            ; First name
+            [:div.user-profile-field-box
+              [:div.user-profile-field-label
+                "FIRST NAME"]
+              [:div.user-profile-field
+                [:input
+                  {:type "text"
+                   :on-change #(change! :first-name (.. % -target -value))
+                   :value (:first-name current-user-data)}]]]
+            ; Current password
+            [:div.user-profile-field-box
+              [:div.user-profile-field-label
+                "CURRENT PASSWORD"]
+              [:div.user-profile-field
+                [:input
+                  {:type "password"
+                   :on-change #(change! :current-password (.. % -target -value))
+                   :value (:current-password current-user-data)}]]]
+            ; Email
+            [:div.user-profile-field-box
+              [:div.user-profile-field-label
+                "EMAIL"]
+              [:div.user-profile-field
+                [:input
+                  {:type "text"
+                   :on-change #(change! :email (.. % -target -value))
+                   :value (:email current-user-data)}]]]]
+          ; Right column
+          [:div.user-profile-column-right
+            ; Last name
+            [:div.user-profile-field-box
+              [:div.user-profile-field-label
+                "LAST NAME"]
+              [:div.user-profile-field
+                [:input
+                  {:type "text"
+                   :on-change #(change! :last-name (.. % -target -value))
+                   :value (:last-name current-user-data)}]]]
+            ; New password
+            [:div.user-profile-field-box
+              [:div.user-profile-field-label
+                "NEW PASSWORD"]
+              [:div.user-profile-field
+                [:input
+                  {:type "password"
+                   :on-change #(change! :password (.. % -target -value))
+                   :value (:password current-user-data)}]]]
+            ; Time zone
+            [:div.user-profile-field-box
+              [:div.user-profile-field-label
+                "TIME ZONE"]
+              [:div.user-profile-field
+                [:select
+                  {:value (:timezone current-user-data)
+                   :on-change #(change! :timezone (.. % -target -value))}
+                  (for [t timezones]
+                    [:option
+                      {:key (str "timezone-" t)}
+                      t])]]]]
+          ; Digest frequency
+          [:div.user-profile-field-box
+            [:div.user-profile-field-label
+              "DIGEST FREQUENCY " [:i.fa.fa-info-circle]]
+            [:div.user-profile-field
+              [:select
+                {:value (:digest-period current-user-data)
+                 :on-change #(change! :digest-period (.. % -target -value))}
+                [:option "DAILY"]
+                [:option "WEEKLY"]
+                [:option "MONTHLY"]]]]]]
+        [:div.user-profile-footer
+          [:button.mlb-reset.mlb-default
+            {:on-click #(dis/dispatch! [:user-profile-save])
+             :disabled (not (:has-changes current-user-data))}
+             (when (:loading current-user-data)
+                (small-loading))
+            "Save"]
+          [:button.mlb-reset.mlb-link-black
+            {:on-click #(dis/dispatch! [:user-profile-reset])
+             :disabled (not (:has-changes current-user-data))}
+            "Cancel"]]]))
