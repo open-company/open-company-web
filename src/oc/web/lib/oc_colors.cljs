be988dadf432c74052aed6104e83cfb70e851bd3
bago2k4 <iacopo@rc0.eu>
2017-06-14 18:08:14 +0200

    Add cr-dark-blue color.

--- src/oc/web/lib/oc_colors.cljs
+++ src/oc/web/lib/oc_colors.cljs
@@ -43,7 +43,8 @@
   :cr-dark-bg-gray "#F3F2EE"
   :cr-text-blue "#4c67fc"
   :cr_orange "#FA6452"
-  :cr-settings-background "#FCFBF7"})
+  :cr-settings-background "#FCFBF7"
+  :cr-dark-blue "#2e405a"})
 
 (defn get-color-by-kw [kw]
   (if (contains? oc-colors kw)
