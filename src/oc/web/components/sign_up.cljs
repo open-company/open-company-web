cdf8613e425755a28dd2c87cea52e8025835ed90
bago2k4 <iacopo@rc0.eu>
2017-06-14 18:08:37 +0200

    Redesign of the home page.

--- src/oc/web/components/sign_up.cljs
+++ src/oc/web/components/sign_up.cljs
@@ -18,64 +18,93 @@
       (login-overlays-handler)
 
       [:div.main.sign-up
+        ; Hope page header
         [:div.cta
-          [:div.container.cta
-            [:div.vertical-center-row
-              [:h1.tagline {} "Transparency Made Simple"]
-              [:div.control-group
-                [:form.form-inline {:id "subscribe"}
-                  [:input {:id "referrer" :type "hidden" :name "refrerrer" :value ""}]
-                  [:div.row
-                    [:div.col-xs-12.center
-                      [:button.submit.domine {:id "submit"
-                                              :name "subscribe"
-                                              :type "submit"
-                                              :onClick #(do
-                                                          (.preventDefault %)
-                                                          (dis/dispatch! [:login-overlay-show :signup-with-slack]))}
-                        "Get Started →"]]]]]]]]
+          [:h1 "Where companies find alignment"]
+          [:div.description
+            "Company updates and stories that keep teams aligned over time."]
+          [:button.get-started-centred-bt.mlb-reset
+            {:on-click #(dis/dispatch! [:login-overlay-show :signup-with-slack])}
+            "Get started for free"]
+          [:div.small-teams
+            "Easy set-up • Free for small teams"]
+          [:img
+            {:src "/img/ML/home_page_screenshot.png"
+             :width 756
+             :height 511}]]
 
-        [:div.sub-tag
-          [:div.container.sub-tag
-            [:div.vertical-center-row
-              [:h2.sub-tagline "Company updates that build trust with employees, investors and customers."]]]]
 
-        [:div.left-to-right
-          [:div.container.info.first
-            [:div.row
-              [:div.col-sm-6.col-xs-12.left
-                [:div
-                  [:h3.info-headline "Create beautiful company updates in less time."]]]
-              [:div.col-sm-6.col-xs-12.right {:style {:padding-bottom "0px"}}
-                [:img.info-image {:id "org-dashboard" :src (str ls/cdn-url "/img/green-labs-mac@4x.png") :alt "Company Dashboard"}]]]]]
+        [:div.illustrations.group
 
-        [:div.right-to-left.web-only
-          [:div.container.info.second
-            [:div.row
-              [:div.col-sm-6.col-xs-12.left.no-bottom
-                [:img.info-image.investor-update {:src (str ls/cdn-url "/img/green-labs-iphone@4x.png") :alt "Investor Update"}]]
-              [:div.col-sm-6.col-xs-12.right
-                [:div
-                  [:h3.info-headline "Keep your team engaged and on the same page. Impress investors and advisors."]]]]]]
+          [:div.illustration.illustration-1.group
+            [:img {:src "/img/ML/home_page_illustration_1.png"}]
+            [:div.description.group
+              [:div.title
+                "Get aligned fast"]
+              [:div.subtitle
+                "Whether you’re adding a quick update about one topic, or writing a monthly update that covers many, getting started is fast and simple."]]]
 
-        [:div.right-to-left.mobile-only
-          [:div.container.info.second
-            [:div.row
-              [:div.col-sm-6.col-xs-12.right
-                [:div
-                  [:h3.info-headline "Keep your team engaged and on the same page. Impress investors and advisors."]]]
-              [:div.col-sm-6.col-xs-12.left.no-bottom
-                [:img.info-image.investor-update {:src (str ls/cdn-url "/img/green-labs-iphone@4x.png") :alt "Investor Update"}]]]]]
+          [:div.illustration.illustration-2.group
+            [:img {:src "/img/ML/home_page_illustration_2.png"}]
+            [:div.description.group
+              [:div.title
+                "See the big picture"]
+              [:div.subtitle
+                "Check out what’s new this past week, or get new employees up to speed in a flash - it’s never been easier to get caught up."]]]
 
-        [:div.left-to-right
-          [:div.container.info.third
-            [:div.row
-              [:div.col-sm-6.col-xs-12.left
-                [:div
-                  [:h3.info-headline "Expand your audience with optional public transparency."]
-                  [:p "Ideal for crowdfunded ventures, Kickstarter campaigns, social enterprises, and others interested in being more open."]]]
-              [:div.col-sm-6.col-xs-12.right
-                [:img.info-image {:id "transparency" :src (str ls/cdn-url "/img/transparency.png") :alt "Transparency toggle"}]]]]]
+          [:div.illustration.illustration-3.group
+            [:img {:src "/img/ML/home_page_illustration_3.png"}]
+            [:div.description.group
+              [:div.title
+                "Spark better conversations"]
+              [:div.subtitle
+                "Share updates that encourage reactions and comments to keep the team in sync - great for distributed teams."]]]
+
+          [:div.illustration.illustration-4.group
+            [:img {:src "/img/ML/home_page_illustration_4.png"}]
+            [:div.description.group
+              [:div.title
+                "Integrate with Slack"]
+              [:div.subtitle
+                "Onboard your Slack team with single sign-on to make team management easy. Sharing to a channel or individual is seamless."]]]
+
+          [:div.illustration.illustration-5.group
+            [:img {:src "/img/ML/home_page_illustration_5.png"}]
+            [:div.description.group
+              [:div.title
+                "Build a bigger audience"]
+              [:div.subtitle
+                "Share updates and stories with anyone outside the company, too, such as recruits, investors and customers."]]]]
+
+        [:div.customers
+          [:div.customers-title
+            [:img {:src "/img/ML/user_avatar_yellow.svg"}]
+            "Our happy clients"]
+          [:div.customers-cards.group
+            [:div.left-arrow
+              [:button.mlb-reset.left-arrow-bt
+                {:disabled true}]]
+            [:div.customers-cards-scroll
+              [:div.customers-card]
+              [:div.customers-card]
+              [:div.customers-card]]
+            [:div.right-arrow
+              [:button.mlb-reset.right-arrow-bt
+                {:disabled true}]]]]
+
+        [:div.try-it
+          [:div.try-it-title
+            "Try it today"]
+          [:div.try-it-subtitle
+            "Easy set-up · Free for small team"]
+          [:div.try-it-combo-field
+            [:input
+              {:type "text"
+               :placeholder "Email address"}]
+            [:button.mlb-reset.try-it-get-started
+              {:on-click #()}
+              "Get Started"]]]
+
       ] ; <!-- .main -->
     ] ;  <!-- #wrap -->
 
