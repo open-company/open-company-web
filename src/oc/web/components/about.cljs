224569265af004a5c9e4e710876b4bf17e14076a
bago2k4 <iacopo@rc0.eu>
2017-06-22 14:28:57 +0200

    Mobile and responsive work for about page.

--- src/oc/web/components/about.cljs
+++ src/oc/web/components/about.cljs
@@ -3,6 +3,7 @@
   (:require [rum.core :as rum]
             [oc.web.urls :as oc-urls]
             [oc.web.router :as router]
+            [oc.web.lib.responsive :as responsive]
             [oc.web.components.ui.site-header :refer (site-header)]
             [oc.web.components.ui.site-footer :refer (site-footer)]
             [oc.web.components.ui.login-overlay :refer (login-overlays-handler)]))
@@ -20,40 +21,52 @@
 
         [:div.divider-line]
 
-        [:div.ovarls-container
+        [:div.ovarls-container.group
 
           [:div.about-subline
             "Transparency is a powerful motivator for teams. It creates a sense of ownership, and builds trust and clarity of purpose."]
-          [:div.paragraph
-            "Serial entrepreneurs ourselves, we’ve learned most of all that transparency is essential for company alignment."]
-          [:div.paragraph
-            "Still, we could never find easy solutions to help us keep growing and distributed teams on the same page."]
+          [:div.paragraphs-container.group
+            (when (responsive/is-mobile-size?)
+              [:div.mobile-only.happy-face.yellow-happy-face])
+            (when (responsive/is-mobile-size?)
+              [:div.mobile-only.happy-face.red-happy-face])
+            [:div.mobile-paragraphs-container.group
+              [:div.paragraph
+                "Serial entrepreneurs ourselves, we’ve learned most of all that transparency is essential for company alignment."]
+              [:div.paragraph
+                "Still, we could never find easy solutions to help us keep growing and distributed teams on the same page."]
+              [:div.paragraph
+                "Now we’ve built the solution we wanted ourselves, based on three principles:"]]
+            (when (responsive/is-mobile-size?)
+              [:div.mobile-only.happy-face.blue-happy-face])
+            (when (responsive/is-mobile-size?)
+              [:div.mobile-only.happy-face.purple-happy-face])
+            (when (responsive/is-mobile-size?)
+              [:div.mobile-only.happy-face.green-happy-face])]]
 
-          [:div.paragraph
-            "Now we’ve built the solution we wanted ourselves, based on three principles:"]]
+        [:div.principles.group
+          [:div.principle.principle-1
+            [:div.principle-oval-bg]
+            [:div.principle-logo]
+            [:div.principle-title "It has to be easy or no one will play."]
+            [:div.principle-description "Alignment might be essential for success, but achieving it has never been easy or fun. We’re changing that. With a simple structure and beautiful writing experience, it can’t be easier. Just say what’s going on, we’ll take care of the rest."]]
 
-        [:div.principle.principle-1
-          [:div.principle-logo]
-          [:div.principle-oval-bg]
-          [:div.principle-title "It has to be easy or no one will play."]
-          [:div.principle-description "Alignment might be essential for success, but achieving it has never been easy or fun. We’re changing that. With a simple structure and beautiful writing experience, it can’t be easier. Just say what’s going on, we’ll take care of the rest."]]
+          [:div.principle.principle-2
+            [:div.principle-oval-bg]
+            [:div.principle-logo]
+            [:div.principle-title "The “big picture” should always be visible. "]
+            [:div.principle-description "No one wants to drill into folders or documents to understand what’s going on, or search through corporate chat archives to find something. It should be easier to get an instant view of what’s happening across the company anytime."]]
 
-        [:div.principle.principle-2
-          [:div.principle-logo]
-          [:div.principle-oval-bg]
-          [:div.principle-title "The “big picture” should always be visible. "]
-          [:div.principle-description "No one wants to drill into folders or documents to understand what’s going on, or search through corporate chat archives to find something. It should be easier to get an instant view of what’s happening across the company anytime."]]
-
-        [:div.principle.principle-3
-          [:div.principle-logo]
-          [:div.principle-oval-bg]
-          [:div.principle-title "Alignment is valuable beyond the team, too."]
-          [:div.principle-description "Once you’ve created the big picture, you should be able to share it with anyone, inside and outside the company. Sharing beautiful updates with recruits, investors and customers is the surest way to keep them engaged and supportive."]]
+          [:div.principle.principle-3
+            [:div.principle-oval-bg]
+            [:div.principle-logo]
+            [:div.principle-title "Alignment is valuable beyond the team, too."]
+            [:div.principle-description "Once you’ve created the big picture, you should be able to share it with anyone, inside and outside the company. Sharing beautiful updates with recruits, investors and customers is the surest way to keep them engaged and supportive."]]]
 
       ] ;<!-- main -->
 
       [:div.about-alignment
-        [:div.quote "“"]
+        [:div.quote]
         [:div.about-alignment-description "Company alignment requires real openness and transparency."]]
 
       [:div.about-team.group
@@ -70,6 +83,14 @@
                   [:div.team-description "Skydiver, future ninja skydiver, guitarist, Swiss design-head and communicator, collector, connector, creator."]
                   [:div.team-media-links
                     [:a.linkedin]
+                    [:a.twitter]]]]
+              [:div.team-card
+                [:div.team-avatar]
+                [:div.team-member
+                  [:div.team-name "Iacopo Carraro"]
+                  [:div.team-description "Skydiver, ninja dev, guitarist, Swiss design-head and communicator, collector, connector, creator."]
+                  [:div.team-media-links
+                    [:a.linkedin]
                     [:a.twitter]]]]]
 
             [:div.column-right.group
@@ -80,16 +101,12 @@
                   [:div.team-description "Skydiver, ninja skydiver spectator, guitarist, Swiss design-head and communicator."]
                   [:div.team-media-links
                     [:a.linkedin]
-                    [:a.twitter]]]]]]
-
-          [:div.team-card
+                    [:a.twitter]]]]
+              [:div.team-card
             [:div.team-avatar]
-            [:div.team-member
-              [:div.team-name "Iacopo Carraro"]
-              [:div.team-description "Skydiver, ninja dev, guitarist, Swiss design-head and communicator, collector, connector, creator."]
-              [:div.team-media-links
-                [:a.linkedin]
-                [:a.twitter]]]]]]
+              [:div.team-member
+                [:div.team-name "You?"]
+                [:div.team-description "We're always looking for talented individuals. Drop us a line if you share our mission."]]]]]]]
 
       [:div.about-footer.group
 
