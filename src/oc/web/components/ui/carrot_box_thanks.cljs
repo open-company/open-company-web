140a9aec95a169a97523e62d9096b76c4a2fc90f
bago2k4 <iacopo@rc0.eu>
2017-06-26 11:57:41 +0200

    Add carrot thanks box component used in the home page.

---
+++ src/oc/web/components/ui/carrot_box_thanks.cljs
@@ -0,0 +1,21 @@
+(ns oc.web.components.ui.carrot-box-thanks
+  (:require [rum.core :as rum]
+            [oc.web.local-settings :as ls]))
+
+(defn get-link []
+  (js/console.log "rand 100000" (rand 100000))
+  (int (rand 100000)))
+
+(rum/defc carrot-box-thanks
+  [email]
+  (let [thanks-message (if-not (empty? email)
+                          [:div.thanks-subheadline "We've sent you a link to " [:span email]]
+                          [:div.thanks-subheadline "we've sent an email with a link"])
+        carrot-link (str "http" (when ls/jwt-cookie-secure "s") "://" ls/web-server "/" (get-link))]
+    [:div.carrot-box-container.group
+      [:img.carrot-box {:src "/img/ML/carrot_box.svg"}]
+      [:div.carrot-box-thanks
+        [:div.thanks-headline "Thanks!"]
+        thanks-message
+        [:div.carrot-early-access-top "Get earlier access when your friends sign up with this link:"]
+        [:a.carrot-early-access-link {:href carrot-link} carrot-link]]]))
