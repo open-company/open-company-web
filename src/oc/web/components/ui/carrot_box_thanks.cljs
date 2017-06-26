1c467f89f79a986797063f51486f35723d387b68
bago2k4 <iacopo@rc0.eu>
2017-06-26 18:45:02 +0200

    Fix message.

--- src/oc/web/components/ui/carrot_box_thanks.cljs
+++ src/oc/web/components/ui/carrot_box_thanks.cljs
@@ -9,7 +9,7 @@
   [& [email]]
   (let [thanks-message (if-not (empty? email)
                           [:div.thanks-subheadline "We've sent you a link to " [:span email] "."]
-                          [:div.thanks-subheadline "we've sent an email with a link."])
+                          [:div.thanks-subheadline "we've sent you an email with a link."])
         carrot-link (str "http" (when ls/jwt-cookie-secure "s") "://" ls/web-server "/" (get-link))]
     [:div.carrot-box-container.group
       [:img.carrot-box {:src "/img/ML/carrot_box.svg"}]
