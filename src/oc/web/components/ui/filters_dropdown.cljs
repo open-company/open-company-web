06884e44290bd3d4928f060923583707d86bd005
bago2k4 <iacopo@rc0.eu>
2017-06-29 18:33:43 +0200

    Change the url instead of changing the app-state to change sort/filter status.

--- src/oc/web/components/ui/filters_dropdown.cljs
+++ src/oc/web/components/ui/filters_dropdown.cljs
@@ -2,6 +2,7 @@
   (:require [rum.core :as rum]
             [cuerdas.core :as s]
             [org.martinklepsch.derivatives :as drv]
+            [oc.web.urls :as oc-urls]
             [oc.web.router :as router]
             [oc.web.dispatcher :as dis]
             [oc.web.lib.utils :as utils]
@@ -22,7 +23,12 @@
     [:div.filters-dropdown-name
       (when (string? board-filters)
         [:button.mlb-reset.board-remove-filter
-          {:on-click #(dis/dispatch! [:board-filters-set (keyword (cook/get-cookie (router/last-board-filter-cookie (router/current-org-slug) (router/current-board-slug))))])}])
+          {:on-click #(let [org-slug (router/current-org-slug)
+                            board-slug (router/current-board-slug)
+                            last-filter (keyword (cook/get-cookie (router/last-board-filter-cookie org-slug board-slug)))]
+                        (if (= last-filter :by-topic)
+                          (router/nav! (oc-urls/board-sort-by-topic))
+                          (router/nav! (oc-urls/board-sort-latest))))}])
       (board-filters-label board-filters)
       [:div.filters-dropdown-container
         [:button.mlb-reset.filters-dropdown-caret.dropdown-toggle
@@ -38,17 +44,17 @@
           [:div.filters-dropdown-list-content
             [:ul
               [:li.category
-                {:on-click #(dis/dispatch! [:board-filters-set :latest])
+                {:on-click #(router/nav! (oc-urls/board-sort-latest))
                  :class (if (= board-filters :latest) "select" "")}
                 "Latest Updates"]
               [:li.category
-                {:on-click #(dis/dispatch! [:board-filters-set :by-topic])
+                {:on-click #(router/nav! (oc-urls/board-sort-by-topic))
                  :class (if (= board-filters :by-topic) "select" "")}
                 "Latest by Topic"]
               [:li.divider]
               (for [t (:topics board-data)]
                 [:li
-                  {:on-click #(dis/dispatch! [:board-filters-set t])
+                  {:on-click #(router/nav! (oc-urls/board-filter-by-topic t))
                    :class (if (= board-filters t) "select" "")
                    :key (str "board-filters-topic-" t)}
                   (s/capital t)])]]]]]))
