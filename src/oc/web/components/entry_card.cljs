797de5bcb1a9d415aa6f6897de4b8c6cbc04fb12
bago2k4 <iacopo@rc0.eu>
2017-06-29 14:42:28 +0200

    New entry-card component show render a card for the new dashboard.

---
+++ src/oc/web/components/entry_card.cljs
@@ -0,0 +1,34 @@
+(ns oc.web.components.entry-card
+  (:require [rum.core :as rum]
+            [clojure.string :as s]
+            [oc.web.lib.utils :as utils]
+            [oc.web.components.ui.user-avatar :refer (user-avatar-image)]
+            [oc.web.components.ui.topic-interactions-summary :refer (topic-interactions-summary)]))
+
+(defn cut-body [entry-body]
+  (.truncate js/$ entry-body (clj->js {:length utils/topic-body-limit :words true})))
+
+(rum/defcs entry-card < rum/static
+  [s entry-data]
+  [:div.entry-card
+    ; Card header
+    [:div.entry-card-head.group
+      ; Card author
+      [:div.entry-card-head-author
+        (user-avatar-image)
+        [:div.name (:name (first (:author entry-data)))]
+        [:div.time-since
+          [:time
+            {:date-time (:updated-at entry-data)}
+            (utils/time-since (:updated-at entry-data))]]]
+      ; Card labels
+      [:div.entry-card-head-right
+        (when true
+          [:div.new "NEW"])]]
+    [:div.entry-card-content.group
+      [:div.entry-card-headline
+        (:headline entry-data)]
+      [:div.entry-card-body
+        {:dangerouslySetInnerHTML #js {:__html (cut-body (:body entry-data))}}]]
+    [:div.entry-card-footer.group
+      (topic-interactions-summary entry-data)]])
