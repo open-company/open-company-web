d26f0239dcadcafcda5e918dbc7503ce8f1c296b
bago2k4 <iacopo@rc0.eu>
2017-06-30 16:29:12 +0200

    Sort updates in all layouts. In by-topic layout sort all updates by topic group first and by update inside the topic group. Add the view all updates button to switch to the filtered view.

--- src/oc/web/components/entries_layout.cljs
+++ src/oc/web/components/entries_layout.cljs
@@ -1,6 +1,8 @@
 (ns oc.web.components.entries-layout
   (:require [rum.core :as rum]
             [cuerdas.core :as s]
+            [oc.web.router :as router]
+            [oc.web.urls :as oc-urls]
             [oc.web.components.entry-card :refer (entry-card entry-card-empty)]))
 
 (rum/defc entries-layout
@@ -9,26 +11,40 @@
     (cond
       ;; :by-topic
       (= layout-type :by-topic)
-      (let [grouped-entries (group-by :topic-slug entries)]
-        (for [[topic entries-group] grouped-entries]
+      (let [grouped-entries (apply merge (map (fn [[k v]] (hash-map k (vec (reverse (sort-by :updated-at v))))) (group-by :topic-slug entries)))
+            sorted-topics (vec (reverse (sort #(compare (:updated-at (first (get grouped-entries %1))) (:updated-at (first (get grouped-entries %2)))) (keys grouped-entries))))]
+        (for [topic sorted-topics
+              :let [entries-group (get grouped-entries topic)
+                    topic-name (:topic-name (first entries-group))
+                    topic-slug (:topic-slug (first entries-group))]]
           [:div.entry-cards-container.by-topic.group
-            {:key (str "entries-topic-group-" topic)}
-            [:div.by-topic-title (s/capital (:topic-slug (first entries-group)))]
-            (for [entry entries-group]
-              (rum/with-key (entry-card entry) (str "entry-by-topic-" topic "-" (:created-at entry))))
-            (when (= (count entries-group) 1)
+            {:key (str "entries-topic-group-" (or topic "uncategorized"))}
+            [:div.by-topic-header.group
+              [:div.by-topic-header-title
+                (or topic-name
+                    (s/capital topic-slug)
+                    "Uncategorized")]
+              (when (> (count entries-group) 14)
+                [:button.view-all-updates.mlb-reset
+                  {:on-click #(router/nav! (oc-urls/board-filter-by-topic topic-slug))}
+                  "VIEW " (count entries-group) " UPDATES"])]
+            (for [entry (take 4 entries-group)]
+              (rum/with-key (entry-card entry false) (str "entry-by-topic-" topic "-" (:created-at entry))))
+            (when (or (= (count entries-group) 1)
+                      (= (count entries-group) 3))
               (entry-card-empty))]))
       ;; by specific topic
       (string? layout-type)
-      (let [filtered-entries (vec (filter #(= (:topic-slug %) layout-type) entries))]
+      (let [filtered-entries (vec (filter #(= (:topic-slug %) layout-type) entries))
+            sorted-entries (vec (reverse (sort-by :updated-at filtered-entries)))]
         [:div.entry-cards-container.by-specific-topic.group
-          (for [entry filtered-entries]
-            (rum/with-key (entry-card entry) (str "entry-topic-" (:topic-slug entry) "-" (:created-at entry))))
-          (when (= (count filtered-entries) 1)
+          (for [entry sorted-entries]
+            (rum/with-key (entry-card entry false) (str "entry-topic-" (:topic-slug entry) "-" (:created-at entry))))
+          (when (= (count sorted-entries) 1)
               (entry-card-empty))])
       ;; :latest layout
       :else
-      (let [sorted-entries (vec (sort-by :updated-at entries))]
+      (let [sorted-entries (vec (reverse (sort-by :updated-at entries)))]
         [:div.entry-cards-container.group
           (for [entry sorted-entries]
-            (rum/with-key (entry-card entry) (str "entry-latest-" (:created-at entry))))]))])
+            (rum/with-key (entry-card entry true) (str "entry-latest-" (:created-at entry))))]))])
