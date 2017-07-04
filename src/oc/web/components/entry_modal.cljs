535fde77cd170c9f6e4db229aeeab98ca3bae18c
bago2k4 <iacopo@rc0.eu>
2017-07-04 16:48:19 +0200

    Remove fake data generator.

--- src/oc/web/components/entry_modal.cljs
+++ src/oc/web/components/entry_modal.cljs
@@ -10,8 +10,6 @@
             [oc.web.components.reactions :refer (reactions)]
             [oc.web.components.comments :refer (comments)]))
 
-(def lorem-gen "<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a,</p>")
-
 (defn close-clicked [_e]
   (if (= (keyword (cook/get-cookie (router/last-board-filter-cookie (router/current-org-slug) (router/current-board-slug)))) :by-topic)
     (router/nav! (oc-urls/board-sort-by-topic))
