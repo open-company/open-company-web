1734182760252bfd1d8939665a09c532a4d0c034
bago2k4 <iacopo@rc0.eu>
2017-07-10 11:54:06 +0200

    Call the right js function to send the email address to the lambda api endpoint. Handle the success case showing the right thank you box.

--- src/oc/web/components/ui/try_it_form.cljs
+++ src/oc/web/components/ui/try_it_form.cljs
@@ -7,41 +7,40 @@
             [oc.web.local-settings :as ls]
             [oc.web.lib.utils :as utils]))
 
-(rum/defcs try-it-form < (rum/local (int (rand 100)) ::input-class)
-                         (rum/local "" ::email-value)
-  [s try-it-input-class & [get-started-cb]]
-  (let [try-it-final-class (or try-it-input-class (str "try-it-class-" @(::input-class s)))]
-    [:form.validate
-      {:action ls/mailchimp-api-endpoint
-       :method "post"
-       :id "mailchimp-api-subscribe-form"
-       :no-validate true}
-      [:div.try-it-combo-field
-        [:div.mc-field-group
-          [:input.mail.required
-            {:type "text"
-             :value @(::email-value s)
-             :class try-it-final-class
-             :id "mce-EMAIL"
-             :on-change #(reset! (::email-value s) (.. % -target -value))
-             :name "email"
-             :placeholder "Email address"}]]
-        [:button.mlb-reset.try-it-get-started
-          {:type "submit"
-           :id "mc-embedded-subscribe"
-           :disabled (not (utils/valid-email? @(::email-value s)))
-           :on-click (fn [e]
-                       (let [email-value @(::email-value s)]
-                         (when (utils/valid-email? email-value)
-                           (js/mailchipApiSubmit e @(::email-value s) #(js/console.log %) #(js/console.log %))
-                           (when (fn? get-started-cb)
-                             (get-started-cb email-value)))))}
-          "Get Early Access"]]]))
+(defn submit-try-it-form [e try-it-form-id form-submit-success-cb]
+  (.preventDefault e)
+  (js/console.log "submit-try-it-form!" (str "#" try-it-form-id) (js/$ (str "#" try-it-form-id)))
+  (js/mailchipApiSubmit e (js/$ (str "#" try-it-form-id)) #(when (fn? form-submit-success-cb) (form-submit-success-cb)) nil))
+
+(rum/defcs try-it-form < (rum/local "" ::email-value)
+  [s try-it-form-id form-submit-success-cb]
+  [:form.validate
+    {:action ls/mailchimp-api-endpoint
+     :method "post"
+     :class "mailchimp-api-subscribe-form"
+     :id try-it-form-id
+     :no-validate true}
+    [:div.try-it-combo-field
+      [:div.mc-field-group
+        [:input.mail.required
+          {:type "email"
+           :value @(::email-value s)
+           :class (str try-it-form-id "-input")
+           :id "mce-EMAIL"
+           :on-change #(reset! (::email-value s) (.. % -target -value))
+           :name "email"
+           :placeholder "Email address"}]]
+      [:button.mlb-reset.try-it-get-started
+        {:type "submit"
+         :id "mc-embedded-subscribe"
+         :disabled (not (utils/valid-email? @(::email-value s)))
+         :on-click #(submit-try-it-form % try-it-form-id form-submit-success-cb)}
+        "Get Early Access"]]])
 
 (defn get-started-click
   []
-  (if (sel1 [:input.try-it-input-central])
-    (.focus (sel1 [:input.try-it-input-central]))
+  (if (sel1 [:input.try-it-form-central-input])
+    (.focus (sel1 [:input.try-it-form-central-input]))
     (router/nav! oc-urls/home-try-it-focus)))
 
 (rum/defcs get-started-button < rum/static
