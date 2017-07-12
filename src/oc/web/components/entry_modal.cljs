1f04245e84a3b7f737c9607463c1bfbb8f2aec9f
bago2k4 <iacopo@rc0.eu>
2017-07-12 14:00:23 +0200

    Do not size the modal the full window height if not necessary. Fade in/out the modal view when necessary.

--- src/oc/web/components/entry_modal.cljs
+++ src/oc/web/components/entry_modal.cljs
@@ -1,6 +1,7 @@
 (ns oc.web.components.entry-modal
   (:require [rum.core :as rum]
             [dommy.core :as dommy :refer-macros (sel1)]
+            [org.martinklepsch.derivatives :as drv]
             [cuerdas.core :as s]
             [oc.web.router :as router]
             [oc.web.urls :as oc-urls]
@@ -10,27 +11,47 @@
             [oc.web.components.reactions :refer (reactions)]
             [oc.web.components.comments :refer (comments)]))
 
-(defn close-clicked [_e]
+(defn dismiss-modal []
   (if (= (keyword (cook/get-cookie (router/last-board-filter-cookie (router/current-org-slug) (router/current-board-slug)))) :by-topic)
     (router/nav! (oc-urls/board-sort-by-topic))
     (router/nav! (oc-urls/board))))
 
-(rum/defcs entry-modal < {:did-mount (fn [s]
+(defn close-clicked [s]
+  (reset! (::dismiss s) true)
+  (utils/after 180 dismiss-modal))
+
+(rum/defcs entry-modal < (rum/local false ::first-render-done)
+                         (rum/local false ::dismiss)
+                         (rum/local false ::animate)
+                         rum/reactive
+                         (drv/drv :entry-modal-fade-in)
+                         {:before-render (fn [s]
+                                           (when (and (not @(::animate s))
+                                                    (= @(drv/get-ref s :entry-modal-fade-in) (:uuid (first (:rum/args s)))))
+                                             (reset! (::animate s) true))
+                                           s)
+                          :did-mount (fn [s]
                                        ;; Add no-scroll to the body to avoid scrolling while showing this modal
                                        (dommy/add-class! (sel1 [:body]) :no-scroll)
                                        ;; Scroll to the bottom of the comments box
                                        (let [el (sel1 [:div.entry-right-column-content])]
                                           (set! (.-scrollTop el) (.-scrollHeight el)))
                                        s)
+                          :after-render (fn [s]
+                                          (when (not @(::first-render-done s))
+                                            (reset! (::first-render-done s) true))
+                                          s)
                           :will-unmount (fn [s]
                                           ;; Remove no-scroll class from the body tag
                                           (dommy/remove-class! (sel1 [:body]) :no-scroll)
                                           s)}
   [s entry-data]
   [:div.entry-modal-container
+    {:class (utils/class-set {:will-appear (or @(::dismiss s) (and @(::animate s) (not @(::first-render-done s))))
+                              :appear (and (not @(::dismiss s)) @(::first-render-done s))})}
     [:div.entry-modal.group
       [:button.close-entry-modal.mlb-reset
-        {:on-click close-clicked}]
+        {:on-click #(close-clicked s)}]
       [:div.entry-modal-inner.group
         [:div.entry-left-column
           [:div.entry-left-column-content
@@ -59,6 +80,6 @@
             [:div.entry-card-footer
               (reactions (:topic-slug entry-data) (:uuid entry-data) entry-data)]]]
         [:div.entry-right-column
-          {:style #js {:min-height (str (.height (js/$ ".entry-left-column")) "px")}}
+          {:style #js {:minHeight (str (.height (js/$ ".entry-left-column")) "px")}}
           [:div.entry-right-column-content
             (comments (:uuid entry-data))]]]]])
