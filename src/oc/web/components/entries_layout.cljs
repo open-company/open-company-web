bc0526a9e9b2f286ae3835a25ae54f9c718ac73d
bago2k4 <iacopo@rc0.eu>
2017-07-21 12:17:22 +0200

    Render the entries in rows to make sure they have the same height on the same row. Using display flex.

--- src/oc/web/components/entries_layout.cljs
+++ src/oc/web/components/entries_layout.cljs
@@ -19,20 +19,35 @@
                     topic-slug (:topic-slug (first entries-group))]]
           [:div.entry-cards-container.by-topic.group
             {:key (str "entries-topic-group-" (or topic "uncategorized"))}
+            ; Title of the topic group
             [:div.by-topic-header.group
               [:div.by-topic-header-title
                 (or topic-name
                     (s/capital topic-slug)
                     "Uncategorized")]
+              ; If there are more than 4 add the button to show all of them
               (when (> (count entries-group) 4)
                 [:button.view-all-updates.mlb-reset
                   {:on-click #(router/nav! (oc-urls/board-filter-by-topic topic-slug))}
                   "VIEW " (count entries-group) " UPDATES"])]
-            (for [entry (take 4 entries-group)]
-              (rum/with-key (entry-card entry false) (str "entry-by-topic-" topic "-" (:uuid entry))))
-            (when (and (not (empty? topic-slug))
-                       (= (count entries-group) 1))
-              (entry-card-empty (:read-only board-data)))]))
+            ;; First row:
+            [:div.entries-cards-container-row.group
+              ; Render the first 2 entries
+              (for [entry (take 2 entries-group)]
+                (rum/with-key (entry-card entry false) (str "entry-by-topic-" topic "-" (:uuid entry))))
+              ; If there is only 1 add the empty placeholder
+              (when (and (not (empty? topic-slug))
+                         (= (count entries-group) 1))
+                (entry-card-empty (:read-only board-data)))]
+            ; If there are more than 2 entries, render the second row
+            (when (> (count entries-group) 2)
+              [:div.entries-cards-container-row.group
+                ; Render the second 2 entries
+                (for [entry (subvec entries-group 2 (min 4 (count entries-group)))]
+                  (rum/with-key (entry-card entry false) (str "entry-by-topic-" topic "-" (:uuid entry))))
+                ; If the total entries are 3 add a placeholder to avoid taking the full width
+                (when (= (count entries-group) 3)
+                  [:div.entry-card.entry-card-placeholder])])]))
       ;; by specific topic
       (string? layout-type)
       (let [filtered-entries (if (= layout-type "uncategorized")
@@ -40,13 +55,42 @@
                                 (vec (filter #(= (:topic-slug %) layout-type) (:entries board-data))))
             sorted-entries (vec (reverse (sort-by :updated-at filtered-entries)))]
         [:div.entry-cards-container.by-specific-topic.group
-          (for [entry sorted-entries]
-            (rum/with-key (entry-card entry false) (str "entry-topic-" (:topic-slug entry) "-" (:uuid entry))))
-          (when (= (count sorted-entries) 1)
-              (entry-card-empty (:read-only board-data)))])
+          ; Calc the number of pairs
+          (let [top-index (js/Math.ceil (/ (count sorted-entries) 2))]
+            ; For each pari
+            (for [idx (range top-index)
+                  ; Calc the entries in the row
+                  :let [start (* idx 2)
+                        end (min (+ start 2) (count sorted-entries))
+                        entries (subvec sorted-entries start end)]]
+              [:div.entries-cards-container-row.group
+                {:key (str "entries-row-" idx)}
+                ; Render the entries in the row
+                (for [entry sorted-entries]
+                  (rum/with-key (entry-card entry false) (str "entry-topic-" (:topic-slug entry) "-" (:uuid entry))))
+                ; If there is only one entry add the empty card placeholder
+                (if (= (count sorted-entries) 1)
+                  (entry-card-empty (:read-only board-data))
+                  ; If there is only one entry in this row, but it's not the first add the placheolder
+                  (when (= (count entries) 1)
+                    [:div.entry-card.entry-card-placeholder]))]))])
       ;; :latest layout
       :else
       (let [sorted-entries (vec (reverse (sort-by :updated-at (:entries board-data))))]
         [:div.entry-cards-container.group
-          (for [entry sorted-entries]
-            (rum/with-key (entry-card entry true) (str "entry-latest-" (:uuid entry))))]))])
+          ; Get the max number of pairs
+          (let [top-index (js/Math.ceil (/ (count sorted-entries) 2))]
+            ; For each pair
+            (for [idx (range top-index)
+                  ; calc the entries that needs to render in this row
+                  :let [start (* idx 2)
+                        end (min (+ start 2) (count sorted-entries))
+                        entries (subvec sorted-entries start end)]]
+              ; Renteder the entries in thisnrow
+              [:div.entries-cards-container-row.group
+                {:key (str "entries-row-" idx)}
+                (for [entry entries]
+                  (rum/with-key (entry-card entry true) (str "entry-latest-" (:uuid entry))))
+                ; If the row contains less than 2, add a placeholder div to avoid having the first cover the full width
+                (when (= (count entries) 1)
+                  [:div.entry-card.entry-card-placeholder])]))]))])
