d2ff8f5daa5006a746c8cbf9d1aa9012fc34dbe0
bago2k4 <iacopo@rc0.eu>
2017-07-21 14:41:22 +0200

    Remove not used code to randomize the interaciton summary. Use only the first 4 authors of the authors array.

--- src/oc/web/components/ui/interactions_summary.cljs
+++ src/oc/web/components/ui/interactions_summary.cljs
@@ -9,16 +9,10 @@
   (let [max-reaction (first (vec (reverse (sort-by :count (reverse reactions)))))]
     max-reaction))
 
-(def comments-authors-data [
-  {:avatar-url "" :first-name "Stuart" :last-name "Levinson" :user-id "1234-1234-1234"}
-  {:avatar-url "" :first-name "Sean" :last-name "Johnson" :user-id "4321-4321-4321"}
-  {:avatar-url "" :first-name "Iacopo" :last-name "Carraro" :user-id (jwt/get-key :user-id)}])
-
 (rum/defcs interactions-summary < rum/static
   [s entry-data]
-  (let [comments-link (or (utils/link-for (:links entry-data) "comments") {:count (int (rand 10))})
+  (let [comments-link (utils/link-for (:links entry-data) "comments")
         comments-authors (vec (sort-by :created-at (:authors comments-link)))
-        comments-count (min (:count comments-link) 4)
         max-reaction (get-max-count-reaction (:reactions entry-data))]
     [:div.interactions-summary
       ;; Reactions
@@ -35,7 +29,7 @@
           ; Comments authors heads
           [:div.is-comments-authors.group
             {:style {:width (str (if (pos? (count comments-authors)) (+ 9 (* 15 (count comments-authors))) 0) "px")}}
-            (for [user-data comments-authors]
+            (for [user-data (take 4 comments-authors)]
               [:div.is-comments-author
                 {:key (str "entry-comment-author-" (:uuid entry-data) "-" (:created-at user-data))}
                 (user-avatar-image user-data true)])]
