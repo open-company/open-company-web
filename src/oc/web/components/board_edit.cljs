98769f27f3358eaa37a0a13d33cdc3d6cc5a5fd5
bago2k4 <iacopo@rc0.eu>
2017-08-03 17:59:23 +0200

    Add slack channels list load and show when the channel element is focused. Filter when the user type in the field.

--- src/oc/web/components/board_edit.cljs
+++ src/oc/web/components/board_edit.cljs
@@ -2,10 +2,14 @@
   (:require [rum.core :as rum]
             [org.martinklepsch.derivatives :as drv]
             [dommy.core :as dommy :refer-macros (sel1)]
+            [cuerdas.core :as string]
             [oc.web.router :as router]
             [oc.web.dispatcher :as dis]
             [oc.web.lib.utils :as utils]
-            [oc.web.components.ui.user-avatar :refer (user-avatar-image)]))
+            [oc.web.components.ui.user-avatar :refer (user-avatar-image)]
+            [oc.web.components.ui.carrot-checkbox :refer (carrot-checkbox)]
+            [goog.events :as events]
+            [goog.events.EventType :as EventType]))
 
 (defn dismiss-modal []
   (dis/dispatch! [:board-edit/dismiss]))
@@ -14,31 +18,86 @@
   (reset! (::dismiss s) true)
   (utils/after 180 #(dismiss-modal)))
 
+(defn filter-team-channels [channels s]
+  (let [look-for (if (string/starts-with? s "#")
+                   (string/strip-prefix s "#")
+                   s)]
+    (vec (filter #(string/includes? (:name %) look-for) channels))))
+
 (rum/defcs board-edit < rum/reactive
                         (drv/drv :board-editing)
                         (drv/drv :current-user-data)
+                        (drv/drv :org-data)
+                        (drv/drv :team-data)
+                        (drv/drv :team-channels)
                         (rum/local false ::first-render-done)
                         (rum/local false ::dismiss)
-                        {:did-mount (fn [s]
+                        (rum/local false ::team-channels-requested)
+                        (rum/local "" ::slack-channel)
+                        (rum/local [] ::channels)
+                        (rum/local false ::show-channels-dropdown)
+                        (rum/local nil ::window-click)
+                        (rum/local true ::slack-enabled)
+                        {:will-mount (fn [s]
+                                      (dis/dispatch! [:teams-get])
+                                      s)
+                         :did-remount (fn [s]
+                                        (when (and (not @(drv/get-ref s :team-channels))
+                                                   (not @(::team-channels-requested s)))
+                                          (when-let [team-data @(drv/get-ref s :team-data)]
+                                            (reset! (::team-channels-requested s) true)
+                                            (dis/dispatch! [:channels-enumerate (:team-id team-data)])))
+                                        s)
+                         :did-mount (fn [s]
                                       ;; Add no-scroll to the body to avoid scrolling while showing this modal
                                       (dommy/add-class! (sel1 [:body]) :no-scroll)
+                                      (when (and (not @(drv/get-ref s :team-channels))
+                                                 (not @(::team-channels-requested s)))
+                                          (when-let [team-data @(drv/get-ref s :team-data)]
+                                            (reset! (::team-channels-requested s) true)
+                                            (dis/dispatch! [:channels-enumerate (:team-id team-data)])))
+                                      (reset! (::window-click s)
+                                        (events/listen js/window EventType/CLICK
+                                          #(when (and @(::show-channels-dropdown s)
+                                                      (not (utils/event-inside? % (sel1 [:div.board-edit-slack-channels-dropdown])))
+                                                      (not (utils/event-inside? % (sel1 [:input.board-edit-slack-channel]))))
+                                             (reset! (::show-channels-dropdown s) false))))
                                       s)
                          :after-render (fn [s]
                                          (when (not @(::first-render-done s))
                                            (reset! (::first-render-done s) true))
                                          s)
+                         :before-render (fn [s]
+                                          (let [chs (:channels (first @(drv/get-ref s :team-channels)))]
+                                            (if (empty? @(::slack-channel s))
+                                              (assoc s ::channels (atom chs))
+                                              (assoc s ::channels (atom (filter-team-channels chs @(::slack-channel s)))))))
                          :will-unmount (fn [s]
                                          ;; Remove no-scroll class from the body tag
                                          (dommy/remove-class! (sel1 [:body]) :no-scroll)
+                                         (events/unlistenByKey @(::window-click s))
                                          s)}
   [s]
   (let [current-user-data (drv/react s :current-user-data)
         board-editing (drv/react s :board-editing)
-        new-board? (not (contains? board-editing :links))]
+        new-board? (not (contains? board-editing :links))
+        show-slack-channels? (not (empty? (:slug board-editing)))]
     [:div.board-edit-container
       {:class (utils/class-set {:will-appear (or @(::dismiss s) (not @(::first-render-done s)))
                                 :appear (and (not @(::dismiss s)) @(::first-render-done s))})}
+      (when @(::show-channels-dropdown s)
+        [:div.board-edit-slack-channels-dropdown
+          (for [c @(::channels s)]
+            [:div.channel
+              {:value (:id c)
+               :on-click #(do
+                            (dis/dispatch! [:input [:board-editing :channel] c])
+                            (reset! (::slack-channel s) (str "#" (:name c)))
+                            (reset! (::show-channels-dropdown s) false))}
+              [:span.ch "#"]
+              (:name c)])])
       [:div.board-edit
+        {:class (when show-slack-channels? "show-slack-channels")}
         [:div.board-edit-header.group
           (user-avatar-image current-user-data)
           (if new-board?
@@ -58,21 +117,36 @@
                           (dis/dispatch! [:input [:board-editing :name] (.. % -target -value)])
                           (dis/dispatch! [:input [:board-editing :board-name-error] nil]))
              :placeholder "Product, Development, Finance, Operations, etc."}]
-          [:div.board-edit-label.board-edit-access-label "BOARD PERMISSIONS" [:span.more-info]]
+          [:div.board-edit-label.board-edit-access-label "BOARD PERMISSIONS"]
           [:div.board-edit-access-field.group
             [:div.board-edit-access-bt.board-edit-access-team-bt
               {:class (when (= (:access board-editing) "team") "selected")
                :on-click #(dis/dispatch! [:input [:board-editing :access] "team"])}
-              [:span.board-edit-access-title "Team"]]
+              [:span.board-edit-access-title "Team" [:span.more-info]]]
             [:div.board-edit-access-bt.board-edit-access-private-bt
               {:class (when (= (:access board-editing) "private") "selected")
                :on-click #(dis/dispatch! [:input [:board-editing :access] "private"])}
-              [:span.board-edit-access-title "Private"]]
+              [:span.board-edit-access-title "Private" [:span.more-info]]]
             [:div.board-edit-access-bt.board-edit-access-public-bt
               {:class (when (= (:access board-editing) "public") "selected")
                :on-click #(dis/dispatch! [:input [:board-editing :access] "public"])}
-              [:span.board-edit-access-title "Public"]]]]
+              [:span.board-edit-access-title "Public" [:span.more-info]]]]]
         [:div.board-edit-divider]
+        (when show-slack-channels?
+          [:div.board-edit-slack-channels-container
+            [:div.board-edit-slack-channels-label.group
+              [:div.title
+                "Connect comments to Slack"
+                [:span.more-info]]
+              (carrot-checkbox {:selected @(::slack-enabled s)
+                                :did-change-cb #(reset! (::slack-enabled s) %)})]
+            [:div.board-edit-slack-channels-field
+              [:input.board-edit-slack-channel
+                {:value @(::slack-channel s)
+                 :on-focus (fn [] (utils/after 100 #(reset! (::show-channels-dropdown s) true)))
+                 :on-change #(reset! (::slack-channel s) (.. % -target -value))
+                 :disabled (not @(::slack-enabled s))
+                 :placeholder "Select Channel..."}]]])
         [:div.board-edit-footer
           [:div.board-edit-footer-left
             (when (and (not (empty? (:slug board-editing)))
