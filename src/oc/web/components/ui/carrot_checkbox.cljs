2c581b9e0f52da3f270c6b88a8c8033832a2ace4
bago2k4 <iacopo@rc0.eu>
2017-08-03 17:58:34 +0200

    Add carrot-checkbox component to simulate a simple iOS on/off input.

---
+++ src/oc/web/components/ui/carrot_checkbox.cljs
@@ -0,0 +1,14 @@
+(ns oc.web.components.ui.carrot-checkbox
+  (:require [rum.core :as rum]
+            [oc.web.lib.utils :as utils]))
+
+(rum/defc carrot-checkbox < rum/static
+  [{:keys [selected disabled did-change-cb]}]
+  [:div.carrot-checkbox
+    {:on-click #(did-change-cb (not selected))
+     :class (utils/class-set {:selected selected
+                              :disabeld disabled})}
+    [:span.dot]
+    (if selected
+      "ON"
+      "OFF")])
