39049351a7bf5d334824869dfd46db9dda943741
bago2k4 <iacopo@rc0.eu>
2017-08-07 14:34:51 +0200

    Split interactions-summary into 3 components to use reactions-summary and comments-summary separately.

--- src/oc/web/components/ui/interactions_summary.cljs
+++ src/oc/web/components/ui/interactions_summary.cljs
@@ -9,32 +9,40 @@
   (let [max-reaction (first (vec (reverse (sort-by :count (reverse reactions)))))]
     max-reaction))
 
+(rum/defc reactions-summary < rum/static
+  [entry-data]
+  (let [max-reaction (get-max-count-reaction (:reactions entry-data))]
+    (when-not (zero? (:count max-reaction))
+      [:div.is-reactions.group
+        [:div.is-reactions-reaction
+          (:reaction max-reaction)]
+        ; Reactions count
+        [:div.is-reactions-summary
+          {:class (str "reaction-" (:uuid entry-data) "-" (:reaction max-reaction))}
+          (:count max-reaction)]])))
+
+(rum/defc comments-summary < rum/static
+  [entry-data]
+  (let [comments-link (utils/link-for (:links entry-data) "comments")
+        comments-authors (vec (sort-by :created-at (:authors comments-link)))]
+    (when-not (zero? (:count comments-link))
+      [:div.is-comments
+        ; Comments authors heads
+        [:div.is-comments-authors.group
+          {:style {:width (str (if (pos? (count comments-authors)) (+ 9 (* 15 (count comments-authors))) 0) "px")}}
+          (for [user-data (take 4 comments-authors)]
+            [:div.is-comments-author
+              {:key (str "entry-comment-author-" (:uuid entry-data) "-" (:created-at user-data))}
+              (user-avatar-image user-data true)])]
+        ; Comments count
+        [:div.is-comments-summary
+          {:class (str "comments-count-" (:uuid entry-data))}
+          (str (:count comments-link) " comment" (when (> (:count comments-link) 1) "s"))]])))
+
 (rum/defcs interactions-summary < rum/static
   [s entry-data]
-  (let [comments-link (utils/link-for (:links entry-data) "comments")
-        comments-authors (vec (sort-by :created-at (:authors comments-link)))
-        max-reaction (get-max-count-reaction (:reactions entry-data))]
-    [:div.interactions-summary.group
-      ;; Reactions
-      (when-not (zero? (:count max-reaction))
-        [:div.is-reactions.group
-          [:div.is-reactions-reaction
-            (:reaction max-reaction)]
-          ; Reactions count
-          [:div.is-reactions-summary
-            {:class (str "reaction-" (:uuid entry-data) "-" (:reaction max-reaction))}
-            (:count max-reaction)]])
-      ; Comments
-      (when-not (zero? (:count comments-link))
-        [:div.is-comments
-          ; Comments authors heads
-          [:div.is-comments-authors.group
-            {:style {:width (str (if (pos? (count comments-authors)) (+ 9 (* 15 (count comments-authors))) 0) "px")}}
-            (for [user-data (take 4 comments-authors)]
-              [:div.is-comments-author
-                {:key (str "entry-comment-author-" (:uuid entry-data) "-" (:created-at user-data))}
-                (user-avatar-image user-data true)])]
-          ; Comments count
-          [:div.is-comments-summary
-            {:class (str "comments-count-" (:uuid entry-data))}
-            (str (:count comments-link) " comment" (when (> (:count comments-link) 1) "s"))]])]))
+  [:div.interactions-summary.group
+    ;; Reactions
+    (reactions-summary entry-data)
+    ; Comments
+    (comments-summary entry-data)])
