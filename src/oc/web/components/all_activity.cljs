05e10fa6f67d973e691fa6c38f2b104094c567a8
bago2k4 <iacopo@rc0.eu>
2017-08-07 13:34:52 +0200

    All activity component.

---
+++ src/oc/web/components/all_activity.cljs
@@ -0,0 +1,16 @@
+(ns oc.web.components.all-activity
+  (:require [rum.core :as rum]
+            [org.martinklepsch.derivatives :as drv]
+            [oc.web.components.entry-card :refer (entry-card)]))
+
+(rum/defcs all-activity < rum/static
+                          rum/reactive
+                          (drv/drv :all-activity)
+  [s]
+  (let [all-activity-data (drv/react s :all-activity)]
+    (js/console.log "all-activity/render data" all-activity-data)
+    [:div.all-activity.group
+      [:div.all-activity-cards
+        (for [e (:entries all-activity-data)]
+          (rum/with-key (entry-card e (not (empty? (:headline e))) (not (empty? (:body e))) true) (str "all-activity-entry-" (:uuid e))))]
+      [:div.all-activity-nav]]))
