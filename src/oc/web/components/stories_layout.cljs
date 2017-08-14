bf07d7be344a38d3a27e45c50b5198df15b53ae7
bago2k4 <iacopo@rc0.eu>
2017-08-14 17:29:59 +0200

    Add vertical left line for storyboard layout and author avatar.

--- src/oc/web/components/stories_layout.cljs
+++ src/oc/web/components/stories_layout.cljs
@@ -1,11 +1,16 @@
 (ns oc.web.components.stories-layout
   (:require [rum.core :as rum]
+            [dommy.core :as dommy :refer-macros (sel1)]
             [cuerdas.core :as s]
             [oc.web.router :as router]
             [oc.web.urls :as oc-urls]
+            [oc.web.components.ui.user-avatar :refer (user-avatar-image)]
             [oc.web.components.entry-card :refer (entry-card entry-card-empty)]))
 
-(rum/defc stories-layout
+(rum/defc stories-layout < rum/static
+                           {:after-render (fn [s]
+                                            (dommy/set-style! (sel1 [:div.stories-vertical-line]) :height (str (+ (.-clientHeight (sel1 [:div.story-cards-container])) 30) "px"))
+                                           s)}
   [story-data]
   [:div.stories-layout
     (let [sorted-stories (vec (reverse (sort-by :created-at (:entries story-data))))]
@@ -13,4 +18,9 @@
         (for [story sorted-stories
               :let [has-headline (not (empty? (:headline story)))
                     has-body (not (empty? (:body story)))]]
-          (rum/with-key (entry-card story has-headline has-body) (str "story-latest-" (:uuid story))))])])
+          [:div.story-card-row.group
+            {:key (str "story-latest-" (:uuid story))}
+            [:div.story-card-row-left.group
+              (user-avatar-image (first (:author story)))]
+            (entry-card story has-headline has-body)])
+        [:div.stories-vertical-line]])])
