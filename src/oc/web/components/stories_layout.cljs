c4540439708112c73d295f3c9e0b5e10cc61f97f
bago2k4 <iacopo@rc0.eu>
2017-08-15 11:36:29 +0200

    Fix stories-layout component.

--- src/oc/web/components/stories_layout.cljs
+++ src/oc/web/components/stories_layout.cljs
@@ -6,21 +6,21 @@
             [oc.web.router :as router]
             [oc.web.lib.utils :as utils]
             [oc.web.components.ui.user-avatar :refer (user-avatar-image)]
-            [oc.web.components.entry-card :refer (entry-card entry-card-empty)]))
+            [oc.web.components.activity-card :refer (activity-card activity-card-empty)]))
 
 (rum/defc stories-layout < rum/static
                            {:after-render (fn [s]
                                             (dommy/set-style! (sel1 [:div.stories-vertical-line]) :height (str (+ (.-clientHeight (sel1 [:div.story-cards-container])) 30) "px"))
                                            s)}
-  [story-data]
+  [storyboard-data]
   [:div.stories-layout
-    (let [sorted-stories (vec (reverse (sort-by :created-at (:entries story-data))))]
+    (let [sorted-stories (vec (reverse (sort-by :created-at (:stories storyboard-data))))]
       [:div.story-cards-container.group
         (for [story sorted-stories
               :let [has-headline (not (empty? (:headline story)))
                     has-body (not (empty? (:body story)))]]
           [:div.story-card-row.group
-            {:key (str "story-latest-" (:uuid story))}
+            {:key (str "story-latest-" (:slug story))}
             (let [author (first (:author story))]
               [:div.story-card-row-left.group
                 (user-avatar-image author)
@@ -30,7 +30,7 @@
                     {:date-time (:created-at story)
                      :data-toggle "tooltip"
                      :data-placement "top"
-                     :title (utils/entry-tooltip story)}
+                     :title (utils/activity-date-tooltip story)}
                     (utils/time-since (:created-at story))]]])
-            (entry-card story has-headline has-body)])
+            (activity-card story has-headline has-body)])
         [:div.stories-vertical-line]])])
