b93cc28cbd48d060ea07356dfc63af096c7d8b6f
bago2k4 <iacopo@rc0.eu>
2017-08-23 18:23:37 +0200

    Copy fix.

--- src/oc/web/components/activity_card.cljs
+++ src/oc/web/components/activity_card.cljs
@@ -26,7 +26,7 @@
 (defn delete-clicked [e activity-data]
   (utils/event-stop e)
   (let [alert-data {:icon "/img/ML/trash.svg"
-                    :message "Delete this activity?"
+                    :message "Delete this?"
                     :link-button-title "No"
                     :link-button-cb #(dis/dispatch! [:alert-modal-hide])
                     :solid-button-title "Yes"
