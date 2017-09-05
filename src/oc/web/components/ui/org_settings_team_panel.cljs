938bee25b7931b2f7855cdb907ae467d357329dc
bago2k4 <iacopo@rc0.eu>
2017-09-05 16:27:12 +0200

    Fix cancel and resend invite buttons.

--- src/oc/web/components/ui/org_settings_team_panel.cljs
+++ src/oc/web/components/ui/org_settings_team_panel.cljs
@@ -12,13 +12,26 @@
   (.tooltip (js/$ "[data-toggle=\"tooltip\"]") "hide")
   (dis/dispatch! [:user-action team-id user action method other-link-params nil]))
 
+(defn real-remove-fn [author user team-id]
+  (when author
+    (api/remove-author author))
+  (user-action team-id user "remove" "DELETE"  {:ref "application/vnd.open-company.user.v1+json"}))
+
+(defn alert-resend-done []
+  (let [alert-data {:icon "/img/ML/invite_resend.png"
+                    :message "Invite resent."
+                    :link-button-title nil
+                    :link-button-cb nil
+                    :solid-button-title nil
+                    :solid-button-cb nil}]
+   (dis/dispatch! [:alert-modal-show alert-data])))
+
 (rum/defcs org-settings-team-panel
   < rum/reactive
-    (drv/drv :current-user-data)
-    (drv/drv :teams-load)
-    (drv/drv :team-data)
+    (drv/drv :invite-users)
+    (rum/local false ::resending-invite)
     {:before-render (fn [s]
-                     (let [teams-load-data @(drv/get-ref s :teams-load)]
+                     (let [teams-load-data @(drv/get-ref s :invite-users)]
                        (when (and (:auth-settings teams-load-data)
                                   (not (:teams-data-requested teams-load-data)))
                          (dis/dispatch! [:teams-get])))
@@ -27,10 +40,16 @@
                      (doto (js/$ "[data-toggle=\"tooltip\"]")
                         (.tooltip "fixTitle")
                         (.tooltip "hide"))
+                     (when @(::resending-invite s)
+                      (let [invite-users-data (:invite-users @(drv/get-ref s :invite-users))]
+                        (when (zero? (count invite-users-data))
+                          (alert-resend-done)
+                          (reset! (::resending-invite s) false))))
                      s)}
   [s org-data]
-  (let [team-data (drv/react s :team-data)
-        cur-user-data (drv/react s :current-user-data)
+  (let [invite-users-data (drv/react s :invite-users)
+        team-data (:team-data invite-users-data)
+        cur-user-data (:current-user-data invite-users-data)
         org-authors (:authors org-data)]
     [:div.org-settings-panel
       ;; Panel rows
@@ -47,9 +66,16 @@
                   :let [user-type (utils/get-user-type user (dis/org-data))
                         author (some #(when (= (:user-id %) (:user-id user)) %) org-authors)
                         remove-fn (fn []
-                                    (when author
-                                      (api/remove-author author))
-                                    (user-action (:team-id team-data) user "remove" "DELETE"  {:ref "application/vnd.open-company.user.v1+json"}))]]
+                                    (let [alert-data {:icon "/img/ML/trash.svg"
+                                                      :message "Cancel invitation?"
+                                                      :link-button-title "No"
+                                                      :link-button-cb #(dis/dispatch! [:alert-modal-hide])
+                                                      :solid-button-title "Yes"
+                                                      :solid-button-cb #(do
+                                                                          (real-remove-fn author user (:team-id team-data))
+                                                                          (dis/dispatch! [:alert-modal-hide]))
+                                                      }]
+                                      (dis/dispatch! [:alert-modal-show alert-data])))]]
               [:tr
                 {:key (str "org-settings-team-" (:user-id user))}
                 [:td
@@ -61,13 +87,16 @@
                     (when (= "pending" (:status user))
                       [:button.mlb-reset.mlb-link
                         {:on-click (fn []
-                                     (dis/dispatch! [:input [:um-invite] {:email (:email user)
-                                                                          :invite-from "email"
-                                                                          :user-type user-type
-                                                                          :error nil}])
-                                     (utils/after 100 #(dis/dispatch! [:invite-user])))}
+                                     (dis/dispatch! [:input [:invite-users]
+                                                      [{:user (:email user)
+                                                        :type "email"
+                                                        :role user-type
+                                                        :error nil}]])
+                                     (reset! (::resending-invite s) true)
+                                     (utils/after 100 #(dis/dispatch! [:invite-users])))}
                         "Resend"])
-                    (when (= "pending" (:status user))
+                    (when (and (= "pending" (:status user))
+                               (utils/link-for (:links user) "remove"))
                       [:button.mlb-reset.mlb-link-red
                         {:on-click remove-fn}
                         "Cancel"])]]
