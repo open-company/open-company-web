2630856e2e4ab43ead411cc906b9fe6660c80f3d
bago2k4 <iacopo@rc0.eu>
2017-09-07 22:28:40 +0200

    Do not redirect when showing the email wall.

--- src/oc/web/actions.cljs
+++ src/oc/web/actions.cljs
@@ -86,7 +86,8 @@
              (not (utils/in? (:route @router/path) "user-profile"))
              (not (utils/in? (:route @router/path) "email-verification"))
              (not (utils/in? (:route @router/path) "about"))
-             (not (utils/in? (:route @router/path) "features")))
+             (not (utils/in? (:route @router/path) "features"))
+             (not (utils/in? (:route @router/path) "email-wall")))
         (let [login-redirect (cook/get-cookie :login-redirect)]
           (cond
             ; redirect to create-company if the user has no companies
