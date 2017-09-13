54ae4ad12ebe02bd9322198202a09f56df2bab66
bago2k4 <iacopo@rc0.eu>
2017-09-13 16:35:04 +0200

    Avoid errors if org-data is empty.

--- src/oc/web/components/ui/org_avatar.cljs
+++ src/oc/web/components/ui/org_avatar.cljs
@@ -9,41 +9,41 @@
 (rum/defcs org-avatar < rum/static
                         (rum/local false ::img-load-failed)
   [s org-data should-show-link & [show-avatar-and-name hide-name]]
-  (let [org-slug (:slug org-data)
-        has-name (not (empty? (:name org-data)))
-        org-name (if has-name
-                    (:name org-data)
-                    (utils/camel-case-str org-slug))
-        org-logo (:logo-url org-data)
-        img-load-failed @(::img-load-failed s)
-        show-org-avatar? (and (not img-load-failed)
-                              (not (clojure.string/blank? org-logo)))
-        avatar-link (if should-show-link
-                      (if (and (= org-slug (router/current-org-slug))
-                               (router/current-board-slug))
-                        (oc-urls/board org-slug (router/current-board-slug))
-                        (oc-urls/org org-slug))
-                      "")]
+  (let [org-logo (:logo-url org-data)]
     [:div.org-avatar
       {:class (when (empty? org-logo) "missing-logo")}
       (when org-data
-        [:a
-          {:href avatar-link
-           :style {:curstor (if should-show-link "pointer" "default")}
-           :on-click (fn [e]
-                       (.preventDefault e)
-                       (when should-show-link
-                         (router/redirect! avatar-link)))}
-          [:div.org-avatar-container.group
-            (when show-org-avatar?
-              [:div.org-avatar-border
-                [:span.helper]
-                [:img.org-avatar-img
-                  {:src org-logo
-                   :on-error #(reset! (::img-load-failed s) true)}]])
-            (when (and (not hide-name)
-                       (or (not show-org-avatar?)
-                           show-avatar-and-name))
-              [:span.org-name
-                {:class (when-not show-org-avatar? "no-logo")}
-                org-name])]])]))
+        (let [org-slug (:slug org-data)
+              has-name (not (empty? (:name org-data)))
+              org-name (if has-name
+                          (:name org-data)
+                          (utils/camel-case-str org-slug))
+              img-load-failed @(::img-load-failed s)
+              show-org-avatar? (and (not img-load-failed)
+                                    (not (clojure.string/blank? org-logo)))
+              avatar-link (if should-show-link
+                            (if (and (= org-slug (router/current-org-slug))
+                                     (router/current-board-slug))
+                              (oc-urls/board org-slug (router/current-board-slug))
+                              (oc-urls/org org-slug))
+                            "")]
+          [:a
+            {:href avatar-link
+             :style {:curstor (if should-show-link "pointer" "default")}
+             :on-click (fn [e]
+                         (.preventDefault e)
+                         (when should-show-link
+                           (router/redirect! avatar-link)))}
+            [:div.org-avatar-container.group
+              (when show-org-avatar?
+                [:div.org-avatar-border
+                  [:span.helper]
+                  [:img.org-avatar-img
+                    {:src org-logo
+                     :on-error #(reset! (::img-load-failed s) true)}]])
+              (when (and (not hide-name)
+                         (or (not show-org-avatar?)
+                             show-avatar-and-name))
+                [:span.org-name
+                  {:class (when-not show-org-avatar? "no-logo")}
+                  org-name])]]))]))
