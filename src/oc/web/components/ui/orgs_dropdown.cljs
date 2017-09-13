ddabc10143b5c6f7681da1e8b996a2a8e46808b5
bago2k4 <iacopo@rc0.eu>
2017-09-13 19:41:56 +0200

    Fix orgs dropdown style.

--- src/oc/web/components/ui/orgs_dropdown.cljs
+++ src/oc/web/components/ui/orgs_dropdown.cljs
@@ -1,14 +1,24 @@
 (ns oc.web.components.ui.orgs-dropdown
   (:require [rum.core :as rum]
             [org.martinklepsch.derivatives :as drv]
+            [oc.web.router :as router]
+            [oc.web.urls :as oc-urls]
             [oc.web.lib.utils :as utils]
             [oc.web.components.ui.org-avatar :refer (org-avatar)]))
 
 (rum/defc org-dropdown-item < rum/static
   [current-slug org]
-  [:li
-    {:class (when (= (:slug org) current-slug) "active")}
-    (org-avatar org true true)])
+  (let [missing-logo? (empty? (:logo-url org))]
+    [:li
+      {:class (when (= (:slug org) current-slug) "active")}
+      [:a
+        {:on-click #(do (utils/event-stop %) (router/nav! (oc-urls/org (:slug org))))
+         :href (oc-urls/org (:slug org))}
+        (when-not missing-logo?
+          (org-avatar org true false true))
+        [:span.org-name
+          {:class (when missing-logo? "no-logo")}
+          (:name org)]]]))
 
 (rum/defcs orgs-dropdown < rum/static
                            rum/reactive
@@ -23,7 +33,7 @@
       {:class (utils/class-set {:dropdown should-show-dropdown?
                                 :org-has-logo (not (empty? (:logo-url org-data)))})}
       [:button.orgs-dropdown-btn
-        {:id "orgs-dropdown"
+        {:id "orgsDropdown"
          :class (when should-show-dropdown? "dropdown-toggle")
          :data-toggle (when should-show-dropdown? "dropdown")
          :aria-haspopup true
@@ -32,7 +42,7 @@
         (org-avatar (some #(when (= (:slug %) current-org-slug) %) orgs) (not should-show-dropdown?))]
       (when should-show-dropdown?
         [:ul.dropdown-menu
-          {:aria-labelledby "orgs-dropdown"}
+          {:aria-labelledby "orgsDropdown"}
           (for [org orgs]
             (rum/with-key
              (org-dropdown-item current-org-slug org)
