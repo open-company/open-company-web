e91e1d8759b2b33ea2fb643bcc08051d4df1ffe9
bago2k4 <iacopo@rc0.eu>
2017-10-16 23:42:10 +0200

    Use the js svg image as background of the carrot tip component.

--- src/oc/web/components/ui/carrot_tip.cljs
+++ src/oc/web/components/ui/carrot_tip.cljs
@@ -1,11 +1,17 @@
 (ns oc.web.components.ui.carrot-tip
   (:require [rum.core :as rum]
-            [oc.web.lib.utils :as utils]))
+            [oc.web.lib.utils :as utils]
+            [oc.web.lib.onboard-tip-svg :refer (get-onboard-image)]))
 
 (rum/defc carrot-tip < rum/static
   [{:keys [x y title message footer on-next-click]}]
   [:div.carrot-tip-container
-    {:style {:background-image (str "url(\"/img/ML/onboard_tip.svg?w=" (.-innerWidth js/window) "&h=" (.-innerHeight js/window) "&p=" (- x 170) "," (+ y 60) "\")")}}
+    (let [width (.-innerWidth js/window)
+          height (.-innerHeight js/window)
+          px (- x 170)
+          py (+ y 60)]
+      [:div.carrot-tip-background
+        {:dangerouslySetInnerHTML #js {"__html" (get-onboard-image width height px py)}}])
     [:div.carrot-tip
       {:style {:left (str (+ x 50) "px") :top (str y "px")}}
       [:div.triangle]
