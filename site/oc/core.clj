2ca26bbc0ed1b313000d2415244c390bf26cf1eb
Sean Johnson <sean@snootymonkey.com>
2017-10-26 05:42:26 -0400

    Update HTML title tagline.

--- site/oc/core.clj
+++ site/oc/core.clj
@@ -17,7 +17,7 @@
    [:meta {:content "width=device-width, initial-scale=1", :name "viewport"}]
    [:meta {:name "slack-app-id" :content (env :oc-slack-app-id)}]
    ;; The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags
-   [:title "Carrot - Get everyone aligned"]
+   [:title "Carrot - Grow together"]
    ;; Bootstrap - Latest compiled and minified CSS
    [:link {:rel "stylesheet" :href "//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" :integrity "sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" :crossorigin "anonymous"}]
    ;; Local css
