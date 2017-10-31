82f535b1394b0bcd3fd1d2d4c0439d4c697b3e37
bago2k4 <iacopo@rc0.eu>
2017-10-31 12:39:35 +0100

    Rollback changes to test jwt stuff.

--- src/oc/web/lib/jwt.cljs
+++ src/oc/web/lib/jwt.cljs
@@ -8,11 +8,12 @@
   (cook/get-cookie :jwt))
 
 (defn decode [encoded-jwt]
-  (try
-    (js/jwt_decode encoded-jwt)
-    (catch js/Object e
-      (timbre/warn "Failed attempt to decode JWT:" encoded-jwt)
-      nil)))
+  (when (exists? js/jwt_decode)
+    (try
+      (js/jwt_decode encoded-jwt)
+      (catch js/Object e
+        (timbre/warn "Failed attempt to decode JWT:" encoded-jwt)
+        nil))))
 
 (defn get-contents []
   (some-> (jwt) decode (js->clj :keywordize-keys true)))
