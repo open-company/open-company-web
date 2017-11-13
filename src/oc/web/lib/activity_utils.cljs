ae39375934259de3b279bd4e0ed007b60394f7d0
bago2k4 <iacopo@rc0.eu>
2017-11-13 17:20:34 +0100

    Pass the body element directly to the truncate-body and get-first-body-thumbnail functions to avoid having to go get them with a selector. Get the default body height checking the route: all posts page has a taller body.

--- src/oc/web/lib/activity_utils.cljs
+++ src/oc/web/lib/activity_utils.cljs
@@ -1,12 +1,14 @@
 (ns oc.web.lib.activity-utils
   (:require [cuerdas.core :as s]
+            [oc.web.router :as router]
             [oc.web.lib.utils :as utils]))
 
 (defn get-first-body-thumbnail
   "Given an entry body get the first thumbnail available.
   Thumbnail type: image, video or chart."
-  [activity-body & [is-ap]]
-  (let [$body (js/$ (str "<div>" activity-body "</div>"))
+  [activity-body]
+  (let [is-ap (= (router/current-board-slug) "all-posts")
+        $body (js/$ (str "<div>" activity-body "</div>"))
         thumb-els (js->clj (js/$ "img:not(.emojione), iframe" $body))
         found (atom nil)]
     (dotimes [el-num (.-length thumb-els)]
@@ -27,41 +29,52 @@
             (reset! found {:type (.data $el "media-type") :thumbnail (.data $el "thumbnail")})))))
     @found))
 
+(defn reset-truncate-body
+  "Reset dotdotdot for the give body element."
+  [body-el]
+  (.trigger (js/$ body-el) "destroy"))
+
 (def default-body-height 72)
 (def default-all-posts-body-height 144)
 
-(defn- truncate-body [body-sel is-all-posts]
-  (let [$body-els (js/$ (str body-sel ">*"))
+(defn truncate-body
+  "Given a body element truncate the body. It iterate on the elements
+  of the body and truncate the first exceeded element found.
+  This is to avoid truncating a DIV with multiple spaced P inside,
+  since this is a problem for the dotdotdot library that we are using."
+  [body-el]
+  (reset-truncate-body body-el)
+  (let [is-ap (= (router/current-board-slug) "all-posts")
+        $body-els (js/$ ">*" body-el)
         partial-heights (atom [])
         found (atom false)]
-    (js/console.log "au/truncate-body $body-els" $body-els)
     (.each $body-els (fn [idx el]
      (when-not @found
        (this-as this
          (let [$this (js/$ this)
                el-h (.outerHeight $this true) ;; Include margins in height calculation
+               container-max-height (if is-ap
+                                     default-all-posts-body-height
+                                     default-body-height)
                prev-height (apply + @partial-heights)
                actual-height (+ prev-height el-h)
-               max-height (if is-all-posts default-all-posts-body-height default-body-height)
                truncate-height  (cond
-                                  (zero? (- max-height prev-height))
+                                  (zero? (- container-max-height prev-height))
                                   0
-                                  (<= (- max-height prev-height) 24)
+                                  (<= (- container-max-height prev-height) 24)
                                   24
-                                  (<= (- max-height prev-height) (* 24 2))
+                                  (<= (- container-max-height prev-height) (* 24 2))
                                   (* 24 2)
-                                  (<= (- max-height prev-height) (* 24 3))
+                                  (<= (- container-max-height prev-height) (* 24 3))
                                   (* 24 3)
-                                  (< (- max-height prev-height) (* 24 4))
+                                  (< (- container-max-height prev-height) (* 24 4))
                                   (* 24 4)
-                                  (< (- max-height prev-height) (* 24 5))
+                                  (< (- container-max-height prev-height) (* 24 5))
                                   (* 24 5)
-                                  (< (- max-height prev-height) (* 24 6))
+                                  (< (- container-max-height prev-height) (* 24 6))
                                   (* 24 6))]
-           (js/console.log "   - $this" $this "el-h" el-h "actual-height" actual-height "max-height" max-height "prev-height" prev-height)
            (swap! partial-heights #(vec (conj % el-h)))
-           (when (>= actual-height max-height)
-             (js/console.log "      apply dotdotdot!" truncate-height)
+           (when (>= actual-height container-max-height)
              (reset! found true)
              (.dotdotdot $this
                #js {:height truncate-height
