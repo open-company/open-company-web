a4985f0420be3b256ca13068c34b46cd9a73ac17
bago2k4 <iacopo@rc0.eu>
2017-11-14 15:15:45 +0100

    Truncate mixin apply the truncation only if the body node exists. Remove unused let.

--- src/oc/web/mixins/activity.cljs
+++ src/oc/web/mixins/activity.cljs
@@ -7,16 +7,14 @@
   {:init (fn [s]
     (assoc s :truncated (atom false)))
    :after-render (fn [s]
-    (let [activity-data (first (:rum/args s))
-          body-el (rum/ref-node s "activity-body")]
+    (when-let [body-el (rum/ref-node s "activity-body")]
       ; Truncate body text with dotdotdot
       (when (compare-and-set! (:truncated s) false true)
         (au/truncate-body body-el)))
     s)
    :did-remount (fn [o s]
     (let [old-activity-data (first (:rum/args o))
-          new-activity-data (first (:rum/args s))
-          body-el (rum/ref-node s "activity-body")]
+          new-activity-data (first (:rum/args s))]
       ;; If the body changed
       (when (not= (:body old-activity-data) (:body new-activity-data))
         ;; Force the truncate algorithm to re-run
