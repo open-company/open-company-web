8fcf321e117becc53e232853d670653fc01d4d26
bago2k4 <iacopo@rc0.eu>
2017-11-30 12:28:41 +0100

    Revert "Add keypress handler, toggle selection on CMD+B key press to apply or undo the bold formatting. Add the font-family property to check if the font is Avenir Heavy."
    This reverts commit 1370dd740b592ccf25c76ca62c861f554fd39a53.

--- resources/public/lib/MediumEditorExtensions/MediumEditorCustomBold/CustomBold.js
+++ resources/public/lib/MediumEditorExtensions/MediumEditorCustomBold/CustomBold.js
@@ -1,47 +1,29 @@
-rangy.init();
-
 var CustomBold = MediumEditor.extensions.button.extend({
   name: 'custombold',
 
-  tagNames: ['b', 'strong'],
+  tagNames: ['b'],
   contentDefault: '<b>B</b>',
-  contentFA: '<i class="fa fa-bold"></i>',
-  aria: 'bold',
-  action: 'custombold',
-  style: {
-    prop: 'font-family',
-    value: 'AvenirLTStd-Heavy, Muli, Arial, sans-serif'
-  },
-  key: "B",
+  aria: 'Bold',
+  action: 'bold',
 
   init: function () {
+    rangy.init();
+
     MediumEditor.extensions.button.prototype.init.call(this);
 
-    this.subscribe('editableKeydown', this.handleKeydown.bind(this));
     this.classApplier = rangy.createClassApplier('bold', {
       elementTagName: 'b',
-      tagNames: ['b', 'strong'],
       normalize: true
     });
-  },
-
-  handleKeydown: function (event) {
-    var keyCode = MediumEditor.util.getKeyCode(event),
-        isMeta = MediumEditor.util.isMetaCtrlKey(event),
-        isShift = !!event.shiftKey,
-        isAlt = !!event.altKey;
-
-    if (keyCode === this.key.charCodeAt(0) &&
-        isMeta && !isShift && !isAlt){
-      event.preventDefault();
-      event.stopPropagation();
-      this.classApplier.toggleSelection();
-      this.base.checkContentChanged();
-    }
   },
 
   handleClick: function (event) {
     this.classApplier.toggleSelection();
     this.base.checkContentChanged();
+    if (this.isActive()){
+      this.setInactive();
+    } else {
+      this.setActive();
+    }
   }
 });
