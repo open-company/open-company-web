6358f1a6ff30384764ce404ea03a144daea3d35e
bago2k4 <iacopo@rc0.eu>
2017-11-30 12:26:14 +0100

    Go back to medium editor default bold extension since the font weight is not a problem anymore. Remove custombold extension inclusion from dev and production envs.

--- src/oc/web/components/rich_body_editor.cljs
+++ src/oc/web/components/rich_body_editor.cljs
@@ -248,14 +248,13 @@
                                                  :willExpand #(reset! (::did-change s) true)}}
         media-picker-ext (js/MediaPicker. (clj->js media-picker-opts))
         buttons (if show-subtitle
-                  ["custombold" "italic" "h2" "unorderedlist" "anchor"]
-                  ["custombold" "italic" "unorderedlist" "anchor"])
+                  ["bold" "italic" "h2" "unorderedlist" "anchor"]
+                  ["bold" "italic" "unorderedlist" "anchor"])
         options {:toolbar #js {:buttons (clj->js buttons)}
                  :buttonLabels "fontawesome"
                  :anchorPreview #js {:hideDelay 500, :previewValueSelector "a"}
                  :extensions #js {"autolist" (js/AutoList.)
-                                  "media-picker" media-picker-ext
-                                  "custombold" (new js/CustomBold)}
+                                  "media-picker" media-picker-ext}
                  :autoLink true
                  :anchor #js {:customClassOption nil
                               :customClassOptionText "Button"
