089616bd182df3b4bf866c86ca24cdd7cdb69990
bago2k4 <iacopo@rc0.eu>
2017-12-06 21:30:58 +0100

    Render only the last reaction in the reactions list for comments.

--- src/oc/web/components/comment_reactions.cljs
+++ src/oc/web/components/comment_reactions.cljs
@@ -16,33 +16,32 @@
   [s item-data]
   (when (seq (:reactions item-data))
     (let [reactions-data (:reactions item-data)
-          reactions-loading (:reactions-loading item-data)]
+          reactions-loading (:reactions-loading item-data)
+          reaction-data (last reactions-data)
+          is-loading (utils/in? reactions-loading (:reaction reaction-data))
+          read-only-reaction (read-only? item-data)
+          r (if is-loading
+              (merge reaction-data {:count (if (:reacted reaction-data)
+                                            (dec (:count reaction-data))
+                                            (inc (:count reaction-data)))
+                                    :reacted (not (:reacted reaction-data))})
+              reaction-data)]
       [:div.comment-reactions
-        (for [idx (range (count reactions-data))
-              :let [reaction-data (get reactions-data idx)
-                    is-loading (utils/in? reactions-loading (:reaction reaction-data))
-                    read-only-reaction (read-only? item-data)
-                    r (if is-loading
-                        (merge reaction-data {:count (if (:reacted reaction-data)
-                                                      (dec (:count reaction-data))
-                                                      (inc (:count reaction-data)))
-                                              :reacted (not (:reacted reaction-data))})
-                        reaction-data)]]
-          [:button.comment-reaction-btn.btn-reset
-            {:key (str "-entry-" (:uuid item-data) "-" idx)
-             :class (utils/class-set {:reacted (:reacted r)
-                                      :can-react (not read-only-reaction)})
-             :on-click (fn [e]
-                         (when (and (not is-loading) (not read-only-reaction))
-                           (when (and (not (:reacted r))
-                                      (not (js/isSafari))
-                                      (not (js/isEdge))
-                                      (not (js/isIE)))
-                             (reactions/animate-reaction e s))
-                           (dis/dispatch! [:reaction-toggle item-data r])))}
-            [:div.reaction
-              {:class (utils/class-set {:no-reactions (not (pos? (:count r)))})}
-              (:reaction r)]
-            [:div.count
-              (when (pos? (:count r))
-                (:count r))]])])))
+        [:button.comment-reaction-btn.btn-reset
+          {:key (str "comment-reactions-" (:uuid item-data))
+           :class (utils/class-set {:reacted (:reacted r)
+                                    :can-react (not read-only-reaction)})
+           :on-click (fn [e]
+                       (when (and (not is-loading) (not read-only-reaction))
+                         (when (and (not (:reacted r))
+                                    (not (js/isSafari))
+                                    (not (js/isEdge))
+                                    (not (js/isIE)))
+                           (reactions/animate-reaction e s))
+                         (dis/dispatch! [:reaction-toggle item-data r])))}
+          [:div.reaction
+            {:class (utils/class-set {:no-reactions (not (pos? (:count r)))})}
+            (:reaction r)]
+          [:div.count
+            (when (pos? (:count r))
+              (:count r))]]])))
