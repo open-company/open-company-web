1910c3c456d7c4c1074d7fcad937483b36e96c43
bago2k4 <iacopo@rc0.eu>
2017-12-15 20:08:09 +0100

    Fix get cookie function.

--- resources/public/js/static-js.js
+++ resources/public/js/static-js.js
@@ -20,24 +20,10 @@ function OCStaticMailchimpApiSubmit(e, form, success, fail){
 }
 
 function OCStaticGetCookie(name) {
-    var dc = document.cookie;
-    var prefix = name + "=";
-    var begin = dc.indexOf("; " + prefix);
-    if (begin == -1) {
-        begin = dc.indexOf(prefix);
-        if (begin != 0) return null;
-    }
-    else
-    {
-        begin += 2;
-        var end = document.cookie.indexOf(";", begin);
-        if (end == -1) {
-        end = dc.length;
-        }
-    }
-    // because unescape has been deprecated, replaced with decodeURI
-    //return unescape(dc.substring(begin + prefix.length, end));
-    return decodeURI(dc.substring(begin + prefix.length, end));
+  var value = "; " + document.cookie;
+  var parts = value.split("; " + name + "=");
+  if (parts.length == 2)
+    return parts.pop().split(";").shift();
 }
 
 function OCStaticCookieName(name){
