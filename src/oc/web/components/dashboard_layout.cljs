e99930245f493ebf5820529c4eda43197a577820
bago2k4 <iacopo@rc0.eu>
2017-12-19 16:34:26 +0100

    Empty board for mobile.

--- src/oc/web/components/dashboard_layout.cljs
+++ src/oc/web/components/dashboard_layout.cljs
@@ -183,16 +183,16 @@
         org-data (dis/org-data)
         sidebar-width (+ responsive/left-navigation-sidebar-width
                          responsive/left-navigation-sidebar-minimum-right-margin)
-        board-container-style {:marginLeft (if responsive/is-mobile-size?
-                                            "0px"
-                                            (str
-                                             (max
-                                              sidebar-width
-                                              (+
-                                               (/
-                                                (- @(::ww s) responsive/board-container-width sidebar-width)
-                                                2)
-                                               sidebar-width)) "px"))}
+        board-container-style {:margin-left (if (responsive/is-mobile-size?)
+                                             "0px"
+                                             (str (max
+                                                   sidebar-width
+                                                   (+
+                                                    (/
+                                                     (- @(::ww s) responsive/board-container-width sidebar-width)
+                                                     2)
+                                                   sidebar-width))
+                                             "px"))}
         entry-topics (distinct (remove empty? (map :topic-slug (vals (:fixed-items board-data)))))
         is-drafts-board (= (:slug board-data) utils/default-drafts-board-slug)
         all-boards (drv/react s :editable-boards)
@@ -299,8 +299,7 @@
                (all-posts all-posts-data)
                (str "all-posts-" (clojure.string/join (keys (:fixed-items all-posts-data)))))
               ;; Empty board
-              (and (not is-mobile-size?)
-                   (not current-activity-id)
+              (and (not current-activity-id)
                    empty-board?)
               (empty-board)
               ;; Layout boards activities
--- src/oc/web/components/ui/empty_board.cljs
+++ src/oc/web/components/ui/empty_board.cljs
@@ -1,18 +1,38 @@
 (ns oc.web.components.ui.empty-board
   (:require [rum.core :as rum]
             [org.martinklepsch.derivatives :as drv]
+            [oc.web.dispatcher :as dis]
             [oc.web.lib.utils :as utils]
-            [oc.web.dispatcher :as dis]))
+            [oc.web.lib.responsive :as responsive]))
+
+(def mobile-image-size
+ {:width 360
+  :height 322
+  :ratio (/ 360 322)})
 
 (rum/defcs empty-board < rum/reactive
                          (drv/drv :board-data)
   [s]
-  (let [board-data (drv/react s :board-data)]
+  (let [board-data (drv/react s :board-data)
+        mobile? (responsive/is-mobile-size?)
+        ww (when mobile? (responsive/ww))]
     [:div.empty-board.group
-      [:div.empty-board-headline
-        (str "There aren’t any posts in " (:name board-data) " yet. ")
-        (when-not (:read-only board-data)
-          [:button.mlb-reset
-            {:on-click #(dis/dispatch! [:entry-edit {:board-slug (:slug board-data) :board-name (:name board-data)}])}
-            "Add one?"])]
-      [:div.empty-board-image]]))
+      (when-not mobile?
+        [:div.empty-board-headline
+          (str "There aren’t any posts in " (:name board-data) " yet. ")
+          (when-not (:read-only board-data)
+            [:button.mlb-reset
+              {:on-click #(dis/dispatch! [:entry-edit {:board-slug (:slug board-data) :board-name (:name board-data)}])}
+              "Add one?"])])
+      [:img.empty-board-image
+        {:src (utils/cdn (str "/img/ML/" (when mobile? "mobile_") "empty_board.svg"))
+         :style {:width (str (if mobile? (- ww 24 24) 416) "px")
+                 :height (str (if mobile? (* (- ww 24 24) (:ratio mobile-image-size)) 424) "px")
+                 :max-width (str (if mobile? (:width mobile-image-size) 416) "px")
+                 :max-height (str (if mobile? (:height mobile-image-size) 424) "px")}}]
+      (when mobile?
+        [:div.empty-board-footer
+          "Shoot, looks like there aren’t any posts in Design yet...."])
+      (when mobile?
+        [:button.mlb-reset.empty-board-create-first-post
+          "Create the first post"])]))
