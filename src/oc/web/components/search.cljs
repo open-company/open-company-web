c0da9aa8952839668af15c13f18c281eb029c5e5
Nathan Zorn <nathan.zorn@gmail.com>
2017-12-28 15:20:31 -0500

    split out view of search results into a view for each type

--- src/oc/web/components/search.cljs
+++ src/oc/web/components/search.cljs
@@ -8,15 +8,40 @@
             [oc.web.actions.search :as search]
             [oc.web.stores.search :as store]))
 
+(rum/defcs entry-display < rum/static
+  [s data]
+  (let [result (:_source data)
+        title (:headline result)]
+    [:div.search-result
+     [:div.author
+      (user-avatar-image {:user-id (first (:author-id result))
+                          :name (first (:author-name result))
+                          :avatar-url (first (:author-url result))} false)
+      [:span (:author-name result)]]
+     [:div
+      [:span title]
+      (let [body-without-preview (utils/body-without-preview (:body result))
+            activity-url (oc-urls/entry (:board-slug result) (:uuid result))
+            emojied-body (utils/emojify body-without-preview)]
+        [:div {:dangerouslySetInnerHTML  emojied-body}])
+      ]]))
+
+(rum/defcs board-display < rum/static
+  [s data]
+  (let [board (:_source data)]
+    [:div.search-result
+     [:div
+      [:span (:name board)]
+      ]
+     ]))
 
 (rum/defcs search-box < (drv/drv store/search-key)
-                        rum/reactive
-                        rum/static
-
-[s c]
-(let [search-results (drv/react s store/search-key)]
-  [:div.search-box
-    [:div.search
+  rum/reactive
+  rum/static
+  [s]
+  (let [search-results (drv/react s store/search-key)]
+    [:div.search-box
+     [:div.search
       {:content-editable true
        :ref "search-input"
        :placeholder "Search..."
@@ -24,22 +49,11 @@
                    (.add (.-classList node) "inactive"))
        :on-key-up #(search/query (.-innerHTML (rum/ref-node s "search-input")))
        }]
-    [:div.search-results {:ref "results"
-                          :class (when (empty? search-results) "inactive")}
-     (for [sr search-results]
-       (let [result (:_source sr)]
-         [:div.search-result {:key (str "result-" (:uuid (:_source sr)))}
-          [:div.author
-           (user-avatar-image {:user-id (:author-id result)
-                               :name (:author-name result)
-                               :avatar-url (first(:author-url result))} false)
-           [:span (:author-name result)]]
-          [:div
-           [:span (:headline result)]
-           [:span (:name result)]
-           (let [body-without-preview (utils/body-without-preview (:body result))
-                 activity-url (oc-urls/entry (:board-slug result) (:uuid result))
-                 emojied-body (utils/emojify body-without-preview)]
-             [:div {:dangerouslySetInnerHTML  emojied-body}])
-          ]]))
-     ]]))
+     [:div.search-results {:ref "results"
+                           :class (when (empty? search-results) "inactive")}
+      (for [sr search-results]
+        (let [key (str "result-" (:uuid (:_source sr)))]
+          (case (:type (:_source sr))
+            "entry" (rum/with-key (entry-display sr) key)
+            "board" (rum/with-key (board-display sr) key))))
+      ]]))
