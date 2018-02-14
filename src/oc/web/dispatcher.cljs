5b7201195e082f83eb8fdf712b83f6248019bf32
bago2k4 <iacopo@rc0.eu>
2018-02-14 17:26:23 +0100

    Remove duplicated derivative spec.

--- src/oc/web/dispatcher.cljs
+++ src/oc/web/dispatcher.cljs
@@ -198,11 +198,6 @@
    :comments-data       [[:base :org-slug :board-slug]
                          (fn [base org-slug board-slug]
                            (get-in base (comments-key org-slug board-slug)))]
-   :activity-comments-data [[:base :org-slug :board-slug :activity-uuid]
-                            (fn [base org-slug board-slug activity-uuid]
-                              (get-in
-                               base
-                               (comments-key org-slug board-slug)))]
    :trend-bar-status    [[:base]
                           (fn [base]
                             (:trend-bar-status base))]
