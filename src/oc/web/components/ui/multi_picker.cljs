96073d634e9a15789ecbebb5386b9253febc4b47
bago2k4 <iacopo@rc0.eu>
2018-02-20 16:13:30 +0100

    Show/hide on click instead of on hover. Call the rich-body-editor functions when clicking the internal menu buttons. Add the id to be identified by the extension as the external picker.

--- src/oc/web/components/ui/multi_picker.cljs
+++ src/oc/web/components/ui/multi_picker.cljs
@@ -1,24 +1,27 @@
 (ns oc.web.components.ui.multi-picker
   (:require [rum.core :as rum]
-            [oc.web.dispatcher :as dis]))
+            [oc.web.dispatcher :as dis]
+            [oc.web.components.rich-body-editor :as rich-body-editor]))
 
 (rum/defcs multi-picker < rum/static
                           (rum/local false ::show-menu)
-  [s]
+  [s picker-button-id]
   [:div.multi-picker-container
-    {:on-mouse-enter #(reset! (::show-menu s) true)
-     :on-mouse-leave #(reset! (::show-menu s) false)}
     [:button.mlb-reset.multi-picker-btn
       {:aria-label "show multi picker"
+       :id picker-button-id
        :on-click #(swap! (::show-menu s) not)}]
     (when @(::show-menu s)
       [:div.multi-picker
         [:button.mlb-reset.multi-picker-choice.choice-images
+          {:on-click #(rich-body-editor/add-photo @rich-body-editor/rich-body-editor-state nil)}
           [:div.multi-picker-choice-icon]
           "Images"]
         [:button.mlb-reset.multi-picker-choice.choice-media
+          {:on-click #(rich-body-editor/add-video @rich-body-editor/rich-body-editor-state)}
           [:div.multi-picker-choice-icon]
           "Media"]
         [:button.mlb-reset.multi-picker-choice.choice-attachment
+          {:on-click #(rich-body-editor/add-attachment @rich-body-editor/rich-body-editor-state nil)}
           [:div.multi-picker-choice-icon]
           "Attachment"]])])
