dc999be6d8fbf64eb13de39f8f6047b75c7e383c
bago2k4 <iacopo@rc0.eu>
2018-02-21 20:54:42 +0100

    Always enable the emoji picker button if a deafult field selector is passed. Use it in the new post view.

--- src/oc/web/components/entry_edit.cljs
+++ src/oc/web/components/entry_edit.cljs
@@ -425,6 +425,7 @@
             (emoji-picker {:add-emoji-cb (partial add-emoji-cb s)
                            :width 20
                            :height 20
+                           :default-field-selector "div.entry-edit-modal div.rich-body-editor"
                            :container-selector "div.entry-edit-modal"})
             [:div.entry-edit-legend-container
               {:on-click #(reset! (::show-legend s) (not @(::show-legend s)))
