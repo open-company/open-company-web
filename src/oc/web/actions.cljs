42a6a9ec4699376da286d63753deb221d2dc584e
Nathan Zorn <nathan.zorn@gmail.com>
2018-04-02 15:00:14 -0400

    remove unused functions

--- src/oc/web/actions.cljs
+++ src/oc/web/actions.cljs
@@ -1,9 +1,6 @@
 (ns oc.web.actions
   (:require [taoensso.timbre :as timbre]
-            [oc.web.dispatcher :as dispatcher]
-            [oc.web.actions.org :as oa]
-            [oc.web.lib.jwt :as jwt]
-            [oc.web.lib.utils :as utils]))
+            [oc.web.dispatcher :as dispatcher]))
 
 ;; ---- Generic Actions Dispatch
 ;; This is a small generic abstraction to handle "actions".
@@ -20,24 +17,6 @@
   (timbre/warn "No handler defined for" (str (first payload)))
   (timbre/info "Full event: " (pr-str payload))
   db)
-
-(defn- org-change [db org-uuid change-at]
-  (timbre/debug "Org change:" org-uuid "at:" change-at)
-  (utils/after 1000 (fn [] (oa/get-org)))
-  db)
-
-(defmethod dispatcher/action :container/change
-  [db [_ {container-uuid :container-id change-at :change-at user-id :user-id}]]
-  (timbre/debug "Container change:" container-uuid "at:" change-at "by:" user-id)
-  (if (not= (jwt/user-id) user-id) ; no need to respond to our own events
-    (if (= container-uuid (:uuid (dispatcher/org-data)))
-      (org-change db container-uuid change-at)
-      db)
-    db))
-
-;; This should be turned into a proper form library
-;; Lomakeets FormState ideas seem like a good start:
-;; https://github.com/metosin/lomakkeet/blob/master/src/cljs/lomakkeet/core.cljs
 
 (defmethod dispatcher/action :input [db [_ path value]]
   (assoc-in db path value))
