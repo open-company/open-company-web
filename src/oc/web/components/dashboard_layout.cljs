7a12e27b87b160243597f033359fda0af7ac557b
bago2k4 <iacopo@rc0.eu>
2018-04-27 12:42:42 +0200

    Show compose button for viewer too but disabled with a tooltip. Do not show it on mobile. Do not show compose button for users out of the current team.

--- src/oc/web/components/dashboard_layout.cljs
+++ src/oc/web/components/dashboard_layout.cljs
@@ -129,6 +129,12 @@
                                   (when @(::scroll-listener s)
                                     (events/unlistenByKey @(::scroll-listener s))
                                     (reset! (::scroll-listener s) nil)))
+                                s)
+                               :did-update (fn [s]
+                                (when-let [$compose-button (js/$ (rum/ref-node s :top-compose-button))]
+                                  (if (clojure.string/blank? (.data $compose-button "original-title"))
+                                    (.tooltip $compose-button "disable")
+                                    (.tooltip $compose-button "enable")))
                                 s)}
   [s]
   (let [org-data (drv/react s :org-data)
@@ -171,12 +177,13 @@
         show-drafts (pos? (:count drafts-link))
         mobile-navigation-sidebar (drv/react s :mobile-navigation-sidebar)
         all-posts-key (str "all-posts-stream-" (clojure.string/join "-" (keys (:fixed-items all-posts-data))))
-        show-compose-button (or (and (or is-all-posts
-                                         is-drafts-board)
-                                     (pos? (count all-boards)))
-                                (and (not is-all-posts)
-                                     (not is-drafts-board)
-                                     (utils/link-for (:links board-data) "create")))]
+        can-compose (or (and (or is-all-posts
+                                 is-drafts-board)
+                             (pos? (count all-boards)))
+                        (and (not is-all-posts)
+                             (not is-drafts-board)
+                             (utils/link-for (:links board-data) "create")))
+        should-show-top-compose (jwt/user-is-part-of-the-team (:team-id org-data))]
       ;; Entries list
       [:div.dashboard-layout.group
         ;; Show create new section for desktop
@@ -243,13 +250,20 @@
                        :title "Visible to the world, including search engines"}
                       "Public"])]
                 ;; Add entry button
-                (when show-compose-button
+                (when should-show-top-compose
                   [:div.new-post-top-dropdown-container.group
-                    [:button.mlb-reset.mlb-default.add-to-board-top-button.group
-                      {:on-click compose-fn}
-                      [:div.add-to-board-pencil]
-                      [:label.add-to-board-label
-                        "Compose"]]
+                    (let [show-tooltip? (and (not can-compose) should-show-top-compose)]
+                      [:button.mlb-reset.mlb-default.add-to-board-top-button.group
+                        {:ref :top-compose-button
+                         :on-click #(when can-compose (compose-fn %))
+                         :class (when-not can-compose "disabled")
+                         :title (when show-tooltip? "You are a view-only user.")
+                         :data-toggle (when show-tooltip? "tooltip")
+                         :data-placement (when show-tooltip? "top")
+                         :data-container (when show-tooltip? "body")}
+                        [:div.add-to-board-pencil]
+                        [:label.add-to-board-label
+                          "Compose"]])
                     (when @(::show-top-boards-dropdown s)
                       (dropdown-list
                        {:items (map
@@ -314,7 +328,7 @@
                   :else
                   (section-stream)))
               ;; Add entry floating button
-              (when show-compose-button
+              (when can-compose
                 (let [opacity (if (responsive/is-tablet-or-mobile?)
                                 1
                                 (calc-opacity (document-scroll-top)))]
