ffa37f76d37a118e965cbbb8367e5a7aafbb1026
bago2k4 <iacopo@rc0.eu>
2018-05-07 20:28:29 +0200

    Restyle attachments.

--- src/oc/web/components/ui/stream_attachments.cljs
+++ src/oc/web/components/ui/stream_attachments.cljs
@@ -14,39 +14,30 @@
         editable? (fn? remove-cb)]
     (when (pos? atc-num)
       [:div.stream-attachments
-        [:div.stream-attachments-title
-          (str atc-num " attachment" (when (> atc-num 1) "s"))]
         [:div.stream-attachments-content
           (for [atch attachments
                 :let [created-at (:created-at atch)
+                      file-name (:file-name atch)
                       size (:file-size atch)
-                      subtitle (str
-                                  (when created-at
-                                    (utils/time-since created-at))
-                                  (when (and created-at size)
-                                    " - ")
-                                  (when size
-                                    (filesize size :binary false :format "%.2f")))]]
+                      subtitle (when size
+                                 (filesize size :binary false :format "%.2f"))]]
             [:a.stream-attachments-item.group
-              {:class (utils/class-set {:double-line (> atc-num 1)
-                                        :editable editable?})
-               :key (str "attachment-" size "-" (:file-url atch))
+              {:key (str "attachment-" size "-" (:file-url atch))
                :href (:file-url atch)
                :target "_blank"}
-              [:div.attachment-icon]
-              [:div.attachment-content
-                [:div.attachment-name
-                  (:file-name atch)]
-                [:div.attachment-description
-                  subtitle]]
-              (when editable?
-                [:button.mlb-reset.remove-attachment-bt
-                  {:data-toggle (when-not (responsive/is-tablet-or-mobile?) "" "tooltip")
-                   :data-placement "top"
-                   :data-container "body"
-                   :title "Remove attachment"
-                   :on-click #(do
-                                (utils/event-stop %)
-                                (utils/remove-tooltips)
-                                (when (fn? remove-cb)
-                                  (remove-cb atch)))}])])]])))
+              [:div.attachment-info
+                {:class (when editable? "editable")}
+                [:span.attachment-name file-name]
+                [:span.attachment-content-separator]
+                [:span.attachment-description subtitle]
+                (when editable?
+                  [:button.mlb-reset.remove-attachment-bt
+                    {:data-toggle (when-not (responsive/is-tablet-or-mobile?) "" "tooltip")
+                     :data-placement "top"
+                     :data-container "body"
+                     :title "Remove attachment"
+                     :on-click #(do
+                                  (utils/event-stop %)
+                                  (utils/remove-tooltips)
+                                  (when (fn? remove-cb)
+                                    (remove-cb atch)))}])]])]])))
