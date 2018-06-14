8e06354eabeeafdd3c68a9cf6d9f5786d5de3db5
bago2k4 <iacopo@rc0.eu>
2018-06-14 17:59:35 +0200

    AP seen mixin with timeout to throttle the scrolling events.
    Given a selector for the items to check and a function to call on every pause of x seconds on the scrolls call the give function on all the visible items.

--- src/oc/web/mixins/ui.cljs
+++ src/oc/web/mixins/ui.cljs
@@ -2,7 +2,9 @@
   (:require [rum.core :as rum]
             [dommy.core :as dommy :refer-macros (sel1)]
             [goog.events :as events]
-            [goog.events.EventType :as EventType]))
+            [goog.events.EventType :as EventType]
+            [oc.web.lib.utils :as utils]
+            [oc.web.lib.responsive :as responsive]))
 
 (def -no-scroll-mixin-class :no-scroll)
 
@@ -127,3 +129,68 @@
       (when resize-listener
         (events/unlistenByKey (:render-on-resize-listener s)))
       (dissoc s :render-on-resize-listener)))})
+
+(defn- is-element-visible?
+   "Given a DOM element return true if it's actually visible in the viewport."
+  [el]
+  (let [rect (.getBoundingClientRect el)
+        zero-pos? #(or (zero? %)
+                       (pos? %))
+        doc-element (.-documentElement js/document)
+        win-height (or (.-innerHeight js/window)
+                       (.-clientHeight doc-element))
+        win-width (or (.-innerWidth js/window)
+                      (.-clientWidth doc-element))]
+    (and ;; Item starts below the navbar
+         (>= (.-top rect) responsive/navbar-height)
+         ;; Item left is not out of the screen
+         (zero-pos? (.-left rect))
+         ;; Item bottom is less than the screen height
+         (<= (.-bottom rect) win-height)
+         ;; Item right is less than the screen width
+         (<= (.-right rect) win-width))))
+
+(def _scroll-intervanl 0)
+
+(defn ref-is-visible-while-scrolling
+  "Give a selector for the items to check under the component root.
+  Wait for user to scroll, when it stops for _scroll-intervanl seconds it checks the visible items
+  and call the passed callback with the element uuid (got via data-uuid set to the element)."
+   [items-selector item-is-visible-cb]
+   (let [scroll-listener-kw :ui-mixin-scrolling-listener
+         check-items-fn (fn [s]
+                         (let [dom-node (rum/dom-node s)
+                               $all-items (js/$ items-selector dom-node)]
+                           (.each $all-items (fn [idx el]
+                             (when (and (fn? item-is-visible-cb)
+                                        (is-element-visible? el))
+                               (item-is-visible-cb s (.-uuid (.-dataset el))))))))
+         _last-timeout (atom nil)]
+     {:will-mount (fn [s]
+       (assoc s scroll-listener-kw
+        (events/listen js/window EventType/SCROLL
+         (fn []
+          (.clearTimeout js/window @_last-timeout)
+          (reset! _last-timeout
+           (utils/after (* _scroll-intervanl 1000)
+            #(check-items-fn s)))))))
+      :did-mount (fn [s]
+       (check-items-fn s)
+       (when @_last-timeout
+         (.clearTimeout js/window @_last-timeout))
+       s)
+      :did-remount (fn [_ s]
+       (when @_last-timeout
+         (.clearTimeout js/window @_last-timeout))
+       (check-items-fn s)
+       s)
+      :will-unmount (fn [s]
+       (when @_last-timeout
+         (.clearTimeout js/window @_last-timeout)
+         (reset! _last-timeout nil)
+         (check-items-fn s))
+       (if-let [scroll-listener (get s scroll-listener-kw)]
+         (do
+           (events/unlistenByKey scroll-listener)
+           (dissoc s scroll-listener-kw))
+         s))}))
