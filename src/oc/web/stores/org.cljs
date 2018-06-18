1fcf591a51120f8b5fafba5db9f9d85c0f7c63f4
bago2k4 <iacopo@rc0.eu>
2018-06-18 18:50:08 +0200

    Do not preserves the AP and drafts special boards in the app-state to avoid sticking posts after deletion.

--- src/oc/web/stores/org.cljs
+++ src/oc/web/stores/org.cljs
@@ -19,7 +19,7 @@
   ;; We need to remove the boards that are no longer in the org except all-posts and drafts
   (let [boards-key (dispatcher/boards-key (:slug org-data))
         old-boards (get-in db boards-key)
-        board-slugs (set (concat (map (comp keyword :slug) (:boards org-data)) [:all-posts :drafts]))
+        board-slugs (set (map (comp keyword :slug) (:boards org-data)))
         filter-board (fn [[k v]]
                        (board-slugs k))
         next-boards (into {} (filter filter-board old-boards))]
