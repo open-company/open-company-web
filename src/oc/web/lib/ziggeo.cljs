c4e71bac4fab8261cbedeef7d90ca59d8703c89f
bago2k4 <iacopo@rc0.eu>
2018-06-27 16:14:04 +0200

    Turn on streaming WebRTC.

--- src/oc/web/lib/ziggeo.cljs
+++ src/oc/web/lib/ziggeo.cljs
@@ -2,6 +2,7 @@
 
 (defn init-ziggeo [& [debug]]
   (let [config {:token "c9b611b2b996ee5a1f318d3bacc36b27"
-                :debug debug}
+                :debug debug
+                :webrtc_streaming true}
         ZiggeoApplication (.. js/ZiggeoApi -V2 -Application)]
     (ZiggeoApplication. (clj->js config))))
