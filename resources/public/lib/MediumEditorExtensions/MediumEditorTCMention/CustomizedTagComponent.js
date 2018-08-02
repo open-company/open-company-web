6eb7331a1f968977d89183c6fd664b6cdd6477df
bago2k4 <iacopo@rc0.eu>
2018-08-02 18:44:33 +0200

    Fix replacing value when selecting a mention, use slack username only if it's the only matching.

--- resources/public/lib/MediumEditorExtensions/MediumEditorTCMention/CustomizedTagComponent.js
+++ resources/public/lib/MediumEditorExtensions/MediumEditorTCMention/CustomizedTagComponent.js
@@ -2,57 +2,22 @@
 //   const trigger = props.currentMentionText.substring(0, 1);
 //   const currentText = props.currentMentionText.substring(1, props.currentMentionText.length).toLowerCase();
 
-//   var mappedUsers = props.users.map(function(user, i) {
-//     if (user["name"] && user["name"].toLowerCase().indexOf(currentText) !== -1)
-//       return Object.assign(user, {"selectedKey": "name"});
-//     else if (user["first-name"] && user["first-name"].toLowerCase().indexOf(currentText) !== -1)
-//       return Object.assign(user, {"selectedKey": "first-name"});
-//     else if (user["last-name"] && user["last-name"].toLowerCase().indexOf(currentText) !== -1)
-//       return Object.assign(user, {"selectedKey": "last-name"});
-//     else if (user["slack-username"] && user["slack-username"].toLowerCase().indexOf(currentText) !== -1)
-//       return Object.assign(user, {"selectedKey": "slack-username"});
-//     else if (user["email"] && user["email"].toLowerCase().indexOf(currentText) !== -1)
-//       return Object.assign(user, {"selectedKey": "email"});
-//     else 
-//       return user;
-//   });
-//   var selectedUsers = mappedUsers.filter(function(user){
-//     return !!user["selectedKey"];
-//   });
-
-//   return React.createElement(
-//     "div",
-//     { "className": "oc-mention-options" },
-//     [ React.createElement(
-//        "label",
-//        { "className": "oc-mention-options-title"},
-//        "@mention to tag someone"),
-//       React.createElement("div",
-//         { "className": "oc-mention-options-list"}
-//         selectedUsers.map(function (user, i) {
-//           return React.createElement(
-//                   "div",
-//                   { "className": "oc-mention-option",
-//                     "data-name": user["name"] || user["first-name"] + " " + user["last-name"],
-//                     "data-user-id": user["user-id"],
-//                     "data-slack-username": user["slack-username"],
-//                     "key": "oc-mention-option-" + i,
-//                     "onClick": function(a, b, c) {
-//                       props.selectMentionCallback("@" + user[user["selectedKey"]], user)}},
-//                   user[user["selectedKey"]]
-//                 );
-//         }))]
-//   );
-// }
-
-
-
 function CustomizedTagComponent(props) {
   const trigger = props.currentMentionText.substring(0, 1);
   const currentText = props.currentMentionText.substring(1, props.currentMentionText.length).toLowerCase();
 
   var mappedUsers = props.users.map(function (user, i) {
-    if (user["name"] && user["name"].toLowerCase().indexOf(currentText) !== -1) return Object.assign(user, { "selectedKey": "name" });else if (user["first-name"] && user["first-name"].toLowerCase().indexOf(currentText) !== -1) return Object.assign(user, { "selectedKey": "first-name" });else if (user["last-name"] && user["last-name"].toLowerCase().indexOf(currentText) !== -1) return Object.assign(user, { "selectedKey": "last-name" });else if (user["slack-username"] && user["slack-username"].toLowerCase().indexOf(currentText) !== -1) return Object.assign(user, { "selectedKey": "slack-username" });else if (user["email"] && user["email"].toLowerCase().indexOf(currentText) !== -1) return Object.assign(user, { "selectedKey": "email" });else return user;
+    if (user["name"] && user["name"].toLowerCase().indexOf(currentText) !== -1)
+      return Object.assign(user, { "selectedKey": "name" });
+    else if (user["first-name"] && user["first-name"].toLowerCase().indexOf(currentText) !== -1)
+      return Object.assign(user, { "selectedKey": "first-name" });
+    else if (user["last-name"] && user["last-name"].toLowerCase().indexOf(currentText) !== -1)
+      return Object.assign(user, { "selectedKey": "last-name" });
+    else if (user["slack-username"] && user["slack-username"].toLowerCase().indexOf(currentText) !== -1)
+      return Object.assign(user, { "selectedKey": "slack-username" });
+    else if (user["email"] && user["email"].toLowerCase().indexOf(currentText) !== -1)
+      return Object.assign(user, { "selectedKey": "email" });
+    else return user;
   });
   var selectedUsers = mappedUsers.filter(function (user) {
     return !!user["selectedKey"];
@@ -67,6 +32,7 @@ function CustomizedTagComponent(props) {
       selectedUsers.map(function (user, i) {
         var avatarStyle = { "backgroundImage": "url(" + user["avatar-url"] + ")" };
         var displayName = (user["name"] && user["name"].length > 0) ? user["name"] : (user["first-name"] + " " + user["last-name"]);
+        var selectedValue = (user["selectedKey"] === "slack-username")? user["slack-username"] : displayName;
         return React.createElement(
           "div",
           { "key": "user-" + (user["user-id"] + "-" + user["email"]),
@@ -74,7 +40,9 @@ function CustomizedTagComponent(props) {
             "data-name": displayName,
             "data-user-id": user["user-id"],
             "data-slack-username": user["slack-username"],
-            onClick: function(){props.selectMentionCallback("@" + user[user["selectedKey"]], user);} },
+            onClick: function(){
+              props.selectMentionCallback("@" + selectedValue);
+            }},
           React.createElement("div", { "className": "oc-mention-option-avatar", style: avatarStyle }),
           React.createElement(
             "div",
