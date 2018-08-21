f7f26776ce744dc1a7e5369617f61950d3ef4320
bago2k4 <iacopo@rc0.eu>
2018-08-21 18:46:45 +0200

    Update lander.

--- site/oc/pages.clj
+++ site/oc/pages.clj
@@ -102,33 +102,71 @@
 
 (def core-values-list
   [:div.core-values-list.group
-    [:div.core-value.key-announcement
-      "Key announcements"]
-    [:div.core-value.company-updates
-      "Company & team updates"]
-    [:div.core-value.strategic-plans
-      "Strategic plans"]
-    [:div.core-value.ideas-discussions
-      "Decisions"]])
+    [:div.core-value-container.key-announcement
+      [:div.core-value-header.group
+        [:div.core-value-icon]
+        [:div.core-value
+          "Key announcements"]]
+      [:div.core-value-white-box
+        [:div.core-value-box-header
+          "Product "
+          [:span.dot "•"]
+          " 45 views"]
+        [:div.core-value-box-title
+          "Updates to billing & subscriptions (beta edition)"]]]
+    [:div.core-value-container.team-updates
+      [:div.core-value-header.group
+        [:div.core-value-icon]
+        [:div.core-value
+          "Team updates"]]
+      [:div.core-value-white-box
+        [:div.core-value-box-header
+          "General "
+          [:span.dot "•"]
+          " 22 views"]
+        [:div.core-value-box-title
+          "June 25, 2018 all hands video highlights"]]]
+    [:div.core-value-container.strategic-plans
+      [:div.core-value-header.group
+        [:div.core-value-icon]
+        [:div.core-value
+          "Strategic plans"]]
+      [:div.core-value-white-box
+        [:div.core-value-box-header
+          "Strategy "
+          [:span.dot "•"]
+          " 67 views"]
+        [:div.core-value-box-title
+          "Product roadmap review presentation and PDF"]]]
+    [:div.core-value-container.stories
+      [:div.core-value-header.group
+        [:div.core-value-icon]
+        [:div.core-value
+          "Stories"]]
+      [:div.core-value-white-box
+        [:div.core-value-box-header
+          "Design "
+          [:span.dot "•"]
+          " 34 views"]
+        [:div.core-value-box-title
+          "How we pulled off our biggest launch ever 🤩"]]]])
 
 (def testimonials-section
-  [:div.hidden]
-  ; [:section.testimonials-section
-  ;   [:div.testimonials-section-title
-  ;     "Don’t take our word for it"]
-  ;   [:div.testimonials-section-subtitle
-  ;     "Here’s how we’re helping teams like yours."]
-  ;   [:div.testimonials-cards-container.group
-  ;     [:img.card
-  ;       {:src (cdn "/img/ML/testimonial_katie.png")
-  ;        :srcSet (str (cdn "/img/ML/testimonial_katie@2x.png") " 2x")}]
-  ;     [:img.card
-  ;       {:src (cdn "/img/ML/testimonial_riley.png")
-  ;        :srcSet (str (cdn "/img/ML/testimonial_riley@2x.png") " 2x")}]
-  ;     [:img.card
-  ;       {:src (cdn "/img/ML/testimonial_matt.png")
-  ;        :srcSet (str (cdn "/img/ML/testimonial_matt@2x.png") " 2x")}]]]
-  )
+  [:section.testimonials-section
+    [:div.testimonials-section-title
+      "Don’t take our word for it"]
+    [:div.testimonials-section-subtitle
+      "Here’s how we’re helping teams like yours."]
+    [:div.testimonials-cards-container.group
+      [:img.card
+        {:src (cdn "/img/ML/testimonial_katie.png")
+         :srcSet (str (cdn "/img/ML/testimonial_katie@2x.png") " 2x")}]
+      [:img.card
+        {:src (cdn "/img/ML/testimonial_riley.png")
+         :srcSet (str (cdn "/img/ML/testimonial_riley@2x.png") " 2x")}]
+      [:img.card
+        {:src (cdn "/img/ML/testimonial_matt.png")
+         :srcSet (str (cdn "/img/ML/testimonial_matt@2x.png") " 2x")}]]])
 
 (def no-credit-card
   [:div.no-credit-card
@@ -144,67 +182,80 @@
 
 (def keep-aligned-section
   [:section.home-keep-aligned
+    [:div.scroll-down]
     [:h2.keep-aligned-title
       "Carrot keeps leaders and teams aligned"]
 
     [:div.keep-aligned-section
-      [:div.keep-aligned-section-row
-        [:div.keep-aligned-section-screenshot.screenshot-2]
-        [:div.keep-aligned-section-copy
-          [:div.keep-aligned-section-copy-title
-            "Create engaging updates"]
-          [:div.keep-aligned-section-list-item
-            "Room for more than a quick chat"]
-          [:div.keep-aligned-section-list-item
-            "Capture video to add a human touch"]
-          [:div.keep-aligned-section-list-item
-            "Attachments from Google, Dropbox, & others"]]]
-
-      [:div.keep-aligned-section-row
+      [:div.keep-aligned-section-row.first-row.group
         [:div.keep-aligned-section-screenshot.screenshot-1]
         [:div.keep-aligned-section-copy
           [:div.keep-aligned-section-copy-title
-            "Get up to speed quickly"]
-          [:div.keep-aligned-section-list-item
-            "“Must see” updates rise to the top"]
-          [:div.keep-aligned-section-list-item
-            "Follow people and topics you can’t miss"]
-          [:div.keep-aligned-section-list-item
-            "Organized and easy to browse"]]]
+            "Update your team in seconds"]
+          [:div.keep-aligned-section-list-item.purple-checkmark
+            "Create compelling updates people want to read"]
+          [:div.keep-aligned-section-list-item.purple-checkmark
+            "Capture video updates to add a human touch"]
+          [:div.keep-aligned-section-list-item.purple-checkmark
+            "Link to Google, Dropbox, wikis and others"]
+          [:div.keep-aligned-section-list-item.purple-checkmark
+          "Automate recurring team updates"]]]
 
-      [:div.keep-aligned-section-row
+      [:div.keep-aligned-section-row.second-row.group
+        [:div.keep-aligned-section-screenshot.screenshot-2]
+        [:div.keep-aligned-section-copy
+          [:div.keep-aligned-section-copy-title
+            "Keep everyone informed"]
+          [:div.keep-aligned-section-list-item.green-checkmark
+            "“Must see” updates rise to the top"]
+          [:div.keep-aligned-section-list-item.green-checkmark
+            "See what’s trending to stay in the loop"]
+          [:div.keep-aligned-section-list-item.green-checkmark
+            "Organized for easy browsing and search"]
+          [:div.keep-aligned-section-list-item.green-checkmark
+            "A morning digest summarizes what’s new"]]]
+
+      [:div.keep-aligned-section-row.third-row.group
         [:div.keep-aligned-section-screenshot.screenshot-3]
         [:div.keep-aligned-section-copy
           [:div.keep-aligned-section-copy-title
-            "Spark better discussions"]
-          [:div.keep-aligned-section-list-item
-            "Encourage more comments and questions"]
-          [:div.keep-aligned-section-list-item
+            "Spark better follow-on discussions"]
+          [:div.keep-aligned-section-list-item.red-checkmark
+            "Encourage thoughtful comments and questions"]
+          [:div.keep-aligned-section-list-item.red-checkmark
             "Keep interactions together for greater context"]
-          [:div.keep-aligned-section-list-item
-            "Sync to Slack to discuss anywhere"]]]
+          [:div.keep-aligned-section-list-item.red-checkmark
+            "Syncs with Slack so discussions can happen anywhere"]]]
 
-      [:div.keep-aligned-section-row
+      [:div.keep-aligned-section-row.fourth-row.group
         [:div.keep-aligned-section-screenshot.screenshot-4]
         [:div.keep-aligned-section-copy
           [:div.keep-aligned-section-copy-title
-            "Know who’s up to date"]
-          [:div.keep-aligned-section-list-item
-            "Eliminate communication gaps"]
-          [:div.keep-aligned-section-list-item
+            "Make sure you're being heard"]
+          [:div.keep-aligned-section-list-item.blue-checkmark
+            "Know who's seen your post"]
+          [:div.keep-aligned-section-list-item.blue-checkmark
             "Send reminders with a single click"]
-          [:div.keep-aligned-section-list-item
-            "Ensures alignment on important items"]]]]])
+          [:div.keep-aligned-section-list-item.blue-checkmark
+            "Eliminate communication gaps"]
+          [:div.keep-aligned-section-list-item.blue-checkmark
+            "Ensure alignment on important items"]]]]])
 
 (def access-anywhere-section
   [:section.access-anywhere-section
     [:div.access-anywhere-section-container
       [:div.access-anywhere-copy
         [:div.access-anywhere-copy-title
-          "Stay informed on the go."]
-        [:div.access-anywhere-copy-subtitle
-          "Fully responsive mobile web app."]
-        [:div.access-anywhere-copy-subtitle.second-line
+          "Stay up to date on the go."]
+        [:div.access-anywhere-copy-title.big-web-only
+          "Access Carrot from anywhere."]
+        [:div.access-anywhere-copy-title.mobile-only
+          "Access Carrot anywhere."]
+        [:div.access-anywhere-copy-subtitle.big-web-only
+          "Fully responsive web app. No app install required."]
+        [:div.access-anywhere-copy-subtitle.mobile-only
+          "Fully responsive web app."]
+        [:div.access-anywhere-copy-subtitle.mobile-only
           "No app install required."]]
       [:div.access-anywhere-screenshot]]])
 
@@ -242,11 +293,11 @@
         [:div.balloon.small-purple]
 
         [:h1.headline
-          "The new way to provide meaningful team communication."]
+          "Where leaders communicate what matters."]
         [:div.subheadline
           (str
-           "Rise above the noise of chat and email to keep your "
-           "growing and distributed teams aligned.")]
+           "Rise above the noise of chat and email to "
+           "keep your growing and distributed teams aligned.")]
         ; (try-it-form "try-it-form-central" "try-it-combo-field-top")
         [:div.get-started-button-container
           [:button.mlb-reset.get-started-button
