1775b9e98b11e8bfed7362c596a1fb869a035665
bago2k4 <iacopo@rc0.eu>
2018-11-01 19:02:55 +0100

    Make sure youtube video urls are accepted even with the t or time_continue parameter in it.

--- src/oc/web/components/ui/media_video_modal.cljs
+++ src/oc/web/components/ui/media_video_modal.cljs
@@ -15,7 +15,7 @@
     (utils/after 180 #(dismiss-modal s))))
 
 (def youtube-regexp
- "https?://(?:www\\.|m\\.)*(?:youtube\\.com|youtu\\.be)/watch/?\\?v=([a-zA-Z0-9_-]{11}/?)")
+ "https?://(?:www\\.|m\\.)*(?:youtube\\.com|youtu\\.be)/watch/?\\?((time_continue|t)=\\d+&)?v=([a-zA-Z0-9_-]{11}).*")
 (def youtube-short-regexp
  "https?://(?:www\\.|m\\.)*(?:youtube\\.com|youtu\\.be)/([a-zA-Z0-9_-]{11}/?)")
 
