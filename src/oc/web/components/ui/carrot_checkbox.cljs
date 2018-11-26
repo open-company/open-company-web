0fc918a666c0602b61429e9ed9c70507bbc00164
bago2k4 <iacopo@rc0.eu>
2018-11-26 21:08:00 +0100

    Refresh the settings panel, the Slack section uses an on/off switch to add the bot.

--- src/oc/web/components/ui/carrot_checkbox.cljs
+++ src/oc/web/components/ui/carrot_checkbox.cljs
@@ -3,9 +3,17 @@
             [oc.web.lib.utils :as utils]))
 
 (rum/defc carrot-checkbox < rum/static
-  [{:keys [selected disabled did-change-cb]}]
+  [{:keys [selected disabled did-change-cb selected-label unselected-label]}]
   [:div.carrot-checkbox
     {:on-click #(did-change-cb (not selected))
      :class (utils/class-set {:selected selected
-                              :disabeld disabled})}
-    [:span.dot]])
+                              :disabled disabled})}
+    (when (and selected
+               selected-label)
+      [:span.selected-label
+        selected-label])
+    [:span.dot]
+    (when (and (not selected)
+               unselected-label)
+      [:span.unselected-label
+        unselected-label])])
