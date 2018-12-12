6343811e8707dd86fe6143c088cded1cf31133f0
bago2k4 <iacopo@rc0.eu>
2018-12-12 20:34:17 +0100

    Change the current activity-removed component to reflect the new design.

--- src/oc/web/components/ui/activity_removed.cljs
+++ src/oc/web/components/ui/activity_removed.cljs
@@ -1,26 +1,36 @@
 (ns oc.web.components.ui.activity-removed
   (:require [rum.core :as rum]
-            [org.martinklepsch.derivatives :as drv]
             [oc.web.urls :as oc-urls]
             [oc.web.router :as router]
             [oc.web.dispatcher :as dis]
-            [oc.web.lib.utils :as utils]))
+            [oc.web.mixins.ui :refer (no-scroll-mixin)]))
 
-(rum/defcs activity-removed < rum/static
-  [s]
+(defn- close-clicked []
+  (router/nav! (oc-urls/all-posts)))
+
+(rum/defc activity-removed < rum/static
+                      no-scroll-mixin
+  []
   [:div.activity-removed-container
-    [:div.activity-removed-wrapper
-      [:div.activity-removed-left
-        [:div.activity-removed-logo]
-        [:div.activity-removed-box]]
-      [:div.activity-removed-right
-        [:div.activity-removed-right-content
-          [:div.info-icon]
-          [:div.content-title
-            "Post Not Available"]
-          [:div.content-description
-            "Looks like you don't have access to the post or it doesn't exist."]
-          [:button.mlb-reset.go-to-homepage-btn
-            {:aria-label "Homepage"
-             :on-click #(router/nav! (utils/your-digest-url))}
-            "Go to homepage"]]]]])
+
+    [:div.activity-removed
+
+      [:button.settings-modal-close.mlb-reset
+        {:on-click #(close-clicked)}]
+
+      [:div.activity-removed-mobile-header
+        [:button.mlb-reset.activity-removed-mobile-close
+          {:on-click #(close-clicked)}]
+        [:div.activity-removed-mobile-header-logo]]
+
+      [:div.activity-removed-icon]
+
+      [:div.activity-removed-title
+        "Post not available"]
+
+      [:div.activity-removed-description
+        "Looks like you don’t have access to the post or it doesn’t exist."]
+
+      [:button.mlb-reset.activity-removed-button
+        {:on-click #(close-clicked)}
+        "Return to all posts"]]])
