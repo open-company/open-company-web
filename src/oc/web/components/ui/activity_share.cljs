8e1938abcd911864137040bad560560103a8ce34
bago2k4 <iacopo@rc0.eu>
2019-01-15 18:05:57 +0100

    Add quotes to the tooltip.

--- src/oc/web/components/ui/activity_share.cljs
+++ src/oc/web/components/ui/activity_share.cljs
@@ -136,7 +136,7 @@
                :data-container "body"
                :data-tooltip (if has-bot? "" "tooltip")
                :data-delay "{\"show\":\"500\", \"hide\":\"0\"}"
-               :title "Team admins can enable the Carrot bot for Slack in Settings"
+               :title "Team admins can enable the ’Carrot bot for Slack’ in Settings"
                :ref "slack-button"
                :on-click (fn [e]
                            (utils/event-stop e)
