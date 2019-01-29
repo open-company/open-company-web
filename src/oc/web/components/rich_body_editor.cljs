80db104b3608c7dd8cba1c78ef6c7289ae542eb0
bago2k4 <iacopo@rc0.eu>
2019-01-29 22:54:23 +0100

    Remove also all style and javascript tags from copy and pasted code in MediumEditor.

--- src/oc/web/components/rich_body_editor.cljs
+++ src/oc/web/components/rich_body_editor.cljs
@@ -307,7 +307,7 @@
                  :paste #js {:forcePlainText false
                              :cleanPastedHTML true
                              :cleanAttrs #js ["style" "alt" "dir" "size" "face" "color" "itemprop" "name" "id"]
-                             :cleanTags #js ["meta" "video" "audio" "img" "button" "svg" "canvas" "figure" "input" "textarea"]
+                             :cleanTags #js ["meta" "video" "audio" "img" "button" "svg" "canvas" "figure" "input" "textarea" "style" "javascript"]
                              :unwrapTags (clj->js (remove nil? ["div" "label" "font" "h1"
                                                    (when-not show-subtitle "h2") "h3" "h4" "h5"
                                                    "h6" "strong" "section" "time" "em" "main" "u" "form" "header" "footer"
