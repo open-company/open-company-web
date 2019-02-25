00d4c9ba5f18fd9ce6bdb63e70ab3032b9426eab
bago2k4 <iacopo@rc0.eu>
2019-02-25 11:47:49 +0100

    Hold on the initial invites count to know if it needs to use the plural in the notification.

--- src/oc/web/components/ui/org_settings_invite_panel.cljs
+++ src/oc/web/components/ui/org_settings_invite_panel.cljs
@@ -79,6 +79,7 @@
     (rum/local (int (rand 10000)) ::rand)
     (rum/local "Send" ::send-bt-cta)
     (rum/local 0 ::sending)
+    (rum/local 0 ::initial-sending)
     {:will-mount (fn [s]
                    (setup-initial-rows s)
                    (nux-actions/dismiss-post-added-tooltip)
@@ -89,22 +90,25 @@
                         (.tooltip "hide"))
                      s)
      :will-update (fn [s]
-                   (let [sending @(::sending s)]
-                     (when (pos? sending)
+                   (let [sending (::sending s)
+                         initial-sending (::initial-sending s)]
+                     (when (pos? @sending)
                        (let [invite-drv @(drv/get-ref s :invite-data)
                              no-error-invites (filter #(not (:error %)) (:invite-users invite-drv))
-                             error-invites (filter :error (:invite-users invite-drv))]
-                         (reset! (::sending s) (count no-error-invites))
+                             error-invites (filter :error (:invite-users invite-drv))
+                             hold-initial-sending @initial-sending]
+                         (reset! sending (count no-error-invites))
                          (when (zero? (count no-error-invites))
                            (utils/after 1000
                              (fn []
-                               (reset! (::sending s) 0)
+                               (reset! sending 0)
+                               (reset! initial-sending 0)
                                (if (zero? (count error-invites))
                                  (do
                                    (reset! (::send-bt-cta s) "Sent")
                                    (utils/after 2500 #(reset! (::send-bt-cta s) "Send"))
                                    (notification-actions/show-notification {:title (str "Invite"
-                                                                                    (when (> (count no-error-invites) 1)
+                                                                                    (when (> hold-initial-sending 1)
                                                                                       "s")
                                                                                     " sent.")
                                                                             :primary-bt-title "OK"
@@ -243,8 +247,9 @@
       ;; Save and cancel buttons
       [:div.org-settings-footer.group
         [:button.mlb-reset.mlb-default.save-btn
-          {:on-click #(do
-                        (reset! (::sending s) (count (filterv valid-user? invite-users)))
+          {:on-click #(let [valid-count (count (filterv valid-user? invite-users))]
+                        (reset! (::sending s) valid-count)
+                        (reset! (::initial-sending s) valid-count)
                         (reset! (::send-bt-cta s) "Sending")
                         (team-actions/invite-users (:invite-users @(drv/get-ref s :invite-data))))
            :class (when (= "Sent" @(::send-bt-cta s)) "no-disable")
