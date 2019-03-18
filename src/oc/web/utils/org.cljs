05e92e6ce0a8b8a91132fbc403426a6022918d4d
bago2k4 <iacopo@rc0.eu>
2019-03-18 15:31:43 +0100

    Strip out www. http:// and https:// from email domains automatically before performing the pre flight check.

--- src/oc/web/utils/org.cljs
+++ src/oc/web/utils/org.cljs
@@ -5,3 +5,11 @@
    :fromSources ["local_file_system"]})
 
 (def org-name-max-length 64)
+
+(defn clean-email-domain [email-domain]
+  (when email-domain
+    (-> email-domain
+     (subs (if (.startsWith email-domain "@") 1 0))
+     (subs (if (.startsWith email-domain "http://") 7 0))
+     (subs (if (.startsWith email-domain "https://") 8 0))
+     (subs (if (.startsWith email-domain "www.") 4 0)))))
