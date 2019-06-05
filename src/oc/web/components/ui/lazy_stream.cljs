075f280294641c53a265a66768f9d9a68befd90d
bago2k4 <iacopo@rc0.eu>
2019-06-05 14:39:50 +0200

    New lazy-stream component used to show an interstitial screen while loading a board/AP.

---
+++ src/oc/web/components/ui/lazy_stream.cljs
@@ -0,0 +1,15 @@
+(ns oc.web.components.ui.lazy-stream
+  (:require [rum.core :as rum]
+            [oc.web.lib.utils :as utils]))
+
+(rum/defcs lazy-stream < rum/static
+                         (rum/local false ::mounted)
+                         {:did-mount (fn [s]
+                           (js/console.log "DBG lazy-stream/did-mount")
+                           (utils/after 180 #(reset! (::mounted s) true))
+                           s)}
+  [s stream-comp]
+  [:div.lazy-stream
+    (if @(::mounted s)
+      (stream-comp)
+      [:div.lazy-stream-interstitial])])
