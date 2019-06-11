fbadc7e27fa522beddc645b1e61de9999f95209d
bago2k4 <iacopo@rc0.eu>
2019-06-11 21:52:10 +0200

    Update or remove the :last-read-at value in read data when marking a post as read/unread to make sure the NEW tag is updated.

--- src/oc/web/stores/activity.cljs
+++ src/oc/web/stores/activity.cljs
@@ -437,9 +437,11 @@
         section-change-key (vec (concat (dispatcher/change-data-key org-slug) [board-uuid :unread]))
         activity-key (dispatcher/activity-key org-slug activity-uuid)
         next-activity-data (assoc (get-in db activity-key) :unread true)
-        temp-val (get-in db section-change-key)]
+        temp-val (get-in db section-change-key)
+        activity-read-key (conj activities-read-key activity-uuid)]
     (-> db
       (update-in section-change-key #(vec (conj (or % []) activity-uuid)))
+      (update-in activity-read-key merge {:last-read-at nil})
       (assoc-in activity-key next-activity-data))))
 
 (defmethod dispatcher/action :mark-read
@@ -449,7 +451,9 @@
         section-change-key (vec (concat (dispatcher/change-data-key org-slug) [board-uuid :unread]))
         activity-key (dispatcher/activity-key org-slug activity-uuid)
         next-activity-data (assoc (get-in db activity-key) :unread false)
-        temp-val (get-in db section-change-key)]
+        temp-val (get-in db section-change-key)
+        activity-read-key (conj activities-read-key activity-uuid)]
     (-> db
       (update-in section-change-key (fn [unreads] (filterv #(not= % activity-uuid) (or unreads []))))
+      (update-in activity-read-key merge {:last-read-at (utils/as-of-now)})
       (assoc-in activity-key next-activity-data))))
