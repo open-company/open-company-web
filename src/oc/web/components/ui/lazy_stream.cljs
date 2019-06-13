fbbf9aa1e953f05544fba50de3e46f3739e0e2b7
bago2k4 <iacopo@rc0.eu>
2019-06-14 01:21:56 +0200

    Try out the shimmer animation instead of the static loading images.

--- src/oc/web/components/ui/lazy_stream.cljs
+++ src/oc/web/components/ui/lazy_stream.cljs
@@ -2,6 +2,15 @@
   (:require [rum.core :as rum]
             [oc.web.lib.utils :as utils]))
 
+(defn- get-interstitials []
+  (vec (repeat 5
+      [:div.lazy-stream-interstitial
+        [:div.lazy-stream-header.animate]
+        [:div.lazy-stream-headline.animate]
+        [:div.lazy-stream-body.animate]
+        [:div.lazy-stream-body-2.animate]
+        [:div.lazy-stream-footer.animate]])))
+
 (rum/defcs lazy-stream < rum/static
                          (rum/local false ::mounted)
                          {:did-mount (fn [s]
@@ -9,6 +18,7 @@
                            s)}
   [s stream-comp]
   [:div.lazy-stream
-    (if @(::mounted s)
-      (stream-comp)
-      [:div.lazy-stream-interstitial])])
+    ; (if @(::mounted s)
+    ;   (stream-comp)
+    ;   [:div.lazy-stream-interstitial])
+    (assoc (get-interstitials) 0 :div.lazy-stream-inner)])
