0c6d57fb061cd6cbc4b2cdcd5b6e071888b2a278
bago2k4 <iacopo@rc0.eu>
2019-06-14 16:58:34 +0200

    Hide the scroll back to the post in the stream when closing an expanded post.

--- src/oc/web/components/ui/lazy_stream.cljs
+++ src/oc/web/components/ui/lazy_stream.cljs
@@ -5,18 +5,16 @@
 
 (rum/defcs lazy-stream < rum/static
                          (rum/local false ::mounted)
-                         (rum/local false ::scrolled)
                          {:did-mount (fn [s]
+                           (utils/scroll-to-y (:scroll-y @router/path) 0)
                            (utils/after 180 #(reset! (::mounted s) true))
-                           s)
-                          :after-render (fn [s]
-                           (when (and (not @(::scrolled s))
-                                      @(::mounted s))
-                             (reset! (::scrolled s) true)
-                             (utils/scroll-to-y (:scroll-y @router/path) 0))
                            s)}
   [s stream-comp]
   [:div.lazy-stream
     (if @(::mounted s)
       (stream-comp)
-      [:div.lazy-stream-interstitial])])
+      [:div.lazy-stream-interstitial
+        {:style {:height (str (+ (:scroll-y @router/path)
+                                 (or (.. js/document -documentElement -clientHeight)
+                                     (.-innerHeight js/window)))
+                           "px")}}])])
