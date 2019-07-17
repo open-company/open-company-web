4bdc5928aa0e1dd14112e168683966855f166a08
bago2k4 <iacopo@rc0.eu>
2019-07-17 14:29:44 +0200

    Fix position of giphy-picker and media-video-modal components.

--- src/oc/web/components/ui/giphy_picker.cljs
+++ src/oc/web/components/ui/giphy_picker.cljs
@@ -1,51 +1,40 @@
 (ns oc.web.components.ui.giphy-picker
   (:require [rum.core :as rum]
-            [oops.core :refer (oget+)]
             [dommy.core :refer-macros (sel1)]
             [oc.web.local-settings :as ls]
             [oc.web.utils.dom :as dom-utils]
             [oc.web.lib.react-utils :as react-utils]))
 
-(def fullscreen-giphy-picker-max-height 408)
-(def default-giphy-picker-max-height 320)
+(def giphy-picker-max-height 408)
 
 (rum/defcs giphy-picker < (rum/local 0 ::offset-top)
-                        {:will-mount (fn [s]
-
-                          (when-let [picker-el (sel1 [:div.medium-editor-media-picker])]
-                            (reset! (::offset-top s) (.-offsetTop picker-el)))
-                         s)}
+                          {:will-mount (fn [s]
+                            (when-let [picker-el (sel1 [:div.medium-editor-media-picker])]
+                              (js/console.log "DBG media-picker offsetTop" (.-offsetTop picker-el))
+                              (reset! (::offset-top s) (.-offsetTop picker-el)))
+                           s)}
   [s {:keys [fullscreen pick-emoji-cb]}]
-  (let [scrolling-element-selector (if fullscreen :div.cmail-content-outer :div.cmail-content)
-        scrolling-element (sel1 [scrolling-element-selector])
+  (let [scrolling-element (if fullscreen (sel1 [:div.cmail-content-outer]) (.-scrollingElement js/document))
         win-height (or (.-clientHeight (.-documentElement js/document))
                        (.-innerHeight js/window))
-        fixed-win-height (if fullscreen win-height (- win-height 52)) ;; Remove bottom padding of Cmail view
-        top-offset-limit (when-not fullscreen (.-offsetTop (sel1 [:div.rich-body-editor-outer-container])))
-        top-limit (if fullscreen
-                    0
-                    ;; On Cmail the top limit is not 0 but it's the dark bar, so let's
-                    ;; calculate the distance btw the rich body editor and the top of the bar
-                    ;; and remove the bar height
-                    (+ (* -1 top-offset-limit)
-                       56))
-        offset-top (if fullscreen
-                    286
-                    ;; On cmail let's get the distance of Cmail from the top of the page
-                    ;; and add the distance between the rich-body-editr container
-                    ;; and the first fixed element (that is the limit to calculate the absolute offset)
-                    (+ (oget+ (.offset (js/$ (sel1 [:div.cmail-outer]))) :top)
-                       (.-offsetTop(sel1 [:div.rich-body-editor-outer-container]))))
+        top-offset-limit (.-offsetTop (sel1 [:div.rich-body-editor-outer-container]))
         ; offset-height (.-scrollHeight scrolling-element)
         scroll-top (.-scrollTop scrolling-element)
         top-position (max 0 @(::offset-top s))
         relative-position (+ top-position
-                             offset-top
+                             top-offset-limit
                              (* scroll-top -1)
-                             (if fullscreen fullscreen-giphy-picker-max-height default-giphy-picker-max-height))
-        adjusted-position (if (> relative-position fixed-win-height) ;; 286 is the top offset of the body
-                            (max top-limit (- top-position (- relative-position fixed-win-height) 16))
+                             giphy-picker-max-height)
+        adjusted-position (if (> relative-position win-height) ;; 286 is the top offset of the body
+                            (max 0 (- top-position (- relative-position win-height) 16))
                             top-position)]
+    (js/console.log "DBG giphy-picker/render win-height" win-height)
+    (js/console.log "DBG    top-offset-limit" top-offset-limit)
+    (js/console.log "DBG    scroll-top" scroll-top)
+    (js/console.log "DBG    top-position" top-position)
+    (js/console.log "DBG    relative" relative-position)
+    (js/console.log "DBG    adjusted-position" adjusted-position)
+
     [:div.giphy-picker
       {:style {:top (str adjusted-position "px")}}
       (react-utils/build (.-Selector js/ReactGiphySelector)
