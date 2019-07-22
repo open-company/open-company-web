16ee68c5c45de347cd823fffd29c69926d872efb
bago2k4 <iacopo@rc0.eu>
2019-07-22 15:34:29 +0200

    Do not call again the setup function to avoid double click events.

--- src/oc/web/components/home_page.cljs
+++ src/oc/web/components/home_page.cljs
@@ -13,11 +13,8 @@
             [oc.web.components.ui.site-mobile-menu :refer (site-mobile-menu)]
             [oc.web.components.ui.login-overlay :refer (login-overlays-handler)]))
 
-(rum/defcs home-page < {:did-mount (fn [s]
-                                    (when (exists? (.-OCWebSetupStaticPagesJS js/window))
-                                      (js/OCWebSetupStaticPagesJS))
-                                    s)}
-  [s]
+(rum/defc home-page
+  []
   [:div
     (site-header)
     (site-mobile-menu)
