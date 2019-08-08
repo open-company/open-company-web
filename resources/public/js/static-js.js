b4fb90a453249a60c4b2b598f689be45ab0fa2b1
bago2k4 <iacopo@rc0.eu>
2019-08-08 22:26:21 +0200

    Fix iPhone X, Xs, Xr and Xs Max detection.

--- resources/public/js/static-js.js
+++ resources/public/js/static-js.js
@@ -426,10 +426,33 @@ function OCStaticTextareaRestoreSelection(range) {
 }
 
 function isiPhoneWithoutPhysicalHomeBt(){
-  return (isSafari() &&
-          WURFL &&
-          (WURFL.complete_device_name === "Apple iPhone XR" ||
-           WURFL.complete_device_name === "Apple iPhone XS Max" ||
-           WURFL.complete_device_name === "Apple iPhone XS" ||
-           WURFL.complete_device_name === "Apple iPhone X"))
+  if (isSafari()) {
+    // Really basic check for the ios platform
+    // https://stackoverflow.com/questions/9038625/detect-if-device-is-ios
+    var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+
+    // Get the device pixel ratio
+    var ratio = window.devicePixelRatio || 1;
+
+    // Define the users device screen dimensions
+    var screen = {
+      width : window.screen.width * ratio,
+      height : window.screen.height * ratio
+    };
+
+    // iPhone X and Xs Detection
+    if (iOS && screen.width === 1125 && screen.height === 2436) {
+      return true;
+    }
+    // iPhone Xr Detection
+    if (iOS && screen.width === 828 && screen.height === 1792) {
+      return true;
+    }
+
+    // iPhone Xs Max Detection
+    if (iOS && screen.width === 1242 && screen.height === 2688) {
+      return true;
+    }
+  }
+  return false;
 }
