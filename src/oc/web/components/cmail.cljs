eb56f675c1a19344a1ecd87e1dc4a2bdb43a1257
bago2k4 <iacopo@rc0.eu>
2019-08-09 18:39:36 +0200

    Typo. Fix follow-ups blue bar check.

--- src/oc/web/components/cmail.cljs
+++ src/oc/web/components/cmail.cljs
@@ -497,10 +497,10 @@
                     (cancel-clicked s)
                     (cmail-actions/cmail-hide)))
         current-user-id (jwt/user-id)
-        follow-up? (and ;; if ther is at least a follow-up
+        follow-up? (and ;; if there is at least a follow-up
                         (seq (:follow-ups cmail-data))
                         ;; That wasn't created by the owner
-                        (every? #(= (-> % :assignee :user-id) (-> % :author :user-id)) (:follow-ups cmail-data)))
+                        (some? #(not= (-> % :assignee :user-id) (-> % :author :user-id)) (:follow-ups cmail-data)))
         can-toggle-follow-ups? (every? #(and (not (:completed? %))
                                              (or (not (:author %))
                                                  (= (-> % :author :user-id) (jwt/user-id))))
