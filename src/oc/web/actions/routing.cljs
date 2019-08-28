a65edafc50b089f2c882451c0373762d1e465b33
bago2k4 <iacopo@rc0.eu>
2019-08-28 14:39:25 +0200

    Use history.back() to go back from an expanded post, adding the onpopstate listener make so the same action will be triggered if the user clicks the browser back button.

--- src/oc/web/actions/routing.cljs
+++ src/oc/web/actions/routing.cljs
@@ -22,7 +22,26 @@
 
 ;; Post modal
 
-(defn open-post-modal [activity-data dont-scroll]
+(defn close-post-modal
+  "The state has been popped, reset the routing so the app re-render the stream.
+   Reset the scroll to the previous position if one is saved in the current route object."
+  []
+  (let [org (router/current-org-slug)
+        board (or (:back-to @router/path) (router/current-board-slug))
+        query-params (router/query-params)
+        route [org board "dashboard"]
+        back-y (or (:back-y @router/path) (utils/page-scroll-top))]
+    (router/set-route! route {:org org
+                              :board board
+                              :sort-type (router/current-sort-type)
+                              :scroll-y back-y
+                              :query-params query-params})
+    (set! (.-onpopstate js/window) nil)))
+
+(defn open-post-modal
+  "Expand a post, reset the route to add the post in it and add the onpopstate listener.
+   Also reset the scroll."
+  [activity-data dont-scroll]
   (let [org (router/current-org-slug)
         old-board (router/current-board-slug)
         board (:board-slug activity-data)
@@ -46,18 +65,13 @@
       (if ua/mobile?
         (utils/after 10 #(utils/scroll-to-y 0 0))
         (utils/scroll-to-y 0 0)))
+    ;; Set the onpopstate listener to go back to the board not only
+    ;; with the internal button but also with the browser back button
+    (set! (.-onpopstate js/window) close-post-modal)
     (.pushState (.-history js/window) #js {} (.-title js/document) post-url)))
 
-(defn dismiss-post-modal []
-  (let [org (router/current-org-slug)
-        board (or (:back-to @router/path) (router/current-board-slug))
-        to-url (oc-urls/board board)
-        query-params (router/query-params)
-        route [org board "dashboard"]
-        back-y (or (:back-y @router/path) (utils/page-scroll-top))]
-    (router/set-route! route {:org org
-                              :board board
-                              :sort-type (router/current-sort-type)
-                              :scroll-y back-y
-                              :query-params query-params})
-    (.pushState (.-history js/window) #js {} (.-title js/document) to-url)))
+(defn dismiss-post-modal
+  "Function used by the button to close the expanded post modal."
+  []
+  ;; Simply call history.back() then the onpopstate will be called automatically
+  (.back (.-history js/window)))
