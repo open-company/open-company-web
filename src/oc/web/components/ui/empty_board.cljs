0b68054f3fb2116e528d11a506409eecbbea68b6
bago2k4 <iacopo@rc0.eu>
2019-09-09 20:42:31 +0200

    Remove second line of copy on empty state for follow-ups and drafts.

--- src/oc/web/components/ui/empty_board.cljs
+++ src/oc/web/components/ui/empty_board.cljs
@@ -22,28 +22,23 @@
         board-data (drv/react s :board-data)
         editable-boards (drv/react s :editable-boards)
         is-all-posts? (= (router/current-board-slug) "all-posts")
-        is-must-see? (= (router/current-board-slug) "must-see")
         is-follow-ups? (= (router/current-board-slug) "follow-ups")
         is-drafts-board? (= (router/current-board-slug) utils/default-drafts-board-slug)
         is-author? (utils/is-admin-or-author? org-data board-data)]
     [:div.empty-board.group
       [:div.empty-board-grey-box
         [:div.empty-board-illustration
-          {:class (utils/class-set {:ap-section (and (not is-must-see?) (not is-drafts-board?))
-                                    :must-see is-must-see?
-                                    :drafts is-drafts-board?
+          {:class (utils/class-set {:drafts is-drafts-board?
                                     :follow-ups is-follow-ups?})}]
         [:div.empty-board-title
           (cond
            ; is-all-posts? "Stay up to date"
-           is-must-see? "Highlight what's important"
            is-drafts-board? "Nothing in drafts"
            is-follow-ups? "You’re all caught up!"
            :else (if-not (:read-only board-data) "Create a post to get started" "There's nothing to see here"))]
-        [:div.empty-board-subtitle
-          (cond
-           is-all-posts? "All posts is a stream of what’s new in Carrot."
-           is-must-see? "When someone marks a post as “must see” everyone will see it here."
-           is-follow-ups? "Posts that require action or that you'd like to read later will show up here."
-           is-drafts-board? "Keep a private draft until you're ready to share it with your team."
-           :else (when is-author? (str "Looks like there aren’t any posts in " (:name board-data) ".")))]]]))
+        (when (and (not is-drafts-board?)
+                   (not is-follow-ups?))
+          [:div.empty-board-subtitle
+            (cond
+             is-all-posts? "All posts is a stream of what’s new in Carrot."
+             :else (when is-author? (str "Looks like there aren’t any posts in " (:name board-data) ".")))])]]))
