239a90a5c720fefafb536fe888833b4a463554a9
bago2k4 <iacopo@rc0.eu>
2019-09-17 14:05:06 +0200

    Pricing page.

--- site/oc/pages/pricing.clj
+++ site/oc/pages/pricing.clj
@@ -16,74 +16,89 @@
 
       (shared/dashed-string 2)
 
-      [:section.pricing-faq
-        [:h2.faq-header
-          "Frequently asked questions."]
-
-        [:div.faq-row
-          [:div.faq-row-question
-            "Can I use Carrot for free?"]
-          [:div.faq-row-answer
-            (str
-             "Absolutely! Carrot can be used for free. The Free plan "
-             "has a storage limit, and doesn't include all of the premium features. "
-             "It’s fast to sign up, and no credit card is required.")]]
-
-        [:div.faq-row
-          [:div.faq-row-question
-            "Do I need a credit card to sign up?"]
-          [:div.faq-row-answer
-            "No! You can use Carrot right away without a credit card."]]
-
-        [:div.faq-row
-          [:div.faq-row-question
-            "What happens if we go over our storage limit in the Free plan?"]
-          [:div.faq-row-answer
-            (str
-             "You can still read, write, edit, and organize existing content, but you won't be able "
-             "to add new attachments and videos.")]]
-
-        [:div.faq-row
-          [:div.faq-row-question
-            "How is the payment being processed?"]
-          [:div.faq-row-answer
-            (str
-             "We use Stripe to process your payment. It's the same payment provider used in "
-             "products such as Slack, Pinterest, and Lyft. We do not handle your credit card "
-             "information directly.")]]
-
-        [:div.faq-row
-          [:div.faq-row-question
-            "Are discounts available for nonprofits?"]
-          [:div.faq-row-answer
-            "Yes! We offer eligible nonprofit organizations a 50% discount. "
-            [:a
-              {:class "intercom-chat-link"
-               :href "mailto:zcwtlybw@carrot-test-28eb3360a1a3.intercom-mail.com"}
-              "Contact us"]
-            " to see if your organization is eligible."]]
-
-        [:div.faq-row
-          [:div.faq-row-question
-            "Are custom plans available?"]
-          [:div.faq-row-answer
-            (str
-             "Absolutely! Our Enterprise plan is 100% flexible and priced based on your "
-             "unique needs. Please contact us to discuss further details.")]]
-
-        [:div.faq-row
-          [:div.faq-row-question
-            "Still have more questions?"]
-          [:div.faq-row-answer
-            [:a.chat-with-us
-              {:class "intercom-chat-link"
-               :href "mailto:zcwtlybw@carrot-test-28eb3360a1a3.intercom-mail.com"}
-              "Get in touch with us"]]]]
+      [:section.pricing-apps
+        [:div.pricing-apps-block.group
+          [:div.pricing-apps-app-block
+            [:div.pricing-apps-app-header
+              "Web app"]
+            [:img.pricing-apps-app-screenshot
+              {:src (shared/cdn "/img/ML/pricing_apps_signup.png")
+               :srcSet (str (shared/cdn "/img/ML/pricing_apps_signup@2x.png") " 2x")}]
+            [:div.pricing-apps-app-links
+              [:a
+                {:href "/sign-up"}
+                "Start your free trial"]]]
+          [:div.pricing-apps-separator]
+          [:div.pricing-apps-app-block
+            [:div.pricing-apps-app-header
+              "Desktop app"]
+            [:img.pricing-apps-app-screenshot
+              {:src (shared/cdn "/img/ML/pricing_apps_desktop.png")
+               :srcSet (str (shared/cdn "/img/ML/pricing_apps_desktop@2x.png") " 2x")}]
+            [:div.pricing-apps-app-links
+              [:a.mac-icon
+                {:href "/apps/mac"}
+                "Mac"]
+              [:a.win-icon
+                {:href "/apps/windows"}
+                "Windows"]]]
+          [:div.pricing-apps-separator]
+          [:div.pricing-apps-app-block
+            [:div.pricing-apps-app-header
+              "Mobile app"]
+            [:img.pricing-apps-app-screenshot
+              {:src (shared/cdn "/img/ML/pricing_apps_mobile.png")
+               :srcSet (str (shared/cdn "/img/ML/pricing_apps_mobile@2x.png") " 2x")}]
+            [:div.pricing-apps-app-links
+              [:a
+                {:href "/apps/ios"}
+                "iOS"]
+              " & "
+              [:a
+                {:href "/apps/android"}
+                "Android"]]]]]
 
       (shared/dashed-string 3)
 
       (shared/testimonial-block :blend-labs)
 
       (shared/dashed-string 4)
+
+      [:section.pricing-faq
+        [:div.pricing-faq-block
+          [:h1.pricing-faq-header
+            "Frequently asked questions."]
+          [:div.pricing-faq-question-block
+            [:div.pricing-faq-question
+              "Do I need a credit card to sign up?"]
+            [:div.pricing-faq-answer
+              "No! You can use Carrot right away without a credit card."]]
+          [:div.pricing-faq-question-block
+            [:div.pricing-faq-question
+              "How is the payment being processed?"]
+            [:div.pricing-faq-answer
+              (str
+               "We use Stripe to process your payment. It's the same payment "
+               "provider used in products such as Slack, Pinterest, and Lyft. "
+               "We do not handle your credit card information directly.")]]
+          [:div.pricing-faq-question-block
+            [:div.pricing-faq-question
+              "Are discounts available for nonprofits?"]
+            [:div.pricing-faq-answer
+              (str
+               "Yes! We offer eligible nonprofit organizations a 50% discount. "
+               "Contact us to see if your organization is eligible.")]]
+          [:div.pricing-faq-question-block
+            [:div.pricing-faq-question
+              "Still have questions?"]
+            [:div.pricing-faq-answer
+              [:a.chat-with-us
+                {:class "intercom-chat-link"
+                 :href "mailto:zcwtlybw@carrot-test-28eb3360a1a3.intercom-mail.com"}
+                "Chat with us about Carrot"]]]]]
+
+      (shared/dashed-string 5)
+
+      shared/pricing-table-section
 
     ]])
