09aa501006006e7dad6d2a3303c01ab65404cfec
Sean Johnson <sean@snootymonkey.com>
2019-09-26 11:16:31 -0400

    Add info to Intercom data.

--- src/oc/web/lib/chat.cljs
+++ src/oc/web/lib/chat.cljs
@@ -1,12 +1,28 @@
 (ns oc.web.lib.chat
-  (:require [oc.web.lib.jwt :as jwt]
+  (:require [oc.web.dispatcher :as dispatcher]
+            [oc.web.lib.jwt :as jwt]
             [oc.web.lib.utils :as utils]
             [taoensso.timbre :as timbre]))
 
 (defn identify []
   (when (jwt/get-key :email)
     (timbre/info "Identify user to Intercom")
-    (utils/after 1 #(.Intercom js/window "update"
-                        (clj->js {:user_id (jwt/user-id)
-                                  :name (jwt/get-key :name)
-                                  :email (jwt/get-key :email)})))))
+    (let [user (dispatcher/current-user-data)
+          org (dispatcher/org-data)]
+      (utils/after 1 #(.Intercom js/window "update"
+                          (clj->js {; Intercom standard
+                                    :user_id (:user-id user)
+                                    :created_at (:created-at user)
+                                    :name (jwt/get-key :name)
+                                    :email (:email user)
+                                    :avatar {
+                                      :type :avatar
+                                      :image_url (:avatar-url (:avatar-url user))
+                                    }
+                                    :company {
+                                      :id (:slug org)
+                                      :name (:name org)
+                                      :created_at (:created-at org)
+                                     }
+                                    ; custom
+                                    :timezone (:timezone user)}))))))
