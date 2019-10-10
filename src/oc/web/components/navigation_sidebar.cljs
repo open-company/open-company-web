c1f67eca5af01bd10e1c3eac2109f1a0315c5a66
bago2k4 <iacopo@rc0.eu>
2019-10-10 18:34:50 +0200

    Make all labels bold when they are selected, unbold AP when there are no unread posts and it's not selected.

--- src/oc/web/components/navigation_sidebar.cljs
+++ src/oc/web/components/navigation_sidebar.cljs
@@ -1,5 +1,6 @@
 (ns oc.web.components.navigation-sidebar
   (:require [rum.core :as rum]
+            [clojure.string :as s]
             [org.martinklepsch.derivatives :as drv]
             [oc.web.lib.jwt :as jwt]
             [oc.web.urls :as oc-urls]
@@ -100,6 +101,7 @@
   (let [org-data (drv/react s :org-data)
         board-data (drv/react s :board-data)
         change-data (drv/react s :change-data)
+        filtered-change-data (into {} (filter #(-> % first (s/starts-with? "0000-0000-0000-") not) change-data))
         current-user-data (drv/react s :current-user-data)
         left-navigation-sidebar-width (- responsive/left-navigation-sidebar-width 20)
         all-boards (:boards org-data)
@@ -148,7 +150,7 @@
              :on-click #(nav-actions/nav-to-url! % "all-posts" (oc-urls/all-posts))}
             [:div.all-posts-icon]
             [:div.all-posts-label
-              {:class (utils/class-set {:new (seq (apply concat (map :unread (vals change-data))))})}
+              {:class (utils/class-set {:new (seq (apply concat (map :unread (vals filtered-change-data))))})}
               "All posts"]])
         (when show-follow-ups
           [:a.follow-ups.hover-item.group
