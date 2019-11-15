9cc3f6126fe5fbecaee170c85bc2a8455d5df536
bago2k4 <iacopo@rc0.eu>
2019-11-15 18:33:57 +0100

    Fix add Slack button in invite picker view.

--- src/oc/web/components/invite_picker_modal.cljs
+++ src/oc/web/components/invite_picker_modal.cljs
@@ -1,6 +1,7 @@
 (ns oc.web.components.invite-picker-modal
   (:require [rum.core :as rum]
             [org.martinklepsch.derivatives :as drv]
+            [oc.web.router :as router]
             [oc.web.actions.org :as org-actions]
             [oc.web.actions.team :as team-actions]
             [oc.web.actions.nav-sidebar :as nav-actions]))
@@ -10,14 +11,16 @@
   ;; Mixins
   rum/reactive
   (drv/drv :org-data)
-  (drv/drv :team-data) 
+  (drv/drv :team-data)
+  (drv/drv :current-user-data)
   {:will-mount (fn [s]
     (let [org-data @(drv/get-ref s :org-data)]
       (org-actions/get-org org-data true)
       (team-actions/teams-get))
     s)}
   [s]
-  (let [team-data (drv/react s :team-data)]
+  (let [team-data (drv/react s :team-data)
+        current-user-data (drv/react s :current-user-data)]
     [:div.invite-picker-modal
       [:button.mlb-reset.modal-close-bt
         {:on-click nav-actions/close-all-panels}]
@@ -40,6 +43,6 @@
               {:on-click #(nav-actions/show-org-settings :invite-slack)}
               "Invite via Slack"]
             [:button.mlb-reset.invite-slack-bt
-              {:on-click #(nav-actions/show-org-settings :invite-slack)}
+              {:on-click #(team-actions/slack-team-add current-user-data (str (router/get-token) "?org-settings=invite-slack"))}
               [:span.disabled "Invite via Slack"]
               [:span.enabled "(add Slack)"]])]]]))
