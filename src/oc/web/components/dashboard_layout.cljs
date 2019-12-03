35ab976ee310536bf44c62aa3617919121ad2cec
bago2k4 <iacopo@rc0.eu>
2019-12-03 21:57:08 +0100

    Use What's New instead of New as title.

--- src/oc/web/components/dashboard_layout.cljs
+++ src/oc/web/components/dashboard_layout.cljs
@@ -228,7 +228,7 @@
                                                   :public (= (:access current-board-data) "public")})
                          :dangerouslySetInnerHTML (utils/emojify (cond
                                                    is-inbox
-                                                   "New"
+                                                   "What's New"
 
                                                    is-all-posts
                                                    "All posts"
