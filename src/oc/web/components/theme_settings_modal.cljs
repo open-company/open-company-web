a3a06d0ab772767db61311d1015beeca257f303b
bago2k4 <iacopo@rc0.eu>
2020-01-30 19:41:21 +0100

    Remove third option box, go back to checkbox with adjustments from Stuart.

--- src/oc/web/components/theme_settings_modal.cljs
+++ src/oc/web/components/theme_settings_modal.cljs
@@ -1,7 +1,6 @@
 (ns oc.web.components.theme-settings-modal
   (:require [rum.core :as rum]
             [org.martinklepsch.derivatives :as drv]
-            [oc.web.lib.utils :as utils]
             [oc.web.actions.ui-theme :as theme-actions]
             [oc.web.actions.nav-sidebar :as nav-actions]
             [oc.web.components.ui.carrot-checkbox :refer (carrot-checkbox)]
@@ -22,44 +21,35 @@
             {:on-click #(nav-actions/hide-theme-settings)}
             "Back"]]
         [:div.theme-settings-body
-          ; (when (theme-actions/support-system-dark-mode?)
-          ;   [:div.theme-settings-auto-container
-          ;     (carrot-checkbox {:selected (= setting-value :auto)
-          ;                       :did-change-cb #(theme-actions/set-ui-theme (if % :auto computed-value))})
-          ;     [:span.auto-label
-          ;       {:on-click #(theme-actions/set-ui-theme (if (= setting-value :auto) computed-value :auto))}
-          ;       "Sync with OS settings"]])
+          (when (theme-actions/support-system-dark-mode?)
+            [:div.theme-settings-auto-container
+              (carrot-checkbox {:selected (= setting-value :auto)
+                                :did-change-cb #(theme-actions/set-ui-theme (if % :auto computed-value))})
+              [:span.auto-label
+                {:on-click #(theme-actions/set-ui-theme (if (= setting-value :auto) computed-value :auto))}
+                "Sync with OS settings"]])
           [:div.theme-settings-rows
-            (when (theme-actions/support-system-dark-mode?)
-              [:button.mlb-reset.theme-settings-row.auto-theme
-                {:class (utils/class-set {:active (= setting-value :auto)
-                                          :light-icon (or (and (= setting-value :auto)
-                                                               (= computed-value :light))
-                                                          (not (theme-actions/system-ui-theme-enabled?)))
-                                          :dark-icon (or (and (= setting-value :auto)
-                                                              (= computed-value :dark))
-                                                         (theme-actions/system-ui-theme-enabled?))})
-                 :on-click #(theme-actions/set-ui-theme :auto)}
-                (carrot-option-button {:selected (= setting-value :auto)
-                                       :disabled false
-                                       :did-change-cb #(theme-actions/set-ui-theme :auto)})
-                [:span.theme-name "Auto"]
-                [:span.theme-icon]
-                [:div.theme-description
-                  "Chosen automatically by your OS"]])
             [:button.mlb-reset.theme-settings-row.light-theme
-              {:class (when (= setting-value :light) "active")
+              {:class (when (= computed-value :light) "active")
                :on-click #(theme-actions/set-ui-theme :light)}
               (carrot-option-button {:selected (= setting-value :light)
                                      :disabled false
                                      :did-change-cb #(theme-actions/set-ui-theme :light)})
               [:span.theme-name "Light"]
-              [:span.theme-icon]]
+              [:span.theme-icon]
+              (when (and (= computed-value :light)
+                         (= setting-value :auto))
+                [:div.theme-description
+                  "Chose automatically by your OS"])]
             [:button.mlb-reset.theme-settings-row.dark-theme
-              {:class (when (= setting-value :dark) "active")
+              {:class (when (= computed-value :dark) "active")
                :on-click #(theme-actions/set-ui-theme :dark)}
               (carrot-option-button {:selected (= setting-value :dark)
                                      :disabled false
                                      :did-change-cb #(theme-actions/set-ui-theme :dark)})
               [:span.theme-name "Dark"]
-              [:span.theme-icon]]]]]]))
+              [:span.theme-icon]
+              (when (and (= computed-value :dark)
+                         (= setting-value :auto))
+                [:div.theme-description
+                  "Chosen automatically by your OS"])]]]]]))
