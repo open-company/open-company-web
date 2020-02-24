7cc5b9f8bdbf057f46087a0e06155c8564ce1c9f
bago2k4 <iacopo@rc0.eu>
2020-02-24 23:15:43 +0100

    Manually add extern in temporarily.

--- resources/public/js/externs.js
+++ resources/public/js/externs.js
@@ -204,3 +204,7 @@ OCCarrotDesktop.isDarkMode = function() {};
 // Stripe
 var Stripe = function(){};
 Stripe.redirectToCheckout = function(){};
+// React virtualized
+var ReactVirtualized = function(){};
+ReactVirtualized.List = function(){};
+ReactVirtualized.List.recomputeRowHeights = function(){};
