5326de8a190ecb3227959ad1f8ef40e4b8ae04fa
bago2k4 <iacopo@rc0.eu>
2020-03-03 21:57:29 +0100

    Fix inline code insertion and adding a space after it if necessary.

--- resources/public/lib/MediumEditorExtensions/MediumEditorAutoInlinecode/autoinlinecode.js
+++ resources/public/lib/MediumEditorExtensions/MediumEditorAutoInlinecode/autoinlinecode.js
@@ -25,21 +25,28 @@ var AutoInlinecode = MediumEditor.Extension.extend({
         if (element && !this.insideCodeTag(element) && paragraphEl && element.innerHTML.match(/`[^`]+`/)) {
           var r = /([^`]*)`([^`]+)`([^`]*)/; //NB Do not use g option or it will infinite loop since it uses look behind    
           element.innerHTML = element.innerHTML.replace(r, function(all,prev,center,after){
-            return prev + "<code>" + center + "</code>" + (after || "&nbsp;");
+            return prev + "<code class=\"oc-latest-code\">" + center + "</code>" + (after || "&nbsp;");
           });
-
-          MediumEditor.selection.moveCursor(this.document, lastAddedCode, 1);
+          var lastAddedCode = element.querySelector("code.oc-latest-code");
+          if (lastAddedCode) {
+            MediumEditor.selection.moveCursor(this.document, element.querySelector("code.oc-latest-code"), 1);
+            lastAddedCode.classList.remove("oc-latest-code");
+          }
         }
-      }
-
-      if (MediumEditor.util.isKey(keyUpEvent, [39])) {
+      } else if (MediumEditor.util.isKey(keyUpEvent, [39])) {
         var codeEl = this.insideCodeTag(this.base.getSelectedParentElement());
 
-        if (codeEl && (!codeEl.nextSibling || codeEl.nextSibling.length == 0)) {
+        if (codeEl && (!codeEl.nextSibling ||
+                       (codeEl.nextSibling.nodeType == 1 &&
+                        codeEl.nextSibling.tagName.toLowerCase() == "br"))) {
           var textNode = this.document.createTextNode('\u00A0');
-          codeEl.parentNode.appendChild(textNode);
+          if (codeEl.nextSibling) {
+            codeEl.parentNode.insertBefore(textNode, codeEl.nextSibling);
+          } else {
+            codeEl.parentNode.appendChild(textNode);
+          }
 
-          MediumEditor.selection.moveCursor(this.document, textNode);
+          MediumEditor.selection.moveCursor(this.document, textNode, 1);
         }
       }
     },
