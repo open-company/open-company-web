0ef0c82a0e8ca9851f93a695adb537c1a5efcb44
bago2k4 <iacopo@rc0.eu>
2020-03-10 22:03:36 +0100

    Add functions to check if element is a poll or a code block element. Do not add listener for paste event in these elements.

--- resources/public/lib/MediumEditorExtensions/MediumEditorPaste/paste.js
+++ resources/public/lib/MediumEditorExtensions/MediumEditorPaste/paste.js
@@ -160,8 +160,24 @@
             }
         },
 
-        handleAddElement: function (event, editable) {
-            this.on(editable, 'paste', this.handlePaste.bind(this));
+        _isPollElement: function(element) {
+            return (element.nodeType === 1 && element.nodeName.toLowerCase() === 'div' && element.classList.contains('media-poll'));
+        },
+
+        _isCodeBlockElement: function(){
+            return (element.nodeType === 1 && element.nodeName.toLowerCase() === 'pre');
+        },
+
+        canHandlePasteOnElement: function(element) {
+           return !(MediumEditor.util.traverseUp(element, this._isPollElement.bing(this)) ||
+                    MediumEditor.util.traverseUp(element, this._isCodeBlockElement.bing(this)));
+        },
+
+        handleAddElement: function (event, editable, element) {
+            var targetEl = event.target;
+            if (this.canHandlePasteOnElement(event.target)) {
+                this.on(editable, 'paste', this.handlePaste.bind(this));
+            }
         },
 
         destroy: function () {
