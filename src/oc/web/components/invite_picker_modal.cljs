0a0a1a252da8c57691b66ebe24cf13cf28fe7c23
bago2k4 <iacopo@rc0.eu>
2020-04-01 14:07:55 +0200

    Show email domains setup only to admins.

--- src/oc/web/components/invite_picker_modal.cljs
+++ src/oc/web/components/invite_picker_modal.cljs
@@ -4,6 +4,7 @@
             [oc.web.lib.jwt :as jwt]
             [oc.web.router :as router]
             [oc.web.dispatcher :as dis]
+            [oc.web.stores.user :as user-store]
             [oc.web.actions.org :as org-actions]
             [oc.web.actions.team :as team-actions]
             [oc.web.actions.nav-sidebar :as nav-actions]
@@ -31,8 +32,10 @@
       (team-actions/teams-get))
     s)}
   [s]
-  (let [team-data (drv/react s :team-data)
-        current-user-data (drv/react s :current-user-data)]
+  (let [org-data (drv/react s :org-data)
+        team-data (drv/react s :team-data)
+        current-user-data (drv/react s :current-user-data)
+        user-role (user-store/user-role org-data current-user-data)]
     [:div.invite-picker-modal
       [:button.mlb-reset.modal-close-bt
         {:on-click nav-actions/close-all-panels}]
@@ -65,4 +68,5 @@
                 {:on-click #(org-actions/bot-auth team-data current-user-data (str (router/get-token) "?org-settings=invite-picker"))}
                 [:span.disabled "Invite via Slack"]
                 [:span.enabled "(add Slack)"]]))
-          (email-domains)]]]))
+          (when (= user-role :admin)
+            (email-domains))]]]))
