96afdf31ddbaea3c80b1ca3d24ecdeadab618086
bago2k4 <iacopo@rc0.eu>
2020-05-06 17:44:23 +0200

    Throttle the refresh on window focus method to avoid refreshing with the server too often.

--- src/oc/web/mixins/section.cljs
+++ src/oc/web/mixins/section.cljs
@@ -1,27 +1,32 @@
 (ns oc.web.mixins.section
-  (:require [oc.web.ws.change-client :as ws-cc]
-            [oc.web.actions.activity :as activity-actions]
+  (:require [taoensso.timbre :as timbre]
             [oc.web.router :as router]
-            [taoensso.timbre :as timbre]))
+            [oc.web.ws.change-client :as ws-cc]
+            [oc.web.actions.activity :as activity-actions])
+  (:import [goog.async Throttle]))
 
 (def container-nav-in
   {:did-mount (fn [s]
    (ws-cc/container-watch)
    s)})
 
-(defn focus-reload
-  []
-  (let [slug (or (router/current-board-slug)
-                 (router/current-contributions-id))]
+(defn focus-reload []
+  (js/console.log "DBG reload!")
+  (when-let [slug (or (router/current-board-slug)
+                      (router/current-contributions-id))]
     (timbre/info "Reloading data for:" slug)
-   (activity-actions/refresh-board-data slug)))
+    (activity-actions/refresh-board-data slug)))
 
 (def window-focus-auto-loader
-  {:did-mount
-   (fn [s]
-     (.addEventListener js/window "focus" focus-reload)
-     s)
-   :will-unmount
-   (fn [s]
-     (.removeEventListener js/window "focus" focus-reload)
-     s)})
+  (let [throttled-refresh (Throttle. focus-reload 5000)
+        fire! (fn [] (js/console.log "DBG try") (.fire throttled-refresh))]
+    {:did-mount
+     (fn [s]
+       (.addEventListener js/window "focus" fire!)
+       s)
+     :will-unmount
+     (fn [s]
+       (.removeEventListener js/window "focus" fire!)
+       (.stop throttled-refresh)
+       (.dispose throttled-refresh)
+       s)}))
