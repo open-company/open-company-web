020349c00fe90e368b4f99e01bc97f94b82e435c
bago2k4 <iacopo@rc0.eu>
2020-05-07 23:58:05 +0200

    Add disabled option to follow-button.

--- src/oc/web/components/ui/follow_button.cljs
+++ src/oc/web/components/ui/follow_button.cljs
@@ -1,5 +1,6 @@
 (ns oc.web.components.ui.follow-button
   (:require [rum.core :as rum]
+            [oc.web.lib.utils :as utils]
             [oc.web.lib.responsive :as responsive]
             [oc.web.actions.user :as user-actions]))
 
@@ -7,7 +8,8 @@
   [{following :following
     resource-type :resource-type
     resource-uuid :resource-uuid
-    button-copy :button-copy}]
+    button-copy :button-copy
+    disabled :disabled}]
   (let [{:keys [active-on active-off hover-on hover-off]}
          (if (map? button-copy)
            button-copy
@@ -16,10 +18,14 @@
             :hover-on "Unfollow"
             :hover-off "Follow"})]
     [:button.mlb-reset.follow-button
-      {:class (if following "on" "off")
-       :on-click #(if (= resource-type :board)
-                    (user-actions/toggle-board resource-uuid)
-                    (user-actions/toggle-publisher resource-uuid))}
+      {:class (utils/class-set {:on following
+                                :off (not following)
+                                :default-copy (not (map? button-copy))
+                                :disabled disabled})
+       :on-click #(when-not disabled
+                    (if (= resource-type :board)
+                      (user-actions/toggle-board resource-uuid)
+                      (user-actions/toggle-publisher resource-uuid)))}
       [:span.main-title
         (if following
           active-on
