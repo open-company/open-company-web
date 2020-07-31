3609e05f899223cd2b38f73a5b20eff42d801afb
bago2k4 <iacopo@rc0.eu>
2020-07-31 12:58:31 +0200

    Always show delete, edit and move if there are the links.

--- src/oc/web/components/stream_item.cljs
+++ src/oc/web/components/stream_item.cljs
@@ -179,10 +179,9 @@
                            :editable-boards editable-boards
                            :external-share (not is-mobile?)
                            :external-bookmark (not is-mobile?)
-                           :show-edit? publisher?
-                           :show-delete? publisher?
-                           :show-move? (and (not is-mobile?)
-                                            publisher?)
+                           :show-edit? true
+                           :show-delete? true
+                           :show-move? (not is-mobile?)
                            :will-close (fn [] (reset! (::force-show-menu s) false))
                            :force-show-menu @(::force-show-menu s)
                            :mobile-tray-menu show-mobile-menu?})
