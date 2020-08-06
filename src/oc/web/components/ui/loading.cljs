3af6119a636b493e49224fe629f631ea2bc3ac41
bago2k4 <iacopo@rc0.eu>
2020-08-06 13:07:33 +0200

    New loading screen and interstitial ghost screen.

--- src/oc/web/components/ui/loading.cljs
+++ src/oc/web/components/ui/loading.cljs
@@ -6,5 +6,14 @@
   [data]
   [:div.oc-loading
     {:class (utils/class-set {:active (:loading data)})}
-    [:div.oc-loading-inner
-      "Wut!"]])
+    [:div.oc-loading-navbar]
+    [:div.oc-loading-page
+      [:div.oc-loading-navigation-sidebar
+        [:div.oc-loading-navigation-sidebar-row]
+        [:div.oc-loading-navigation-sidebar-row]
+        [:div.oc-loading-navigation-sidebar-row]
+        [:div.oc-loading-navigation-sidebar-row]
+        [:div.oc-loading-navigation-sidebar-row]]
+      [:div.oc-loading-dashboard
+        [:div.oc-loading-qp]
+        [:div.oc-loading-feed]]]])
