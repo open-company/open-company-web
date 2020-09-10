2a217ecd4cb6c53582654f00d883c747341579d4
bago2k4 <iacopo@rc0.eu>
2020-09-10 23:50:31 +0200

    Update loading components to the latest.

--- src/main/oc/web/components/ui/loading.cljs
+++ src/main/oc/web/components/ui/loading.cljs
@@ -2,18 +2,33 @@
   (:require [rum.core :as rum]
             [oc.web.lib.utils :as utils]))
 
-(rum/defc loading < rum/static
+(rum/defc shaky-carrot < rum/static
   [data]
   [:div.oc-loading
-    {:class (utils/class-set {:active (:loading data)})}
-    [:div.oc-loading-navbar]
-    [:div.oc-loading-page
-      [:div.oc-loading-navigation-sidebar
-        [:div.oc-loading-navigation-sidebar-row]
-        [:div.oc-loading-navigation-sidebar-row]
-        [:div.oc-loading-navigation-sidebar-row]
-        [:div.oc-loading-navigation-sidebar-row]
-        [:div.oc-loading-navigation-sidebar-row]]
-      [:div.oc-loading-dashboard
-        [:div.oc-loading-qp]
-        [:div.oc-loading-feed]]]])
+   {:class (utils/class-set {:active (:loading data)})}
+   [:div.oc-loading-inner
+    [:div.oc-loading-heart]
+    [:div.oc-loading-body]]])
+
+(rum/defc ghost-screen < rum/static
+  [data]
+  [:div.oc-loading-ghost-screen
+   {:class (utils/class-set {:active (:loading data)})}
+   [:div.oc-loading-navbar]
+   [:div.oc-loading-page
+    [:div.oc-loading-navigation-sidebar
+     [:div.oc-loading-navigation-sidebar-row]
+     [:div.oc-loading-navigation-sidebar-row]
+     [:div.oc-loading-navigation-sidebar-row]
+     [:div.oc-loading-navigation-sidebar-row]
+     [:div.oc-loading-navigation-sidebar-row]]
+    [:div.oc-loading-dashboard
+     [:div.oc-loading-qp]
+     [:div.oc-loading-feed]]]])
+
+(rum/defc loading < rum/static
+  [{:keys [current-org-slug jwt loading]}]
+  (if (and current-org-slug
+           jwt)
+    (ghost-screen {:loading loading})
+    (shaky-carrot {:loading loading})))
