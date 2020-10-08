2d9525387f6c4bf61befcbfcbe5f6ada1236c84d
bago2k4 <iacopo@rc0.eu>
2020-10-08 16:54:53 +0200

    Fix static pages title.

--- site/oc/shared.clj
+++ site/oc/shared.clj
@@ -606,7 +606,7 @@
     [:meta {:content "width=device-width, height=device-height, initial-scale=1", :name "viewport"}]
     [:meta {:name "slack-app-id" :content (env :oc-slack-app-id)}]
     ;; The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags
-    [:title "Wut! | wuts.io"]
+    [:title (str (or (env :product-name) "Carrot") " | " (or (env :oc-web-server-name) "carrot.io"))]
     (circular-book-font)
     (circular-bold-font)
     google-fonts
