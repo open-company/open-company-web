4dcec60f38663df5ddd1353fdb2a77b3176e63b1
bago2k4 <iacopo@rc0.eu>
2020-10-12 17:24:33 +0200

    Remove logs.

--- src/oc/web/mixins/seen.cljs
+++ src/oc/web/mixins/seen.cljs
@@ -9,14 +9,11 @@
     {:did-mount (fn [s]
       (reset! container-slug (if (dis/current-board-slug) (keyword (dis/current-board-slug)) (dis/current-contributions-id)))
       (reset! sort-type (dis/current-sort-type))
-      (js/console.log "DBG container-nav-mixin/did-mount" @container-slug @sort-type)
       (aa/container-nav-in @container-slug @sort-type)
      s)
      :did-remount (fn [_ s]
-      (js/console.log "DBG container-nav-mixin/did-remount" @container-slug @sort-type)
       (aa/container-nav-in @container-slug @sort-type)
      s)
      :will-unmount (fn [s]
-      (js/console.log "DBG container-nav-mixin/will-unmount" @container-slug @sort-type)
       (aa/container-nav-out @container-slug @sort-type)
      s)}))
