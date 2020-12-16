fefd8e8e3c16eea9caa5bdb5bd6a0be1a431671e
bago2k4 <iacopo@rc0.eu>
2020-12-16 15:39:21 +0100

    Better log messages for WS.

--- src/main/oc/web/ws/utils.cljs
+++ src/main/oc/web/ws/utils.cljs
@@ -11,7 +11,7 @@
 ;; Connection check
 
 (defn sentry-report [message chsk-send! ch-state & [action-id infos]]
-  (timbre/debug "Sentry report:" message)
+  (timbre/debug "Sentry report: " message)
   (let [connection-status (when @ch-state
                             @@ch-state)
         ch-send-fn? (fn? @chsk-send!)
@@ -21,7 +21,7 @@
              :infos infos
              :sessionURL (fullstory/session-url)}]
     (sentry/capture-message-with-extra-context! ctx message)
-    (timbre/error message ctx)))
+    (timbre/warn message ctx)))
 
 ;; Real send
 
@@ -38,25 +38,25 @@
 (defonce cmd-queue (atom {}))
 
 (defn buffer-cmd [service-name args]
-  (timbre/debug "Queuing message for" service-name args)
+  (timbre/debug "Queuing message for " service-name args)
   (let [service-key (keyword service-name)
         service-queue (service-key @cmd-queue)
         service-next-queue (conj service-queue args)]
     (swap! cmd-queue assoc service-key service-next-queue)))
 
 (defn reset-queue [service-name]
-  (timbre/debug "Reset queue for" service-name)
+  (timbre/debug "Reset queue for " service-name)
   (reset! cmd-queue (assoc @cmd-queue (keyword service-name) [])))
 
 (defn send-queue [service-name chsk-send! ch-state]
   (when (and @chsk-send! (:open? @@ch-state))
-    (timbre/debug "Send queue for" service-name (count ((keyword service-name) @cmd-queue)))
+    (timbre/debug "Send queue for " service-name (count ((keyword service-name) @cmd-queue)))
     (doseq [qargs ((keyword service-name) @cmd-queue)]
       (real-send service-name chsk-send! ch-state qargs))
     (reset-queue service-name)))
 
 (defn send! [service-name chsk-send! ch-state args]
-  (timbre/debug "Send!" service-name args)
+  (timbre/debug "Send! " service-name args)
   (if (and @chsk-send! (:open? @@ch-state))
     (do
       ;; empty queue first
@@ -67,7 +67,7 @@
     (buffer-cmd service-name args)))
 
 (defn check-interval [last-interval service-name chsk-send! ch-state reconnect-cb]
-  (timbre/debug "Set check-interval for" service-name)
+  (timbre/debug "Set check-interval for " service-name)
   (when @last-interval
     (.clearInterval js/window @last-interval))
   (reset! last-interval
@@ -75,25 +75,25 @@
     #(when (or (not @ch-state)
                (not @@ch-state)
                (not (:open? @@ch-state)))
-       (timbre/debug "WS check-interval connection down for" service-name)
+       (timbre/debug "WS check-interval connection down for " service-name)
        ;; WS connection is closed
        (if (:udt-next-reconnect @@ch-state)
          ;; There is an auto reconnect set, let's wait for it
-         (timbre/debug "Will reconnect automatically at" (utils/js-date (:udt-next-reconnect @@ch-state)))
+         (timbre/debug "Will reconnect automatically at " (utils/js-date (:udt-next-reconnect @@ch-state)))
          ;; no auto reconnect, let's force a reconnect
          (do
            (timbre/debug "No auto reconnect set, forcing reconnection")
-           (sentry-report (str "No auto reconnect set for" service-name ". Forcing reconnect!") chsk-send! ch-state)
+           (sentry-report (str "No auto reconnect set for " service-name ". Forcing reconnect!") chsk-send! ch-state)
            (utils/after 0 reconnect-cb))))
     (* ls/ws-monitor-interval 1000))))
 
 (defn reconnected [last-interval service-name chsk-send! ch-state reconnect-cb]
-  (timbre/debug "Reconnect" service-name)
+  (timbre/debug "Reconnect " service-name)
   (send-queue service-name chsk-send! ch-state)
   (check-interval last-interval service-name chsk-send! ch-state reconnect-cb))
 
 (defn report-invalid-jwt [service-name ch-state rep]
-  (timbre/debug "Report invalid-jwt" service-name)
+  (timbre/debug "Report invalid-jwt " service-name)
   (let [connection-status (when @ch-state
                             @@ch-state)
         ctx {:jwt (j/jwt)
@@ -102,26 +102,26 @@
              :rep rep
              :sessionURL (fullstory/session-url)}]
     (sentry/capture-message-with-extra-context! ctx (str service-name " WS: not valid JWT, force refresh and retry!"))
-    (timbre/error service-name "WS: not valid JWT" ctx)))
+    (timbre/error service-name " WS: not valid JWT " ctx)))
 
 (defn report-connect-timeout [service-name ch-state]
-  (timbre/debug "Report connection-timeout" service-name)
+  (timbre/debug "Report connection-timeout " service-name)
   (let [connection-status (when @ch-state
                             @@ch-state)
         ctx {:timestamp (.getTime (new js/Date))
              :connection-status connection-status
              :sessionURL (fullstory/session-url)}]
     (sentry/capture-message-with-extra-context! ctx (str service-name " WS: handshake timeout"))
-    (timbre/error service-name "WS: handshake timeout" ctx)))
+    (timbre/error service-name " WS: handshake timeout " ctx)))
 
 (defn auth-check [service-name ch-state chsk-send! channelsk jwt-refresh-cb reconnect-cb success-cb rep]
-  (timbre/debug "Auth-check" service-name)
+  (timbre/debug "Auth-check " service-name)
   (if (and (sente/cb-success? rep)
            (:valid rep))
     (when (fn? success-cb)
       (success-cb rep))
     (do
-      (timbre/warn "Disconnecting client due to invalid JWT!" rep)
+      (timbre/warn "Disconnecting client due to invalid JWT! " rep)
       (sente/chsk-disconnect! @channelsk)
       (cond
         (and (j/get-contents)
