57be46e35f8e8684820782b56fc8b01b5061594c
bago2k4 <iacopo@rc0.eu>
2021-03-01 23:08:41 +0100

    Fix check for load-item-if-needed fn.

--- src/main/oc/web/utils/notification.cljs
+++ src/main/oc/web/utils/notification.cljs
@@ -44,7 +44,7 @@
                 (oc-urls/comment-url board-slug entry-uuid interaction-uuid)
                 (oc-urls/entry board-slug entry-uuid))]
       #(let [activity-data (dis/activity-data entry-uuid)]
-         (if (or (map? activity-data) (seq activity-data))
+         (if (or (keyword? activity-data) (seq activity-data))
            (router/nav! url)
            (cmail-actions/get-entry-with-uuid board-slug entry-uuid
                                               (fn [success status]
