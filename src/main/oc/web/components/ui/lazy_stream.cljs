298ba1fc4833267e7df56f24f2ae7a286506790b
bago2k4 <iacopo@rc0.eu>
2021-03-03 20:34:21 +0100

    Try removing the ghost screen when switching feed context to make it more reliable.

--- src/main/oc/web/components/ui/lazy_stream.cljs
+++ src/main/oc/web/components/ui/lazy_stream.cljs
@@ -17,19 +17,22 @@
                            s)}
   [s stream-comp]
   (let [container-data (drv/react s :container-data)
-        activity-data (drv/react s :activity-data)
         current-board-slug (drv/react s :board-slug)
         foc-layout (drv/react s :foc-layout)
-        ready? (and @(::delayed s)
-                    (map? container-data))
+        data-ready? (map? container-data)
+        delay? (not @(::delayed s))
         collapsed-foc? (or (= foc-layout dis/other-foc-layout)
                            (= current-board-slug "replies"))]
+    (js/console.log "DBG lazy-stream/render delay?" delay? "data-ready?" data-ready?)
     [:div.lazy-stream
-      (if ready?
-        (stream-comp)
-        [:div.lazy-stream-interstitial
-          {:class (when collapsed-foc? "collapsed")
-           :style {:height (str (+ (dis/route-param :scroll-y)
-                                   (or (.. js/document -documentElement -clientHeight)
-                                       (.-innerHeight js/window)))
-                             "px")}}])]))
+      (cond (not data-ready?)
+            [:div.lazy-stream-interstitial
+             {:class (when collapsed-foc? "collapsed")
+              :style {:height (str (+ (dis/route-param :scroll-y)
+                                      (or (.. js/document -documentElement -clientHeight)
+                                          (.-innerHeight js/window)))
+                                   "px")}}]
+            delay?
+            [:div]
+            :else
+            (stream-comp))]))
