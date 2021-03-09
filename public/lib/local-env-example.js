86e5fe3b52f73cfe768532f9476865a14e9b95ec
bago2k4 <iacopo@rc0.eu>
2021-03-09 01:59:08 +0100

    Add web DSN to replaced env vars in local-env file.

--- public/lib/local-env-example.js
+++ public/lib/local-env-example.js
@@ -1,2 +1,3 @@
 var OCEnv = {"web-hostname": "${LOCAL_DEV_HOST}",
-             "web-port": "${LOCAL_DEV_PORT}"};
+             "web-port": "${LOCAL_DEV_PORT}",
+             "dsn": "${OPEN_COMPANY_SENTRY_WEB}"};
