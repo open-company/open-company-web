f9dabfe4bf370b0650bd1949399798f771af4ffe
bago2k4 <iacopo@rc0.eu>
2021-05-07 01:48:40 +0200

    Upgrade @sentry/browser to latest, add @sentry/tracing for browser tracing integration.

--- src/main/oc/web/lib/sentry.cljs
+++ src/main/oc/web/lib/sentry.cljs
@@ -2,45 +2,51 @@
   (:require [oc.web.local-settings :as ls]
             [oc.web.lib.responsive :as responsive]
             [oc.web.lib.jwt :as jwt]
-            [oops.core :refer (oget ocall)]
             [cuerdas.core :as s]
+            [oops.core :refer (oget ocall)]
             ["@sentry/browser" :as sentry-browser]
+            ["@sentry/tracing" :as sentry-tracing]
             [taoensso.timbre :as timbre]
             [oc.web.utils.sentry :as sentry-utils]))
 
 (def p? (comp not s/empty-or-nil?))
 
-(defn ^:export init-parameters [dsn]
+(def BrowserTracing (oget sentry-tracing "Integrations.BrowserTracing"))
+
+(defn ^:export init-parameters []
   (cond-> {:tags {:isMobile (responsive/is-mobile-size?)
                   :hasJWT (not (not (jwt/jwt)))}
            :sourceRoot ls/web-server
+           :integrations (clj->js [(BrowserTracing.)])
            :debug (= ls/log-level "debug")
-           :dsn dsn
+           :dsn ls/local-dsn
+           :tracesSampleRate 1.0
            :normalizeDepth 6}
     (p? ls/sentry-release) (assoc :release ls/sentry-release)
     (and (p? ls/sentry-release)
          (p? ls/deploy-key)) (assoc-in [:tags :deploy-key] ls/deploy-key)
     (and (p? ls/sentry-release)
          (p? ls/deploy-key)) (merge {:deploy ls/deploy-key
-                                :dist ls/deploy-key})
+                                     :dist ls/deploy-key
+                                     :distribution ls/deploy-key})
     (p? ls/sentry-env) (assoc :environment ls/sentry-env)))
 
 (defn sentry-setup []
-  (let [sentry-params (init-parameters ls/local-dsn)]
-    (if-not (:dsn sentry-params)
-      (timbre/warnf "Empty Sentry DSN: %s" sentry-params)
-      (do
-        (timbre/debugf "Setup Sentry: %s" sentry-params)
-        (ocall sentry-browser "init" (clj->js sentry-params))
-        (ocall sentry-browser "configureScope" (fn [scope]
-                                                 (ocall scope "setTag" "isMobile" (responsive/is-mobile-size?))
-                                                 (ocall scope "setTag" "hasJWT" (not (not (jwt/jwt))))
-                                                 (when (jwt/jwt)
-                                                   (timbre/debugf "Set Sentry user: %s" (jwt/get-key :user-id))
-                                                   (ocall scope "setUser" (clj->js {:user-id (jwt/get-key :user-id)
-                                                                                    :id (jwt/get-key :user-id)
-                                                                                    :first-name (jwt/get-key :first-name)
-                                                                                    :last-name (jwt/get-key :last-name)})))))))))
+  (if (seq ls/local-dsn)
+    (timbre/warnf "Empty Sentry DSN: %s" ls/local-dsn)
+    (do
+      (timbre/debug "Setting up Sentry...")
+      (ocall sentry-browser "init" (clj->js (init-parameters)))
+      (timbre/debug "Configuring Sentry scope")
+      (.configureScope ^js sentry-browser (fn [scope]
+                                            (.setTag ^js scope "isMobile" (responsive/is-mobile-size?))
+                                            (.setTag ^js scope "hasJWT" (not (not (jwt/jwt))))
+                                            (when (jwt/jwt)
+                                              (timbre/debugf "Set Sentry user: %s" (jwt/user-id))
+                                              (.setUser ^js scope (clj->js {:user-id (jwt/user-id)
+                                                                            :id (jwt/user-id)
+                                                                            :first-name (jwt/get-key :first-name)
+                                                                            :last-name (jwt/get-key :last-name)}))))))))
 
 (def capture-error! sentry-utils/capture-error!)
 
