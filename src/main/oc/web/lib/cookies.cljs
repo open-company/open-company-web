4414f4ce725377937661bf2220de809ab082e033
bago2k4 <iacopo@rc0.eu>
2021-05-11 18:20:46 +0200

    Update cookies lib to conform to the updated version.

--- src/main/oc/web/lib/cookies.cljs
+++ src/main/oc/web/lib/cookies.cljs
@@ -2,12 +2,22 @@
   (:require [taoensso.timbre :as timbre]
             [oc.web.local-settings :as ls]
             ["jwt-decode" :as jwt-decode]
-            [oops.core :refer (oget ocall)]
+            [oops.core :refer (ocall)]
             [oc.web.utils.sentry :as sentry])
   (:import [goog.net Cookies]
            [goog.string format]))
 
-(def max-cookie-length (oget Cookies "MAX_COOKIE_LENGTH"))
+(def default-cookie-expire (* 60 60 24 6))
+
+(def ^{:export true} cookies-static-obj (ocall Cookies "getInstance" js/document))
+
+(defn- cookie-name [c-name]
+  (str ls/cookie-name-prefix (name c-name)))
+
+(def default-expire -1)
+(def default-path "/")
+
+(def max-cookie-length Cookies/MAX_COOKIE_LENGTH)
 
 (defn- calc-length [cval]
   (when (string? cval)
@@ -33,37 +43,27 @@
                                                                  (str cname))}
                                                    "Cookie value exceeds max allowed length"))))
 
-(def default-cookie-expire (* 60 60 24 6))
-
-(def ^{:export true} cookies-static-obj (Cookies. js/document))
-
-(defn- cookie-name [c-name]
-  (str ls/cookie-name-prefix (name c-name)))
-
-(def default-expire -1)
-(def default-path "/")
-
-(defn- cookie-options [expiry c-path c-domain c-secure]
+(defn- cookie-options [c-max-age c-path c-domain c-secure]
   (clj->js {:sameSite true
             :secure c-secure
             :domain c-domain
             :path c-path
-            :maxAge expiry}))
+            :maxAge c-max-age}))
 
-(defn- cookie-expiration-date [expiry]
-  (js/Date. (+ (.getTime (js/Date.)) (* expiry 1000))))
+(defn- cookie-expiration-date [c-max-age]
+  (js/Date. (+ (.getTime (js/Date.)) (* c-max-age 1000))))
 
 (defn ^:export set-cookie!
   ([c-name c-value]
    (set-cookie! c-name c-value default-expire default-path ls/jwt-cookie-domain ls/jwt-cookie-secure))
-  ([c-name c-value expiry]
-   (set-cookie! c-name c-value expiry default-path ls/jwt-cookie-domain ls/jwt-cookie-secure))
-  ([c-name c-value expiry c-path]
-   (set-cookie! c-name c-value expiry c-path ls/jwt-cookie-domain ls/jwt-cookie-secure))
-  ([c-name c-value expiry c-path c-domain c-secure]
-   (timbre/debug (format "Setting cookie \"%s\" with expiration %s (%d seconds from now). Value: %s" c-name (cookie-expiration-date expiry) expiry c-value))
+  ([c-name c-value c-max-age]
+   (set-cookie! c-name c-value c-max-age default-path ls/jwt-cookie-domain ls/jwt-cookie-secure))
+  ([c-name c-value c-max-age c-path]
+   (set-cookie! c-name c-value c-max-age c-path ls/jwt-cookie-domain ls/jwt-cookie-secure))
+  ([c-name c-value c-max-age c-path c-domain c-secure]
+   (timbre/debug (format "Setting cookie \"%s\" with expiration %s (%d seconds from now). Value: %s" c-name (cookie-expiration-date c-max-age) c-max-age c-value))
    (check-length c-name (str c-value))
-   (ocall cookies-static-obj "set" (cookie-name c-name) c-value (cookie-options expiry c-path c-domain c-secure))))
+   (ocall cookies-static-obj "set" (cookie-name c-name) c-value (cookie-options c-max-age c-path c-domain c-secure))))
 
 (defn ^:export get-cookie
   "Get a cookie with the name provided pre-fixed by the environment."
